5bf3f871ae0c3cb022e6d5d5b869fd6ca3558d2a
Andrew Tait <andrew.tait@sage.com>
2015-10-15 16:58:28 +0100

    added demo for easier testing

---
+++ example/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router';
+import CarbonRoute from 'utils/route';
+
+var routes = (
+  <Route path="/" />
+);
+
+CarbonRoute(routes);
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var BuildTask = require('carbon-factory/lib/gulp/build');
 var SpecTask = require('carbon-factory/lib/gulp/spec');
+
+gulp.task('default', BuildTask({
+  src: './example/main.js',
+  jsDest: './example',
+  cssDest: './example'
+}));
 
 gulp.task('test', SpecTask());
