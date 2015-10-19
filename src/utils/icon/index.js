59b8504170e187a6b2b23fe4e38434b251ee7a56
Alexander.Ramin <alexander.ramin@sage.com>
2015-10-19 10:24:21 +0100

    click event on icon added

--- src/utils/icon/index.js
+++ src/utils/icon/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 var Icons = (props) => {
-  debugger
-  var { className, type, ...other } = this.props;
+  var { className, type, ...other } = props;
   var iconClass = className + " icon-" + type
   return <span className={ iconClass } {...other} ></span>;
 };
