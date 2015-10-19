6cf211b2adab1281fbe0c18f49ef67a6a79a9746
Andrew Tait <andrew.tait@sage.com>
2015-10-19 11:21:46 +0100

    fixed dropdown suggest test code

--- example/main.js
+++ example/main.js
@@ -1,53 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router';
 import CarbonRoute from 'utils/route';
-import Textbox from 'components/textbox';
-import TTFM from 'components/table-fields-for-many';
-import ImmutableHelper from 'utils/helpers/immutable';
-import DDSuggest from 'components/dropdown-suggest';
 
 class testView extends React.Component {
 
   render () {
-
-    var data = ImmutableHelper.parseJSON({
-      foo: [
-        {foo_1: 'Hello', ledger_account: {resource_name: 'Please Select', id: '1'}}
-      ]
-    });
-
-    var fields = [
-      <Textbox  
-        name='foo_1'
-        key='1'
-      />,
-      <DDSuggest
-        path=""
-        value= { { name: '', id: '' } }
-        key='0'
-        resource_key='resource_name'
-        name='ledger_account'
-       />
-
-    ];
-
-    var deleteLineItem = () => {
-      return;
-    }
-
-    var updateLineItem = () => {
-      return;
-    }
     return(
-      <TTFM
-        name='foo'
-        data={ data.get('foo') }
-        deleteRowHandler={ deleteLineItem }
-        updateRowHandler={ updateLineItem }
-        fields={ fields }
-      />
+      <div></div>
     )
   }
+
 }
 
 var routes = (
