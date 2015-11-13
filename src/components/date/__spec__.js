39a07059b4d86bc4f2a9f7725cf833c5e6c2234c
Andrew Tait <andrew.tait@sage.com>
2015-11-13 12:51:30 +0000

    removed empty specs

--- src/components/date/__spec__.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
-import Date from './index';
-
-describe('Date', () => {
-  it('failing test demo', () => {
-    expect(1).toEqual(2);
-  });
-});
