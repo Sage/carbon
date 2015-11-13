33feb563028770f7c59db68c38ebcd5277415708
Andrew Tait <andrew.tait@sage.com>
2015-11-13 15:40:40 +0000

    updated specs for decimal

--- src/components/decimal/__spec__.js
+++ src/components/decimal/__spec__.js
@@ -2,218 +2,179 @@ import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils';
 import Decimal from './index';
 import I18n from "i18n-js";
+import Events from './../../utils/helpers/events';
 
 describe('Decimal', () => {
   var instance;
-  var visibleInputField;
 
-  beforeEach(() => {
-    instance = TestUtils.renderIntoDocument(
-      <Decimal
-        name="total"
-        value = { '100000.00' }
-        defaultValue = { '10' }
-        label = { "Label" }
-        onChange={ jasmine.createSpy('dummy') }
-    />);
-
-    visibleInputField = instance.refs.visible;
-  });
-
-  describe("render", () => {
-    it("renders a hidden input", () => {
-      var input = instance.refs.hidden;
-      expect(input.tagName).toEqual("INPUT");
-      expect(input.type).toBe('hidden');
-      expect(input.readOnly).toBeTruthy();
-    });
-
-    it("renders a visible input with a label", () => {
-      var input = instance.refs.visible;
-      expect(input.tagName).toEqual("INPUT");
-      expect(input.getAttribute('label')).toBe('Label');
-    });
-  });
-
-  describe("I18n", () => {
-    it('returns a hash on I18n options', () => {
-      var result = instance.i18n();
-      expect(typeof(result)).toBe("object");
-    });
-  });
-
-  describe("formatHiddenValue", () => {
-    it("removes basic delimiters", () => {
-      var baseValue = '300,000,000.00';
-      var formattedValue = instance.formatHiddenValue(baseValue);
-      expect(formattedValue).toBe('300000000.00');
-    });
-
-    it("removes custom delimiters", () => {
-      spyOn(instance, 'i18n').and.returnValue( { delimiter: '*', separator: '.' } );
-      var baseValue = '300*000*000.00';
-      var formattedValue = instance.formatHiddenValue(baseValue);
-
-      expect(formattedValue).toBe('300000000.00');
-    });
-
-    it("changes custom separator to a dot(.)", () => {
-      spyOn(instance, 'i18n').and.returnValue( { delimiter: '.', separator: ',' } );
-      var baseValue = '300.000.000,00';
-      var formattedValue = instance.formatHiddenValue(baseValue);
-      expect(formattedValue).toBe('300000000.00');
-    });
-
-    describe("when value is not passed and a props value is set", () => {
-      it("uses the set props value", () => {
-        var formattedValue = instance.formatHiddenValue();
-        expect(formattedValue).toBe('100000.00');
-      });
-    });
-
-    describe("when value and props aren't set", () => {
-      it("uses the defaultProp value", () => {
-        instance.props = {};
-        spyOn(instance, 'getDefaultValue').and.returnValue('1');
-
-        var formattedValue = instance.formatHiddenValue();
-        expect(instance.getDefaultValue).toHaveBeenCalled();
-        expect(formattedValue).toBe('1');
-      });
-    });
-  });
-
-  describe("formatVisibleValue", () => {
-    it("sets the precision to two decimal places", () => {
-      var baseValue = '30.0056';
-      var formattedValue = instance.formatVisibleValue(baseValue);
-      expect(formattedValue).toBe('30.01');
-    });
-
-    it("adds two decimal places if none given", () => {
-      var baseValue = '30';
-      var formattedValue = instance.formatVisibleValue(baseValue);
-      expect(formattedValue).toBe('30.00');
-    });
-
-    it("adds delimiter from i18n value", () => {
-      var baseValue = '3000000.00';
-      var formattedValue = instance.formatVisibleValue(baseValue);
-      expect(formattedValue).toBe('3,000,000.00');
-    });
-
-    it("adds custom delimiters and separators based on i18n", () => {
-      window.foo = true
-      spyOn(instance, 'i18n').and.returnValue( { delimiter: '.', separator: ',' } );
-      var baseValue = '300000';
-      var formattedValue = instance.formatVisibleValue(baseValue);
-      expect(formattedValue).toBe('300.000,00');
-    });
-
-    describe("when value is not passed and a props value is set", () => {
-      it("uses the set props value", () => {
-        var formattedValue = instance.formatVisibleValue();
-        expect(formattedValue).toBe('100,000.00');
-      });
-    });
-
-    describe("when value and props aren't set", () => {
-      it("uses the defaultProp value", () => {
-        instance.props = {};
-        spyOn(instance, 'getDefaultValue').and.returnValue('1');
-
-        var formattedValue = instance.formatVisibleValue();
-        expect(instance.getDefaultValue).toHaveBeenCalled();
-        expect(formattedValue).toBe('1.00');
-      });
-    });
-  });
-
-  describe("getDefaultValue", () => {
-    it("gets the defaultValue from props on page load", () => {
-      spyOn(instance, 'getDefaultValue').and.callThrough();
-
-      instance = TestUtils.renderIntoDocument(
-        <Decimal
-          name="total"
-          defaultValue = { '333' }
-          label = { "Label" }
-          onChange={ jasmine.createSpy('dummy') }
-      />);
-
-      expect(instance.refs.visible.value).toBe('333.00');
-    });
-
-    it("gets the current value once set", () => {
-      expect(instance.getDefaultValue()).toBe('100000.00');
-    });
-  });
-
-  // Should this force a prop update
-  // Does react test utils keydown cause an update of props
-  describe("componentWillReceiveProps", () => {
-
+  describe('with no options', () => {
     beforeEach(() => {
-      spyOn(instance, 'setState');
+      instance = TestUtils.renderIntoDocument(<Decimal name="total" />);
     });
 
-    describe("when element has focus", () => {
-      it("doesnt set the components state", () => {
-        instance.doc = { activeElement: instance.refs.visible }
-        instance.componentWillReceiveProps({value: '123'});
+    describe('initialize', () => {
+      it('sets defaultValue to 0.00', () => {
+        expect(instance.props.defaultValue).toEqual('0.00');
+      });
+    });
+
+    describe('handleBlur using default value', () => {
+      it('calls set state with the formatted value', () => {
+        instance.refs.hidden.value = "9999";
+        spyOn(instance, 'setState');
+        instance.handleBlur();
+        expect(instance.setState).toHaveBeenCalledWith({ visibleValue: "9,999.00" });
+      });
+    });
+  });
+
+  describe('with options', () => {
+    beforeEach(() => {
+      instance = TestUtils.renderIntoDocument(
+        <Decimal name="total" value="1000000.00000" />
+      );
+    });
+
+    it('sets the visibleValue state to a formatted version of the value', () => {
+      expect(instance.state.visibleValue).toEqual("1,000,000.00");
+    });
+
+    describe('with alternative I18n options', () => {
+      beforeEach(() => {
+        I18n.translations = { en: { number: { format: {
+          delimiter: ".",
+          separator: ","
+        } } } };
+
+        instance = TestUtils.renderIntoDocument(
+          <Decimal name="total" value="1000000.00000" />
+        );
+      });
+
+      afterEach(() => {
+        I18n.translations = {};
+      });
+
+      it('sets the visibleValue state to a formatted version using i18n opts', () => {
+        expect(instance.state.visibleValue).toEqual("1.000.000,00");
+      });
+    });
+  });
+
+  describe('component methods', () => {
+    beforeEach(() => {
+      instance = TestUtils.renderIntoDocument(
+        <Decimal name="total" value="1000.00" />
+      );
+    });
+
+    afterEach(() => {
+      instance = null;
+    });
+
+    describe('componentWillReceiveProps', () => {
+      beforeEach(() => {
+        spyOn(instance, 'setState');
+      });
+
+      it('re-evaluates the formatted visible value if input does not have focus', () => {
+        instance.componentWillReceiveProps({ value: '1001.00' });
+        expect(instance.setState).toHaveBeenCalledWith({ visibleValue: '1,001.00' });
+      });
+
+      it('does not re-evaluate the formatted visible value if input has focus', () => {
+        instance.doc = {
+          activeElement: instance.refs.visible
+        };
+        instance.componentWillReceiveProps({ value: '1001.00' });
         expect(instance.setState).not.toHaveBeenCalled();
       });
     });
 
-    describe("when element does NOT have focus", () => {
-      it("sets the components state for visibleValue", () => {
-        instance.componentWillReceiveProps({value: '123'});
-        expect(instance.setState).toHaveBeenCalledWith({
-          visibleValue: '123.00'
+    describe('emitOnChangeCallback', () => {
+      beforeEach(() => {
+        spyOn(instance, '_handleOnChange');
+        instance.emitOnChangeCallback('100');
+      });
+
+      it('sets the hiddenField value as if it had been changed', () => {
+        expect(instance.refs.hidden.value).toEqual('100');
+      });
+
+      it('calls _handleOnChange with a dummy event', () => {
+        expect(instance._handleOnChange).toHaveBeenCalledWith({ target: instance.refs.hidden });
+      });
+    });
+
+    describe('handleVisibleInputChange', () => {
+      beforeEach(() => {
+        spyOn(instance, 'setState');
+        spyOn(instance, 'emitOnChangeCallback');
+        TestUtils.Simulate.change(instance.refs.visible, { target: { value: "1,0,0,0.00" } });
+      });
+
+      it('calls setState with the exact visibleValue from the visible input', () => {
+        expect(instance.setState).toHaveBeenCalledWith({ visibleValue: "1,0,0,0.00" });
+      });
+
+      it('calls emitOnChangeCallback with a formatted hidden value', () => {
+        expect(instance.emitOnChangeCallback).toHaveBeenCalledWith("1000.00");
+      });
+    });
+
+    describe('handleBlur', () => {
+      beforeEach(() => {
+        spyOn(instance, 'setState');
+        TestUtils.Simulate.blur(instance.refs.visible);
+      });
+
+      it('calls setState with the formatted visible value', () => {
+        expect(instance.setState).toHaveBeenCalledWith({ visibleValue: "1,000.00" });
+      });
+    });
+
+    describe('inputProps', () => {
+      var spy, mockEvent;
+
+      beforeEach(() => {
+        spy = jasmine.createSpy('spy');
+        mockEvent = {
+          preventDefault: spy
+        }
+      });
+
+      it('sets the ui-decimal__input class to the input', () => {
+        expect(instance.refs.visible.classList[0]).toEqual('ui-decimal__input');
+      });
+
+      it('sets value to the visible value', () => {
+        expect(instance.refs.visible.value).toEqual("1,000.00");
+      });
+
+      describe('if a valid keydown occurs', () => {
+        it('prevents default', () => {
+          spyOn(Events, 'isValidDecimalKey').and.returnValue(true);
+          TestUtils.Simulate.keyDown(instance.refs.visible, mockEvent);
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('if an invalid keydown occurs', () => {
+        it('prevents default', () => {
+          spyOn(Events, 'isValidDecimalKey').and.returnValue(false);
+          TestUtils.Simulate.keyDown(instance.refs.visible, mockEvent);
+          expect(spy).toHaveBeenCalled();
         });
       });
     });
-  });
 
-  describe("updateVisibleValue", () => {
-    it("sets the state of visibleValue", () => {
-      spyOn(instance, 'setState');
-      instance.updateVisibleValue();
-      expect(instance.setState).toHaveBeenCalledWith({
-        visibleValue: '100,000.00'
+    describe('hiddenInputProps', () => {
+      it('sets type and readOnly', () => {
+        expect(instance.refs.hidden.type).toEqual("hidden");
+        expect(instance.refs.hidden.readOnly).toBeTruthy();
+        expect(instance.refs.hidden.value).toEqual("1000.00");
+        expect(instance.refs.hidden.defaultValue).toEqual("1000.00");
       });
-    });
-  });
-
-  describe("handleVisibleInputChange", () => {
-    it("should set the visibleValue state and emit a callback", () => {
-      spyOn(instance, 'emitOnChangeCallback');
-      spyOn(instance, 'setState');
-
-      visibleInputField.value = '2000.00';
-      TestUtils.Simulate.change(visibleInputField); 
-
-      expect(instance.setState).toHaveBeenCalledWith({ visibleValue: '2000.00' });
-      expect(instance.emitOnChangeCallback).toHaveBeenCalledWith('2000.00');
-    });
-  });
-
-  describe("emitOnChangeCallback", () => {
-    describe("when a onChange event has taken place", () => {
-      it("should send onChange the updated value", () => {
-        instance.emitOnChangeCallback('2000.00');
-        expect(instance.props.onChange).toHaveBeenCalled();
-      });
-    });
-  });
-
-  describe("customInputProps", () => {
-    it("returns the props for the visible field", () => {
-    });
-  });
-
-  describe("hiddenFieldProps", () => {
-    it("returns the props for the hidden field", () => {
     });
   });
 });
