92e64346d25d762cd1b89ab1e2f0edc7bcf3d07c
Andrew Tait <andrew.tait@sage.com>
2015-11-24 12:28:43 +0000

    added :rainbow: component

---
+++ src/components/rainbow/index.js
@@ -0,0 +1,233 @@
+import React from 'react';
+import ReactHighcharts from 'react-highcharts/dist/bundle/highcharts';
+
+/**
+ * A rainbow chart using the Highcharts API.
+ *
+ * == How to use a Rainbow in a component:
+ *
+ * In your file:
+ *
+ *   import Rainbow from 'carbon/lib/components/rainbow';
+ *
+ * To render the Rainbow:
+ *
+ *   let myImmutableData = Immutable.fromJS([
+ *     {
+ *       y: 30,
+ *       name: 'First Bit',
+ *       label: 'label for first bit',
+ *       tooltip: 'more info about this bit'
+ *     }, {
+ *       y: 70,
+ *       name: 'Second Bit',
+ *       label: 'label for second bit',
+ *       tooltip: 'more info about this bit'
+ *     }
+ *   ]);
+ *
+ *   <Rainbow title="My Chart" data={ myImmutableData } />
+ *
+ * @class Rainbow
+ * @constructor
+ */
+class Rainbow extends React.Component {
+
+  static propTypes = {
+    /**
+     * A title for the component.
+     *
+     * @property title
+     * @type {String}
+     */
+    title: React.PropTypes.string.isRequired,
+
+    /**
+     * The data set for the component.
+     *
+     * @property data
+     * @type {Object}
+     */
+    data: React.PropTypes.object.isRequired
+  }
+
+  /**
+   * Always returns false, but uses the Highcharts API to update the charts
+   * data or title if they have been updated.
+   *
+   * @method shouldComponentUpdate
+   * @param {Object} nextProps
+   */
+  shouldComponentUpdate(nextProps) {
+    let chart = this.refs.chart.chart;
+
+    // use the highchart api to update its title
+    if (this.props.title !== nextProps.title) {
+      chart.setTitle({ text: nextProps.title });
+    }
+
+    // use the highchart api to update its data
+    if (this.props.data !== nextProps.data) {
+      chart.series[0].setData(nextProps.data.toJS());
+    }
+
+    // never re-render the component
+    return false;
+  }
+
+  /**
+   * Renders the component.
+   *
+   * @method render
+   */
+  render() {
+    let config = generateConfig(this.props.data, this.props.title);
+
+    return (
+      <div className="ui-rainbow">
+        <ReactHighcharts ref="chart" config={config}></ReactHighcharts>
+      </div>
+    );
+  }
+
+}
+
+/**
+ * Uses the Highcharts API to apply z-index to the current segment.
+ *
+ * @method focusSegment
+ * @private
+ */
+function focusSegment() {
+  this.graphic.zIndexSetter(1);
+}
+
+/**
+ * Uses the Highcharts API to apply z-index to the current segment.
+ *
+ * @method unfocusSegment
+ * @private
+ */
+function unfocusSegment() {
+  this.graphic.zIndexSetter(0);
+}
+
+/**
+ * Calculates the position for the tooltip.
+ *
+ * @method tooltipPosition
+ * @param {Number} tooltipWidth
+ * @param {Number} tooltipHeight
+ * @param {Object} point
+ * @private
+ * @return {Object}
+ */
+function tooltipPosition(tooltipWidth, tooltipHeight, point) {
+  let x = point.plotX - (tooltipWidth / 2);
+  let y = point.plotY - (tooltipHeight - 5);
+
+  return { x: x, y: y };
+}
+
+/**
+ * Generates the config for the Highchart.
+ *
+ * @method generateConfig
+ * @param {Object} immutableData
+ * @param {String} title
+ * @private
+ * @return {Object}
+ */
+function generateConfig(immutableData, title) {
+  let data = immutableData.toJS();
+
+  return {
+    credits: {
+      enabled: false
+    },
+    chart: {
+      height: 250,
+      plotBackgroundColor: null,
+      plotBorderWidth: 0,
+      plotShadow: false,
+      spacing: [10,0,0,0],
+      width: 400
+    },
+    title: {
+      align: 'center',
+      style: {
+        "color": "#696d75",
+        "fontFamily": "inherit",
+        "fontSize": "25px",
+        "fontWeight": "bold",
+        "line-height": "16px",
+        "textAlign": "center"
+      },
+      text: title,
+      useHTML: true,
+      verticalAlign: 'top',
+      y: 35
+    },
+    tooltip: {
+      borderRadius: 100,
+      borderWidth: 0,
+      followPointer: true,
+      headerFormat: '',
+      pointFormatter: function() {
+        return '<span style="color: ' + this.color  + '; font-weight: bold">' + this.tooltip + '</span>';
+      },
+      positioner: function(tooltipWidth, tooltipHeight, point) {
+        return tooltipPosition(tooltipWidth, tooltipHeight, point);
+      },
+      style: {
+        "fontFamily": "inherit"
+      },
+      shadow: false
+    },
+    plotOptions: {
+      pie: {
+        animation: {
+          duration: 400
+        },
+        borderWidth: 0,
+        center: ['50%', '100%'],
+        colors: ['#23B492', '#F8B74C', '#EA4D5B'],
+        dataLabels: {
+          connectorWidth: 0,
+          defer: false,
+          distance: 25,
+          enabled: true,
+          formatter: function () {
+            return '<span style="display: block; text-align: center; text-size: 12px; color:' + this.point.color + '">' + this.point.name + '<br><span style="font-weight: normal;">' + this.point.label + '</span></span>';
+          },
+          padding: 0,
+          style: {
+            "fontSize": "12px",
+            "fontFamily": "inherit"
+          },
+          useHTML: true
+        },
+        endAngle: 90,
+        point: {
+          events: {
+            mouseOver: focusSegment,
+            mouseOut: unfocusSegment
+          }
+        },
+        startAngle: -90,
+        states: {
+          hover: {
+            halo: false
+          }
+        }
+      }
+    },
+    series: [{
+      data: data,
+      innerSize: '65%',
+      type: 'pie'
+    }]
+  };
+}
+
+export default Rainbow;
