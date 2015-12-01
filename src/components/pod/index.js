1ffec082afea0e573ff93676119148d432f2c646
Sean Armstrong <sean.armstrong@sage.com>
2015-12-01 16:53:28 +0000

    add title and description to pod. started collapse work

--- src/components/pod/index.js
+++ src/components/pod/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Icon from 'components/icon';
 
 /**
  * A Pod widget.
@@ -20,15 +22,100 @@ import React from 'react';
  */
 class Pod extends React.Component {
 
+  state = {
+    collapsible: undefined
+  }
+
+  componentWillMount = () => {
+    if(this.props.collapsible) {
+      if(this.props.collaspible == 'closed') {
+        this.setState({ collapsible: 'closed' });
+      } else {
+        this.setState({ collapsible: 'open' });
+      }
+    }
+  }
+
+  /**
+   * Returns HTML and text for the dialog title.
+   *
+   * @method podTitle
+   */
+  get podTitle() {
+    let pod;
+    if(this.state.collapsible) {
+      pod = this.podCollapsible;
+    }
+
+    return (
+        this.props.title ?
+          <div className="ui-pod__title" onClick={ this.toggleCollapse }>
+            <h2>{ this.props.title }</h2>
+          { pod }
+          </div> :
+          null
+    );
+  }
+
+  /**
+   * Returns HTML and text for the dialog title.
+   *
+   * @method podTitle
+   */
+  get podDescription() {
+    return (
+        this.props.description ?
+          <div className="ui-pod__description">{ this.props.description }</div> :
+          null
+    );
+  }
+
+  get podCollapsible() {
+    let className = 'ui-pod__arrow ui-pod__arrow--' + this.state.collapsible;
+
+    return(
+      <Icon type='dropdown' className={ className } /> 
+    );
+  }
+
+  toggleCollapse = () => {
+    let newState = this.state.collapsible === 'open' ? 'closed' : 'open'
+    this.setState({ collapsible: newState });
+  };
+
   /**
    * Renders the component.
    *
    * @method render
    */
   render() {
+    let className = "ui-pod";
+    let content;
+    className += this.props.className ? this.props.className : '';
+
+    if(this.state.collapsible) {
+      className += " collapsible " + this.state.collapsible; 
+
+      if(this.state.collapsible === 'open') {
+        content = (<div className='ui-pod__full-content'>
+          { this.podDescription }
+          <div className='ui-pod__content' >
+            { this.props.children }
+          </div>
+        </div>);
+      }
+    }
+
+
     return (
-      <div className="ui-pod">
-        { this.props.children }
+      <div className={ className } >
+        { this.podTitle }
+        <ReactCSSTransitionGroup
+          transitionName='pod'
+          transitionEnterTimeout={ 100 }
+          transitionLeaveTimeout={ 100 } >
+          { content }
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
