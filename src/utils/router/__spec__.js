2e953f458c6807b0bb562d696be604eb757aaa78
Andrew Tait <andrew.tait@sage.com>
2015-12-11 10:21:40 +0000

    renamed more files from index to class name

--- src/utils/router/__spec__.js
+++ src/utils/router/__spec__.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
-import { startRouter } from './index';
+import { startRouter } from './router';
 
 describe('startRouter', () => {
   let render, router, routes;
