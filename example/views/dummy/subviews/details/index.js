d59e3008b43ba68e6d8566077ec552f87056bd4d
Andrew Tait <andrew.tait@sage.com>
2015-12-24 14:07:51 +0000

    updated examples to use new dropdowns

--- example/views/dummy/subviews/details/index.js
+++ example/views/dummy/subviews/details/index.js
@@ -4,10 +4,10 @@ import Textbox from 'components/textbox';
 import Date from 'components/date';
 import Checkbox from 'components/checkbox';
 import Row from 'components/row';
+import FinancesActions from './../../../../actions/finances';
 import Dropdown from 'components/dropdown';
 import DropdownFilter from 'components/dropdown-filter';
 import DropdownFilterAjax from 'components/dropdown-filter-ajax';
-import FinancesActions from './../../../../actions/finances';
 
 class Details extends React.Component {
 
@@ -19,15 +19,10 @@ class Details extends React.Component {
     return (
       <Row>
 
-        <Dropdown name="foo" options={ this.props.options } onChange={ FinancesActions.financesValueUpdated } value={ this.props.foo } />
-
-        <DropdownFilter
-          name="accounts"
-          options={ this.props.options }
-          onChange={ FinancesActions.financesValueUpdated }
-          value={ this.props.accounts }
-          create={ true }
-        />
+        <Textbox name="name" value={ this.props.name } onChange={ FinancesActions.financesValueUpdated } validations={ [Presence()] } />
+        <Date name="date_from" value={ this.props.dateFrom } onChange={ FinancesActions.financesValueUpdated } />
+        <Checkbox name="discount" label="Apply Discount?" value={ this.props.discount } onChange={ FinancesActions.financesValueUpdated } />
+        <Dropdown name="accounts" options={ this.props.options } onChange={ FinancesActions.financesValueUpdated } value={ this.props.accounts }/>
 
         <DropdownFilterAjax
           name="country"
