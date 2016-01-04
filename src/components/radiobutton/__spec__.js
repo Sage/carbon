eb93acc8ea94c58202e20e7167b1f4d25460cb75
William Hearn <william.hearn@sage.com>
2016-01-04 22:40:26 +0000

    Initial RadioButton Files

---
+++ src/components/radiobutton/__spec__.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TestUtils from 'react/lib/ReactTestUtils';
+import Radiobutton from './radiobutton';
+
+describe('Radiobutton', () => {
+  it('failing test demo', () => {
+    expect(1).toEqual(2);
+  });
+});
---
+++ src/components/radiobutton/radiobutton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Radiobutton extends React.Component {
+
+  /**
+   * Renders the component.
+   *
+   * @method render
+   */
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default Radiobutton;
