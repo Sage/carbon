95b8ef54aa47045f9821b067e5c9796190d72754
Sean Armstrong <sean.armstrong@sage.com>
2016-01-05 10:48:41 +0000

    wip multi action button

---
+++ src/components/multi-action-button/__spec__.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TestUtils from 'react/lib/ReactTestUtils';
+import MultiActionButton from './multi-action-button';
+
+describe('MultiActionButton', () => {
+  it('failing test demo', () => {
+    expect(1).toEqual(2);
+  });
+});
