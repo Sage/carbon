ab059a351c3cf64493f708c16169fb5de93056ee
Alexander.Ramin <alexander.ramin@sage.com>
2016-01-08 15:32:16 +0000

    changed lodash function for grid column header

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GridRow from './grid_row';
 import CommonGrid from './../../utils/decorators/common_grid';
-import { capitalize }  from 'lodash';
+import { startCase }  from 'lodash';
 
 /**
  * Grid is a component to display a collection of data or line items.
@@ -92,7 +92,7 @@ class Grid extends React.Component {
    */
   get columns() {
     return this.props.fields.map((column, index) => {
-      let displayName = column.displayName || capitalize(column.name);
+      let displayName = column.displayName || startCase(column.name);
 
       return (
         <th key={ index } className={ this.cellClasses(column) }>{ displayName }</th>
