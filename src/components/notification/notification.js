1e973e9296c42f800760c437169e0cb9c4439807
Sean Armstrong <sean.armstrong@sage.com>
2016-01-11 10:57:52 +0000

    update notification to use props

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -3,6 +3,67 @@ import Button from './../button';
 
 class Notification extends React.Component {
 
+  static propTypes = {
+
+    /**
+     * Customizes the appearance through icon and colour
+     * Can be set to 'info', 'warning', 'success'
+     *
+     * @property as
+     * @type {String}
+     * @default 'secondary'
+     */
+    as: React.PropTypes.string,
+
+    /**
+     * Title to be displayed.
+     *
+     * @property title
+     * @type {String}
+     */
+    title: React.PropTypes.string.isRequired,
+
+    /**
+     * Message to be displayed.
+     *
+     * @property message
+     * @type {String}
+     */
+    message: React.PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    as: 'info'
+  }
+
+  get mainClasses() {
+    let className = 'ui-notification';
+
+    if (this.props.className) {
+      className += ' ' + this.props.className;
+    }
+
+    className += ' ui-notification--' + this.props.as;
+    return className;
+  }
+
+  get buttonClasses() {
+    let className = 'ui-notification__action__button';
+
+    className += ' ui-notification__action__button--' + this.props.as;
+    return className;
+  }
+
+  get buttonText() {
+    let text = 'Got it!';
+
+    if (this.props.as == 'warning') {
+      text = 'Log out now';
+    }
+
+    return text;
+  }
+
   /**
    * Renders the component.
    *
@@ -10,7 +71,7 @@ class Notification extends React.Component {
    */
   render() {
     return (
-      <div className='ui-notification'>
+      <div className={ this.mainClasses }>
 
         <div className='ui-notification__icon'>
           icon goes here
@@ -18,15 +79,15 @@ class Notification extends React.Component {
 
         <div className='ui-notification__info'>
           <div className='ui-notification__title'>
-            Just to let you know...
+            { this.props.title }
           </div>
           <div className='ui-notification__message'>
-            To bring you some exciting new features, we'll be doing some maintenance on Wednesday night from 23:00 EST - sorry you won't be able to use Sage One then.
+            { this.props.message }
           </div>
         </div>
 
         <div className='ui-notification__action'>
-          <Button className='ui-notification__action__button'>Got it!</Button> 
+          <Button className={ this.buttonClasses }>{ this.buttonText }</Button> 
         </div>
 
       </div>
