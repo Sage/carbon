d98cb7ac693be1f6748347dae8cbe8dbe8f15168
Alexander.Ramin <alexander.ramin@sage.com>
2016-02-02 13:07:25 +0000

    removed old tests

--- src/components/table/table-row/__spec__.js
+++ src/components/table/table-row/__spec__.js
@@ -34,38 +34,7 @@ describe('TableRow', () => {
   describe('if the row is clickable', () => {
     it('adds a clickable class', () => {
       let tr = TestUtils.findRenderedDOMComponentWithTag(clickableInstance, 'tr');
-      expect(tr.className).toMatch('ui-table-row__clickable');
-    })
-  });
-
-  describe('with delete action', () => {
-    let func, spy;
-
-    beforeEach(() => {
-      spy = jasmine.createSpy();
-
-      func = function() {
-        spy();
-      };
-
-      instance = TestUtils.renderIntoDocument(
-        <Table>
-          <TableRow className="foo" onDelete={ func }>
-            <TableCell />
-          </TableRow>
-        </Table>
-      );
-    });
-
-    it('renders a actions cell', () => {
-      let tr = TestUtils.findRenderedDOMComponentWithClass(instance, 'ui-table-cell--actions');
-      expect(tr).toBeDefined();
-    });
-
-    it('sets up click event on icon', () => {
-      let icon = TestUtils.findRenderedComponentWithType(instance, Icon);
-      icon.props.onClick();
-      expect(spy).toHaveBeenCalled();
+      expect(tr.className).toMatch('ui-table-row--clickable');
     });
   });
 
--- src/components/table/table-row/table-row.js
+++ src/components/table/table-row/table-row.js
@@ -8,28 +8,15 @@ import classNames from 'classnames';
  *
  * See documentation for Table component.
  *
-<<<<<<< HEAD
  * You can set a property of 'onDelete' which should be a function. This will
  * enable a delete action on the row.
  * If you add an onClick event to a Table Row, will display the cursor as a pointer
  * when hovering over the row.
  *
-=======
->>>>>>> master
  * @class TableRow
  * @constructor
  */
 class TableRow extends React.Component {
-
-  static propTypes = {
-    /**
-     * Enables delete button and defined action on click.
-     *
-     * @property onDelete
-     * @type {Function}
-     */
-    onDelete: React.PropTypes.func
-  }
 
   /**
    * Classes to be applied to the table row component
@@ -51,19 +38,10 @@ class TableRow extends React.Component {
    */
   render() {
     let { ...props } = this.props;
-    let children = React.Children.toArray(props.children);
-
-    if (props.onDelete) {
-      children.unshift(
-        <TableCell key="actions" className="ui-table-cell--actions">
-          <Icon type="delete" onClick={ props.onDelete } />
-        </TableCell>
-      );
-    }
 
     return (
       <tr { ...props } className={ this.mainClasses }>
-        { children }
+        { props.children }
       </tr>
     );
   }
