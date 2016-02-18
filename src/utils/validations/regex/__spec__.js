6628702837048d428d3750ee12194741ad96bd8d
Sean Armstrong <sean.armstrong@sage.com>
2016-02-18 15:07:47 +0000

    remove fdescribe from regex spec

--- src/utils/validations/regex/__spec__.js
+++ src/utils/validations/regex/__spec__.js
@@ -3,8 +3,7 @@ import I18n from "i18n-js";
 import Validator from './regex';
 
 describe('Regex Validator', () => {
-
-  fdescribe('when value is undefined', () => {
+  describe('when value is undefined', () => {
     it('passes validation', () => {
       expect(Validator(/[a-z]/).validate(null)).toBeTruthy();
     });
