31fd3a482325de7f8f530c27fa8a899feb610bba
Sean Armstrong <sean.armstrong@sage.com>
2016-03-09 10:48:17 +0000

    add proptypes to table

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -46,15 +46,127 @@ import Pager from './../pager';
  *     { tableRows }
  *   </Table>
  *
+ * == Pagination
+ *
+ * To add a pagination footer to the table you will need to pass some extra props to the table
+ *
+ *  let sizeOptions = Immutable.fromJS([{ id: '10', name: 10 }, { id: '25', name: 25 }, { id: '50', name: 50 }]),
+ *
+ * <Table
+ *   paginate={ true }                        // Show the pagination footer
+ *   currentPage='1'                          // Required - Current visible page
+ *   pageSize='10'                            // Required - Number of records to show per page
+ *   totalRecords                             // Required - Total number of records
+ *   showPageSizeSelection={ false }          // Options  - Show page size selection
+ *   pageSizeSelectionOptions={ sizeOptions } // Optional - Page Size Options
+ * />
+ *
+ *
+ *
+ *
  * @class Table
  * @constructor
  */
 class Table extends React.Component {
 
+  static propTypes = {
+    /**
+     * Emitted when table component changes e.g.
+     * Pager, sorting, filter
+     *
+     * @property onChange
+     * @type {Function}
+     */
+    onChange: React.PropTypes.func,
+
+    /**
+     * Show the pagination footer
+     *
+     * @property paginate
+     * @type {Boolean}
+     */
+    paginate: React.PropTypes.bool,
+    /**
+     * Pagination
+     * Current Visible Page
+     *
+     * @property currentPage
+     * @type {String}
+     */
+    currentPage: React.PropTypes.string,
+
+    /**
+     * Pagination
+     * Page Size of grid (number of visible records)
+     *
+     * @property pageSize
+     * @type {String}
+     */
+    pageSize: React.PropTypes.string,
+
+    /**
+     * Pagination
+     * Options for pageSize default - 10, 25, 50
+     *
+     * @property pageSizeSelectionOptions
+     * @type {Object} Immutable
+     */
+    pageSizeSelectionOptions: React.PropTypes.object,
+
+    /**
+     * Pagination
+     * Is the page size dropdown visible
+     *
+     * @property showPageSizeSelection
+     * @type {Boolean}
+     */
+    showPageSizeSelection: React.PropTypes.bool,
+
+    /**
+     * Pagination
+     * Total number of records in the grid
+     *
+     * @property totalRecords
+     * @type {String}
+     */
+    totalRecords: React.PropTypes.string
+  }
+
+  /**
+   * Emit onChange event with options
+   * needed to fetch the new data
+   *
+   * @method emitOnChangeCallback
+   * @param {String} element changed element
+   * @param {Object} options base and updated options
+   * @return {Void}
+   */
   emitOnChangeCallback = (element, options) => {
     this.props.onChange(element, options);
   }
 
+  /**
+   * Handlers when the pager emits a onChange event
+   * Passes data to emitOnChangeCallback in the correct
+   * format
+   *
+   * @method onPagination
+   * @param {String} currentPage
+   * @param {String} pageSize
+   * @return {Void}
+   */
+  onPagination = (currentPage, pageSize) => {
+    let options = this.emitOptions;
+    options.currentPage = currentPage;
+    this.emitOnChangeCallback('pager', options);
+  }
+
+  /**
+   * Base Options to be emitted by onChange
+   *
+   * @method emitOptions
+   * @return {Object} options to emit
+   */
   get emitOptions() {
     return {
       currentPage: this.props.currentPage,
@@ -62,12 +174,12 @@ class Table extends React.Component {
     };
   }
 
-  onPagination = (currentPage, pageSize) => {
-    let options = this.emitOptions;
-    options.currentPage = currentPage;
-    this.emitOnChangeCallback('pager', options);
-  }
-
+  /**
+   * Props to pass to pager component
+   *
+   * @method pagerProps
+   * @return {Object} props
+   */
   get pagerProps() {
     return {
       currentPage: this.props.currentPage,
@@ -79,6 +191,12 @@ class Table extends React.Component {
     }
   }
 
+  /**
+   * Returns the pager if paginate is true
+   *
+   * @method pager
+   * @return {JSX} pager
+   */
   get pager() {
     if (this.props.paginate) {
       return (<Pager { ...this.pagerProps } />);
@@ -104,7 +222,6 @@ class Table extends React.Component {
       </div>
     );
   }
-
 }
 
 export {
