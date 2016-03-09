3fb0c3c39b9aee98bbfcb86e4371838c0baa9207
Andrew Tait <andrew.tait@sage.com>
2016-03-09 09:35:38 +0000

    added more comments to to-array

--- src/utils/helpers/to-array/to-array.js
+++ src/utils/helpers/to-array/to-array.js
@@ -22,11 +22,13 @@ export default function(str) {
     result.push(str);
   } else {
     if (index > 0) {
-      // if the square bracket is not the first character
+      // if the square bracket is not the first character, add the name at the
+      // start of the string (not in brackets)
       let name = str.slice(0, index);
       result.push(name);
     }
 
+    // pull out the names in brackets and add to the array
     let nestedNames = str.match(/[^[\]]+(?=])/g);
 
     result = result.concat(nestedNames);
