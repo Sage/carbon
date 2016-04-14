7236ba947cebf38db4296931e9ee14d0f887f877
Alexander.Ramin <alexander.ramin@sage.com>
2016-04-14 14:55:27 +0100

    added docs

--- src/utils/decorators/tooltip-decorator/tooltip-decorator.js
+++ src/utils/decorators/tooltip-decorator/tooltip-decorator.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Tooltip from './../../../components/tooltip';
-import ReactDOM from 'react-dom';
 
 /**
- * ToolTipIdecorator.
+ * ToolTipIDecorator.
  *
  * This decorator attaches a tooltip to a component.
  *
@@ -11,16 +10,38 @@ import ReactDOM from 'react-dom';
  *
  * In your file:
  *
- *   import ToolTip from 'carbon/lib/utils/decorators/tooltip';
+ *   import ToolTip from 'carbon/lib/utils/decorators/tooltip-decorator';
  *
  * To use the decorator, wrap your component with it:
  *
- *   const MyComponent = ToolTip(
+ *   const MyComponent = ToolTipIDecorator(
  *   class MyComponent extends React.Component {
  *     ...
  *   });
  *
- * @method ToolTipIcon
+ *  Your decorated component must add a conditional class name of 'target-tooltip';
+ *
+ * e.g.
+ *
+ *  if (this.props.tooltipMessage) {
+ *    classes += ' target-tooltip '
+ *  }
+ *
+ * You must also output the tooltip HTML in your component's render method:
+ *
+ * e.g.
+ *
+ * render() {
+ *   return (
+ *     <div>
+ *       { this.tooltipHTML }
+ *       ...your components JSX
+ *     </div>
+ *   );
+ * }
+ *
+ *
+ * @method ToolTipIDecorator
  * @param {Class} ComposedComponent class to decorate
  * @return {Object} Decorated Component
  */
@@ -31,34 +52,48 @@ let TooltipIDecorator = (ComposedComponent) => class Component extends ComposedC
   }
 
   state = {
+    /**
+     * Whether tooltip currently showing
+     *
+     * @property showTooltip
+     * @type {Boolean}
+     * @default false
+     */
     showTooltip: false
   };
 
-  showHandler = () => {
+  /**
+   * Shows tooltip
+   *
+   * @method onShow
+   * @return {void}
+   */
+  onShow = () => {
     setTimeout(() => {
       this.setState({ showTooltip: true });
       this.positionTooltip();
     }, 300 );
   }
 
-  hideHandler = () => {
+  /**
+   * Hides tooltip
+   *
+   * @method onHide
+   * @return {void}
+   */
+  onHide = () => {
     this.setState({ showTooltip: false });
   }
 
-  get componentProps() {
-    let props = super.componentProps;
-
-    props.tooltipPosition = props.tooltipPosition || 'top';
-    props.onMouseEnter = this.showHandler;
-    props.onMouseLeave = this.hideHandler;
-    props.onTouchEnd = this.state.showTooltip ? this.hideHandler : this.showHandler;
-
-    return props;
-  }
-
+  /**
+   * Positions tooltip relative to target
+   *
+   * @method positionTooltip
+   * @return {Void}
+   */
   positionTooltip = () => {
     if (this.state.showTooltip) {
-      let target = document.getElementsByClassName('target-tooltip')[0],
+      let target  = document.getElementsByClassName('target-tooltip')[0],
           tooltip = document.getElementsByClassName('ui-tooltip')[0],
           pointer = document.getElementsByClassName('ui-tooltip__pointer')[0];
 
@@ -90,6 +125,29 @@ let TooltipIDecorator = (ComposedComponent) => class Component extends ComposedC
     }
   }
 
+  /**
+   * Additional Props for decorated component
+   *
+   * @method componentProps
+   * @return {Object} props
+   */
+  get componentProps() {
+    let props = super.componentProps;
+
+    props.tooltipPosition = props.tooltipPosition || 'top';
+    props.onMouseEnter = this.onShow;
+    props.onMouseLeave = this.onHide;
+    props.onTouchEnd = this.state.showTooltip ? this.onHide : this.onShow;
+
+    return props;
+  }
+
+  /**
+   * Calculated props for pointer
+   *
+   * @method pointerProps
+   * @return {Object} props
+   */
   get pointerProps() {
     let props = {};
 
