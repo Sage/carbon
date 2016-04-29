2d9ea50fd50766d4591ef232db0f52e0eb065821
Andrew Tait <andrew.tait@sage.com>
2016-04-29 11:36:47 +0100

    fixed tests

--- src/components/radio-button/__spec__.js
+++ src/components/radio-button/__spec__.js
@@ -50,7 +50,7 @@ describe('RadioButton', () => {
 
   describe('fieldHelpClasses', () => {
     it('returns ui-radio-button__help-text', () => {
-      expect(instance.fieldHelpClasses).toEqual('ui-radio-button__help-text');
+      expect(instance.fieldHelpClasses).toEqual('ui-radio-button__help-text common-input__help-text');
     });
   });
 });
