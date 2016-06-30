4ec22c36a61d95c35ec36617dfb232de1cfcf1f3
Alexander.Ramin <alexander.ramin@sage.com>
2016-06-30 15:32:41 +0100

    initial work

---
+++ src/utils/helpers/poller/poller.js
@@ -0,0 +1,44 @@
+import { Request } from 'superagent';
+
+/**
+ * A helper to make poll an endpoint with a GET request
+ *
+ * The helper takes the following params
+ * - options: an object containing the url and optional data object with additional params
+ * - timeout: the time period at which the request is re-submitted
+ * - conditionMet: a function that accepts the ajax response. Use this to return a test a desired condition and return a boolean.
+ * - callback: A callback function to call when the conditionMet returns true
+ * - handleError: a callback function that takes an error and handles it.
+ *
+ * ===Example Usage===
+ *
+ * import poller from 'carbon/lib/utils/helpers/poller';
+ *
+ * let options = {url: 'http://foo/bar/1'};
+ * let timeout = 10000;
+ * let conditionMet = function(response) => { return response.body.status === 'complete' };
+ * let callback = function(response) => { doSomethingFancy(reponse) };
+ * let handleError = function(err) => { FlashMessage("Failed", err) };
+ *
+ * ajaxPoller(options, timeout, conditionMet, callback, handleError);
+ *
+ */
+poller(options, timeout, conditionMet, callback, handleError) {
+  return new Promise((resolve, reject) => {
+    (poll() => {
+       Request
+         .get(options.url)
+         .data(options.data)
+         .end((err, response) => {
+           if (!err) {
+             if (conditionMet(response)) return resolve(callback(response));
+             setTimeout(poll, timeout);
+           } else {
+             reject(handleError(err))
+           }
+         })
+     })();
+  });
+}
+
+export default poller;
