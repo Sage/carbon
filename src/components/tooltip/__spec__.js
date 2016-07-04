ac392a750fb73de4dfd391c44e9c94e06ba1af96
Sean Armstrong <sean.armstrong@sage.com>
2016-07-04 08:29:52 +0100

    upgrade to react 15.2.0

--- src/components/tooltip/__spec__.js
+++ src/components/tooltip/__spec__.js
@@ -45,7 +45,7 @@ describe('tooltip', () => {
         expect(tooltip.classList[0]).toEqual('ui-tooltip');
       });
 
-      fit('renders a pointer span', () => {
+      it('renders a pointer span', () => {
         debugger
         expect(tooltip.children[0].children[1].classList[0]).toEqual('ui-tooltip__pointer');
       });
