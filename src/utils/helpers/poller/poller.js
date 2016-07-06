95b517e47c02df47bdc1b4495a59ba557cc3685c
Alexander.Ramin <alexander.ramin@sage.com>
2016-07-06 13:42:12 +0100

    update documentation

--- src/utils/helpers/poller/poller.js
+++ src/utils/helpers/poller/poller.js
@@ -5,70 +5,39 @@ import './../../promises';
  * A helper to make poll an endpoint with a GET request
  *
  * The helper takes the following params
- * - queryOptions: an object containing the url, optional data object with additional params, and a headers object
- * -- url,
- * -- data,
- * -- headers
- * - timeout: the time period after which the request is re-submitted
+ *
+ * - queryOptions:
+ * -- url - the url to make the GET request to
+ * -- data - any data you wish to pass with the request
+ * -- headers - any header you wish to pass with the request
+ *
  * - functions: An object containing the custom functions
- * --  conditionMet: Use this to return a test a desired condition and return a boolean.
- * --  callback: A callback function to call when the conditionMet returns true
- * --  handleError: a callback function that takes an error and handles it.
+ * -- conditionMet: Use this to test a desired condition in the response and return a boolean.
+ *                  If the condition is true, the callback will be executed.
+ * -- callback: A callback function to call when the conditionMet returns true
+ * -- handleError: a callback function that takes an error and handles it
+ * -- terminate: Use this to test a desired condition in the response and return a boolean.
+ *               If the condition is true, the polling will end.
+ * options:
+ * -- interval - the interval after which the request is re-submitted
+ * -- retries - number of times to re-submit the request before giving up
+ * -- endTime - a time period after which to end the polling
  *
  * ===Example Usage===
  *
  * import poller from 'carbon/lib/utils/helpers/poller';
+ * import Poller from 'carbon/lib/utils/helpers/poller';
  *
- * let options = {url: 'http://foo/bar/1'};
- * let timeout = 10000;
+ * let queryOptions = {  url: '/foo/bar/1' }
+ * let options = { interval: 5000 };
  * let functions = {
  *   conditionMet: function(response) => { return response.body.status === 'complete' };
  *   callback: function(response) => { doSomethingFancy(reponse) };
  *   handleError: function(err) => { FlashMessage("Failed", err) };
- *}
+ * }
  *
- * poller(options, timeout, conditionMet, callback, handleError);
- *
+ * Poller(options, timeout, conditionMet, callback, handleError);
  */
-
-function getQueryOptions(queryOptions) {
-  return {
-    url: queryOptions.url,
-    data: queryOptions.data || {},
-    headers: queryOptions.headers || {}
-  };
-}
-
-function getOptions(options) {
-  return {
-    interval: options.interval || 3000,
-    endTime:  Number(new Date()) + options.endTime || Infinity,
-    retries: options.retries || Infinity
-  };
-}
-
-function getFunctions(functions) {
-  return {
-    callback: functions.callback,
-    handleError: functions.handleError || null,
-    conditionMet: functions.conditionMet || (() => { return false; }),
-    terminate: functions.terminate || (() => { return false; })
-  };
-}
-
-function setupValid(queryOptions, functions) {
-  if (queryOptions === null || typeof queryOptions.url === 'undefined') {
-    console.error('You must provide a url to the poller'); // eslint-disable-line no-console
-    return false;
-  }
-
-  if (typeof functions.callback === 'undefined') {
-    console.error('You must provide a callback function to the poller'); // eslint-disable-line no-console
-    return false;
-  }
-  return true;
-}
-
 export default (queryOptions, functions, options) => {
   if (!setupValid(queryOptions, functions)) {
     return;
@@ -80,6 +49,7 @@ export default (queryOptions, functions, options) => {
   options = getOptions(options);
 
   return new Promise((resolve, reject) => {
+    // pollCount for use if retries option passed
     let pollCount = 1;
 
     (function poll() {
@@ -113,3 +83,65 @@ export default (queryOptions, functions, options) => {
       console.error(err); // eslint-disable-line no-console
     });
 };
+
+/**
+ * Sets default queryOptions
+ *
+ * @method getQueryOptions
+ * @return {Object} queryOptions
+ */
+function getQueryOptions(queryOptions) {
+  return {
+    url: queryOptions.url,
+    data: queryOptions.data || {},
+    headers: queryOptions.headers || {}
+  };
+}
+
+/**
+ * Sets default Options for poller
+ *
+ * @method getOptions
+ * @return {Object} options
+ */
+function getOptions(options) {
+  return {
+    interval: options.interval || 3000,
+    endTime:  Number(new Date()) + options.endTime || Infinity,
+    retries: options.retries || Infinity
+  };
+}
+
+/**
+ * Sets default functions for poller
+ *
+ * @method getFunctions
+ * @return {Object} functions
+ */
+function getFunctions(functions) {
+  return {
+    callback: functions.callback || null,
+    handleError: functions.handleError || null,
+    conditionMet: functions.conditionMet || (() => { return false; }),
+    terminate: functions.terminate || (() => { return false; })
+  };
+}
+
+/**
+ * Checks that required params are present
+ *
+ * @method setupValid
+ * @return {Boolean}
+ */
+function setupValid(queryOptions, functions) {
+  if (queryOptions === null || typeof queryOptions.url === 'undefined') {
+    console.error('You must provide a url to the poller'); // eslint-disable-line no-console
+    return false;
+  }
+
+  if (typeof functions.conditionMet !== 'undefined' && typeof functions.callback === 'undefined') {
+    console.error('You must provide a callback function if you are testing a condition with conditionMet'); // eslint-disable-line no-console
+    return false;
+  }
+  return true;
+}
