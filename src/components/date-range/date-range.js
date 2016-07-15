479b339e5bf4ab984282061d4dc6235edaee7d5e
Alexander.Ramin <alexander.ramin@sage.com>
2016-07-15 12:04:30 +0100

    update docs & readme

--- src/components/date-range/date-range.js
+++ src/components/date-range/date-range.js
@@ -5,43 +5,120 @@ import DateRangeValidator from './../../utils/validations/date-range';
 
 class DateRange extends React.Component {
   static propTypes = {
+
+    /**
+     * Custom callback - receives array of startDate and endDate
+     *
+     * @property onChange
+     * @type {Func}
+     */
     onChange: PropTypes.func.isRequired,
+
+    /**
+     * An array containing the value of startDate and endDate
+     *
+     * @property value
+     * @type {Array}
+     */
     value: PropTypes.array.isRequired,
+
+    /**
+     * Optional label for startDate field
+     *
+     * @property startLabel
+     * @type {String}
+     */
     startLabel: PropTypes.string,
+
+    /**
+     * Optional label for endDate field
+     *
+     * @property endDate
+     * @type {String}
+     */
     endLabel: PropTypes.string,
+
+    /**
+     * Custom message for startDate field
+     *
+     * @property startDate
+     * @type {String}
+     */
     startMessage: PropTypes.string,
+
+    /**
+     * Custom message for endDate field
+     *
+     * @property endDate
+     * @type {String}
+     */
     endMessage: PropTypes.string
   };
 
+  /**
+   * onChange function -triggers validations on both fields and updates opposing field when one changed.
+   *
+   * @property _onChange
+   * @type {func}
+   * @param{String} the date field that has changedDate
+   * @param{Object} ev the event containing the new date value
+   */
   _onChange = (changedDate, ev) => {
     let newValue = ev.target.value;
 
     if(changedDate === 'startDate') {
       this.props.onChange([newValue, this.endDate]);
+      // resets validations on opposing field
       this._endDate._handleContentChange();
     }
 
     if (changedDate === 'endDate') {
       this.props.onChange([this.startDate, newValue]);
+      // resets validations on opposing field
       this._startDate._handleContentChange();
     }
 
+    // Triggers validations on both fields
     this._startDate._handleBlur();
     this._endDate._handleBlur();
   }
 
+  /**
+   * The startDate value
+   *
+   * @method startDate
+   * @return {String} the value of the start date
+   */
   get startDate() {
     return this.props.value[0];
   }
 
+  /**
+   * The ednDate value
+   *
+   * @method endDate
+   * @return {String} the value of the end date
+   */
   get endDate() {
     return this.props.value[1];
   }
 
+  /**
+   * The error message for the start message.
+   *
+   * @method startMessage
+   * @return {String}
+   */
   get startMessage() {
     return this.props.startMessage || I18n.t('errors.messages.date_range');
   }
 
+  /**
+   * The error message for the end message.
+   *
+   * @method endMessage
+   * @return {String}
+   */
   get endMessage() {
     return this.props.endMessage || I18n.t('errors.messages.date_range');
   }
