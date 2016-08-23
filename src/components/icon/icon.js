4e8a273cc083e4af755e7cbd364453bce9f81910
Hartley <daniel.hartley@sage015353.sageukie.adinternal.com>
2016-08-23 09:56:25 +0100

    updated class names to use new carbon naming policy

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -720,7 +720,7 @@ const Icon = TooltipDecorator(class Icon extends React.Component {
    */
   get renderKeyIcon() {
     return {
-      __html: '<svg class="ui-icon__svg ui-icon__svg--key" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#92959D" fill-rule="evenodd" d="M9.71 9.314C9.896 9.84 10 10.41 10 11c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c.59 0 1.156.102 1.682.29L12.64.36c.2-.2.576-.36.855-.36h2.01c.273 0 .495.218.495.5v2.032c0 .276-.23.497-.504.492l-.99-.016c-.278-.004-.504.206-.504.497v.99c0 .28-.214.505-.505.505h-.992c-.28 0-.505.214-.505.505v.99c0 .28-.15.658-.35.86l-1.94 1.96zM4.5 13c.828 0 1.5-.672 1.5-1.5S5.328 10 4.5 10 3 10.672 3 11.5 3.672 13 4.5 13z"/></svg>'
+      __html: '<svg class="carbon-icon__svg carbon-icon__svg--key" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#92959D" fill-rule="evenodd" d="M9.71 9.314C9.896 9.84 10 10.41 10 11c0 2.76-2.24 5-5 5s-5-2.24-5-5 2.24-5 5-5c.59 0 1.156.102 1.682.29L12.64.36c.2-.2.576-.36.855-.36h2.01c.273 0 .495.218.495.5v2.032c0 .276-.23.497-.504.492l-.99-.016c-.278-.004-.504.206-.504.497v.99c0 .28-.214.505-.505.505h-.992c-.28 0-.505.214-.505.505v.99c0 .28-.15.658-.35.86l-1.94 1.96zM4.5 13c.828 0 1.5-.672 1.5-1.5S5.328 10 4.5 10 3 10.672 3 11.5 3.672 13 4.5 13z"/></svg>'
     };
   }
 
@@ -732,7 +732,7 @@ const Icon = TooltipDecorator(class Icon extends React.Component {
    */
   get renderPaymentIcon() {
     return {
-      __html: '<svg class="ui-icon__svg ui-icon__svg--payment" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#9DA0A7" fill-rule="evenodd" d="M16 5V3.002C16 2.456 15.552 2 15 2H1c-.555 0-1 .45-1 1.002V5h16zm0 2v5.998C16 13.55 15.555 14 15 14H1c-.552 0-1-.456-1-1.002V7h16z"/></svg>'
+      __html: '<svg class="carbon-icon__svg carbon-icon__svg--payment" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#9DA0A7" fill-rule="evenodd" d="M16 5V3.002C16 2.456 15.552 2 15 2H1c-.555 0-1 .45-1 1.002V5h16zm0 2v5.998C16 13.55 15.555 14 15 14H1c-.552 0-1-.456-1-1.002V7h16z"/></svg>'
     };
   }
 
