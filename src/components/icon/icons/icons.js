e65d7c709cb20c8db3eb2e664c73bb1685f1e11a
Sean Armstrong <sean.armstrong@sage.com>
2016-09-16 15:09:35 +0100

    icon refactor

---
+++ src/components/icon/icons/icons.js
@@ -0,0 +1,66 @@
+import Basket from './svgs/basket';
+import Bin from './svgs/bin';
+import Chevron from './svgs/chevron';
+import Completed from './svgs/completed';
+import Email from './svgs/email';
+import ExternalLink from './svgs/external-link';
+import Help from './svgs/help';
+import Individual from './svgs/individual';
+import Information from './svgs/information';
+import Key from './svgs/key';
+import Location from './svgs/location';
+import Maintenance from './svgs/maintenance';
+import Message from './svgs/message';
+import Minus from './svgs/minus';
+import Mobile from './svgs/mobile';
+import New from './svgs/new';
+import PDF from './svgs/pdf';
+import Paperclip from './svgs/paperclip';
+import Payment from './svgs/payment';
+import Phone from './svgs/phone';
+import Plus from './svgs/plus';
+import Print from './svgs/print';
+import Processing from './svgs/processing';
+import Progress from './svgs/progress';
+import Refresh from './svgs/refresh';
+import SortDown from './svgs/sort-down';
+import SortUp from './svgs/sort-up';
+import Submitted from './svgs/submitted';
+import Sync from './svgs/sync';
+import Warning from './svgs/warning';
+import WhiteTick from './svgs/white-tick';
+
+
+export default {
+  basket: Basket,
+  bin: Bin,
+  chevron: Chevron,
+  completed: Completed,
+  email: Email,
+  'external-link': ExternalLink,
+  help: Help,
+  individual: Individual,
+  information: Information,
+  key: Key,
+  location: Location,
+  maintenance: Maintenance,
+  message: Message,
+  minus: Minus,
+  mobile: Mobile,
+  new: New,
+  pdf: PDF,
+  paperclip: Paperclip,
+  payment: Payment,
+  phone: Phone,
+  plus: Plus,
+  print: Print,
+  processing: Processing,
+  progress: Progress,
+  refresh: Refresh,
+  'sort-down': SortDown,
+  'sort-up': SortUp,
+  submitted: Submitted,
+  sync: Sync,
+  warning: Warning,
+  'white-tick': WhiteTick
+}
