29517e92a265274a0e6ee9d7224b1e75006e7452
Sean Armstrong <sean.armstrong@sage.com>
2016-10-14 10:38:46 +0100

    CARBON-493: initial build with tests
    * three components: `VerticalMenu`, `Submenu` and `ListItem`
    * tests provided with Enzyme for Vertcial Menu and Submenu components

--- demo/views/homepage/homepage.js
+++ demo/views/homepage/homepage.js
@@ -1,15 +1,52 @@
 import React from 'react';
+
+import Link from 'components/link';
 import Pod from 'components/pod';
+import TextBox from 'components/textbox';
+
+import Submenu from 'components/submenu';
+
+const components = [
+   { name: 'One' },
+   { name: 'Two' }
+];
+const patterns = [
+   { name: 'One' },
+   { name: 'Two' }
+];
 
 class Homepage extends React.Component {
   /**
    * @method render
    */
+  get componentsHTML() {
+    return components.map((item, i) => {
+      return <Link key={ i } href='#' name={ item.name }>{ item.name }</Link>;
+    });
+  }
+  get patternsHTML() {
+    return patterns.map((item, i) => {
+      return <Link key={ i } href='#' name={ item.name }>{ item.name }</Link>;
+    });
+  }
+
   render() {
     return (
-      <Pod className="carbon-homepage">
-        Carbon
-      </Pod>
+      <Submenu
+        className='home-menu'
+        toggleable={ false }
+        initiallyOpen={ true }
+      >
+        <Link>Getting Started</Link>
+        <Submenu title='Components' filter={ true }>
+          { this.componentsHTML }
+        </Submenu>
+        <Submenu title='Patterns' filter={ false }>
+          { this.patternsHTML }
+        </Submenu>
+        <Link>Style</Link>
+        <Link>Articles</Link>
+      </Submenu>
     );
   }
 }
