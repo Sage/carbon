fa1dd04555c450588f264dfabc9075cc5a9ee81c
Alexander.Ramin <alexander.ramin@sage.com>
2016-10-21 09:48:35 +0100

    add dynamic year and unicode copyright symbol

--- demo2/views/chrome/app-footer/app-footer.js
+++ demo2/views/chrome/app-footer/app-footer.js
@@ -8,12 +8,12 @@ class AppFooter extends React.Component {
       <div className="app-footer">
         <div className="app-footer__icons">
           <Link href='#'><Icon type='twitter' /></Link>
-          <Link href='#'><Icon type='dribble' /></Link>
+          <Link href='#'><Icon type='dribbble' /></Link>
           <Link href='#'><Icon type='github' /></Link>
         </div>
         <div className="app-footer__legal">
           <span className='app-footer__sage-icon' />
-          <span className='legal__text'>&copy; Sage(UK) Ltd. 2016, All rights reserved. Last updated on October 20th 2016</span>
+          <span className='legal__text'>{ `\u00A9 Sage(UK) Ltd. ${new Date().getFullYear()}, All rights reserved. Last updated on October 20th 2016` }</span>
         </div>
       </div>
     );
