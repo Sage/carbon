cafa9a9434c9ac099acfa1a290156ff2a90de4e1
Toni-Leigh Sharpe <toni.leigh@sage.com>
2016-10-21 10:22:43 +0100

    tweaks side-bar
    * a side bar was already loaded
    * some css was over-ridden from the base side bar in order to make it plain white
    * menu elements were brought in from the other home page where they existed

--- demo2/views/chrome/app-sidebar/app-sidebar.js
+++ demo2/views/chrome/app-sidebar/app-sidebar.js
@@ -1,7 +1,28 @@
 import React from 'react';
+
+import Link from 'components/link';
 import { Sidebar, SidebarHeader } from 'components/sidebar';
+import Submenu from 'components/submenu';
+
+const components = [
+   { name: 'One' },
+   { name: 'Two' }
+];
+const patterns = [
+   { name: 'One' },
+   { name: 'Two' }
+];
 
 class AppSidebar extends React.Component {
+  /**
+   * @method render
+   */
+  get componentsHTML() {
+    return components.map((item, i) => {
+      return <Link key={ i } href='#' name={ item.name }>{ item.name }</Link>;
+    });
+  }
+
   render() {
     return (
       <Sidebar
@@ -15,7 +36,24 @@ class AppSidebar extends React.Component {
           Carbon
         </SidebarHeader>
 
-        Menu
+        <Submenu
+          className='carbon-app-sidebar__menu'
+          toggleable={ false }
+          initiallyOpen={ true }
+        >
+          <Link>Getting Started</Link>
+          <Submenu title='Components' filter={ true }>
+            { this.componentsHTML }
+          </Submenu>
+          <Submenu title='Patterns' filter={ false }>
+            <Link href='#' name={ 'Items' }>Items</Link>
+            <Link href='#' name={ 'Stuff' }>Stuff</Link>
+            <Link href='#' name={ 'Things' }>Things</Link>
+          </Submenu>
+          <Link>Style</Link>
+          <Link>Articles</Link>
+        </Submenu>
+
       </Sidebar>
     );
   }
