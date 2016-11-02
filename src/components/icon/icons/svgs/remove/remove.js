d9bfe8b8f9c2e4f2c416ab6f7e423f4cd65697e9
Hartley <daniel.hartley@sage.com>
2016-11-02 15:19:24 +0000

    updated remove icon to use currentColor

--- src/components/icon/icons/svgs/remove/remove.js
+++ src/components/icon/icons/svgs/remove/remove.js
@@ -1,6 +1,6 @@
 export default {
   __html:
     '<svg class="carbon-icon__svg carbon-icon__svg--remove" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">' +
-      '<rect width="14" height="2" x="1" y="7" fill="#9DA0A7" fill-rule="evenodd" rx="1"/>' +
+      '<rect width="14" height="2" x="1" y="7" fill="currentColor" fill-rule="evenodd" rx="1"/>' +
     '</svg>'
 };
