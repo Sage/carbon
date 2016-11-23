ee455fbfa5d9bf968a9014b42f20104235c3fd69
Toni-Leigh Sharpe <toni.leigh@sage.com>
2016-11-23 14:00:42 +0000

    demo site copied feedback for ColorPanel

--- demo2/views/components/color-panel/color-panel.js
+++ demo2/views/components/color-panel/color-panel.js
@@ -16,13 +16,14 @@ export default props =>
   <CopyActionWrapper
     className={ `demo-color-panel demo-color-panel--${props.color.fontContrast}` }
     style={ { backgroundColor: props.color.hex } }
-    valueToCopy={ props.color.hex }
+    valueToCopy={ props.color.name }
   >
     <div className='demo-color-panel__details'>
       <h3 className='demo-color-panel__title'>{ props.color.name }</h3>
       <div className='demo-color-panel__copy'>
         <span className='demo-color-panel__supplementary'>{ props.color.hex }</span>
         <span className='demo-color-panel__copy-popup'>Copy</span>
+        <span className='demo-color-panel__copied-feedback'>Copied!</span>
       </div>
     </div>
   </CopyActionWrapper>
