9b735be7597f552ffbe91607b45e68cd779fc777
Toni-Leigh Sharpe <toni.leigh@sage.com>
2016-12-28 14:45:43 +0000

    adds component page and component preview page section

---
+++ demo2/views/page-sections/component-preview/component-preview.js
@@ -0,0 +1,137 @@
+// React
+import classNames from 'classnames';
+import Highlight from 'react-highlight';
+import ImmutableHelper from 'utils/helpers/immutable';
+import React from 'react';
+
+// Carbon
+import Checkbox from 'components/checkbox';
+import Dropdown from 'components/dropdown';
+import Form from 'components/form';
+import Textarea from 'components/textarea';
+import Textbox from 'components/textbox';
+
+import DefinitionHelper from 'utils/helpers/definition-helper';
+
+// App Components
+import ComponentCodeBuilder from './../../../utils/component-code-builder';
+import PageContentArea from '../../page-sections/page-content-area';
+
+// Flux
+import ComponentActions from '../../../actions/component';
+
+/**
+ * Simple, site wrapped content area that loads a heading
+ *
+ * @param {object} props
+ * @param {String} props.title
+ * @return {PageContentArea}
+ */
+export default props => (
+  <PageContentArea
+    title='Preview'
+    link={ `https://github.com/Sage/carbon/tree/master/src/components/${props.definition.key}` }
+  >
+    <div className= { `component-preview component-preview--${props.definition.key}` }>
+      <div className='component-preview__component-wrapper'>
+        { React.createElement(props.definition.component, props.definition.demoProps) }
+      </div>
+      <div className='component-preview__interaction'>
+        <form className='component-preview__controls'>
+          { _buildFields(props) }
+        </form>
+        <code className='component-preview__code'>
+          { _buildCode(props) }
+        </code>
+      </div>
+    </div>
+  </PageContentArea>
+);
+
+/**
+ * builds code output
+ *
+ * @private
+ * @method _buildCode
+ * @param {Object} def - definition
+ * @return {String} code string
+ */
+const _buildCode = (props) => {
+  let codeObj = new ComponentCodeBuilder(props.name);
+
+  for (var prop in props.definition.demoProps) {
+    codeObj.addProp(prop, props.definition.demoProps[prop]);
+  }
+
+  return codeObj.toString();
+}
+
+/**
+ * builds fields for dynamically editing props
+ *
+ * @private
+ * @method _buildCode
+ * @param {Object} def - definition
+ * @return {String} code string
+ */
+const _buildFields = (props) => {
+  let fieldObj = [];
+
+  // get the props
+  let demoProps = props.definition.demoProps;
+  let sortedProps = Object.keys(demoProps).sort();
+
+  for (let i = 0; i < sortedProps.length; i ++) {
+    let propKey = sortedProps[i];
+
+    let demoPropData = demoProps[propKey],
+        propOptions = props.definition.propOptions ? props.definition.propOptions[propKey] : null;
+
+    if (propOptions) {
+      let opts = propOptions.map((option) => {
+        return { id: option, name: option };
+      });
+
+      fieldObj[i] = (
+        <Dropdown
+          label={ propKey }
+          onChange={ ComponentActions.updateDefinition.bind(this, propKey, props.name) }
+          options={ ImmutableHelper.parseJSON(opts) }
+          value={ demoPropData }
+        />
+      );
+    } else {
+      if (propKey === 'children') {
+        if (['object','array'].indexOf(typeof demoProps[propKey]) < 0) {
+          fieldObj[i] = (
+            <Textarea
+              label={ propKey }
+              onChange={ ComponentActions.updateDefinition.bind(this, propKey, props.name) }
+              value={ demoPropData }
+            />
+          );
+        }
+      } else if (DefinitionHelper.commonBooleans().indexOf(propKey) >= 0) {
+        fieldObj[i] = (
+          <Checkbox
+            label={ propKey }
+            onChange={ ComponentActions.updateDefinition.bind(this, propKey, props.name) }
+            value={ demoPropData }
+          />
+        );
+      } else if (DefinitionHelper.commonEvents().indexOf(propKey) >= 0) {
+        console.log('event triggered');
+      } else {
+        fieldObj[i] = (
+          <Textbox
+            label={ propKey }
+            onChange={ ComponentActions.updateDefinition.bind(this, propKey, props.name) }
+            value={ demoPropData }
+          />
+        );
+      }
+    }
+  }
+
+  return fieldObj;
+}
---
+++ demo2/views/pages/component-page/component-page.js
@@ -0,0 +1,105 @@
+// React
+import React from 'react';
+import ImmutableHelper from 'utils/helpers/immutable';
+
+// App Components
+import Definitions from './../../../definitions';
+import PageContentArea from '../../page-sections/page-content-area';
+import SubPageChrome from '../../sub-page-chrome';
+import ComponentPreview from '../../page-sections/component-preview';
+
+// Flux Components
+import { connect } from 'utils/flux';
+import ComponentActions from '../../../actions/component';
+import ComponentStore from '../../../stores/component';
+
+import DefinitionHelper from 'utils/helpers/definition-helper';
+
+let definitionKeys = Object.keys(Definitions).sort();
+
+class ComponentPage extends React.Component {
+  render() {
+    let def = this.state.componentStore.get(this.props.name);
+
+    if (def.toJS) { def = def.toJS(); }
+
+    let position = this._componentPosition(def);
+
+    return (
+      <SubPageChrome
+        subtitle={ def.text.description }
+        title={ def.text.name }
+        titleAppend={ def.text.type }
+        previousPage={ this._prepareSubnavObject(this._previousComponent(position)) }
+        nextPage={ this._prepareSubnavObject(this._nextComponent(position)) }
+      >
+        <ComponentPreview definition={ def } name={ this.props.name } />
+        <PageContentArea title='Designer Notes'>
+          { def.text.details }
+        </PageContentArea>
+      </SubPageChrome>
+    );
+  }
+
+  /**
+   * gets the component array position for populating next and previous
+   *
+   * @private
+   * @method _componentPosition
+   * @param {Object} definition
+   * @return {Number} position of the component in the array
+   */
+  _componentPosition = (def) => {
+    return definitionKeys.indexOf(def.key);
+  }
+
+  /**
+   * cyclically retrieves next component
+   *
+   * @private
+   * @method _nextComponent
+   * @param {Number} current - current position
+   * @return {Object} Definitions element
+   */
+  _nextComponent = (current) => {
+    let pos = current === definitionKeys.length - 1
+      ? 0
+      : current + 1;
+
+    return Definitions[definitionKeys[pos]];
+  }
+
+  /**
+   * cyclically retrieves previous component
+   *
+   * @private
+   * @method _previousComponent
+   * @param {Number} current - current position
+   * @return {Object} Definitions element
+   */
+  _previousComponent = (current) => {
+    let pos = current === 0
+      ? definitionKeys.length - 1
+      : current - 1;
+
+    return Definitions[definitionKeys[pos]];
+  }
+
+  /**
+   * prepares an object for sub navigation, using href and name keys, from a definition
+   *
+   * @private
+   * @method _prepareSubnavObject
+   * @param {Object} def - a component definition
+   * @return {Object}
+   */
+  _prepareSubnavObject = (def) => {
+    return {
+      name: def.text.name,
+      href: `/components/${def.key}`
+    };
+  }
+}
+
+export default connect(ComponentPage, ComponentStore);
+
