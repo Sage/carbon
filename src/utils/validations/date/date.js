246dd9dcdbe08d66ba7a396c8e05d85c027acc9f
Sean Armstrong <sean.armstrong@sage.com>
2017-02-06 10:38:28 +0000

    reduce long line

--- src/utils/validations/date/date.js
+++ src/utils/validations/date/date.js
@@ -20,7 +20,10 @@ class DateValidator {
    * @return {Boolean} true if value is valid
    */
   validate = (value) => {
-    let validFormats = I18n.t('date.formats.inputs', { defaultValue: ["MMM/DD/YY", "DD/MM", "DD/MM/YYYY", "DD/MMM/YYYY", "YYYY/MM/DD"] });
+    let validFormats = I18n.t(
+      'date.formats.inputs',
+      { defaultValue: ["MMM/DD/YY", "DD/MM", "DD/MM/YYYY", "DD/MMM/YYYY", "YYYY/MM/DD"] }
+    );
     return !value || moment(this._sanitize(value), validFormats, I18n.locale, true).isValid();
   }
 
