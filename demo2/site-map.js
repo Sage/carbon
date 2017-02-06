a21354e6e43efef737f809ce70f9bb84fa96ee7f
Andrew Tait <andrew.tait@sage.com>
2017-02-06 12:39:44 +0000

    removed getting started test

--- demo2/site-map.js
+++ demo2/site-map.js
@@ -2,7 +2,6 @@ import Definitions from './definitions';
 import Colors from './views/pages/style/colors';
 import Icons from './views/pages/style/icons';
 import Component from './views/pages/component';
-import Textbox from 'components/textbox';
 import SiteMapHelper from './utils/site-map-helper';
 
 // Available options per route:
@@ -10,9 +9,6 @@ import SiteMapHelper from './utils/site-map-helper';
 //  * items (a hash of sub-routes)
 //  * filter (will enable a filter over the route's sub-routes)
 export default new SiteMapHelper({
-  "/getting-started": {
-    component: Textbox
-  },
   "/style": {
     items: {
       colors: {
