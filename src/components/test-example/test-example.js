826eee5db125d959c30ed40b917d945c189eac28
Toni-Leigh Sharpe <toni.leigh@sage.com>
2017-02-21 11:23:35 +0000

    WIP: re-usable, clearer structure tests

---
+++ src/components/test-example/test-example.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Button from 'components/button';
+import Heading from 'components/heading';
+import Link from 'components/link';
+
+const TestExample = () =>
+  <div>
+    <Button>Click me</Button>
+    <Button>Go back</Button>
+    <Heading />
+    <Link />
+  </div>
+;
+
+export default TestExample;
