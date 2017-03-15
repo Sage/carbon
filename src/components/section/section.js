0fac7d46e3f14735939adf240675ace77ca47e52
Tom Winter <tomw@kyan.com>
2017-03-15 15:07:55 +0000

    Refactor Section into functional, stateless component

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,56 +1,15 @@
 import React from 'react';
 
-/**
- * A section wrapper.
- *
- * This is a standalone section wrapper used for layout.
- *
- * == How to use a Section in a component:
- *
- * In your file
- *
- *   import Section from 'carbon/lib/components/section';
- *
- * To render the Section:
- *
- *   <Section />
- *
- * @class Section
- * @constructor
- */
-class Section extends React.Component {
-  static propTypes = {
-    /**
-     * The elements to be rendered in the section
-     *
-     * @property children
-     * @type {Node}
-     */
-    children: React.PropTypes.node,
+let Section = (props) =>
+  <div className={ `carbon-section ${props.className}` }>
+    <h2 className='carbon-section__title'>{ props.title }</h2>
+    { props.children }
+  </div>
+;
 
-    /**
-     * Pass a custom value for the title
-     *
-     * @property title
-     * @type {String}
-     */
-    title: React.PropTypes.string.isRequired
-  }
-
-  /**
-   * Renders the component.
-   *
-   * @method render
-   * @return {Object} JSX
-   */
-  render() {
-    return (
-      <div className={ `carbon-section ${this.props.className}` }>
-        <h2 className='carbon-section__title'>{ this.props.title }</h2>
-        { this.props.children }
-      </div>
-    );
-  }
-}
+Section.propTypes = {
+  children: React.PropTypes.node,
+  title:    React.PropTypes.string.isRequired
+};
 
 export default Section;
