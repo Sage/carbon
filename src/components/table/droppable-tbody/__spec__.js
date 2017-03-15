e9eb2c088028563e51dc2dc318603f9e698dde86
Ian Oxley <ian.oxley@sage.com>
2017-03-15 12:33:20 +0000

    Add droppable-tbody component

---
+++ src/components/table/droppable-tbody/__spec__.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TestUtils from 'react/lib/ReactTestUtils';
+import DroppableTbody from './droppable-tbody';
+
+describe('DroppableTbody', () => {
+  it('failing test demo', () => {
+    expect(1).toEqual(2);
+  });
+});
