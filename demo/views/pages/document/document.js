9fc79768e384a937c031f5150912fd7073b176ba
Andrew Tait <andrew.tait@sage.com>
2017-03-21 16:37:30 +0000

    added styling for doc pages

--- demo/views/pages/document/document.js
+++ demo/views/pages/document/document.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'utils/flux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import DocumentStore from './../../../stores/document';
 import DocumentActions from './../../../actions/document';
 import Request from 'superagent';
@@ -7,6 +8,7 @@ import marked from 'marked';
 import Spinner from 'components/spinner';
 import Row from 'components/row';
 import { enableMock, disableMock } from './../../../xhr-mock';
+import Highlight from 'react-highlight';
 
 class Document extends React.Component {
   state = { guide: '' }
@@ -35,16 +37,37 @@ class Document extends React.Component {
   }
 
   render() {
-    let content = this.getContent();
+    let content = this.getContent(),
+        spinner = null;
 
     if (content) {
-      content = marked(content);
-      return (
-        <div dangerouslySetInnerHTML={{ __html: content }} />
+      content = (
+        <div className="demo-document__content">
+          <Highlight innerHTML={ true }>
+            { marked(content) }
+          </Highlight>
+        </div>
       );
     } else {
-      return <Row><Spinner columnAlign="center" size="small" /></Row>;
+      spinner = <Row><Spinner columnAlign="center" size="small" /></Row>;
     }
+
+    return (
+      <div className="demo-document">
+        { content }
+
+        <ReactCSSTransitionGroup
+          className="demo-document__loading"
+          transitionName="demo-document__loading"
+          transitionAppear={ true }
+          transitionAppearTimeout={ 300 }
+          transitionEnterTimeout={ 300 }
+          transitionLeaveTimeout={ 0 }
+        >
+          { spinner }
+        </ReactCSSTransitionGroup>
+      </div>
+    );
   }
 }
 
