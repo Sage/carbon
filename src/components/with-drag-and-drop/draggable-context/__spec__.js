c4b6024abd81b33562c78c343ecb99e0210ecfb5
Ian Oxley <ian.oxley@sage.com>
2017-03-22 18:50:41 +0000

    Add test that child nodes are rendered.
    `render` should render all child nodes.

--- src/components/with-drag-and-drop/draggable-context/__spec__.js
+++ src/components/with-drag-and-drop/draggable-context/__spec__.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { DragDropContext } from 'react-dnd';
 import DraggableContext from './draggable-context';
+import { mount } from 'enzyme';
 
-fdescribe('DraggableContext', () => {
+describe('DraggableContext', () => {
 
   it('is wrapped in a DragDropContextContainer', () => {
     expect(DraggableContext.name).toBe('DragDropContextContainer');
@@ -10,5 +11,21 @@ fdescribe('DraggableContext', () => {
 
   it('has a DecoratedComponent pointing to the original component', () => {
     expect(DraggableContext.DecoratedComponent.name).toBe('DraggableContext');
+  });
+
+  describe('render', () => {
+    it('renders this.props.children', () => {
+      let wrapper = mount(
+        <DraggableContext>
+          <div>
+            <p>One</p>
+            <p>Two</p>
+          </div>
+        </DraggableContext>
+      );
+
+      expect(wrapper.find('div').length).toEqual(1);
+      expect(wrapper.find('p').length).toEqual(2);
+    });
   });
 });
