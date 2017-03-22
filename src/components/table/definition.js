7cc4f6944918d8c85269696c61b48d53ff7eec32
Andrew Tait <andrew.tait@sage.com>
2017-03-22 09:40:33 +0000

    added todo around table data for demo

--- src/components/table/definition.js
+++ src/components/table/definition.js
@@ -6,6 +6,8 @@ import tableCellDefinition from './table-cell/definition';
 import tableHeaderDefinition from './table-header/definition';
 import tableSubheaderDefinition from './table-subheader/definition';
 
+// TODO: see if we can remove the need for this - this populates the data by
+// fetching it from the mock service
 setTimeout(() => {
   // trigger action to init table with data
   ComponentActions.updateTable();
