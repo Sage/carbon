9b0ab612fc5cc3bbc10f3a6614ddbcd642495926
Andrew Tait <andrew.tait@sage.com>
2017-03-22 09:39:00 +0000

    updated alignment of vars and fixed css path

--- demo/site-map.js
+++ demo/site-map.js
@@ -4,6 +4,9 @@ import Icons from './views/pages/style/icons';
 import Component from './views/pages/component';
 import SiteMapHelper from './utils/site-map-helper';
 
+const guidesPath = '/docs/guides';
+const tutorialsPath = '/docs/tutorials';
+
 // Available options per route:
 //  * component (will render that component for the route)
 //  * items (a hash of sub-routes)
@@ -11,15 +14,15 @@ import SiteMapHelper from './utils/site-map-helper';
 export default new SiteMapHelper({
   "/getting-started": {
     items: {
-      "setting-up-your-environment": "/docs/guides/setting-up-your-environment.md",
-      "hello-world": "/docs/guides/getting-started.md",
-      "a-basic-example-of-flux": "/docs/guides/a-basic-example.md"
+      "setting-up-your-environment": `${guidesPath}/setting-up-your-environment.md`,
+      "hello-world":                 `${guidesPath}/getting-started.md`,
+      "a-basic-example-of-flux":     `${guidesPath}/a-basic-example.md`
     }
   },
   "/components/:name": {
     component: Component,
-    items: Object.keys(Definitions),
-    filter: true
+    items:     Object.keys(Definitions),
+    filter:    true
   },
   "/style": {
     items: {
@@ -33,21 +36,21 @@ export default new SiteMapHelper({
   },
   "/guides": {
     items: {
-      "flux": "/docs/guides/flux.md",
-      "immutable": "/docs/guides/immutable.md",
-      "validations": "/docs/guides/validations.md",
-      "assets": "/docs/guides/assets.md",
-      "decorators": "/docs/guides/decorators.md",
-      "handlers": "/docs/guides/handlers.md",
-      "retrieving-data": "/docs/guides/retrieving-data.md",
-      "integrating-other-code": "/docs/guides/integrating-with-other-ui.md",
+      "flux":                   `${guidesPath}/flux.md`,
+      "immutable":              `${guidesPath}/immutable.md`,
+      "validations":            `${guidesPath}/validations.md`,
+      "assets":                 `${guidesPath}/assets.md`,
+      "decorators":             `${guidesPath}/decorators.md`,
+      "handlers":               `${guidesPath}/handlers.md`,
+      "retrieving-data":        `${guidesPath}/retrieving-data.md`,
+      "integrating-other-code": `${guidesPath}/integrating-with-other-ui.md`,
     }
   },
   "/tutorials": {
     items: {
-      "rails-part-1:-hello-world": "/docs/tutorials/carbon-rails/hello-world.md",
-      "rails-part-2:-introducing-data": "/docs/tutorials/carbon-rails/introducing-data.md",
-      "rails-part-2:-updating-data": "/docs/tutorials/carbon-rails/updating-data.md"
+      "rails-part-1:-hello-world":      `${tutorialsPath}/carbon-rails/hello-world.md`,
+      "rails-part-2:-introducing-data": `${tutorialsPath}/carbon-rails/introducing-data.md`,
+      "rails-part-2:-updating-data":    `${tutorialsPath}/carbon-rails/updating-data.md`
     }
   },
 });
