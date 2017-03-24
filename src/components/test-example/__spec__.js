b7948fadcf133b6c327a88fe3633cd03d05ff20e
Toni-Leigh Sharpe <toni.leigh@sage.com>
2017-03-24 15:15:44 +0000

    Abstracts out forEach into helper
    * demonstrates that the forEach contents can be abstracted out of the tests into a sharable helper

--- src/components/test-example/__spec__.js
+++ src/components/test-example/__spec__.js
@@ -14,33 +14,49 @@ let specDefinition = [
     component: Button,
     instances: [ {
       expectedInstances: 2,
-      details: { as: 'primary',   children: 'Click me', theme: 'blue' }
+      props: { as: 'primary',   children: 'Click me', theme: 'blue' }
     }, {
       expectedInstances: 1,
-      details: { as: 'secondary', children: 'Go back',  theme: 'grey' }
+      props: { as: 'secondary', children: 'Go back',  theme: 'grey' }
     } ]
   }, {
     component: Heading,
     instances: [ {
       expectedInstances: 1,
-      details: { divider: true, title: 'Test' }
+      props: { divider: true, title: 'Test' }
     } ]
   }, {
     component: Link,
     instances: [ {
       expectedInstances: 1,
-      details: { href: 'http://test.com' }
+      props: { href: 'http://test.com' }
     } ]
   }, {
     component: Content,
     instances: [ {
       expectedInstances: 3,
-      details: { title: 'Counter', children: 11 }
+      props: { title: 'Counter', children: 11 }
     } ]
   }
 ];
 
-describe('TestExample', () => {
+let testStructure = (wrapper, comp) => {
+  let components = wrapper.find(comp.component);
+
+  let expectedInstances = comp.instances.reduce((a, b) => a + b.expectedInstances, 0),
+      instanceArray = comp.instances,
+      wrapperNodes = components.nodes.map(n => n.props);
+
+  // correct number of elements are rendered
+  expect(components.length).toEqual(expectedInstances);
+
+  // check we can find all this elements instances in the wrapper nodes
+  instanceArray.forEach((instance) => {
+    expect(_.filter(wrapperNodes, instance.props).length).toEqual(instance.expectedInstances);
+  });
+};
+
+fdescribe('TestExample', () => {
   let wrapper;
 
   beforeEach(() => {
@@ -55,21 +71,7 @@ describe('TestExample', () => {
 
   describe('structure', () => {
     it("is rendered", () => {
-      specDefinition.forEach((comp) => {
-        let components = wrapper.find(comp.component);
-
-        let expectedInstances = comp.instances.reduce((a, b) => a + b.expectedInstances, 0),
-            instanceArray = comp.instances,
-            wrapperNodes = components.nodes.map(n => n.props);
-
-        // correct number of elements are rendered
-        expect(components.length).toEqual(expectedInstances);
-
-        // check we can find all this elements instances in the wrapper nodes
-        instanceArray.forEach((instance) => {
-          expect(_.filter(wrapperNodes, instance.details).length).toEqual(instance.expectedInstances);
-        });
-      });
+      specDefinition.forEach(testStructure.bind(this, wrapper));
     });
   });
 });
