8d8771ed90172b9d63a5e69efbfe6f8bbbb7aef4
Ian Oxley <ian.oxley@sage.com>
2017-03-28 16:36:35 +0100

    Add missing unit tests
    Move the `itemSource` and `itemTarget` code out of `with-drag-and-drop.js` and in to
    their own modules. They can then be tested in isolation.
    
    Move the `ItemTypes` constants into their own module.
    
    Add `getActiveElement` to `Browser` to help test around the use of
    `document.activeElement`;

---
+++ src/components/with-drag-and-drop/item-target/item-target.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ItemTarget = {
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      console.log(1);
+      return;
+    }
+
+    // Determine rectangle on screen
+    const hoverBoundingRect = ReactDOM.findDOMNode(component).getBoundingClientRect();
+
+    // Get vertical middle
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+    // Determine mouse position
+    const clientOffset = monitor.getClientOffset();
+
+    // Get pixels to the top
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      console.log(2);
+      return;
+    }
+
+    // Dragging upwards
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      console.log(3);
+      return;
+    }
+
+    // Time to actually perform the action
+    props.moveItem(dragIndex, hoverIndex);
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    monitor.getItem().index = hoverIndex;
+  }
+};
+
+export default ItemTarget;
