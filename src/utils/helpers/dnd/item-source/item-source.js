09f47b49100872ed3e0f87b18d9dc0b78b65a2d5
Ian Oxley <ian.oxley@sage.com>
2017-04-05 16:08:05 +0100

    Add ItemSourceHelper
    ItemSourceHelper has a single function `onBeginDrag` that returns an object
    with a single `index` property. `index` is set to the value of `props.index`.

---
+++ src/utils/helpers/dnd/item-source/item-source.js
@@ -0,0 +1,16 @@
+const ItemSourceHelper = {
+
+  /**
+   * Helper function that returns an object with a single index property.
+   * The index property is set to the value of props.index.
+   *
+   */
+  onBeginDrag(props) {
+    return {
+      index: props.index
+    };
+  }
+
+};
+
+export default ItemSourceHelper;
