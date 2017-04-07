2471eb3f937de86a7070b375fa2bb20546d09a53
Ian Oxley <ian.oxley@sage.com>
2017-04-07 11:19:38 +0100

    Refactor tests to use enzyme
    Tests use enzyme instead of TestUtils.

--- src/components/menu/__spec__.js
+++ src/components/menu/__spec__.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
 import { Menu } from './menu';
+import { shallow } from 'enzyme';
 
 describe('Menu', () => {
-  let instance;
+  let wrapper;
 
   beforeEach(() => {
-    instance = TestUtils.renderIntoDocument(
+    wrapper = shallow(
       <Menu className="foobar">
         foo
       </Menu>
@@ -14,17 +14,16 @@ describe('Menu', () => {
   });
 
   it('renders with correct classes', () => {
-    let div = TestUtils.findRenderedDOMComponentWithTag(instance, 'div');
-    expect(div.className).toEqual('carbon-menu foobar carbon-menu--primary');
+    expect(wrapper.hasClass('carbon-menu foobar carbon-menu--primary')).toEqual(true);
   });
 
   it('renders with secondary class', () => {
-    instance = TestUtils.renderIntoDocument(
+    wrapper = shallow(
       <Menu className="foobar" as="secondary">
         foo
       </Menu>
     );
-    let div = TestUtils.findRenderedDOMComponentWithTag(instance, 'div');
-    expect(div.className).toEqual('carbon-menu foobar carbon-menu--secondary');
+    expect(wrapper.hasClass('carbon-menu foobar carbon-menu--secondary')).toEqual(true);
   });
+
 });
