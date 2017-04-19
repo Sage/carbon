10923282ee83ecdda3c7297083e0893f99b7219f
Sean Armstrong <sean.armstrong@sage.com>
2017-04-19 14:13:49 +0100

    update radio button spec to enzyme

--- src/components/radio-button/__spec__.js
+++ src/components/radio-button/__spec__.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
+import { shallow } from 'enzyme';
 import RadioButton from './radio-button';
 
 describe('RadioButton', () => {
-  let instance;
+  let wrapper;
 
   beforeEach(() => {
-    instance = TestUtils.renderIntoDocument(
+    wrapper = shallow(
       <RadioButton
         name='radiobutton'
         label='radiobutton'
@@ -15,47 +15,51 @@ describe('RadioButton', () => {
   });
 
   describe('render', () => {
-    it('renders a parent div with a pod CSS class', () => {
-      let radioButtonNode = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'div')[0];
-      expect(radioButtonNode.classList[0]).toEqual('carbon-radio-button');
+    it('renders a parent div with a radio button class', () => {
+      expect(wrapper.hasClass('carbon-radio-button')).toBeTruthy();
     });
 
-    it('renders an input with type radiobutton and a checked value of false', () => {
-      let radiobutton = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'input')[0];
-      expect(radiobutton.type).toEqual('radio');
-      expect(radiobutton.checked).toBeFalsy();
+    describe('input', () => {
+      it('renders an input with type radiobutton', () => {
+        const input = wrapper.find('.carbon-radio-button__input');
+        expect(input.props().type).toEqual('radio');
+      });
+
+      it('renders a radiobuttonSprite to be used as the visible input', () => {
+        expect(wrapper.find('.carbon-radio-button__sprite').length).toEqual(1);
+      });
     });
 
-   it('renders a radiobuttonSprite to be used as the visible input', () => {
-      let div = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'div')[2];
-      let sprite = div.firstChild;
-      let radio = sprite.getElementsByTagName('path')[0]
+    describe('fieldHelp', () => {
+      describe('when reversed', () => {
+        it('sets a field help class of reverse', () => {
+          wrapper = shallow(
+            <RadioButton
+              name='radiobutton'
+              label='radiobutton'
+              reverse={ true }
+              fieldHelp='foo'
+            />
+          );
+          expect(wrapper.find('.carbon-radio-button__help-text').length).toEqual(1);
+          expect(wrapper.find('.carbon-radio-button__help-text--reverse').length).toEqual(1);
+        });
 
-      expect(sprite.getAttribute('viewBox')).toEqual('0 0 15 15');
-      expect(radio.className.baseVal).toEqual('radio-button-outline');
-    });
-  });
-
-  describe('mainClasses', () => {
-    it('returns carbon-radio-button and additional decorated classes', () => {
-      expect(instance.mainClasses).toEqual('carbon-radio-button common-input');
-    });
-  });
-
-  describe('inputClasses', () => {
-    it('returns carbon-radio-button__input and additional decorated classes', () => {
-      expect(instance.inputClasses).toEqual('carbon-radio-button__input common-input__input');
-    });
-  });
-
-  describe('fieldHelpClasses', () => {
-    it('returns carbon-radio-button__help-text', () => {
-      expect(instance.fieldHelpClasses).toEqual('carbon-radio-button__help-text common-input__help-text');
-    });
-
-    describe('when label is reversed', () => {
-      it('returns a modified reverse class for the fieldInput', () => {
-
+        describe('when inline', () => {
+          it('sets a field help class of inline', () => {
+            wrapper = shallow(
+              <RadioButton
+                name='radiobutton'
+                label='radiobutton'
+                fieldHelpInline={ true }
+                reverse={ true }
+                fieldHelp='foo'
+              />
+            );
+            expect(wrapper.find('.carbon-radio-button__help-text').length).toEqual(1);
+            expect(wrapper.find('.carbon-radio-button__help-text--inline').length).toEqual(1);
+          });
+        });
       });
     });
   });
