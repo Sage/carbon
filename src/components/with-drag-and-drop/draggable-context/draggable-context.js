dee30b7327d5b5a469368fe4d734d529fd5da688
Ian Oxley <ian.oxley@sage.com>
2017-04-21 10:52:13 +0100

    Add comments to compoent and component methods

--- src/components/with-drag-and-drop/draggable-context/draggable-context.js
+++ src/components/with-drag-and-drop/draggable-context/draggable-context.js
@@ -2,22 +2,103 @@ import React from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
+/**
+ * A draggable context component
+ *
+ * == How to use a draggable context in a component:
+ *
+ * In your file
+ *
+ *   import DraggableContext from 'carbon/lib/components/with-drag-and-drop'
+ *
+ * A draggable context is used to define an area in the page
+ * where drag and drop can be used on one or more elements:
+ *
+ *   <ol>
+ *     <DraggableContext
+ *       moveItem={ onItemMoved }
+ *       canDrag={ itemCanDrag }
+ *       beginDrag={ onBeginDrag }
+ *       hover={ onHover }
+ *     >
+ *       <li>Spring roll</li>
+ *       <li>Prawn toast</li>
+ *       <li>Hot and sour soup</li>
+ *       <li>Spare ribs</li>
+ *     </DraggableContext>
+ *   </ol>
+ *
+ * @class DraggableContext
+ * @constructor
+ */
 class DraggableContext extends React.Component {
 
   static propTypes = {
+    /**
+     * Callback function for when an item has been dragged and dropped
+     * e.g. to update data in a store
+     *
+     * @property moveItem
+     * @type {Function}
+     */
     moveItem: React.PropTypes.func,
+
+    /**
+     * Callback function that determines whether the item can be
+     * dragged.
+     *
+     * Synonymous with React DnD's Drag Source Specification canDrag
+     * - see https://react-dnd.github.io/react-dnd/docs-drag-source.html
+     *
+     * @property canDrag
+     * @type {Function}
+     */
     canDrag: React.PropTypes.func,
+
+    /**
+     * Callback function called when dragging starts.
+     *
+     * Synonymous with React DnD's Drag Source Specification beginDrag
+     * - see https://react-dnd.github.io/react-dnd/docs-drag-source.html
+     *
+     * @property beginDrag
+     * @type {Function}
+     */
     beginDrag: React.PropTypes.func,
+
+    /**
+     * Callback function called when an item is hovered over the
+     * component.
+     *
+     * Synonymous with React DnD's Drop Target Specification
+     * - see https://react-dnd.github.io/react-dnd/docs-drop-target.html
+     *
+     * @property hover
+     * @type {Function}
+     */
     hover: React.PropTypes.func
   }
 
+  /**
+   * Defines a context object for child components of the draggable context component.
+   * https://facebook.github.io/react/docs/context.html
+   *
+   * @property childContextTypes
+   * @type {Object}
+   */
   static childContextTypes = {
-    moveItem: React.PropTypes.func,
-    canDrag: React.PropTypes.func,
-    beginDrag: React.PropTypes.func,
-    hover: React.PropTypes.func
+    moveItem: React.PropTypes.func, // See propTypes.moveItem
+    canDrag: React.PropTypes.func, // See propTypes.canDrag
+    beginDrag: React.PropTypes.func, // See propTypes.beginDrag
+    hover: React.PropTypes.func // See propTypes.hover
   }
 
+  /**
+   * Returns this draggable context object to child components.
+   *
+   * @method getChildContext
+   * @return {void}
+   */
   getChildContext() {
     return {
       moveItem: this.props.moveItem,
@@ -27,6 +108,12 @@ class DraggableContext extends React.Component {
     };
   }
 
+  /**
+   * Renders the component
+   *
+   * @method render
+   * @return {Object} JSX
+   */
   render() {
     return this.props.children;
   }
