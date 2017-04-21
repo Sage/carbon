f76cbaa611b502d67e9a13f4a50efa9cfc5d3a1f
Ian Oxley <ian.oxley@sage.com>
2017-04-21 11:25:55 +0100

    Add comments to objects and functions

--- src/components/with-drag-and-drop/item-types.js
+++ src/components/with-drag-and-drop/item-types.js
@@ -1,3 +1,10 @@
+/**
+ * Defines the string types of items
+ * that can be dragged and dropped.
+ *
+ * NB drag sources and drop targets only interact
+ * if they have the same string type.
+ */
 const ItemTypes = {
   TABLE_ROW: 'TableRow'
 };
