ae3253682b0ff501bf592e5ef5a5f5df92ef45ec
Ian Oxley <ian.oxley@sage.com>
2017-04-21 11:25:55 +0100

    Add comments to objects and functions

--- src/components/with-drag-and-drop/item-source/item-source.js
+++ src/components/with-drag-and-drop/item-source/item-source.js
@@ -1,10 +1,23 @@
-// item-source.js
-
+/**
+ * Helper object used when calling React DnD's DragSource.
+ *
+ * Defines two functions from React DnD's Drag Source Specification:
+ *  canDrag
+ *  beginDrag
+ */
 const ItemSource = {
+  /**
+   * If props.canDrag is defined it calls props.canDrag and
+   * passes props and monitor as arguments.
+   */
   canDrag(props, monitor) {
     return props.canDrag && props.canDrag(props, monitor);
   },
 
+  /**
+   * Calls props.beginDrag, and passes props, monitor, and component
+   * as arguments
+   */
   beginDrag(props, monitor, component) {
     return props.beginDrag(props, monitor, component);
   }
