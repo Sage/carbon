4128e8443dbd42b657d752e29bde4bba1e51aa0d
Sean Armstrong <sean.armstrong@sage.com>
2017-05-09 15:03:50 +0100

    remove test code from demo site

--- demo/views/pages/home/get-started/get-started.js
+++ demo/views/pages/home/get-started/get-started.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import I18n from 'i18n-js';
-import Row from 'components/row';
 
 // Demo
 import GetCodeButtons from './../../../../components/get-code-buttons';
@@ -10,10 +9,6 @@ class GetStarted extends React.Component {
   render () {
     return (
       <div className='get-started'>
-        <Row>
-          <div/>
-          <div/>
-        </Row>
         <Wrapper>
           <span className='get-started__text'>{ I18n.t('homepage.get_started.ready') }</span>
           <span className='get-started__checkout-text'>{ I18n.t('homepage.get_started.checkout') }</span>
