7c95f4ad6ec2e3941b22b685f86c664598efdf09
Sean Armstrong <sean.armstrong@sage.com>
2017-05-23 13:16:39 +0100

    electron deployment

--- electron.js
+++ electron.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+var express = require('express');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,7 +9,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, '/deploy/icon.icns')
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +23,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -32,7 +37,19 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow();
+
+  var exp = express();
+  // serve files from here
+  exp.use(express.static(__dirname + '/deploy'));
+  // always serve index.h
+  exp.get('/*', function(req, res){
+    res.sendFile(__dirname + '/deploy/index.html');
+  });
+
+  exp.listen(8095);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
