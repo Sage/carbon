42360ee65c97bb49d94dc8ac5ea4060fb1b4431d
Paul Sturgess <paul@kyanmedia.com>
2017-05-25 15:55:53 +0100

    Update definition files for table components

--- src/components/table/table-subheader/definition.js
+++ src/components/table/table-subheader/definition.js
@@ -4,11 +4,15 @@ import Definition from './../../../../demo/utils/definition';
 let definition = new Definition('table-subheader', TableSubheader, {
   propTypes: {
     align: "String",
+    children: "Node",
+    className: "String",
     name: "String",
     sortable: "Boolean"
   },
   propDescriptions: {
     align: "Aligns the text in the cell. Can be set to left, center or right.",
+    children: "This component supports children.",
+    className: "Classes to apply to the component.",
     name: "This will normally match the key for data displayed in this column, it is used to identify the sort column in the table.",
     sortable: "Turn sortable on/off for this column."
   }
