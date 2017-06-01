b47bb43ab4be03ea22c8b07da23b4363590fee4d
Andrew Tait <andrew.tait@sage.com>
2017-06-01 13:05:17 +0100

    ensure correct npm version is used when making a release

---
+++ check-version.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').exec;
+
+exec("npm -v", function(err, stdout, stderr) {
+  if (parseFloat(stdout) < 5) {
+    console.error("Using npm version: " + stdout);
+    console.error("You need to be using at least npm version 5. To upgrade use `npm install npm -g`.\n");
+    process.exit(1);
+  }
+});
