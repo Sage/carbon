9afef70f28becad04e36b416e0aaf2c401b91014
Paul Sturgess <paul@kyanmedia.com>
2017-06-06 15:27:35 +0100

    Column component linting updates

--- src/components/row/column/definition.js
+++ src/components/row/column/definition.js
@@ -4,12 +4,14 @@ import Definition from './../../../../demo/utils/definition';
 let definition = new Definition('column', Column, {
   type: 'layout',
   propTypes: {
+    className: "String",
     columnOffset: "String",
     columnSpan: "String",
     columnAlign: "String",
     children: "Node"
   },
   propDescriptions: {
+    className: "Classes to apply to the component.",
     columnOffset: "Offset this column by a certain number of columns.",
     columnSpan: "Span this column by a certain number of columns.",
     columnAlign: "Align the content of this column.",
