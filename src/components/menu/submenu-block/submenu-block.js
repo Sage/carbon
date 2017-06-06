5c703f3b5858c0140d7fbc474aabaff4607e277c
Paul Sturgess <paul@kyanmedia.com>
2017-06-06 11:48:27 +0100

    SubmenuBlock component linting updates

--- src/components/menu/submenu-block/submenu-block.js
+++ src/components/menu/submenu-block/submenu-block.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { tagComponent } from '../../../utils/helpers/tags';
 
@@ -6,6 +7,24 @@ import { tagComponent } from '../../../utils/helpers/tags';
  * Renders a menu block for inside of a submenu.
  */
 class SubmenuBlock extends React.Component {
+  static propTypes = {
+    /**
+     * Children elements
+     *
+     * @property children
+     * @type {Node}
+     */
+    children: PropTypes.node.isRequired,
+
+    /**
+     * Custom className
+     *
+     * @property className
+     * @type {String}
+     */
+    className: PropTypes.string
+  }
+
   /**
    * Returns the classes for the component.
    *
@@ -14,7 +33,7 @@ class SubmenuBlock extends React.Component {
    */
   get classes() {
     return classNames(
-      "carbon-submenu-block",
+      'carbon-submenu-block',
       this.props.className
     );
   }
