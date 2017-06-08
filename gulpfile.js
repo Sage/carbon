2e79d20f141d356e8c275c1be23145d01c3789a9
Andrew Tait <andrew.tait@sage.com>
2017-06-08 15:33:04 +0100

    resolved scss assets for cdn

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,15 @@ var SpecTask = require('carbon-factory/lib/gulp/spec').default;
 var generateColors = require('./script/generate-demo-colors').default;
 var generateDocs = require('./script/generate-docs').default;
 var deploy = require('./script/deploy').default;
+var setConfig = require('./script/set-config').default;
 var express = require('express');
 var gutil = require('gulp-util');
-var fs = require('fs');
 
 var argv = yargs.argv;
 
 var dir = 'deploy';
-var configPath = 'demo/config.js';
-var imagePath = '/assets/images';
 
-if (argv.cdn) {
-  imagePath = 'https://assets.na.sageone.com/carbon/demo/latest/assets/images';
-}
-
-fs.unlink(configPath, function(err) {
-  fs.appendFile(configPath, `export default { imagePath: '${imagePath}' };`, function (err) {
-    if (err) throw err;
-    gutil.log(gutil.colors.cyan('Using images from ') + gutil.colors.green(imagePath));
-  });
-});
+setConfig(argv.cdn);
 
 gulp.task('prepare-demo', function() {
   generateColors();
