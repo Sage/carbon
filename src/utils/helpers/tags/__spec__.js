f3c4483dd06004e11728a62cce37af09a5e56e20
Toni-Leigh Sharpe <toni.leigh@sage.com>
2017-06-09 13:35:44 +0100

    data-component is now over-ridden if provide dby the user
    * The root of the component is the correct place for a `data-component` tag, but if the user provides one, the Carbon defaults will be applied instead.
    
    In the following example:
    
    ```
    <Wrapper>
      <AppComp data-role='x'/>
      <AppComp data-role='x'/>
      <AppComp data-role='z/>
    
      <OtherComp data-role='1' />
      <OtherComp data-role='2' />
    </Wrapper>
    ```
    
    If all five child components had `Pod` at their root level then they would all show up as `data-component='pod'` when `data-component='app-comp'` and `data-component='other-comp'` makes more sense.
    
    The role needs to be kept unique, for identifying each individually and the element relates to the component's position within it's parent, so neither are correct.
    
    Therefore, the `data-component` on the Carbon component should be over-ridden by a user provided prop.

--- src/utils/helpers/tags/__spec__.js
+++ src/utils/helpers/tags/__spec__.js
@@ -7,6 +7,12 @@ describe('tagComponent', () => {
     });
   });
 
+  describe("when a data-component tag is sent", () => {
+    it("uses this over the Carbon component tag", () => {
+      expect(tagComponent('my-component', { ['data-component']: 'specific'})).toEqual({ ['data-component']: 'specific' });
+    });
+  });
+
   describe('when role and element props are sent', () => {
     it('adds those to the tagProps object', () => {
       expect(tagComponent('my-component', { ['data-element']: 'my-component', ['data-role']: 'contacts'}))
