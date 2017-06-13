35f5b25cc552a188a6396b3ca150df707600d0ba
Ian Oxley <ian.oxley@sage.com>
2017-06-13 16:10:59 +0100

    Add missing props to definition
    Add `propTypes` and `propDescription`.

--- src/components/portrait/definition.js
+++ src/components/portrait/definition.js
@@ -1,9 +1,9 @@
+import OptionsHelper from 'utils/helpers/options-helper';
 import Portrait from './';
 import Definition from './../../../demo/utils/definition';
-import OptionsHelper from 'utils/helpers/options-helper';
 
-let definition = new Definition('portrait', Portrait, {
-  description: `Represents a person with their initials or an avatar.`,
+const definition = new Definition('portrait', Portrait, {
+  description: 'Represents a person with their initials or an avatar.',
   designerNotes: `
 * Useful to represent a person, user, or organisation.
 * Use initials rather than an avatar if you prefer.
@@ -17,22 +17,24 @@ let definition = new Definition('portrait', Portrait, {
     shape: OptionsHelper.shapesVaried
   },
   propTypes: {
-    size: "String",
-    src: "String",
-    gravatar: "String",
-    alt: "String",
-    shape: "String",
-    initials: "String",
-    darkBackground: "Boolean"
+    alt: 'String',
+    className: 'String',
+    darkBackground: 'Boolean',
+    gravatar: 'String',
+    initials: 'String',
+    shape: 'String',
+    size: 'String',
+    src: 'String'
   },
   propDescriptions: {
-    size: "Defines the size of the Portrait.",
-    src: "Define an image source.",
-    gravatar: "Define an email address registered with gravatar.",
-    alt: "Defines the alt HTML string.",
-    shape: "Defines the shape of the Portrait.",
-    initials: "Define some initials to render in the Portrait.",
-    darkBackground: "Switch to a dark background (requires a re-render)."
+    alt: 'Defines the alt HTML string.',
+    className: 'A custom class name for the component.',
+    darkBackground: 'Switch to a dark background (requires a re-render).',
+    gravatar: 'Define an email address registered with gravatar.',
+    initials: 'Define some initials to render in the Portrait.',
+    shape: 'Defines the shape of the Portrait.',
+    size: 'Defines the size of the Portrait.',
+    src: 'Define an image source.'
   }
 });
 
