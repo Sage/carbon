e0fb7af0835631e54e0a281e8024f44a52c3747a
Toni-Leigh Sharpe <toni.leigh@sage.com>
2017-06-14 11:45:16 +0100

    Linting

--- src/utils/css/css.js
+++ src/utils/css/css.js
@@ -1,11 +1,11 @@
 export default {
-  clearfix: "clearfix",
-  input: "common-input",
-  hidden: "hidden",
-  unselectable: "unselectable",
-  textTag: "text--tag",
-  textBold: "text--bold",
-  textItalic: "text--italic",
-  textSecondary: "text--secondary",
-  textInactive: "text--inactive"
+  clearfix: 'clearfix',
+  input: 'common-input',
+  hidden: 'hidden',
+  unselectable: 'unselectable',
+  textTag: 'text--tag',
+  textBold: 'text--bold',
+  textItalic: 'text--italic',
+  textSecondary: 'text--secondary',
+  textInactive: 'text--inactive'
 };
