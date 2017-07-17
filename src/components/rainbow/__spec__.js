a5aa567dd3017a851d76cbc89746cb12983cce7c
Ian Oxley <ian.oxley@sage.com>
2017-07-17 10:06:25 +0100

    Remove highcharts config tests
    Remove highcharts config tests, as they seem to be testing highcharts
    code rather than Carbon code: removing the tests has no effect on code
    coverage i.e. still 100%.

--- src/components/rainbow/__spec__.js
+++ src/components/rainbow/__spec__.js
@@ -52,8 +52,6 @@ describe('Rainbow', () => {
     beforeEach(() => {
       chart.setTitle = jest.fn();
       chart.series[0].setData = jest.fn();
-      // spyOn(chart, 'setTitle');
-      // spyOn(chart.series[0], 'setData');
     });
 
     describe('when title changes', () => {
@@ -90,98 +88,6 @@ describe('Rainbow', () => {
       it('returns base and custom class', () => {
         render({ className: 'customClass' });
         expect(wrapper.instance().mainClasses).toEqual('carbon-rainbow customClass');
-      });
-    });
-  });
-
-  xdescribe('highcharts config', () => {
-    it("sets the config to use the data", () => {
-      expect(chart.userOptions.series[0].data).toEqual(wrapper.instance().props.data.toJS());
-    });
-
-    it("sets the config to use the title", () => {
-      expect(chart.userOptions.title.text).toEqual(wrapper.instance().props.title);
-    });
-
-    it("calls zIndexSetter when triggering mouse over on a segment", () => {
-      let point = chart.series[0].data[0];
-      let graphic = point.graphic;
-
-      spyOn(graphic, 'zIndexSetter');
-      point.onMouseOver();
-
-      expect(graphic.zIndexSetter).toHaveBeenCalledWith(1);
-    });
-
-
-    it("calls zIndexSetter when triggering mouse over on a segment", () => {
-      let point = chart.series[0].data[0];
-      let graphic = point.graphic;
-
-      spyOn(graphic, 'zIndexSetter');
-      point.onMouseOut();
-
-      expect(graphic.zIndexSetter).toHaveBeenCalledWith(0);
-    });
-
-    it("positions the tooltip correctly", () => {
-      let opts = {
-        plotX: 100,
-        plotY: 200
-      };
-      expect(chart.userOptions.tooltip.positioner(50, 100, opts)).toEqual({ x: 75, y: 105 });
-    });
-
-    it("sets the label to be display block if the pointer is visible", () => {
-      chart.series[0].setData([{
-        y: 30,
-        name: 'First Bit',
-        label: 'label for first bit',
-        tooltip: 'more info about this bit',
-        visible: true
-      }]);
-      let display = chart.series[0].points[0].dataLabel.div.children[0].children[0].style["display"];
-      expect(display).toEqual('block');
-    });
-
-    it("sets the label to be display none if the pointer is not visible", () => {
-      chart.series[0].setData([{
-        y: 30,
-        name: 'First Bit',
-        label: 'label for first bit',
-        tooltip: 'more info about this bit',
-        visible: false
-      }]);
-      let display = chart.series[0].points[0].dataLabel.div.children[0].children[0].style["display"];
-      expect(display).toEqual('none');
-    });
-
-    describe('config property', () => {
-      describe('uses some default chart config when no config is passed', () => {
-        it("can use the config property to override some chart properties", () => {
-          expect(chart.options.chart.backgroundColor).toEqual(null);
-          expect(chart.options.credits.enabled).toEqual(false);
-        });
-      });
-
-      describe('it is possible to override the chart properties using the config props', () => {
-        let config = {
-          credits: {
-            enabled: true
-          },
-          chart: {
-            backgroundColor: '#ff00cc'
-          }
-        };
-
-        beforeEach(() => {
-          render({config});
-        });
-
-        it("can use the config property to override some chart properties", () => {
-          expect(chart.options.chart.backgroundColor).toEqual('#ff00cc');
-          expect(chart.options.credits.enabled).toEqual(true);
-        });
       });
     });
   });
