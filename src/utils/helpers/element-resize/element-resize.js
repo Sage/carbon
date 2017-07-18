b73f11e736bfff15f75b3b08a1d0f0174a792eea
Andrew Tait <andrew.tait@sage.com>
2017-07-18 16:14:40 +0100

    specs for sticky dialog

--- src/utils/helpers/element-resize/element-resize.js
+++ src/utils/helpers/element-resize/element-resize.js
@@ -1,71 +1,4 @@
-import Browser from './../browser';
 import Bowser from 'bowser';
-
-/**
- * Creates a child 'object' or document which mimics the size of the given element and is
- * able to use the browser 'resize' event listener to trigger a callback.
- *
- * Adapted from http://www.backalleycoder.com/2013/03/18/cross-browser-event-based-element-resize-detection/
- *
- *   __resizeListenerCallbacks__ - an array of callbacks to trigger on this element when it is resized
- *   __resizeTrigger__ - the element to listen for resize events. The browser creates a fake object
- *   which supports resize events.
- */
-
-/**
- * Sets up an event listener for when an element is resized.
- *
- * @method addResizeListener
- * @param {HTMLElement} element - the html element to watch
- * @param {Function} fn - the callback
- */
-const addResizeListener = (element, callback) => {
-
-  // if there is no resize listener on this element yet, create an object to apply the listener to
-  if (!element.__resizeTrigger__) {
-    element.__resizeListenerCallbacks__ = [];
-
-    // ensure that the element has position relative for the child object to work properly
-    if (getComputedStyle(element).position == 'static') {
-      element.style.position = 'relative';
-    }
-
-    // creates an object which will support the resize event listener
-    const obj = element.__resizeTrigger__ = document.createElement('object'); 
-    obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1;');
-    // when the object is ready, add the event listener
-    obj.onload = objectLoad(obj, element);
-    obj.type = 'text/html';
-
-    const isMS = Bowser.msie || Bowser.msedge;
-
-    if (isMS) {
-      element.appendChild(obj);
-    }
-
-    obj.data = 'about:blank';
-
-    if (!isMS) {
-      element.appendChild(obj);
-    }
-  }
-
-  // add the callback to the array of listeners
-  element.__resizeListenerCallbacks__.push(callback);
-};
-
-const removeResizeListener = (element, callback) => {
-  // remove the event listener from the array
-  element.__resizeListenerCallbacks__.splice(element.__resizeListenerCallbacks__.indexOf(callback), 1);
-
-  // if there are no event listeners left, time to detach the event
-  if (!element.__resizeListenerCallbacks__.length) {
-    // remove the event listener on the object
-    element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
-    // remove the fake object
-    element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
-  }
-}
 
 /**
  * The event listener to be triggered.
@@ -77,7 +10,7 @@ const resizeListener = (ev) => {
   trigger.__resizeListenerCallbacks__.forEach((fn) => {
     fn.call(trigger, ev);
   });
-}
+};
 
 /**
  * Return a function to be triggered when the fake object has been loaded into the DOM.
@@ -88,10 +21,92 @@ const objectLoad = (obj, element) => {
     obj.contentDocument.defaultView.__resizeTrigger__ = element;
     // apply the event listener to the new object's document
     obj.contentDocument.defaultView.addEventListener('resize', resizeListener);
-  }
-}
+  };
+};
 
-export {
-  addResizeListener,
-  removeResizeListener
-}
+/**
+ * Creates a child 'object' or document which mimics the size of the given element and is
+ * able to use the browser 'resize' event listener to trigger a callback.
+ *
+ * Adapted from http://www.backalleycoder.com/2013/03/18/cross-browser-event-based-element-resize-detection/
+ *
+ *   __resizeListenerCallbacks__ - an array of callbacks to trigger on this element when it is resized
+ *   __resizeTrigger__ - the element to listen for resize events. The browser creates a fake object
+ *   which supports resize events.
+ */
+const ElementResize = {
+  /**
+   * Sets up an event listener for when an element is resized.
+   *
+   * @method addListener
+   * @param {HTMLElement} element - the html element to watch
+   * @param {Function} fn - the callback
+   */
+  addListener: (element, callback) => {
+    // if there is no resize listener on this element yet, create an object to apply the listener to
+    if (!element.__resizeTrigger__) {
+      element.__resizeListenerCallbacks__ = [];
+
+      // ensure that the element has position relative for the child object to work properly
+      if (getComputedStyle(element).position === 'static') {
+        element.style.position = 'relative';
+      }
+
+      // creates an object which will support the resize event listener
+      const obj = element.__resizeTrigger__ = document.createElement('object');
+      obj.setAttribute('style', `
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        overflow: hidden;
+        pointer-events: none;
+        z-index: -1;
+      `);
+      // when the object is ready, add the event listener
+      obj.onload = objectLoad(obj, element);
+      obj.type = 'text/html';
+
+      const isMS = Bowser.msie || Bowser.msedge;
+
+      if (isMS) {
+        element.appendChild(obj);
+      }
+
+      obj.data = 'about:blank';
+
+      if (!isMS) {
+        element.appendChild(obj);
+      }
+    }
+
+    // add the callback to the array of listeners
+    element.__resizeListenerCallbacks__.push(callback);
+  },
+
+  /**
+   * Removes event listeners for when an element is resized.
+   *
+   * @method removeListener
+   * @param {HTMLElement} element - the html element to watch
+   * @param {Function} fn - the callback
+   */
+  removeListener: (element, callback) => {
+    if (element.__resizeListenerCallbacks__) {
+      // remove the event listener from the array
+      element.__resizeListenerCallbacks__.splice(element.__resizeListenerCallbacks__.indexOf(callback), 1);
+
+      // if there are no event listeners left, time to detach the event
+      if (!element.__resizeListenerCallbacks__.length) {
+        // remove the event listener on the object
+        element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
+        // remove the fake object
+        element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
+      }
+    }
+  }
+};
+
+export default ElementResize;
