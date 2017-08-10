5222dd00fdb4c4f8e7fa2ac041fc4ff136d225c8
Alexander.Ramin <alexander.ramin@sage.com>
2017-08-10 15:27:31 +0100

    refactor props usage to satisfy babel complaining about typecasting * set npm v5 as minimum

--- check-version.js
+++ check-version.js
@@ -1,9 +1,9 @@
 var exec = require('child_process').exec;
 
 exec("npm -v", function(err, stdout, stderr) {
-  if (parseFloat(stdout) < 4) {
+  if (parseFloat(stdout) < 5) {
     console.error("Using npm version: " + stdout);
-    console.error("You need to be using at least npm version 4. To upgrade use `npm install npm@5.3.0 -g`.\n");
+    console.error("You need to be using at least npm version 5. To upgrade use `npm install npm@latest -g`.\n");
     process.exit(1);
   }
 });
