3cc903118e55e1c9d7c69cf588f5681ff414bbff
Rob Kilby <rob.kilby@sage.com>
2017-11-10 15:27:10 +0000

    Add designer notes and related components.
    Signed-off-by: Rob Kilby <rob.kilby@sage.com>

--- src/components/button-toggle-group/__definition__.js
+++ src/components/button-toggle-group/__definition__.js
@@ -3,6 +3,13 @@ import Definition from './../../../demo/utils/definition';
 
 const definition = new Definition('button-toggle-group', ButtonToggleGroup, {
   description: 'Adds support for validation, warning and info messages on a group of Button Toggles.',
+  designerNotes: `
+* Similar to [Button Toggle](/components/button-toggle), but use this component when you need your button toggles to act like a single form component, with field help and validation.
+* Unlike [Button Toggle](/components/button-toggle), this component doesn't require one option to be auto-selected.
+  `,
+  relatedComponentsNotes: `
+* Don't need validation on your options? [Try Button Toggle](/components/button-toggle).
+  `,
   type: 'form',
   propValues: {
     children: `<ButtonToggle name={ 'grouped' } id={ 'foo' } value={ 'foo' }>
