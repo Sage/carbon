4e10e2ac773d0ead9cc10e2b0a901ce0dc54a5cd
Phil Earley <phillip.earley@sage.com>
2017-11-14 14:16:37 +0000

    Set default state, add constant and create a function for timeout
    Set default state to 'closed' to as it's only ever closed or open.
    Add constant for the timeout, if it is ever changed from '500' we know the timeout will always be inline.
    Created a function to control if the open prop is true or false to control the state.

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -5,6 +5,7 @@ import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Events from './../../utils/helpers/events';
 import Browser from './../../utils/helpers/browser';
 
+const TIMEOUT = 500;
 /**
  * A Modal Component
  *
@@ -102,8 +103,8 @@ class Modal extends React.Component {
     modal: PropTypes.object
   }
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     /**
      * Tracks if event listeners are on for modal
@@ -112,7 +113,29 @@ class Modal extends React.Component {
      * @type {Boolean}
      */
     this.listening = false;
-    this.state = { state: 'default' };
+
+    this.state = {
+      /**
+       * Sets the initial data-state of the modal
+       * @property state
+       * @type {String}
+       */
+      state: this.props.open ? 'open' : 'closed'
+    };
+  }
+
+  /**
+   * Updates the value used for the css data-state. This uses a timeout to match the length of any transition to ensure
+   * UI automation is able to target elements once they are visually ready.
+   * @method updateDataState
+   * @return {void}
+   *
+   */
+  updateDataState = () => {
+    clearTimeout(this.openTimeout);
+    this.openTimeout = setTimeout(() => {
+      this.setState({ state: this.props.open ? 'open' : 'closed' });
+    }, TIMEOUT);
   }
 
 
@@ -156,15 +179,13 @@ class Modal extends React.Component {
       // it was added as the Portal library we use messes up the callstack
       // when assigning the ref and triggering componentDidUpdate
       setTimeout(() => {
-        clearTimeout(this.openTimeout);
-        this.openTimeout = setTimeout(() => { this.setState({ state: 'open' }); }, 500);
+        this.updateDataState();
         this.onOpening; // eslint-disable-line no-unused-expressions
       });
       _window.addEventListener('keyup', this.closeModal);
     } else if (!this.props.open && this.listening) {
-      clearTimeout(this.openTimeout);
-      this.openTimeout = setTimeout(() => { this.setState({ state: 'closed' }); }, 500);
       this.listening = false;
+      this.updateDataState();
       this.onClosing; // eslint-disable-line no-unused-expressions
       _window.removeEventListener('keyup', this.closeModal);
     }
@@ -241,8 +262,8 @@ class Modal extends React.Component {
         <CSSTransitionGroup
           component='div'
           transitionName={ this.transitionName }
-          transitionEnterTimeout={ 500 }
-          transitionLeaveTimeout={ 500 }
+          transitionEnterTimeout={ TIMEOUT }
+          transitionLeaveTimeout={ TIMEOUT }
         >
           { modalHTML }
         </CSSTransitionGroup>
@@ -250,8 +271,8 @@ class Modal extends React.Component {
         <CSSTransitionGroup
           component='div'
           transitionName={ this.backgroundTransitionName }
-          transitionEnterTimeout={ 500 }
-          transitionLeaveTimeout={ 500 }
+          transitionEnterTimeout={ TIMEOUT }
+          transitionLeaveTimeout={ TIMEOUT }
         >
           { backgroundHTML }
         </CSSTransitionGroup>
