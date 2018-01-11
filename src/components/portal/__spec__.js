c222a7da94017187d16b74fef956b5e3a8efd86e
John Barrie <john.barrie@kyan.com>
2018-01-11 11:39:17 +0000

    react-portal removed

--- src/components/portal/__spec__.js
+++ src/components/portal/__spec__.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Portal as ReactPortal } from 'react-portal';
 import Portal from './portal';
 
 describe('Portal', () => {
-  let child, wrapper, reactPortal;
+  let child, wrapper;
 
   describe('when open', () => {
     beforeEach(() => {
       wrapper = shallow(
         <Portal open>
-          <p className='child-element'></p>
+          <p className='child-element'/>
         </Portal>
       );
-    });
-
-    it('renders an instance of ReactPortal', () => {
-      reactPortal = wrapper.find(ReactPortal);
-      expect(reactPortal.length).toEqual(1);
     });
 
     it('renders children props', () => {
@@ -35,9 +29,9 @@ describe('Portal', () => {
       );
     });
 
-    it('does NOT render a ReactPortal element', () => {
-      reactPortal = wrapper.find(ReactPortal);
-      expect(reactPortal.length).toEqual(0);
+    it('renders children props', () => {
+      child = wrapper.find('.child-element');
+      expect(child.length).toEqual(1);
     });
   });
 });
--- src/components/portal/portal.js
+++ src/components/portal/portal.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Portal as ReactPortal }  from 'react-portal';
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 
+const isDomAvailable = !!(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
 class Portal extends React.Component {
+
   static propTypes = {
     /**
      * Determine if the portal is visible or not
@@ -18,16 +24,40 @@ class Portal extends React.Component {
      * @property children
      * @type {Node}
      */
-    children: PropTypes.node
+    children: PropTypes.node,
+    /**
+     * The DomNode 
+     *
+     * @property node
+     * @type {Any}
+     */
+    node: PropTypes.any
+  }
+
+  componentWillUnmount() {
+    if (this.defaultNode) {
+      document.body.removeChild(this.defaultNode);
+    }
+    this.defaultNode = null;
   }
 
   render() {
+    if (!this.props.open && !isDomAvailable) {
+      return null;
+    }
+
+    if (!this.props.node && !this.defaultNode) {
+      this.defaultNode = document.createElement('div');
+      this.defaultNode.classList.add('portal');
+      document.body.appendChild(this.defaultNode);
+    }
+
+    this.node = (this.props.node) ? this.props.node : this.defaultNode;
     return (
-      this.props.open && <ReactPortal>
-        <div ref={ (node) => { this._portal = node; } }>
-          { this.props.children }
-        </div>
-      </ReactPortal>
+      this.props.open && createPortal(
+        this.props.children,
+        this.node
+      )
     );
   }
 }
