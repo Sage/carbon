08be68832ee415629632cce537a0ba8f02d30e05
John Barrie <john.barrie@kyan.com>
2018-01-16 11:46:16 +0000

    Better portal tests

--- src/components/portal/__spec__.js
+++ src/components/portal/__spec__.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import Portal from './portal';
+import Icon from './../icon';
 import Browser from '../../utils/helpers/browser';
 
 describe('Portal', () => {
   let wrapper;
   describe('when using default node', () => {
     beforeEach(() => {
-      wrapper = shallow(
+      wrapper = mount(
         <Portal>
-          <p className='child-element' />
+          <Icon
+            tooltipMessage='Test'
+            tooltipAlign='left'
+            tooltipPosition='top'
+            type='tick'
+          />
         </Portal>
       );
     });
 
-    it('will unmount correctly', () => {
+    afterEach(() => {
       wrapper.unmount();
-      expect(document.body.innerHTML).toEqual('');
     });
 
-    it('remount second portal', () => {
-      wrapper.unmount();
-      const wrapper2 = shallow(
+    it('will mount correctly on document', () => {
+      expect(document.body.innerHTML).toEqual('<div class="carbon-portal"><span class="carbon-icon icon-tick" type="tick" data-component="icon"></span></div>');
+    });
+
+    it('can be able to access Icon', () => {
+      expect(wrapper.find(Icon).length).toBe(1);
+    });    
+
+    it('will mount second portal', () => {
+      const wrapper2 = mount(
         <Portal>
-          <p className='child-element' />
+          <Icon
+            tooltipMessage='Test'
+            tooltipAlign='left'
+            tooltipPosition='top'
+            type='tick'
+          />
+        </Portal>
+      );
+      
+      expect(document.body.getElementsByClassName('carbon-portal').length).toEqual(2);
+      wrapper2.unmount();
+    });
+
+    it('will unmount two portals', () => {
+      wrapper.unmount();
+      const wrapper2 = mount(
+        <Portal>
+          <Icon
+            tooltipMessage='Test'
+            tooltipAlign='left'
+            tooltipPosition='top'
+            type='tick'
+          />
         </Portal>
       );
       wrapper2.unmount();
@@ -33,21 +67,31 @@ describe('Portal', () => {
     it('to match snapshot ', () => {
       expect(wrapper).toMatchSnapshot();
     });
+
   });
 
-  describe('if Dom is not present', () => {
-    beforeEach(() => {
-      window.document.createElement = null;
+  it('mount a <p/> tag as child', () => {
+    const wrapper = mount(
+      <Portal>
+        <p>john</p>
+      </Portal>
+    );
 
-      wrapper = shallow(
-        <Portal>
-          <p className='child-element' />
-        </Portal>
+    expect(document.body.innerHTML).toEqual('<div class="carbon-portal"><p>john</p></div>');
+  });
+
+  it('will NOT mount with no DOM', () => {
+    spyOn(Browser, 'getWindow').and.returnValue(undefined);
+    const wrapper = mount(
+      <Portal>
+        <Icon
+          tooltipMessage='Test'
+          tooltipAlign='left'
+          tooltipPosition='top'
+          type='tick'
+        />
+      </Portal>
       );
-    });
-
-    it('will not mount if node DOM found ', () => {
-      expect(wrapper.html()).toBe(null);
-    });
+    expect(wrapper.html()).toBe(null);
   });
 });
