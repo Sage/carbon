227d1162942eee61ae28345c73c5370237eed585
John Barrie <john.barrie@kyan.com>
2018-01-23 09:21:51 +0000

    basic functionality

--- src/utils/decorators/input-validation/input-validation.js
+++ src/utils/decorators/input-validation/input-validation.js
@@ -246,6 +246,10 @@ const InputValidation = (ComposedComponent) => {
       }
     }
 
+    onReposition = () => {
+      this.positionMessage();
+    }
+
     /**
      * Positions the message relative to the icon.
      *
@@ -254,45 +258,49 @@ const InputValidation = (ComposedComponent) => {
      */
     positionMessage = () => {
       if (!this.state.valid || this.state.warning || this.state.info) {
-        // calculate the position for the message relative to the icon
-        const icon = this.validationIcon._target,
-            message = this.validationMessage;
+        setTimeout(() => {
+          // calculate the position for the message relative to the icon
+          const icon = this.validationIcon._target,
+              message = this.validationMessage;
+          if (icon && message && message.offsetHeight) {
+            let messagePositionLeft = (icon.offsetLeft + (icon.offsetWidth / 2));
+            const topOffset = icon.offsetTop - icon.offsetHeight,
+                messageOffsetWidth = message.offsetWidth;
+            // set initial position for message
+            //message.style.left = `${messagePositionLeft}px`;
+            //message.style.top = `-${message.offsetHeight - topOffset}px`;
 
-        if (icon && message && message.offsetHeight) {
-          let messagePositionLeft = (icon.offsetLeft + (icon.offsetWidth / 2));
-          const topOffset = icon.offsetTop - icon.offsetHeight,
-              messageOffsetWidth = message.offsetWidth;
+            // figure out if the message is positioned offscreen
+            const messageScreenPosition = message.getBoundingClientRect().left + messageOffsetWidth;
 
-          // set initial position for message
-          message.style.left = `${messagePositionLeft}px`;
-          message.style.top = `-${message.offsetHeight - topOffset}px`;
+            let shouldFlip = false;
 
-          // figure out if the message is positioned offscreen
-          const messageScreenPosition = message.getBoundingClientRect().left + messageOffsetWidth;
+            // change the position if it is offscreen
+            if (this.context.modal && this.context.modal.getDialog()) {
+              // if in a modal check its position relative to that
+              const dialog = this.context.modal.getDialog();
+              const domNode = ReactDOM.findDOMNode(this); // eslint-disable-line react/no-find-dom-node
+              shouldFlip = (this._window.innerWidth > (message.offsetLeft + domNode.offsetLeft + messageOffsetWidth + dialog.offsetWidth));
+            } else {
+              // otherwise check relative to the window
+              shouldFlip = messageScreenPosition > this._window.innerWidth;
+            }
 
-          let shouldFlip = false;
-
-          // change the position if it is offscreen
-          if (this.context.modal && this.context.modal.getDialog()) {
-            // if in a modal check its position relative to that
-            const dialog = this.context.modal.getDialog();
-            const domNode = ReactDOM.findDOMNode(this); // eslint-disable-line react/no-find-dom-node
-            shouldFlip = (message.offsetLeft + domNode.offsetLeft + messageOffsetWidth) > dialog.offsetWidth;
-          } else {
-            // otherwise check relative to the window
-            shouldFlip = messageScreenPosition > this._window.innerWidth;
+            if (shouldFlip) {
+              //messagePositionLeft -= messageOffsetWidth;
+              //message.style.left = `${messagePositionLeft}px`;
+              message.className += ' common-input__message--flipped';
+              message.style.left = `${(icon.getBoundingClientRect().left - message.getBoundingClientRect().width) + (icon.getBoundingClientRect().width / 2)}px`;
+              message.style.top = `${(icon.getBoundingClientRect().top - message.getBoundingClientRect().height) - (icon.getBoundingClientRect().height)}px`;
+              this.flipped = true;
+            } else {
+              message.style.left = `${icon.getBoundingClientRect().left + (icon.getBoundingClientRect().width / 2)}px`;
+              message.style.top = `${(icon.getBoundingClientRect().top - message.getBoundingClientRect().height) - (icon.getBoundingClientRect().height)}px`;
+              message.classList.remove('common-input__message--flipped');
+              this.flipped = false;
+            }
           }
-
-          if (shouldFlip) {
-            messagePositionLeft -= messageOffsetWidth;
-            message.style.left = `${messagePositionLeft}px`;
-            message.className += ' common-input__message--flipped';
-            this.flipped = true;
-          } else {
-            message.classList.remove('common-input__message--flipped');
-            this.flipped = false;
-          }
-        }
+        });
       }
     }
 
@@ -554,8 +562,6 @@ const InputValidation = (ComposedComponent) => {
      */
     showMessage = () => {
       if (this.messageExists()) {
-        this.positionMessage();
-
         this.setState({
           messageShown: true,
           immediatelyHideMessage: false
@@ -564,6 +570,7 @@ const InputValidation = (ComposedComponent) => {
         if (this.context.form) {
           this.context.form.setActiveInput(this);
         }
+        this.positionMessage();
       }
     }
 
@@ -576,7 +583,8 @@ const InputValidation = (ComposedComponent) => {
     hideMessage = () => {
       if (this.messageExists()) {
         this.setState({
-          messageShown: false
+          messageShown: false,
+          messageLocked: false
         });
       }
     }
@@ -634,16 +642,21 @@ const InputValidation = (ComposedComponent) => {
       }
 
       if (this.state.messageLocked) { messageClasses += ' common-input__message--locked'; }
+
       if (this.flipped) { messageClasses += ' common-input__message--flipped'; }
-      
-      const errorMessage = this.state.messageShown && <Portal><div key='1' className='common-input__message-wrapper'>
-        <div
-          ref={ (validationMessage) => { this.validationMessage = validationMessage; } }
-          className={ messageClasses }
-        >
-          { this.state.errorMessage || this.state.warningMessage || this.state.infoMessage }
+
+      this.errorMessage = this.state.messageShown &&
+      (<Portal key='1' onReposition={ this.positionMessage }><div className='common-input__message-wrapper'>
+          <div
+            ref={ (validationMessage) => {
+              this.validationMessage = validationMessage; 
+            } }
+            className={ messageClasses }
+          >
+            { this.state.errorMessage || this.state.warningMessage || this.state.infoMessage }
+          </div>
         </div>
-      </div></Portal>;
+      </Portal>);
 
       return [
         <Icon
@@ -653,7 +666,7 @@ const InputValidation = (ComposedComponent) => {
           className={ iconClasses }
           style={ iconStyle }
         />,
-        errorMessage
+        this.errorMessage
       ];
     }
 
