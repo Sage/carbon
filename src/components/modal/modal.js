cc1e4b4e80f8d72ad2a5ff80a3b822b9611bda6d
John Barrie <john.barrie@kyan.com>
2018-01-25 14:13:48 +0000

    Don't need getDialog() in modal.

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -148,20 +148,9 @@ class Modal extends React.Component {
   getChildContext() {
     return {
       modal: {
-        onCancel: this.props.onCancel,
-        getDialog: this.getDialog
+        onCancel: this.props.onCancel
       }
     };
-  }
-
-  /**
-   * Returns the reference to the dialog if it exists.
-   *
-   * @method getDialog
-   * @return {HTMLElement}
-   */
-  getDialog = () => {
-    return this._dialog;
   }
 
   /**
