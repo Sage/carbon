add75b00c4f179bce0ca0e23b75063c68d74b62d
Sean Armstrong <sean.armstrong@sage.com>
2018-05-21 11:05:38 +0100

    update webpack port to 8095

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = require('carbon-factory/webpack.config')({
   entryPoint: '/demo/main.js',
   outputPath: '/deploy/assets',
+  port: 8095,
   serverBase: '/deploy',
   parcelifyPaths: [`${process.cwd()}/demo`],
   singlePageApp: true
