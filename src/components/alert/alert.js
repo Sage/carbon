5995e59640aff976d2cf115912340ee2ea66f3d7
Amaal Ali <amaal.ali@opencastsoftware.com>
2018-10-04 21:51:06 +0100

    Using recompose for composing existing components with props.

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import classNames from 'classnames';
 import Dialog from '../dialog';
+import { withProps } from 'recompose';
 
 /**
  * Alert component - used to display system messages, similar to a JavaScript `alert('message')`.
  */
-const Alert = props => (
-  <Dialog
-    { ...props }
-    className={ classNames('carbon-alert', props.className) }
-    componentTag='alert'
-  />
-);
+
+const additionalAlertProps = props => ({
+  className: classNames('carbon-alert', props.className),
+  componentTag: 'alert'
+})
+
+const Alert = withProps(additionalAlertProps)(Dialog)
 
 Alert.defaultProps = {
   'data-role': 'alertdialog',
