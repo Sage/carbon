2fc2aec17620f73cf25032293308e0947bcb4368
Harpal Singh <harpal.singh@sage.com>
2018-11-05 13:20:27 +0000

    added dls article work

---
+++ demo/views/pages/home/alert-banner/alert-banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import I18n from 'i18n-js';
+
+import './alert-banner.scss';
+
+/**
+ * Shows off some of the components
+ *
+ * @param {object} props
+ * @return {AlertBanner}
+ */
+export default props => (
+  <div className='alert-banner'>
+    <span className='alert-banner__text'>Coming Soon - The Sage Design System</span>
+    <Button
+      size="small"
+      theme="white"
+      href="/news/sage-design-system"
+    >
+      Learn more
+    </Button>
+  </div>
+);
+
