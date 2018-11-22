b137483a37762a2142b10fb97853fa96e03eb396
Toni-Leigh Sharpe <toni.leigh@sage.com>
2018-11-22 10:59:40 +0000

    Add step sequence component #1865
    * Horizontal and vertical versions are provided.
    * Flex is used to make this completely flexible. Any size label, parent width or number of items will render correctly. Step indicators are also fully flexible.
    * Style config is used to allow both components to have access to the sdame colours without depending on each other or duplicating.
    * Inheritence is used to make sure the sequence styles (mostly layout) are handled by the sequence and not by the item, again for fully flexible re-use.

---
+++ src/components/step-sequence/step-sequence-item/step-sequence-item.js
@@ -0,0 +1,29 @@
+import Icon from 'components/icon';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './step-sequence-item.scss';
+
+const baseClass = 'carbon-step-sequence-item';
+const classes = state => `${baseClass} ${baseClass}--${state}`;
+
+const stepMarker = (state, indicator) => {
+  return state === 'complete' ? <Icon type='tick' /> : indicator;
+};
+
+const StepSequenceItem = ({ label, state, indicator }) => (
+  <li className={ classes(state) }>
+    <span className='carbon-step-sequence-item__label'>
+      <span className='carbon-step-sequence-item__indicator'>{ stepMarker(state, indicator) }</span>
+      { label }
+    </span>
+  </li>
+);
+
+StepSequenceItem.propTypes = {
+  label: PropTypes.string,
+  state: PropTypes.string,
+  indicator: PropTypes.string
+};
+
+export default StepSequenceItem;
---
+++ src/components/step-sequence/step-sequence-item/step-sequence-item.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import StepSequenceItem from './step-sequence-item';
+
+describe('<StepSequenceItem />', () => {
+  let stepSequenceItem;
+
+  beforeAll(() => {
+    stepSequenceItem = shallow(
+      <StepSequenceItem
+        state='foo'
+        label='bar'
+        indicator='2'
+      />
+    );
+  });
+
+  test('basic render', () => {
+    expect(stepSequenceItem).toMatchSnapshot();
+  });
+
+  test('completed render', () => {
+    stepSequenceItem.setProps({ state: 'complete' });
+    expect(stepSequenceItem).toMatchSnapshot();
+  });
+});
