9f41d803da12a68d73ea5df454b5b3e65c76d6dc
Andrew Tait <andrew.tait@sage.com>
2018-12-07 09:30:37 +0000

    started input component

---
+++ src/components/input/input-container.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputContainer = ({ children }) => (
+  <div className='carbon-input-container'>
+    { children }
+  </div>
+);
+
+export default InputContainer;
--- src/components/textbox/textbox.js
+++ src/components/textbox/textbox.js
@@ -1,81 +1,28 @@
 import React from 'react';
-import Input from '../../utils/decorators/input';
+import { Input, InputContainer } from './../input';
 import InputLabel from '../../utils/decorators/input-label';
 import InputValidation from '../../utils/decorators/input-validation';
 import { validProps } from '../../utils/ether';
 import tagComponent from '../../utils/helpers/tags';
 import './textbox.scss';
 
-/**
- * A textbox widget.
- *
- * == How to use a Textbox in a component:
- *
- * In your file
- *
- *   import Textbox from 'carbon-react/lib/components/textbox';
- *
- * To render a Textbox:
- *
- *   <Textbox name="myTextbox" />
- *
- * @class Textbox
- * @constructor
- * @decorators {Input,InputLabel,InputValidation}
- */
-const Textbox = Input(InputLabel(InputValidation(class Textbox extends React.Component {
-  /**
-   * Main Class getter
-   *
-   * @method mainClasses
-   * @return {void}
-   */
-  get mainClasses() {
-    return 'carbon-textbox';
-  }
-
-  /**
-   * Input class getter
-   *
-   * @method inputClasses
-   */
-  get inputClasses() {
-    return 'carbon-textbox__input';
-  }
-
-  /**
-   * A getter that combines props passed down from the input decorator with
-   * textbox specific props.
-   *
-   * @method inputProps
-   * @return {Object} props for the input
-   */
-  get inputProps() {
-    const { ...props } = validProps(this);
-    props.className = this.inputClasses;
-    return props;
-  }
-
-  /**
-   * Renders the component.
-   *
-   * @method render
-   * @return {Object} JSX
-   */
+const Textbox = InputLabel(InputValidation(class Textbox extends React.Component {
   render() {
     return (
       <div
-        className={ this.mainClasses }
-        ref={ (comp) => { this._target = comp; } }
+        className='carbon-textbox'
+        ref={ comp => this._target = comp }
         { ...tagComponent('textbox', this.props) }
       >
         { this.labelHTML }
-        { this.inputHTML }
+        <InputContainer>
+          <Input { ...this.props } />
+        </InputContainer>
         { this.validationHTML }
         { this.fieldHelpHTML }
       </div>
     );
   }
-})));
+}));
 
 export default Textbox;
