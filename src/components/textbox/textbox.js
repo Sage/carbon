d52e4d85d4c4c2d24905c25be8e1df512c97fb84
Andrew Tait <andrew.tait@sage.com>
2018-12-14 10:43:55 +0000

    reset changes to textbox

--- src/components/textbox/textbox.js
+++ src/components/textbox/textbox.js
@@ -1,27 +1,67 @@
 import React from 'react';
-import { Input, InputBox } from './../input';
-import InputDecorator from '../../utils/decorators/input';
+import Input from '../../utils/decorators/input';
 import InputLabel from '../../utils/decorators/input-label';
 import InputValidation from '../../utils/decorators/input-validation';
 import { validProps } from '../../utils/ether';
 import tagComponent from '../../utils/helpers/tags';
 import './textbox.scss';
 
-const Textbox = InputDecorator(InputLabel(InputValidation(class Textbox extends React.Component {
+/**
+ * A textbox widget.
+ *
+ * == How to use a Textbox in a component:
+ *
+ * In your file
+ *
+ *   import Textbox from 'carbon-react/lib/components/textbox';
+ *
+ * To render a Textbox:
+ *
+ *   <Textbox name="myTextbox" />
+ *
+ * @class Textbox
+ * @constructor
+ * @decorators {Input,InputLabel,InputValidation}
+ */
+const Textbox = Input(InputLabel(InputValidation(class Textbox extends React.Component {
+  /**
+   * Main Class getter
+   *
+   * @method mainClasses
+   * @return {void}
+   */
   get mainClasses() {
     return 'carbon-textbox';
   }
 
+  /**
+   * Input class getter
+   *
+   * @method inputClasses
+   */
   get inputClasses() {
     return 'carbon-textbox__input';
   }
 
+  /**
+   * A getter that combines props passed down from the input decorator with
+   * textbox specific props.
+   *
+   * @method inputProps
+   * @return {Object} props for the input
+   */
   get inputProps() {
     const { ...props } = validProps(this);
     props.className = this.inputClasses;
     return props;
   }
 
+  /**
+   * Renders the component.
+   *
+   * @method render
+   * @return {Object} JSX
+   */
   render() {
     return (
       <div
@@ -30,9 +70,7 @@ const Textbox = InputDecorator(InputLabel(InputValidation(class Textbox extends 
         { ...tagComponent('textbox', this.props) }
       >
         { this.labelHTML }
-        <InputBox { ...this.fieldProps }>
-          <Input { ...this.inputProps } />
-        </InputBox>
+        { this.inputHTML }
         { this.validationHTML }
         { this.fieldHelpHTML }
       </div>
