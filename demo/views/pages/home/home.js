f002b0f43240037e46bffcf7a066de6bdfafbab8
samhinton88 <sam.hinton88@gmail.com>
2019-01-24 13:31:43 +0000

    revert changes to demo home page

--- demo/views/pages/home/home.js
+++ demo/views/pages/home/home.js
@@ -1,25 +1,14 @@
 import React from 'react';
+
+import AlertBanner from './alert-banner';
+import ComponentShowcase from './component-showcase';
+import GetStarted from './get-started';
+import PageHeaderLarge from '../../common/page-header-large';
+import SageLovesCarbon from './sage-loves-carbon';
+import SellingPoints from './selling-points';
 import Sectioniser from './sectioniser';
-import styled from 'styled-components';
-
-import { ScrollableList, ScrollableItemWrapper, ScrollableListItem }  from '../../../../src/components/scrollable-list';
+import Wrapper from './../../common/wrapper';
 import './demo-site.scss';
-
-const Divider = ({ background }) => <div style={ { height: '100%', width: '100%', background } }>divider</div>;
-const MiniApp = () => {
-  return (
-    <div style={ { display: 'flex' } }>
-      <h2>Mini App</h2>
-      <button>Increment - </button>
-      <button>Decrement + </button>
-    </div>
-  )
-}
-
-const CustomItem = styled.div`
-  background: ${({isSelected}) => isSelected && `rgba(40, 250, 20, 0.8)`};
-`;
-
 
 class Home extends React.Component {
   /**
@@ -27,32 +16,17 @@ class Home extends React.Component {
    */
   render() {
     return (
-      <div style={{ height: '400px', width: '500px'}}>
-        <ScrollableList 
-          onLazyLoad={() => console.log('lazy load now')}
-          onSelect={(id) => console.log(id, 'SELECTED')}
-          keyNavigation
-        >
-          <div style={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgba(40, 40, 250, 0.5)'}}>This header should not be selectable:</div>
-          {/* <ScrollableListItem >sub 0</ScrollableListItem>
-          <div>--- nor should this div be ---</div>
-          <ScrollableListItem >sub 3</ScrollableListItem>
-          <ScrollableListItem >sub 4</ScrollableListItem>
-          <div>--- nor should this div be ---</div>
-          <ScrollableListItem >sub 3</ScrollableListItem>
-          <ScrollableListItem >sub 4</ScrollableListItem>
-          <div>--- nor should this div be ---</div>
-          <CustomItem isSelectable>Style changes on select</CustomItem>
-          <ScrollableListItem >sub 3</ScrollableListItem>
-          <ScrollableListItem >sub 4</ScrollableListItem>
-          <div>--- nor should this div be ---</div>
-          <ScrollableListItem >sub 3</ScrollableListItem>
-          <MiniApp />
-          <ScrollableListItem >sub 4</ScrollableListItem>
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'rgba(240, 40, 250, 0.5)'}}>This footer should not be selectable:</div> */}
-        </ScrollableList>
-      </div>
-     
+      <Sectioniser
+        minDepth='0'
+        maxDepth='0'
+      >
+        <AlertBanner />
+        <PageHeaderLarge />
+        <ComponentShowcase />
+        <SellingPoints />
+        <SageLovesCarbon />
+        <GetStarted />
+      </Sectioniser>
     );
   }
 }
