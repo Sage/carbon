7c60308b6d2ca9ff63cb7aaebed5ae7e8f2d1f08
Andrew Tait <andrew.tait@sage.com>
2019-01-30 16:47:55 +0000

    InputDecoratorBridge component

---
+++ src/__experimental__/components/input-decorator-bridge/input-decorator-bridge.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import InputDecorator from '../../../utils/decorators/input/input';
+import InputLabel from '../../../utils/decorators/input-label/input-label';
+import InputValidation from '../../../utils/decorators/input-validation/input-validation';
+import InputIcon from '../../../utils/decorators/input-icon/input-icon';
+import { validProps } from '../../../utils/ether/ether';
+import Textbox from '../textbox';
+
+// This component creates a bridge between the new Textbox component and the old decorator classes.
+// As we remove the decorators, this bridge will become less useful and can be removed.
+const InputDecoratorBridge = InputDecorator(InputLabel(InputValidation(InputIcon(
+  class InputDecoratorBridge extends React.Component {
+    static propTypes = {
+      children: PropTypes.node, // optional: will add additional child elements to the input (eg. icons)
+      inputIcon: PropTypes.string, // optional: hooks into the InputIcon decorator to add a button to the input
+      formattedValue: PropTypes.string, // optional: will display this in the input instead value
+      forwardedRef: PropTypes.object
+    }
+
+    // this method is required as part of the InputDecorator API
+    get inputProps() {
+      return validProps(this);
+    }
+
+    render() {
+      const { ...inputProps } = this.inputProps;
+      inputProps.ref = this.props.forwardedRef;
+      if (typeof this.props.formattedValue === 'string') inputProps.value = this.props.formattedValue;
+
+      return (
+        <div className={ this.mainClasses }>
+          { this.labelHTML }
+          <div { ...this.fieldProps }>
+            <Textbox { ...inputProps }>
+              { this.props.children }
+            </Textbox>
+            { this.props.inputIcon && this.inputIconHTML(this.props.inputIcon) }
+          </div>
+          { this.validationHTML }
+          { this.fieldHelpHTML }
+        </div>
+      );
+    }
+  }
+))));
+
+export default React.forwardRef((props, ref) => (
+  <InputDecoratorBridge { ...props } forwardedRef={ ref } />
+));
