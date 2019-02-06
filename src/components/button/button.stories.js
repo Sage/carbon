d763dcf4b89f3bc27889599d7e872f8e39e176c5
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-06 11:27:44 +0000

    link to example

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { text, boolean } from '@storybook/addon-knobs';
+import { linkTo } from '@storybook/addon-links';
 import notes from './notes.md';
 import Button from './button';
 
@@ -13,12 +14,21 @@ storiesOf('Button', module)
     }
   })
   .add('with text', () => (
-    <Button disabled={ boolean('disabled', false) } onClick={ action('click') }>
+    <Button
+      disabled={ boolean('disabled', false) }
+      onClick={ linkTo('Button', 'with some emoji') }
+    >
       {text('Label', 'Hello Storybook')}
     </Button>
   ))
   .add('with some emoji', () => (
-    <Button><span role='img' aria-label='so cool'>😀 😎 👍 💯</span></Button>
+    <Button
+      onClick={ () => {
+        action('click')();
+        linkTo('Button', 'with text')();
+      } }
+    ><span role='img' aria-label='so cool'>😀 😎 👍 💯</span>
+    </Button>
   ))
   .add('accessibility', () => (
     <Button style={ { background: 'black', color: 'black' } }>Bad</Button>
