291ac4a769dbfc9357f8410ff89375838648146b
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-06 13:50:14 +0000

    adding addon viewport

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,3 +3,4 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-notes/register'; 
 import '@storybook/addon-links/register';
+import '@storybook/addon-viewport/register';
