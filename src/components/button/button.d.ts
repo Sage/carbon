e01fc55beac3ea0b272e6b148bbea8dbec85d9d2
Ed Leeks <edward.leeks@sage.com>
2019-02-08 08:13:46 +0000

    splits the linter script commands, fixes ts linter warnings an errors

--- src/components/button/button.d.ts
+++ src/components/button/button.d.ts
@@ -1,10 +1,10 @@
 import * as React from 'react';
 export interface ButtonProps {
-  as?: "primary" | "secondary",
-  disabled?: boolean,
-  theme?: string,
-  size?: string,
-  subtext?: any
+  as?: 'primary' | 'secondary';
+  disabled?: boolean;
+  theme?: string;
+  size?: string;
+  subtext?: any;
 }
 declare const Button: React.Component<ButtonProps, {}>;
 export default Button;
--- src/components/link/link.d.ts
+++ src/components/link/link.d.ts
@@ -1,10 +1,17 @@
 import * as React from 'react';
 export interface LinkProps {
-  as?: "primary" | "secondary",
-  disabled?: boolean,
-  theme?: string,
-  size?: string,
-  subtext?: any
+  className?: string;
+  disabled?: boolean;
+  href?: string;
+  icon?: string;
+  iconAlign?: string;
+  onClick?: (...args: any[]) => any;
+  onKeyDown?: (...args: any[]) => any;
+  tabbable?: boolean;
+  to?: string;
+  tooltipMessage?: string;
+  tooltipPosition?: string;
+  tooltipAlign?: string;
 }
 declare const Link: React.Component<LinkProps, {}>;
 export default Link;
