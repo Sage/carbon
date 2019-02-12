3341c5bd790d164d284628daae1787dd1c3041d1
Ed Leeks <edward.leeks@sage.com>
2019-02-12 11:04:56 +0000

    adds legacy handling to new validator

--- src/utils/validations/validator/validator.js
+++ src/utils/validations/validator/validator.js
@@ -1,17 +1,30 @@
-const validator = validationFunctions => async (value) => {
-  let results = [];
-  if (Array.isArray(validationFunctions)) {
-    results = validationFunctions.map(func => (func(value)));
-  } else {
-    results = [validationFunctions(value)];
-    // Promise.resolve() ?????
-  }
-  const validate = await Promise.all(results)
-    .catch((error) => {
-      console.log('Error : ', error.message);
-      throw error;
-    });
-  console.log('Validate status : ', validate);
-  return validate;
-};
+const isLegacy = (func) => { return typeof func === 'object'; };
+
+const validator = validationFunctions => (value, props) => (
+  new Promise(async (resolve, reject) => {
+    let results = [];
+
+    if (Array.isArray(validationFunctions)) {
+      results = validationFunctions.map((func) => {
+        if (isLegacy(func)) {
+          const valid = func.validate(value, props);
+          if (valid) {
+            return Promise.resolve();
+          }
+          return Promise.reject(func.message());
+        }
+        return func(value, props);
+      });
+    } else {
+      results = [validationFunctions(value, props)];
+    }
+
+    try {
+      await Promise.all(results);
+      return resolve();
+    } catch (err) {
+      return reject(err);
+    }
+  })
+);
 export default validator;
--- src/utils/validations/validator/validator.spec.js
+++ src/utils/validations/validator/validator.spec.js
@@ -1,4 +1,5 @@
 import Validator from '.';
+import PresenceValidator from '../presence';
 
 describe('Validator', () => {
   const isLong = value => new Promise((resolve, reject) => {
@@ -25,11 +26,21 @@ describe('Validator', () => {
     }
   });
 
-  const funcArr = [presence, isLong, isNotZero];
+  const asyncValidation = value => new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value === 'foo') {
+        resolve();
+      } else {
+        reject(new Error('not foo!'));
+      }
+    }, 3000);
+  });
+
+  let funcArr = [presence, isLong, isNotZero];
 
   it('receives an array of functions and resolves', async () => {
     const validate = await Validator(funcArr);
-    expect(validate('foobar')).toBeTruthy();  //resolve.toMatchSnapshot()
+    expect(validate('foobar')).toBeTruthy();
   });
 
   it('receives an array of functions and rejects', async () => {
@@ -45,5 +56,57 @@ describe('Validator', () => {
   it('receives a single input and rejects', async () => {
     const validate = await Validator(presence);
     expect(validate('')).rejects.toThrow();
+  });
+
+  it('handles async validations as part of an array and resolves', async () => {
+    funcArr = [presence, asyncValidation, isNotZero];
+    const validate = await Validator(funcArr);
+    expect(validate('foo')).toBeTruthy();
+  });
+
+  it('handles async validations as a single input and resolves', async () => {
+    const validate = await Validator(asyncValidation);
+    expect(validate('foo')).toBeTruthy();
+  });
+
+  it('handles async validations as part of an array and rejects', async () => {
+    funcArr = [presence, asyncValidation, isNotZero];
+    const validate = await Validator(funcArr);
+    expect(validate('bar')).toBeTruthy();
+  });
+
+  it('handles async validations as a single input and rejects', async () => {
+    const validate = await Validator(asyncValidation);
+    expect(validate('bar')).rejects.toThrow();
+  });
+
+  describe('old validators', () => {
+    // describe('isLegacy', () => {
+    //   it('returns true when a legacy validation is passed to the Validator', () => {
+        
+    //   });
+    // });
+    const presVal = new PresenceValidator();
+    it('handles legacy validations as part of an array and resolves', async () => {
+      funcArr = [presence, presVal, isNotZero];
+      const validate = await Validator(funcArr);
+      expect(validate('foo')).toBeTruthy();
+    });
+
+    it('handles legacy validations as a single input and resolves', async () => {
+      const validate = await Validator(presVal);
+      expect(validate('foo')).toBeTruthy();
+    });
+
+    it('handles legacy validations as part of an array and rejects', async () => {
+      funcArr = [presVal, isNotZero];
+      const validate = await Validator(funcArr);
+      expect(validate()).toBeTruthy();
+    });
+
+    it('handles legacy validations as a single input and rejects', async () => {
+      const validate = await Validator(presVal);
+      expect(validate('bar')).rejects.toThrow();
+    });
   });
 });
