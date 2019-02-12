2fe17983227cd1d0a10a40a4074a782eb250f96b
Ed Leeks <edward.leeks@sage.com>
2019-02-12 14:27:54 +0000

    adds support for single legacy validations and specs to test

--- src/utils/validations/validator/validator.spec.js
+++ src/utils/validations/validator/validator.spec.js
@@ -1,112 +1,138 @@
-import Validator from '.';
+import validator from '.';
 import PresenceValidator from '../presence';
 
-describe('Validator', () => {
-  const isLong = value => new Promise((resolve, reject) => {
-    if (value.length > 5) {
-      resolve(value);
-    } else {
-      reject(new Error('value is too short!'));
-    }
-  });
+const shortErr = 'value is too short!';
+const presErr = 'this value is required!';
+const asyncErr = 'not foo!';
+const legacyErr = '[missing "en.errors.messages.blank" translation]';
 
-  const presence = value => new Promise((resolve, reject) => {
-    if (value) {
-      resolve(true);
-    } else {
-      reject(new Error('this value is required!'));
-    }
-  });
-
-  const isNotZero = value => new Promise((resolve, reject) => {
-    if (value !== 0) {
-      resolve(true);
-    } else {
-      reject(new Error('this is zero!'));
-    }
-  });
-
-  const asyncValidation = value => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (value === 'foo') {
-        resolve();
+const runValidation = async (validations, value) => {
+  const validate = validator(validations);
+  let valid;
+  let index = 0;
+  await validate(value)
+    .then(() => {
+      valid = true;
+      index += 1;
+    })
+    .catch((err) => {
+      if (err.message) {
+        valid = err.message;
       } else {
-        reject(new Error('not foo!'));
+        valid = Array.isArray(validations) ? validations[index].message() : validations.message();
       }
-    }, 3000);
+    });
+
+  return valid;
+};
+
+const isLong = value => new Promise((resolve, reject) => {
+  if (value.length > 5) {
+    resolve(value);
+  } else {
+    reject(new Error('value is too short!'));
+  }
+});
+
+const presence = value => new Promise((resolve, reject) => {
+  if (value) {
+    resolve(true);
+  } else {
+    reject(new Error('this value is required!'));
+  }
+});
+
+const isNotZero = value => new Promise((resolve, reject) => {
+  if (value !== 0) {
+    resolve(true);
+  } else {
+    reject(new Error('this is zero!'));
+  }
+});
+
+const asyncValidation = value => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (value === 'foo') {
+      resolve();
+    } else {
+      reject(new Error('not foo!'));
+    }
+  }, 500);
+});
+
+describe('validator', () => {
+  it('receives an array of functions and returns true when all promises resolve', async (done) => {
+    const validate = await runValidation([presence, isLong, isNotZero], 'foobar');
+    expect(validate).toEqual(true);
+    done();
   });
 
-  let funcArr = [presence, isLong, isNotZero];
-
-  it('receives an array of functions and resolves', async () => {
-    const validate = await Validator(funcArr);
-    expect(validate('foobar')).toBeTruthy();
+  it('receives an array of functions and returns an error message for the first promise that rejects', async (done) => {
+    const validate = await runValidation([presence, isLong, isNotZero], 'foo');
+    expect(validate).toEqual(shortErr);
+    done();
   });
 
-  it('receives an array of functions and rejects', async () => {
-    const validate = await Validator(funcArr);
-    expect(validate('foo')).rejects.toThrow();
+  it('receives a single input and returns true when the promise resolves', async (done) => {
+    const validate = await runValidation(presence, 'foo');
+    expect(validate).toEqual(true);
+    done();
   });
 
-  it('receives a single input and resolves', async () => {
-    const validate = await Validator(presence);
-    expect(validate('foo')).toBeTruthy();
+  it('receives a single input and returns the error message when the promise rejects', async (done) => {
+    const validate = await runValidation(presence, '');
+    expect(validate).toEqual(presErr);
+    done();
   });
 
-  it('receives a single input and rejects', async () => {
-    const validate = await Validator(presence);
-    expect(validate('')).rejects.toThrow();
+  it('handles async validations as part of an array and returns true when all th epromises resolve', async (done) => {
+    const validate = await runValidation([presence, asyncValidation, isNotZero], 'foo');
+    expect(validate).toEqual(true);
+    done();
   });
 
-  it('handles async validations as part of an array and resolves', async () => {
-    funcArr = [presence, asyncValidation, isNotZero];
-    const validate = await Validator(funcArr);
-    expect(validate('foo')).toBeTruthy();
+  it('handles async validations as a single input and returns true the promise resolves', async (done) => {
+    const validate = await runValidation(asyncValidation, 'foo');
+    expect(validate).toEqual(true);
+    done();
   });
 
-  it('handles async validations as a single input and resolves', async () => {
-    const validate = await Validator(asyncValidation);
-    expect(validate('foo')).toBeTruthy();
+  it('handles async validations in an array and returns an error message of first promise to reject', async (done) => {
+    const validate = await runValidation([presence, asyncValidation, isNotZero], 'bar');
+    expect(validate).toEqual(asyncErr);
+    done();
   });
 
-  it('handles async validations as part of an array and rejects', async () => {
-    funcArr = [presence, asyncValidation, isNotZero];
-    const validate = await Validator(funcArr);
-    expect(validate('bar')).toBeTruthy();
+  it('handles a single async validation, returning the error message when the promise rejects', async (done) => {
+    const validate = await runValidation(asyncValidation, 'bar');
+    expect(validate).toEqual(asyncErr);
+    done();
   });
 
-  it('handles async validations as a single input and rejects', async () => {
-    const validate = await Validator(asyncValidation);
-    expect(validate('bar')).rejects.toThrow();
-  });
-
-  describe('old validators', () => {
-    // describe('isLegacy', () => {
-    //   it('returns true when a legacy validation is passed to the Validator', () => {
-        
-    //   });
-    // });
+  describe('legacy validations', () => {
     const presVal = new PresenceValidator();
-    it('handles legacy validations as part of an array and resolves', async () => {
-      funcArr = [presence, presVal, isNotZero];
-      const validate = await Validator(funcArr);
-      expect(validate('foo')).toBeTruthy();
+    it('handles legacy validations as part of an array and returns true when all promises resolve', async (done) => {
+      const valid = await runValidation([presence, presVal, isNotZero], 'foo');
+      expect(valid).toEqual(true);
+      done();
     });
 
-    it('handles legacy validations as a single input and resolves', async () => {
-      const validate = await Validator(presVal);
-      expect(validate('foo')).toBeTruthy();
+    it('handles a single legacy validation and returns true when the promise resolves', async (done) => {
+      const validate = await runValidation(presVal, 'foo');
+      expect(validate).toEqual(true);
+      done();
     });
 
-    it('handles legacy validations as part of an array and rejects', async () => {
-      funcArr = [presVal, isNotZero];
-      const validate = await Validator(funcArr);
-      expect(validate()).toBeTruthy();
+    it('handles legacy validations in an array and returns an error message when the promise rejects', async (done) => {
+      const validate = await runValidation([presVal, isNotZero], '');
+      expect(validate).toEqual(legacyErr);
+      done();
     });
 
-    it('handles legacy validations as a single input and rejects', async () => {
-      const validate = await Validator(presVal);
-      expect(validate('bar')).rejects.toThrow();
+    it('handles a single legacy validation and returns the error message when the promise rejects', async (done) => {
+      const validate = await runValidation(presVal, '');
+      expect(validate).toEqual(legacyErr);
+      done();
     });
   });
 });
