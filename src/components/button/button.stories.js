a296a7b8b6b2582a9e74f69430e0c0ce9c041fa8
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-12 12:52:49 +0100

    FE-1244 - button stories

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,46 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
+import { text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
-import { text, boolean } from '@storybook/addon-knobs';
-import { linkTo } from '@storybook/addon-links';
+import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './notes.md';
 import Button from './button';
 
-const ThemedButton = styled.button`
-  background: ${({ theme }) => theme.main};
-  height: 50px;
-  width: 150px;
-`;
+const clickAction = action('click');
 
 storiesOf('Button', module)
   .addParameters({
     info: {
       inline: true,
-      header: false
+      header: false,
+      propTablesExclude: [],
+      propTables: [Button],
+      source: false
     }
   })
-  .add('themed', () => <ThemedButton>Themed Button</ThemedButton>)
-  .add('with text', () => (
-    <Button
-      disabled={ boolean('disabled', false) }
-      onClick={ linkTo('Button', 'with some emoji') }
-    >
-      {text('Label', 'Hello Storybook')}
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button
-      onClick={ () => {
-        action('click')();
-        linkTo('Button', 'with text')();
-      } }
-    ><span role='img' aria-label='so cool'>😀 😎 👍 💯</span>
-    </Button>
-  ))
-  .add('accessibility', () => (
-    <Button style={ { background: 'black', color: 'black' } }>Bad</Button>
-  ))
-  .add('with notes', () => <Button>Button with notes</Button>, {
+  .add('default', () => {
+    const asOption = select('as', OptionsHelper.themesBinary, OptionsHelper.themesBinary[0]);
+    const children = text('children', 'Example Button');
+    const disabled = boolean('disabled', false);
+    const theme = select('theme', OptionsHelper.buttonColors, OptionsHelper.buttonColors[0]);
+    const size = select('size', OptionsHelper.sizesRestricted, OptionsHelper.sizesRestricted[1]);
+    const subtext = text('subtext', '');
+
+    return (
+      <Button
+        as={ asOption }
+        disabled={ disabled }
+        theme={ theme }
+        size={ size }
+        subtext={ subtext }
+        onClick={ clickAction }
+      >
+        {children}
+      </Button>
+    );
+  }, {
     notes: { markdown: notes }
   });
--- src/components/button/notes.md
+++ src/components/button/notes.md
@@ -1,4 +1,12 @@
-# My Markdown File
-- one
-- two 
-- three
+# Designer Notes
+- Avoid using buttons for navigation (taking the user somewhere else) - use them for performing an action or command.
+- Use a Primary configuration for positive actions that are the main goal for the user (e.g. Save, Submit, Continue).
+- Avoid placing Secondary actions on a form if you can.
+- If an action is particularly destructive (e.g. Delete or Clear), consider using Red configuration. It might be a good idea to show a confirmation dialog, or the ability to undo it too.
+- Try to create a single path to completion by aligning your action with your inputs.
+- You can disable a Button, but try to avoid this. If you need to, make it clear what the user needs to do in order to activate the Button. A good case for disabling an action is to prevent a form being submitted twice (e.g. on a payment page).
+
+# Related Components
+- Range of buttons where one is more important? Try Split Button. <!-- TODO link -->
+- Range of buttons all of the same importance? Try Multi Action Button. <!-- TODO link -->
+- Choosing one option from a highly visible range? Try Button Toggle. <!-- TODO link -->
