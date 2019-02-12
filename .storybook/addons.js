458659daefad94fc902add2112b4e604790bb473
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-12 14:55:40 +0100

    FE-1244 - removing unnecessaries addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,6 +1,4 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-notes/register'; 
-import '@storybook/addon-links/register';
+import '@storybook/addon-notes/register';
 import '@storybook/addon-viewport/register';
