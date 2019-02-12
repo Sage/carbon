3e42ea95631dbf660377bd0add657003ad416d28
samhinton88 <sam.hinton88@gmail.com>
2019-02-12 15:50:46 +0000

    fix jsdoc on atOpacity

--- src/style/utils/at-opacity.js
+++ src/style/utils/at-opacity.js
@@ -1,7 +1,7 @@
-/*
-Given a base color hex will return a function that expects
-some given opactiy and returns the base color at that
-opacity.
+/**
+ * Given a base color hex will return a function that expects
+ * some given opactiy and returns the base color at that
+ * opacity.
  */
 export default (base) => {
   const rgb = base.match(/[^#]{2}/g)
