e66b8c68fa322dbee7233d3723a8c120bf16f8ad
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-13 15:13:31 +0100

    FE-1244 - a11y storybook addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,4 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
 import '@storybook/addon-viewport/manager';
+import '@storybook/addon-a11y/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withNotes } from '@storybook/addon-notes';
 import { withInfo } from '@storybook/addon-info';
+import { checkA11y } from '@storybook/addon-a11y';
 import AppLayout from 'storybookHelpers/components/app-layout/app-layout';
 
 
@@ -15,6 +16,7 @@ function loadStories() {
 addDecorator(withKnobs);
 addDecorator(withNotes);
 addDecorator(withInfo);
+addDecorator(checkA11y);
 
 // give all stories access to themes
 addDecorator(story => <AppLayout>{story()}</AppLayout>);
