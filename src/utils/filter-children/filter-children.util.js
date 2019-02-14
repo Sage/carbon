74b5e6109464ed8ce4735aaef30103477e4565b8
Ed Leeks <edward.leeks@sage.com>
2019-02-14 10:41:52 +0000

    added documentation for filter util

--- src/utils/filter-children/filter-children.util.js
+++ src/utils/filter-children/filter-children.util.js
@@ -9,6 +9,26 @@ const defaultFilter = (text, value) => {
   return text.includes(value);
 };
 
+/**
+ * Create a filter for React children:
+ * 
+ *   filterChildren({ value: 'my filter' })(children);
+ * 
+ * You can also pass in:
+ *   * custom filters
+ *   * 'no results' callback
+ *   * a callback for each child
+ * 
+ * For example:
+ * 
+ *   const filter = filterChildren({
+ *     value: 'my filter',
+ *     filter: (text, value) => value !== text,
+ *     onNoResults: () => 'no results!'
+ *   });
+ * 
+ *   filter(children, (child) => <div>{ child }</div>);
+ */
 const filterChildren = ({
   value, filter = defaultFilter, onNoResults
 } = {}) => (children, callback) => {
