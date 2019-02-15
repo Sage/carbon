f2241793d323ef6ae8a4600bed7870fdd1f62541
Ed Leeks <edward.leeks@sage.com>
2019-02-15 14:35:44 +0000

    refactor some main function into ternary

--- src/utils/validations/validator/validator.js
+++ src/utils/validations/validator/validator.js
@@ -1,21 +1,17 @@
-const isLegacy = (func) => { return typeof func === 'object'; };
+const isLegacy = func => typeof func === 'object';
 
 const handleLegacyValidation = (func, value, props) => {
-  if (func.validate(value, props)) {
-    return Promise.resolve();
-  }
+  if (func.validate(value, props)) return Promise.resolve();
   return Promise.reject(func.message());
 };
 
 const validator = validationFunctions => (value, props) => {
   const validations = Array.isArray(validationFunctions) ? validationFunctions : [validationFunctions];
-  const results = validations.map((func) => {
-    if (isLegacy(func)) {
-      return handleLegacyValidation(func, value, props);
-    }
-    return func(value, props);
-  });
-
+  const handleValidation = func => (
+    isLegacy(func) ? handleLegacyValidation(func, value, props) : func(value, props)
+  );
+  const results = validations.map(handleValidation);
   return Promise.all(results);
 };
+
 export default validator;
