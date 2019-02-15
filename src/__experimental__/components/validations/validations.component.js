5cb4617dd34427f086daf59e367965f7421f06b9
Ed Leeks <edward.leeks@sage.com>
2019-02-15 16:06:31 +0000

    updates react dependencies and creates basic file structure

---
+++ src/__experimental__/components/validations/validations.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import validator from '../../../utils/validations/validator';
+
+class Validations extends React.Component {
+
+}
+
+export default Validations;
---
+++ src/__experimental__/components/validations/validations.spec.js
@@ -0,0 +1,1 @@
+import Validations from '.';
