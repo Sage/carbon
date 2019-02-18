96aae9a60e549c7d4f0421e677d8f7fb57266ca9
Michal Maciorowski <m.maciorowski@customcode.pl>
2019-02-18 16:11:00 +0100

    FE-1244 - adds alert story with button

--- src/components/alert/alert.stories.js
+++ src/components/alert/alert.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
-import { StateDecorator, Store, State } from '@sambego/storybook-state';
+import { State, Store } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import Button from '../button/button';
@@ -21,60 +21,41 @@ const handleOpen = () => {
   action('open')();
 };
 
-const Wrapper = ({
-  onCancel,
-  title,
-  enableBackgroundUI,
-  disableEscKey,
-  ariaRole,
-  height,
-  showCloseIcon,
-  size,
-  stickyFormFooter,
-  subtitle,
-  open,
-  children
-}) => {
-  const knobsOpen = boolean('open', store.get('open'));
-
-  if (knobsOpen !== open) {
-    store.set({ open: knobsOpen });
-  }
-
-  return (
-  <>
-    <Button onClick={ handleOpen }>Open Preview</Button>
-    <Alert
-      onCancel={ onCancel }
-      title={ title }
-      enableBackgroundUI={ enableBackgroundUI }
-      disableEscKey={ disableEscKey }
-      ariaRole={ ariaRole }
-      height={ height }
-      showCloseIcon={ showCloseIcon }
-      size={ size }
-      stickyFormFooter={ stickyFormFooter }
-      subtitle={ subtitle }
-      open={ store.get('open') }
-    >
-      {children}
-    </Alert>
-  </>
-  );
-};
-
-Wrapper.propTypes = { ...Alert.propTypes };
-Wrapper.displayName = 'Alert';
-
 storiesOf('Alert', module)
-  .addDecorator(StateDecorator(store))
-  .addParameters({
-    info: {
-      propTablesExclude: [State, Wrapper],
-      propTables: [Alert]
-    }
-  })
   .add('default', () => {
+    const title = text('title', 'Attention');
+    const subtitle = text('subtitle', '');
+    const children = text('children', 'This is an example of a alert.');
+    const enableBackgroundUI = boolean('enableBackgroundUI', false);
+    const disableEscKey = boolean('disableEscKey', false);
+    const ariaRole = text('ariaRole', 'dialog');
+    const height = text('height', '');
+    const showCloseIcon = boolean('showCloseIcon', true);
+    const size = select('size', OptionsHelper.sizesFull, OptionsHelper.sizesFull[0]);
+    const stickyFormFooter = boolean('stickyFormFooter', false);
+    const open = boolean('open', false);
+
+    return (
+      <Alert
+        onCancel={ handleCancel }
+        title={ title }
+        enableBackgroundUI={ enableBackgroundUI }
+        disableEscKey={ disableEscKey }
+        ariaRole={ ariaRole }
+        height={ height }
+        showCloseIcon={ showCloseIcon }
+        size={ size }
+        stickyFormFooter={ stickyFormFooter }
+        subtitle={ subtitle }
+        open={ open }
+      >
+        {children}
+      </Alert>
+    );
+  }, {
+    notes: { markdown: notes }
+  })
+  .add('with button', () => {
     const title = text('title', 'Attention');
     const subtitle = text('subtitle', '');
     const children = text('children', 'This is an example of a alert.');
@@ -87,21 +68,25 @@ storiesOf('Alert', module)
     const stickyFormFooter = boolean('stickyFormFooter', false);
 
     return (
-      <Wrapper
-        onCancel={ handleCancel }
-        title={ title }
-        enableBackgroundUI={ enableBackgroundUI }
-        disableEscKey={ disableEscKey }
-        ariaRole={ ariaRole }
-        height={ height }
-        showCloseIcon={ showCloseIcon }
-        size={ size }
-        stickyFormFooter={ stickyFormFooter }
-        subtitle={ subtitle }
-        open={ store.get('open') }
-      >
-        {children}
-      </Wrapper>
+      <State store={ store }>
+        <Button key='button' onClick={ handleOpen }>Open Preview</Button>
+        <Alert
+          key='alert'
+          onCancel={ handleCancel }
+          title={ title }
+          enableBackgroundUI={ enableBackgroundUI }
+          disableEscKey={ disableEscKey }
+          ariaRole={ ariaRole }
+          height={ height }
+          showCloseIcon={ showCloseIcon }
+          size={ size }
+          stickyFormFooter={ stickyFormFooter }
+          subtitle={ subtitle }
+          open={ store.get('open') }
+        >
+          {children}
+        </Alert>
+      </State>
     );
   }, {
     notes: { markdown: notes }
