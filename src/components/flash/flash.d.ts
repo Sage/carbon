098eb1b92c5f3fc3c328eb498c9e06169f1bfff5
Bence Laky <bence.laky@sage.com>
2019-02-18 21:22:22 +0100

    ts-doc comments added to flash.d.ts file

--- src/components/flash/flash.d.ts
+++ src/components/flash/flash.d.ts
@@ -1,11 +1,32 @@
 import * as React from 'react';
 
 export interface FlashProps {
-  as?: flashTypes;
-  onDismiss?: (...args: any[]) => any;
-  message?: string;
   className?: string;
+  /** Sets the theme of the notification */
+
+  as?: flashTypes;
+  /** A callback for when the notification is dismissed. You can use this prop to close the notification */
+  onDismiss?: (...args: any[]) => any;
+
+  /**
+   * The message provided to the flash component. This can be built in multiple formats e.g.
+   * A string: 'Alert' Array: ['Alert One', 'Alert Two']
+   * An object with description:
+   *   { description: 'Alert' }
+   * An object with key/value pair:
+   *   { first_name: 'is required', last_name: 'is required' }
+   * An object with description & nested key/value pairs:
+   *   { description: { first_name: 'is required', last_name: 'is required' } }
+   */
+  message?: string | string[] | { description: string } | object;
+
+  /** A boolean to control the open/closed state of the notification */
+
   open?: boolean;
+  /**
+   * Sets the time in Milliseconds the flash remains on the screen. After the timeout it will call the onDimiss
+   * callback. This will remove the close icon when set.
+   */
   timeout?: number;
 }
 
