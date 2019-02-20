0208ec5beba7a0735ced9994fbf8b0f7ea693b28
jgrabkowski <jgrabkowski@minijgrabkowski.pgs-soft.com>
2019-02-20 10:56:48 +0100

    refactor ico toggle validation css moved to validated-input-icon-toggle

--- src/components/validated-input-icon-toggle/validated-input-icon-toggle.js
+++ src/components/validated-input-icon-toggle/validated-input-icon-toggle.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import InputIconToggle from '../input-icon-toggle';
+import './validated-input-icon-toggle.scss';
 
 /**
  * An ValidatedInputIconToggle Component.
