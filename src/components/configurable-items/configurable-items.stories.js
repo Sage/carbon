913a0cf0ee4974edad6e599b5272fb34aa740e04
Michal Maciorowski (PGS Software) <mmaciorowski@pgs-soft.com>
2019-02-26 11:40:03 +0100

    FE-1260 - removes unnecessary info

--- src/components/configurable-items/configurable-items.stories.js
+++ src/components/configurable-items/configurable-items.stories.js
@@ -1,86 +1,94 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { } from '@storybook/addon-knobs';
-import { State, Store } from "@sambego/storybook-state";
+import { State, Store } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
 import notes from './notes.md';
-import { ConfigurableItems, ConfigurableItemRow } from './configurable-items.js'
+import { ConfigurableItems, ConfigurableItemRow } from './configurable-items.js';
 
 const store = new Store(
-    {
-        configurableItemsData: [
-            {
-                enabled: true,
-                locked: false,
-                name: 'test 1'
-            },
-            {
-                enabled: true,
-                locked: false,
-                name: 'test 2'
-            },
-            {
-                enabled: true,
-                locked: false,
-                name: 'test 3'
-            }
-        ]
-    }
+  {
+    configurableItemsData: [
+      {
+        enabled: true,
+        locked: false,
+        name: 'test 1',
+        key: '1'
+      },
+      {
+        enabled: true,
+        locked: false,
+        name: 'test 2',
+        key: '2'
+      },
+      {
+        enabled: true,
+        locked: false,
+        name: 'test 3',
+        key: '3'
+      }
+    ]
+  }
 );
 const handleDrag = (draggedIndex, hoveredIndex) => {
-    const { configurableItemsData } = store.state;
-    const draggedItem = configurableItemsData[draggedIndex];
+  const { configurableItemsData } = store.state;
+  const draggedItem = configurableItemsData[draggedIndex];
 
-    configurableItemsData[draggedIndex] = configurableItemsData[hoveredIndex];
-    configurableItemsData[hoveredIndex] = draggedItem;
+  configurableItemsData[draggedIndex] = configurableItemsData[hoveredIndex];
+  configurableItemsData[hoveredIndex] = draggedItem;
 
-    store.set('configurableItemsData', configurableItemsData);
-    action('dragged')();
+  store.set('configurableItemsData', configurableItemsData);
+  action('dragged')();
 };
-const handleChange = (rowIndex) => () => {
-    const { configurableItemsData } = store.state;
+const handleChange = rowIndex => () => {
+  const { configurableItemsData } = store.state;
 
-    configurableItemsData[rowIndex].enabled = !configurableItemsData[rowIndex].enabled;
+  configurableItemsData[rowIndex].enabled = !configurableItemsData[rowIndex].enabled;
 
-    store.set('configurableItemsData', configurableItemsData);
-    action('changed')();
+  store.set('configurableItemsData', configurableItemsData);
+  action('changed')();
 };
-const handleSave = () => {
-    event.preventDefault();
-    event.stopPropagation();
+const handleSave = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
 
-    action('saved')();
+  action('saved')();
 };
-const rows = (data) => data.map((column, rowIndex) => {
-    return (
-        <ConfigurableItemRow
-            enabled={column.enabled}
-            key={rowIndex}
-            locked={column.locked}
-            name={column.name}
-            rowIndex={rowIndex}
-            onChange={handleChange(rowIndex)}
-        />
-    );
+const rows = data => data.map((column, rowIndex) => {
+  return (
+    <ConfigurableItemRow
+      enabled={ column.enabled }
+      key={ column.key }
+      locked={ column.locked }
+      name={ column.name }
+      rowIndex={ rowIndex }
+      onChange={ handleChange(rowIndex) }
+    />
+  );
 });
 
 storiesOf('ConfigurableItems', module)
-    .add('default', () => {
-        return (
-            <ConfigurableItems
-                onDrag={handleDrag}
-                onCancel={action('canceled')}
-                onReset={action('reset')}
-                onSave={handleSave}
-            >
-                <State store={store}>
-                    {state => [
-                        rows(state.configurableItemsData)
-                    ]}
-                </State>
-            </ConfigurableItems>
-        );
-    },
-        {
-            notes: { markdown: notes }
-        });
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  })
+  .add('default', () => {
+    return (
+      <ConfigurableItems
+        onDrag={ handleDrag }
+        onCancel={ action('canceled') }
+        onReset={ action('reset') }
+        onSave={ handleSave }
+      >
+        <State store={ store }>
+          {state => [
+            rows(state.configurableItemsData)
+          ]}
+        </State>
+      </ConfigurableItems>
+    );
+  },
+  {
+    notes: { markdown: notes }
+  });
