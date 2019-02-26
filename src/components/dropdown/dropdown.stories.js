630b5861f4df889169499a93f866dfe61323baf2
dquilter <david@kyan.com>
2019-02-26 14:29:02 +0000

    Adds Storybook Component for Dropdown Component

---
+++ src/components/dropdown/dropdown.stories.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, select, boolean, number } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { State, Store } from '@sambego/storybook-state';
+import OptionsHelper from '../../utils/helpers/options-helper';
+import ImmutableHelper from '../../utils/helpers/immutable';
+import notes from './notes.md';
+import Dropdown from './dropdown';
+import { Map } from 'immutable';
+
+const store = new Store({
+  value: ''
+});
+
+const onChange = (evt) => {
+  store.set({ value: evt.target.value });
+  action('change')();
+};
+
+storiesOf('Dropdown', module)
+  .add('default', () => {
+    const autoFocus = boolean('autoFocus', false);
+    const cacheVisibleValue = boolean('cacheVisibleValue', true);
+    const disabled = boolean('disabled', false);
+    const name = text('name', 'Name');
+    const readOnly = boolean('readOnly', false);
+    const timeToDisappear = number('timeToDisappear', 0);
+    const label = text('label', 'Dropdown Label');
+    const labelInline = boolean('labelInline', true);
+    const labelWidth = labelInline ? text('labelWidth', '') : null;
+    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : null;
+    const labelHelp = text('labelHelp', 'This is help text');
+    const inputWidth = text('inputWidth', '');
+    const fieldHelp = text('fieldHelp', 'This is field help text');
+    const fieldHelpInline = boolean('fieldHelpInline', false);
+    const options = ImmutableHelper.parseJSON([
+      {
+        id: 1, name: "Orange"
+      }, {
+        id: 2, name: "Blue"
+      }
+    ]);
+
+    return (
+      <State store={ store }>
+        <Dropdown 
+          autoFocus={autoFocus}
+          cacheVisibleValue={cacheVisibleValue}
+          disabled={disabled}
+          name={name}
+          readOnly={readOnly}
+          timeToDisappear={timeToDisappear}
+          label={label}
+          labelInline={labelInline}
+          labelWidth={labelWidth}
+          labelAlign={labelAlign}
+          labelHelp={labelHelp}
+          inputWidth={inputWidth}
+          fieldHelp={fieldHelp}
+          fieldHelpInline={fieldHelpInline}
+          options={options}
+          onChange={onChange}
+          value={store.get('value')}
+        />
+      </State>
+    );
+  }, {
+    notes: { markdown: notes }
+  });
---
+++ src/components/dropdown/notes.md
@@ -0,0 +1,12 @@
+# Designer Notes
+- Behaves like a simple browser menu control - the user can select an item using the mouse or keyboard.
+- Useful to show more than about 5 options.
+- Use placeholder content like ‘Please select…’ to make it clear to the user that the Dropdown is unset.
+- Consider a ‘smart default’ selection, based on what your user is likely to choose. But, users may well leave the defaults in place, so make sure any consequences are easy to undo, and not harmful.
+
+# Related Components
+- Filtering menu options to find the right one? [Try Dropdown Filter](/components/dropdown-filter "Try Dropdown Filter").
+- Adding a new option within the menu? [Try Dropdown Filter](/components/dropdown-filter "Try Dropdown Filter").
+- Choosing one option from a shorter list? [Try Radio Button](/components/radio-button "Try Radio Button").
+- Choosing more than one option? [Try Checkbox](/components/checkbox "Try Checkbox").
+- Choosing one option from a highly visible range? [Try Button Toggle](/components/button-toggle "Try Button Toggle").
