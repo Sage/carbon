0eb1cd94985866917d85b20215b16fd666d2783f
grabkowski <jakub@grabkowski.com>
2019-02-27 18:52:13 +0100

    Change placed label and label help inside form field context

--- src/__experimental__/components/input-label/input-label.component.js
+++ src/__experimental__/components/input-label/input-label.component.js
@@ -2,78 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Help from '../../../components/help';
 import Label from './input-label.style';
+import { FormFieldContext } from '../form-field';
 
 /**
- * InputLabel decorator.
+ * InputLabel component.
  *
  * This decorator provides HTML for input labels.
  *
- * == How to use InputLabel decorator in a component:
+ * == How to use InputLabel component:
  *
  * In your file:
  *
- *   import InputLabel from 'carbon-react/lib/utils/decorators/input-label';
- *
- * To use the decorator, wrap your component with it:
- *
- *   const MyComponent = InputLabel(
- *   class MyComponent extends React.Component {
- *     ...
- *   })
- *
- * In the render method for your component, you can now output the HTML:
- *
- *   render() {
- *     return (
- *       <div>
- *         { this.labelHTML() }
- *         <input />
- *       </div>
- *     );
- *   }
- *
- * The label decorator adds additional props to your component for:
- *
- *  * `label` - either a string or false to turn the label off
- *  * `labelInline` - pass true to format the input/label inline
- *  * `labelWidth` - pass a percentage to define the width of the label when it
- *  is displayed inline.
- *  * `inputWidth` - pass a percentage to define the width of the input when it
- *  is displayed inline.
+ *   import InputLabel from 'carbon-react/lib/components/input-label';
  *
  * @method InputLabel
- * @param {Class} ComposedComponent class to decorate
- * @return {Object} Decorated Component
  */
 const InputLabel = (props) => {
   const {
     label,
     labelHelp,
-    isInline,
-    inputId
+    labelInline,
+    labelWidth,
+    labelAlignRight
   } = props;
 
-  function renderLabelHelp() {
-    if (!labelHelp) return null;
-
-    return (
-      <Help>
-        { labelHelp }
-      </Help>
-    );
-  }
-
   return (
-    <Label
-      htmlFor={ inputId }
-      data-element='label'
-      isInline={ isInline }
-    >
-      { label }
-      { renderLabelHelp() }
-    </Label>
+    <FormFieldContext.Consumer>
+      { context => (
+        <Label
+          htmlFor={ context.inputId }
+          data-element='label'
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          labelAlignRight={ labelAlignRight }
+        >
+          { label }
+          { renderLabelHelp(labelHelp) }
+        </Label>
+      )}
+    </FormFieldContext.Consumer>
   );
 };
+
+function renderLabelHelp(labelHelp) {
+  if (!labelHelp) return null;
+
+  return (
+    <Help>
+      { labelHelp }
+    </Help>
+  );
+}
 
 InputLabel.propTypes = {
   /**
@@ -87,11 +66,15 @@ InputLabel.propTypes = {
   /**
    * Position of the label
    */
-  isInline: PropTypes.bool,
+  labelInline: PropTypes.bool,
   /**
-   * Id of the input described by the label
+   * Width of the inline label
    */
-  inputId: PropTypes.string.isRequired
+  labelWidth: PropTypes.number,
+  /**
+   * Text position of the inline label
+   */
+  labelAlignRight: PropTypes.bool
 };
 
 export default InputLabel;
--- src/__experimental__/components/input-label/input-label.style.js
+++ src/__experimental__/components/input-label/input-label.style.js
@@ -8,64 +8,11 @@ const Label = styled.label`
   cursor: pointer;
   font-weight: bold;
   padding: 6px;
-  ${props => props.isInline && `
-    width: 30%;
+  ${({ labelInline, labelWidth, labelAlignRight }) => labelInline && `
+    box-sizing: border-box;
+    width: ${labelWidth ? `${labelWidth}%` : '30%'};
+    text-align: ${labelAlignRight ? 'right' : 'left'};
   `}
 `;
 
 export default Label;
-
-/*
-@import "./../../../style-config/colors";
-@import "./../../../style-config/general";
-
-.common-input__label {
-  line-height: 14px;
-  margin: 0 0 8px 0;
-  padding: 0 6px;
-  vertical-align: middle;
-
-  .carbon-help {
-    top: -2px;
-    margin-bottom: -2px; // required to ensure the input maintains it's position
-  }
-}
-
-.common-input--label-inline {
-  margin-left: -($grid-margin);
-
-  .common-input__help-text {
-    margin-left: 30%;
-    padding-left: $grid-margin;
-  }
-}
-
-.common-input__label--inline {
-  box-sizing: border-box;
-  display: inline-block;
-  margin: 10px 0 0;
-  padding: 0 0 0 $grid-margin;
-  width: 30%;
-  vertical-align: top;
-
-  ~ .common-input__field {
-    box-sizing: border-box;
-    display: inline-block;
-    padding-left: $grid-margin;
-    width: 70%;
-  }
-}
-
-.common-input__label--align-right {
-  text-align: right;
-}
-
-.common-input__help-text {
-  color: $grey-dark-blue-80;
-  display: block;
-  line-height: 20px;
-  margin-left: 6px;
-  margin-top: 5px;
-  white-space: pre-wrap;
-}
-*/
