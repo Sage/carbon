c082608fda2783cdc859df10a308f55d744309cb
dquilter <david@kyan.com>
2019-02-27 11:52:13 +0000

    Store Added for Storybook Drag and Drop

--- src/components/drag-and-drop/drag-and-drop.stories.js
+++ src/components/drag-and-drop/drag-and-drop.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import { State, Store } from '@sambego/storybook-state';
 import notes from './notes.md';
 import { DraggableContext } from './drag-and-drop';
 import {
@@ -11,30 +12,33 @@ import {
   TableCell
 } from '../table';
 
-const dndData = [{
-  id: '0',
-  name: 'UK'
-}, {
-  id: '1',
-  name: 'Germany'
-}, {
-  id: '2',
-  name: 'China'
-}, {
-  id: '3',
-  name: 'US'
-}]
+const store = new Store({
+  dndData: [{
+    id: '0',
+    name: 'UK'
+  }, {
+    id: '1',
+    name: 'Germany'
+  }, {
+    id: '2',
+    name: 'China'
+  }, {
+    id: '3',
+    name: 'US'
+  }]
+});
 
 const onDrag = (originalIndex, newIndex) => {
-  console.log(originalIndex, newIndex)
   action('drag')();
-  const sortedItem = dndData.slice(originalIndex);
-  dndData.splice(originalIndex, 1);
-  dndData.splice(newIndex, 0, sortedItem);
+  let array = store.get('dndData');
+  const sortedItem = array.slice(originalIndex);
+  array.splice(originalIndex, 1);
+  array.splice(newIndex, 0, sortedItem[0]);
+  store.set({'dndData': array});
 }
 
-const buildRows = () => (
-  dndData.map((row, index) => (
+const BuildRows = (props) => (
+  props.dndData.map((row, index) => (
     <TableRow key={ row.id } uniqueID={ row.id } index={ index }>
       <TableCell>{ row.name }</TableCell>
     </TableRow>
@@ -59,7 +63,9 @@ storiesOf('DraggableContext', module)
               </TableRow>
             </thead>
             <tbody>
-              {buildRows()}
+              <State store={ store }>
+                <BuildRows />
+              </State>
             </tbody>
           </Table>
         </div>
