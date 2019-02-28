5217969721fbf87d843c8f52af041e8e04d3347d
dquilter <david@kyan.com>
2019-02-28 09:47:42 +0000

    Adds Stories for Dropdown Create Methods

--- src/components/dropdown-filter-ajax/dropdown-filter-ajax.stories.js
+++ src/components/dropdown-filter-ajax/dropdown-filter-ajax.stories.js
@@ -17,10 +17,45 @@ const store = new Store({
   value: ''
 });
 
+const create = () => {
+    
+}
+
+// Shared Props
 const onChange = (evt) => {
   store.set({ value: evt.target.value });
   action('change')();
 };
+const autoFocus = boolean('autoFocus', false);
+const cacheVisibleValue = boolean('cacheVisibleValue', true);
+const disabled = boolean('disabled', false);
+const name = text('name', 'Name');
+const readOnly = boolean('readOnly', false);
+const timeToDisappear = number('timeToDisappear', 0);
+const label = text('label', 'Dropdown Label');
+const labelInline = boolean('labelInline', true);
+const labelWidth = labelInline ? text('labelWidth', '') : null;
+const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : null;
+const labelHelp = text('labelHelp', 'This is help text');
+const inputWidth = text('inputWidth', '');
+const fieldHelp = text('fieldHelp', 'This is field help text');
+const fieldHelpInline = boolean('fieldHelpInline', false);
+const createText = text('createText', '');
+const createIconType = text('createIconType', '');
+const suggest = boolean('suggest', false);
+const freetext = boolean('freetext', false);
+const path = text('path', '/');
+const acceptHeader = text('acceptHeader', 'application/json');
+const rowsPerRequest = text('rowsPerRequest', '25');
+const dataRequestTimeout = number('dataRequestTimeout', 500);
+const withCredentials = boolean('withCredentials', false);
+const options = ImmutableHelper.parseJSON([
+  {
+    id: 1, name: 'Orange'
+  }, {
+    id: 2, name: 'Blue'
+  }
+]);
 
 storiesOf('DropdownFilterAjax', module)
   .addParameters({
@@ -29,39 +64,6 @@ storiesOf('DropdownFilterAjax', module)
     }
   })
   .add('default', () => {
-    const autoFocus = boolean('autoFocus', false);
-    const cacheVisibleValue = boolean('cacheVisibleValue', true);
-    const disabled = boolean('disabled', false);
-    const name = text('name', 'Name');
-    const readOnly = boolean('readOnly', false);
-    const timeToDisappear = number('timeToDisappear', 0);
-    const label = text('label', 'Dropdown Label');
-    const labelInline = boolean('labelInline', true);
-    const labelWidth = labelInline ? text('labelWidth', '') : null;
-    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : null;
-    const labelHelp = text('labelHelp', 'This is help text');
-    const inputWidth = text('inputWidth', '');
-    const fieldHelp = text('fieldHelp', 'This is field help text');
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    // TODO: Storybook needs a boolean, but the value is actually a function
-    // const create = boolean('create', false);
-    const createText = text('createText', '');
-    const createIconType = text('createIconType', '');
-    const suggest = boolean('suggest', false);
-    const freetext = boolean('freetext', false);
-    const path = text('path', '/');
-    const acceptHeader = text('acceptHeader', 'application/json');
-    const rowsPerRequest = text('rowsPerRequest', '25');
-    const dataRequestTimeout = number('dataRequestTimeout', 500);
-    const withCredentials = boolean('withCredentials', false);
-    const options = ImmutableHelper.parseJSON([
-      {
-        id: 1, name: 'Orange'
-      }, {
-        id: 2, name: 'Blue'
-      }
-    ]);
-
     return (
       <State store={ store }>
         <DropdownFilterAjax
@@ -83,7 +85,44 @@ storiesOf('DropdownFilterAjax', module)
           createIconType={ createIconType }
           suggest={ suggest }
           freetext={ freetext }
-          // create={ create }
+          path={ path }
+          acceptHeader={ acceptHeader }
+          rowsPerRequest={ rowsPerRequest }
+          getCustomHeaders={ () => true }
+          dataRequestTimeout={ dataRequestTimeout }
+          withCredentials={ withCredentials }
+          options={ options }
+          onChange={ onChange }
+          value={ store.get('value') }
+        />
+      </State>
+    );
+  }, {
+    notes: { markdown: notes }
+  })
+  .add('withCreate', () => {
+    return (
+      <State store={ store }>
+        <DropdownFilterAjax
+          autoFocus={ autoFocus }
+          cacheVisibleValue={ cacheVisibleValue }
+          disabled={ disabled }
+          name={ name }
+          readOnly={ readOnly }
+          timeToDisappear={ timeToDisappear }
+          label={ label }
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          labelAlign={ labelAlign }
+          labelHelp={ labelHelp }
+          inputWidth={ inputWidth }
+          fieldHelp={ fieldHelp }
+          fieldHelpInline={ fieldHelpInline }
+          createText={ createText }
+          createIconType={ createIconType }
+          suggest={ suggest }
+          freetext={ freetext }
+          create={ create }
           path={ path }
           acceptHeader={ acceptHeader }
           rowsPerRequest={ rowsPerRequest }
--- src/components/dropdown-filter/dropdown-filter.stories.js
+++ src/components/dropdown-filter/dropdown-filter.stories.js
@@ -17,10 +17,38 @@ const store = new Store({
   value: ''
 });
 
+const create = (evt, component) => component.state.filter;
+
+// Shared Props
 const onChange = (evt) => {
   store.set({ value: evt.target.value });
   action('change')();
 };
+const autoFocus = boolean('autoFocus', false);
+const cacheVisibleValue = boolean('cacheVisibleValue', true);
+const disabled = boolean('disabled', false);
+const name = text('name', 'Name');
+const readOnly = boolean('readOnly', false);
+const timeToDisappear = number('timeToDisappear', 0);
+const label = text('label', 'Dropdown Label');
+const labelInline = boolean('labelInline', true);
+const labelWidth = labelInline ? text('labelWidth', '') : null;
+const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : null;
+const labelHelp = text('labelHelp', 'This is help text');
+const inputWidth = text('inputWidth', '');
+const fieldHelp = text('fieldHelp', 'This is field help text');
+const fieldHelpInline = boolean('fieldHelpInline', false);
+const createText = text('createText', '');
+const createIconType = text('createIconType', '');
+const suggest = boolean('suggest', false);
+const freetext = boolean('freetext', false);
+const options = ImmutableHelper.parseJSON([
+  {
+    id: 1, name: 'Orange'
+  }, {
+    id: 2, name: 'Blue'
+  }
+]);
 
 storiesOf('DropdownFilter', module)
   .addParameters({
@@ -29,34 +57,6 @@ storiesOf('DropdownFilter', module)
     }
   })
   .add('default', () => {
-    const autoFocus = boolean('autoFocus', false);
-    const cacheVisibleValue = boolean('cacheVisibleValue', true);
-    const disabled = boolean('disabled', false);
-    const name = text('name', 'Name');
-    const readOnly = boolean('readOnly', false);
-    const timeToDisappear = number('timeToDisappear', 0);
-    const label = text('label', 'Dropdown Label');
-    const labelInline = boolean('labelInline', true);
-    const labelWidth = labelInline ? text('labelWidth', '') : null;
-    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : null;
-    const labelHelp = text('labelHelp', 'This is help text');
-    const inputWidth = text('inputWidth', '');
-    const fieldHelp = text('fieldHelp', 'This is field help text');
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    // TODO: Storybook needs a boolean, but the value is actually a function
-    // const create = boolean('create', false);
-    const createText = text('createText', '');
-    const createIconType = text('createIconType', '');
-    const suggest = boolean('suggest', false);
-    const freetext = boolean('freetext', false);
-    const options = ImmutableHelper.parseJSON([
-      {
-        id: 1, name: 'Orange'
-      }, {
-        id: 2, name: 'Blue'
-      }
-    ]);
-
     return (
       <State store={ store }>
         <DropdownFilter
@@ -78,7 +78,38 @@ storiesOf('DropdownFilter', module)
           createIconType={ createIconType }
           suggest={ suggest }
           freetext={ freetext }
-          // create={create}
+          options={ options }
+          onChange={ onChange }
+          value={ store.get('value') }
+        />
+      </State>
+    );
+  }, {
+    notes: { markdown: notes }
+  })
+  .add('withCreate', () => {
+    return (
+      <State store={ store }>
+        <DropdownFilter
+          autoFocus={ autoFocus }
+          cacheVisibleValue={ cacheVisibleValue }
+          disabled={ disabled }
+          name={ name }
+          readOnly={ readOnly }
+          timeToDisappear={ timeToDisappear }
+          label={ label }
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          labelAlign={ labelAlign }
+          labelHelp={ labelHelp }
+          inputWidth={ inputWidth }
+          fieldHelp={ fieldHelp }
+          fieldHelpInline={ fieldHelpInline }
+          createText={ createText }
+          createIconType={ createIconType }
+          suggest={ suggest }
+          freetext={ freetext }
+          create={create}
           options={ options }
           onChange={ onChange }
           value={ store.get('value') }
