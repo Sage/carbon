d05f8883f9b98a423e736e9e00a629418d029f8a
samhinton88 <sam.hinton88@gmail.com>
2019-02-28 12:17:16 +0000

    fix linting

--- src/style/themes/classic/classic-theme.config.js
+++ src/style/themes/classic/classic-theme.config.js
@@ -5,7 +5,7 @@ export default (palette) => {
         base: palette.productGreen,
         primary: palette.productGreenShade(21),
         secondary: palette.productGreenShade(41),
-        tertiary: palette.productGreenShade(61),
+        tertiary: palette.productGreenShade(61)
       }
     }
   );
