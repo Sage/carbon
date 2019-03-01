d68147b39e39b47899a7c436dbd82227cae385fd
dquilter <david@kyan.com>
2019-03-01 09:54:36 +0000

    Storybook Story for Help Component

---
+++ src/components/help/help.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, select } from '@storybook/addon-knobs';
+import OptionsHelper from '../../utils/helpers/options-helper';
+import notes from './notes.md';
+import Help from './help';
+
+storiesOf('Help', module)
+  .add('default', () => {
+    const children = text('children', 'This is help text');
+    const tooltipPosition = children ? select(
+      'tooltipPosition',
+      OptionsHelper.positions,
+      Help.defaultProps.tooltipPosition
+    ) : undefined;
+    const tooltipAlign = children ? select(
+      'tooltipAlign',
+      OptionsHelper.alignAroundEdges,
+      Help.defaultProps.tooltipAlign
+    ) : undefined;
+    const href = text('href', '');
+
+    return (
+      <Help
+        tooltipPosition={ tooltipPosition }
+        tooltipAlign={ tooltipAlign }
+        href={ href }
+      >
+        {children}
+      </Help>
+    );
+  }, {
+    notes: { markdown: notes }
+  });
