b160d3750b1a9fce3605b10f79ca43016a242a40
dquilter <david@kyan.com>
2019-03-01 13:50:27 +0000

    Storybook Story for InlineInputs

---
+++ src/components/inline-inputs/inline-inputs.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+import InlineInputs from './inline-inputs';
+import Textbox from '../textbox';
+
+storiesOf('InlineInputs', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [Textbox]
+    }
+  })
+  .add('default', () => {
+    const label = text('label', 'Inline Inputs');
+
+    return (
+      <InlineInputs
+        label={ label }
+      >
+        <Textbox value="" />
+        <Textbox value="" />
+      </InlineInputs>
+    );
+  });
