3407eacb43cd97f228abc3727a8a98794e3e5265
dquilter <david@kyan.com>
2019-03-01 11:13:43 +0000

    Storybook Story for i18n Component

---
+++ src/components/i18n/i18n.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean } from '@storybook/addon-knobs';
+import i18n from 'i18n-js';
+import notes from './notes.md';
+import I18nComponent from './i18n';
+
+i18n.translations = {
+  en: {
+    my: {
+      example: '# My __example__ translation.'
+    }
+  }
+};
+
+storiesOf('I18nComponent', module)
+  .add('default', () => {
+    const markdown = boolean('markdown', true);
+    const inline = markdown ? boolean('inline', I18nComponent.defaultProps.inline) : undefined;
+    const scope = text('scope', 'my.example');
+    return (
+      <I18nComponent
+        markdown={ markdown }
+        inline={ inline }
+        scope={ scope }
+      />
+    );
+  }, {
+    notes: { markdown: notes }
+  });
