665981ed306b493dd9f6a3b56f568a8027b5184a
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-03-04 16:12:08 +0100

    FE-1260 - correction for checkbox and decimal stories

--- src/components/decimal/decimal.stories.js
+++ src/components/decimal/decimal.stories.js
@@ -3,38 +3,80 @@ import { storiesOf } from '@storybook/react';
 import {
   select, number, text, boolean
 } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { State, Store } from '@sambego/storybook-state';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './notes.md';
 import Decimal from './decimal.js';
 
+const store = new Store({
+  value: '0'
+});
+const handleChange = (event) => {
+  action('changed')(event);
+
+  store.set({ value: event.target.value });
+};
+
 storiesOf('Decimal', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  })
   .add('default', () => {
     const align = select('align', OptionsHelper.alignBinary, Decimal.defaultProps.align);
-    const precision = number('precision', Decimal.defaultProps.precision);
-    const timeToDisappear = number('timeToDisappear', 0);
+    const precision = number('precision', Decimal.defaultProps.precision, {
+      range: true,
+      min: 0,
+      max: 20,
+      step: 1
+    });
+    const timeToDisappear = number('timeToDisappear', 0, {
+      range: true,
+      min: 0,
+      max: 150,
+      step: 10
+    });
     const label = text('label', 'Example Decimal');
     const labelHelp = text('labelHelp', 'This text provides more information for the label.');
-    const inputWidth = number('inputWidth', 0);
+    const inputWidth = number('inputWidth', 0, {
+      range: true,
+      min: 0,
+      max: 150,
+      step: 10
+    });
     const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
     const fieldHelpInline = boolean('fieldHelpInline', false);
     const labelInline = boolean('labelInline', false);
-    const labelWidth = number('labelWidth', 0);
-    const labelAlign = select('labelAlign', OptionsHelper.alignBinary, OptionsHelper.alignBinary[0]);
+    const labelWidth = labelInline ? number('labelWidth', 0, {
+      range: true,
+      min: 0,
+      max: 150,
+      step: 10
+    }) : undefined;
+    const labelAlign = labelInline ? select(
+      'labelAlign', OptionsHelper.alignBinary, OptionsHelper.alignBinary[0]
+    ) : undefined;
 
     return (
-      <Decimal
-        align={ align }
-        precision={ precision }
-        timeToDisappear={ timeToDisappear }
-        label={ label }
-        labelHelp={ labelHelp }
-        inputWidth={ inputWidth }
-        fieldHelp={ fieldHelp }
-        fieldHelpInline={ fieldHelpInline }
-        labelInline={ labelInline }
-        labelWidth={ labelWidth }
-        labelAlign={ labelAlign }
-      />
+      <State store={ store }>
+        <Decimal
+          onChange={ handleChange }
+          align={ align }
+          precision={ precision }
+          timeToDisappear={ timeToDisappear }
+          label={ label }
+          labelHelp={ labelHelp }
+          inputWidth={ inputWidth }
+          fieldHelp={ fieldHelp }
+          fieldHelpInline={ fieldHelpInline }
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          labelAlign={ labelAlign }
+          value={ store.get('value') }
+        />
+      </State>
     );
   }, {
     notes: { markdown: notes }
