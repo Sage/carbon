23b10e02c8a1efe57d53b926cfbf2ffad32648f2
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-03-05 16:53:58 +0100

    FE-1260 - open button for confirm component

--- src/components/confirm/confirm.stories.js
+++ src/components/confirm/confirm.stories.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
-  text, boolean, select
+  text, boolean, select, button
 } from '@storybook/addon-knobs';
+import { State, Store } from '@sambego/storybook-state';
 import { action } from '@storybook/addon-actions';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './notes.md';
 import Confirm from './confirm.js';
 
+const store = new Store({
+  open: false
+});
+const handleCancel = () => {
+  action('cancel')();
+  store.set({ open: false });
+};
+const handleOpen = () => {
+  action('open')();
+  store.set({ open: true });
+};
+
 storiesOf('Confirm', module)
   .add('default', () => {
+    button('open', handleOpen);
     const children = text('children', 'This is an example of a confirm.');
     const title = text('title', 'Are you sure?');
-    const open = boolean('open', true);
     const enableBackgroundUI = boolean('enableBackgroundUI', false);
     const disableEscKey = boolean('disableEscKey', false);
     const ariaRole = text('ariaRole', Confirm.defaultProps.ariaRole);
@@ -26,25 +39,27 @@ storiesOf('Confirm', module)
     const cancelLabel = text('cancelLabel', '');
 
     return (
-      <Confirm
-        title={ title }
-        open={ open }
-        enableBackgroundUI={ enableBackgroundUI }
-        disableEscKey={ disableEscKey }
-        ariaRole={ ariaRole }
-        height={ height }
-        subtitle={ subtitle }
-        size={ size }
-        showCloseIcon={ showCloseIcon }
-        autoFocus={ autoFocus }
-        stickyFormFooter={ stickyFormFooter }
-        confirmLabel={ confirmLabel }
-        cancelLabel={ cancelLabel }
-        onConfirm={ action('confirm') }
-        onCancel={ action('cancel') }
-      >
-        { children }
-      </Confirm>
+      <State store={ store }>
+        <Confirm
+          title={ title }
+          open={ store.get('open') }
+          enableBackgroundUI={ enableBackgroundUI }
+          disableEscKey={ disableEscKey }
+          ariaRole={ ariaRole }
+          height={ height }
+          subtitle={ subtitle }
+          size={ size }
+          showCloseIcon={ showCloseIcon }
+          autoFocus={ autoFocus }
+          stickyFormFooter={ stickyFormFooter }
+          confirmLabel={ confirmLabel }
+          cancelLabel={ cancelLabel }
+          onConfirm={ () => action('confirm')() }
+          onCancel={ handleCancel }
+        >
+          { children }
+        </Confirm>
+      </State>
     );
   }, {
     notes: { markdown: notes }
