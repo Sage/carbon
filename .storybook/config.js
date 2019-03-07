2a4c3971939c43cf51b805af74d486a4f71e8ee0
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-03-07 16:14:32 +0100

    FE-1303 - adds info, notes, a11y addons to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+import { withA11y } from '@storybook/addon-a11y';
 import AppLayout from 'helpers/components/app-layout/app-layout';
 
-
 const req = require.context('../src/components', true, /\.stories\.js$/);
+const infoOptions = {
+  header: false,
+  inline: true
+};
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addParameters({
-  a11y: {
-    element: '#foo'
-  }
-  });
 addDecorator(withKnobs);
+addDecorator(withInfo(infoOptions));
+addDecorator(withA11y);
 
 // give all stories access to themes
 addDecorator(story => <AppLayout>{story()}</AppLayout>);
