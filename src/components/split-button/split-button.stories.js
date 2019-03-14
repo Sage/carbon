4b2eea9371c3ec71189394b46aa7351815ddc21b
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 10:44:40 +0100

    remove descriptors split-button

--- src/components/split-button/split-button.stories.js
+++ src/components/split-button/split-button.stories.js
@@ -5,27 +5,57 @@ import SplitButton from './split-button';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import Button from '../button';
 import notes from './notes.md';
+import { StoryHeader, StoryCode, StoryCodeBlock } from '../../../.storybook/style/storybook-info.styles';
 
-storiesOf('Split Button', module).add(
-  'default',
-  () => {
-    const as = select('as', OptionsHelper.themesBinary, SplitButton.defaultProps.as);
-    const dataElement = text('data-element');
-    const dataRole = text('data-role');
-    const disabled = boolean('disabled', SplitButton.defaultProps.disabled);
-    const textContent = text('text', 'Example Split Button');
+storiesOf('Split Button', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [Button]
+    }
+  })
+  .add(
+    'default',
+    () => {
+      const as = select('as', OptionsHelper.themesBinary, SplitButton.defaultProps.as);
+      const dataElement = text('data-element');
+      const dataRole = text('data-role');
+      const disabled = boolean('disabled', SplitButton.defaultProps.disabled);
+      const textContent = text('text', 'Example Split Button');
 
-    return (
-      <SplitButton
-        as={ as } data-element={ dataElement }
-        data-role={ dataRole } disabled={ disabled }
-        text={ textContent }
-      >
-        <Button>Example Button</Button>
-        <Button>Example Button</Button>
-        <Button>Example Button</Button>
-      </SplitButton>
-    );
-  },
-  { notes: { markdown: notes } }
-);
+      return (
+        <SplitButton
+          as={ as } data-element={ dataElement }
+          data-role={ dataRole } disabled={ disabled }
+          text={ textContent }
+        >
+          <Button>Example Button</Button>
+          <Button>Example Button</Button>
+          <Button>Example Button</Button>
+        </SplitButton>
+      );
+    },
+    {
+      info: {
+        text: (
+          <div>
+            <p> Split Button component </p>
+            <StoryHeader> Implementation</StoryHeader>
+
+            <p>Import the component:</p>
+            <StoryCode padded>
+              {'import SplitButton from "react-carbon/lib/components/split-button"'}
+            </StoryCode>
+
+            <p>To render a SplitButton (developer can add any buttons to dropdown):</p>
+            <StoryCodeBlock>
+              {'<SplitButton text="Main Button" onClick={ clickHandler }>'}
+              {'  <Button onClick="buttonClickHandler1">Button name 1</Button>'}
+              {'  <Button onClick="buttonClickHandler2">Button name 2</Button>'}
+              {'</SplitButton>'}
+            </StoryCodeBlock>
+          </div>
+        )
+      }
+    },
+    { notes: { markdown: notes } }
+  );
