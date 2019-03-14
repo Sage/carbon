f03398fe7d3dcc87d95c4234441f9c55da9c60d8
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 12:28:58 +0100

    update implementation describe in table storybook

--- src/components/table/table.stories.js
+++ src/components/table/table.stories.js
@@ -11,6 +11,7 @@ import {
 } from './table';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './notes.md';
+import { StoryHeader, StoryCode, StoryCodeBlock } from '../../../.storybook/style/storybook-info.styles';
 
 const store = new Store({
   sortOrder: 'asc',
@@ -60,59 +61,135 @@ const buildRows = (pageSizeFromKnobs) => {
   return rows;
 };
 
-storiesOf('Table', module).add(
-  'default',
-  () => {
-    const pageSize = text('pageSize', '5');
-    const selectable = boolean('selectable', false);
-    const highlightable = boolean('hightTable', false);
-    const shrink = boolean('shrink', false);
-    const caption = text('Caption', 'Country and Country Codes');
-    const totalRecords = text('totalRecords', '50');
-    const paginate = boolean('paginate', false);
-    const showPageSizeSelection = paginate && boolean('showPageSizeSelection', false);
-    const theme = select('theme', OptionsHelper.themesBinary, Table.defaultProps.theme);
+storiesOf('Table', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  }).add(
+    'default',
+    () => {
+      const pageSize = text('pageSize', '5');
+      const selectable = boolean('selectable', false);
+      const highlightable = boolean('hightTable', false);
+      const shrink = boolean('shrink', false);
+      const caption = text('Caption', 'Country and Country Codes');
+      const totalRecords = text('totalRecords', '50');
+      const paginate = boolean('paginate', false);
+      const showPageSizeSelection = paginate && boolean('showPageSizeSelection', false);
+      const theme = select('theme', OptionsHelper.themesBinary, Table.defaultProps.theme);
 
-    return (
-      <State store={ store }>
-        {state => (
-          <Table
-            actionToolbarChildren={ (context) => {
-              return [
-                <Button disabled={ context.disabled } key='single-action'>
+      return (
+        <State store={ store }>
+          {state => (
+            <Table
+              actionToolbarChildren={ (context) => {
+                return [
+                  <Button disabled={ context.disabled } key='single-action'>
                   Test Action
-                </Button>,
-                <MultiActionButton
-                  text='Actions' disabled={ context.disabled }
-                  key='multi-actions'
-                >
-                  <Button>foo</Button>
-                  <Button>bar</Button>
-                  <Button>qux</Button>
-                </MultiActionButton>
-              ];
-            } }
-            path='/countries'
-            sortOrder={ state.sortOrder }
-            sortedColumn={ state.sortedColumn }
-            caption={ caption }
-            currentPage={ state.currentPage }
-            shrink={ shrink }
-            highlightable={ highlightable }
-            pageSize={ pageSize }
-            selectable={ selectable }
-            paginate={ paginate }
-            actions={ { delete: { icon: 'bin' }, settings: { icon: 'settings' } } }
-            totalRecords={ totalRecords }
-            showPageSizeSelection={ showPageSizeSelection }
-            onChange={ handleChange }
-            theme={ theme }
-          >
-            {buildRows(pageSize)}
-          </Table>
-        )}
-      </State>
-    );
-  },
-  { notes: { markdown: notes } }
-);
+                  </Button>,
+                  <MultiActionButton
+                    text='Actions' disabled={ context.disabled }
+                    key='multi-actions'
+                  >
+                    <Button>foo</Button>
+                    <Button>bar</Button>
+                    <Button>qux</Button>
+                  </MultiActionButton>
+                ];
+              } }
+              path='/countries'
+              sortOrder={ state.sortOrder }
+              sortedColumn={ state.sortedColumn }
+              caption={ caption }
+              currentPage={ state.currentPage }
+              shrink={ shrink }
+              highlightable={ highlightable }
+              pageSize={ pageSize }
+              selectable={ selectable }
+              paginate={ paginate }
+              actions={ { delete: { icon: 'bin' }, settings: { icon: 'settings' } } }
+              totalRecords={ totalRecords }
+              showPageSizeSelection={ showPageSizeSelection }
+              onChange={ handleChange }
+              theme={ theme }
+            >
+              {buildRows(pageSize)}
+            </Table>
+          )}
+        </State>
+      );
+    }, {
+      info: {
+        text: (
+          <div>
+            <p>Table component</p>
+            <StoryHeader> Implementation</StoryHeader>
+
+            <p>Import the component:</p>
+            <StoryCode padded>
+              {'import { Table, TableRow, TableCell, TableHeader } from "carbon-react/lib/components/table";'}
+            </StoryCode>
+
+            <p>To render a Table:</p>
+            <StoryCodeBlock>
+              {'// map data to table rows'}
+              {'let tableRows = ('}
+              {'  this.props.data.map((datum, key) => {'}
+              {'    return ('}
+              {'      <TableRow>'}
+              {'        <TableCell>'}
+              {'          { datum.firstName }'}
+              {'        </TableCell>'}
+              {' '}
+              {'        <TableCell>'}
+              {'          { datum.lastName }'}
+              {'        </TableCell>'}
+              {'      </TableRow>'}
+              {'    );'}
+              {'  });'}
+              {');'}
+              {' '}
+              {'// prepend array of rows with a header row'}
+              {'tableRows.unshift('}
+              {'  <TableRow>'}
+              {'    <TableHeader>First Name</TableHeader>'}
+              {'    <TableHeader>Last Name</TableHeader>'}
+              {'  </TableRow>'}
+              {');'}
+              {' '}
+              {'// render the table with the table rows'}
+              {'<Table>'}
+              {'  { tableRows }'}
+              {'</Table>'}
+            </StoryCodeBlock>
+
+            <p>Pagination:</p>
+
+            <p>To add a pagination footer to the table you will need to pass some extra props to the table</p>
+
+            <StoryCodeBlock>
+              {'let sizeOptions = Immutable.fromJS([{ id: "10", name: 10 }, { id: "25", name: 25 }, { id: "50", name: 50 }]),'}
+              {' '}
+              {' <Table'}
+              {'  paginate // Show the pagination footer'}
+              {'  currentPage="1" // Required - Current visible page'}
+              {'  pageSize="10" // Required - Number of records to show per page'}
+              {'  totalRecords // Required - Total number of records'}
+              {'  showPageSizeSelection={ false } // Options  - Show page size selection'}
+              {'  pageSizeSelectionOptions={ sizeOptions } // Optional - Page Size Options'}
+              {'  thead={ TableRow } // Optional - A TableRow to be wrapped in <thead>'}
+              {'/>'}
+            </StoryCodeBlock>
+
+            <p>Sorting:</p>
+
+            <p> To enable column sorting, you will need to configure the
+              <StoryCode>{' <TableHeader /> '}</StoryCode> component.
+            </p>
+          </div>
+        )
+      }
+    },
+    { notes: { markdown: notes } }
+  );
