4f8a66fd610db3dc184d9e3bc2117201a4774e97
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 15:03:57 +0100

    update textarea storybook

--- src/components/textarea/textarea.stories.js
+++ src/components/textarea/textarea.stories.js
@@ -7,6 +7,7 @@ import { State, Store } from '@sambego/storybook-state';
 import Textarea from './textarea';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './notes.md';
+import { StoryHeader, StoryCode } from '../../../.storybook/style/storybook-info.styles';
 
 const store = new Store({
   value: ''
@@ -23,50 +24,71 @@ const rangeOptions = {
   step: 1
 };
 
-storiesOf('Textarea', module).add(
-  'default',
-  () => {
-    const { alignBinary } = OptionsHelper;
+storiesOf('Textarea', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  }).add(
+    'default',
+    () => {
+      const { alignBinary } = OptionsHelper;
 
-    const warnOverLimit = boolean('warnOverLimit', Textarea.defaultProp.warnOverLimit);
-    const labelInline = boolean('labelInline', false);
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    const expandable = boolean('expandable', Textarea.defaultProp.expandable);
-    const characterLimit = number('characterLimit', 50, rangeOptions);
-    const enforceCharacterLimit = characterLimit ? boolean(
-      'enforceCharacterLimit',
-      Textarea.defaultProp.enforceCharacterLimit
-    ) : undefined;
-    const inputWidth = number('inputWidth', 0, rangeOptions);
-    const labelWidth = labelInline ? number('labelWidth', 0, rangeOptions) : undefined;
-    const cols = number('cols', 0, rangeOptions);
-    const rows = number('rows', 0, rangeOptions);
-    const label = text('label', 'Example Textarea');
-    const labelHelp = text('labelHelp', 'This text provides more information for the label.');
-    const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
-    const labelAlign = labelInline ? select('labelAlign', alignBinary, alignBinary[0]) : undefined;
+      const warnOverLimit = boolean('warnOverLimit', Textarea.defaultProp.warnOverLimit);
+      const labelInline = boolean('labelInline', false);
+      const fieldHelpInline = boolean('fieldHelpInline', false);
+      const expandable = boolean('expandable', Textarea.defaultProp.expandable);
+      const characterLimit = number('characterLimit', 50, rangeOptions);
+      const enforceCharacterLimit = characterLimit ? boolean(
+        'enforceCharacterLimit',
+        Textarea.defaultProp.enforceCharacterLimit
+      ) : undefined;
+      const inputWidth = number('inputWidth', 0, rangeOptions);
+      const labelWidth = labelInline ? number('labelWidth', 0, rangeOptions) : undefined;
+      const cols = number('cols', 0, rangeOptions);
+      const rows = number('rows', 0, rangeOptions);
+      const label = text('label', 'Example Textarea');
+      const labelHelp = text('labelHelp', 'This text provides more information for the label.');
+      const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
+      const labelAlign = labelInline ? select('labelAlign', alignBinary, alignBinary[0]) : undefined;
 
-    return (
-      <State store={ store }>
-        <Textarea
-          warnOverLimit={ warnOverLimit }
-          labelInline={ labelInline }
-          labelAlign={ labelAlign }
-          enforceCharacterLimit={ enforceCharacterLimit }
-          characterLimit={ String(characterLimit) }
-          labelWidth={ labelWidth }
-          cols={ cols }
-          fieldHelpInline={ fieldHelpInline }
-          expandable={ expandable }
-          label={ label }
-          labelHelp={ labelHelp }
-          rows={ rows }
-          fieldHelp={ fieldHelp }
-          inputWidth={ inputWidth }
-          onChange={ handleChange }
-        />
-      </State>
-    );
-  },
-  { notes: { markdown: notes } }
-);
+      return (
+        <State store={ store }>
+          <Textarea
+            warnOverLimit={ warnOverLimit }
+            labelInline={ labelInline }
+            labelAlign={ labelAlign }
+            enforceCharacterLimit={ enforceCharacterLimit }
+            characterLimit={ String(characterLimit) }
+            labelWidth={ labelWidth }
+            cols={ cols }
+            fieldHelpInline={ fieldHelpInline }
+            expandable={ expandable }
+            label={ label }
+            labelHelp={ labelHelp }
+            rows={ rows }
+            fieldHelp={ fieldHelp }
+            inputWidth={ inputWidth }
+            onChange={ handleChange }
+          />
+        </State>
+      );
+    },
+    {
+      info: {
+        text: (
+          <div>
+            <p>Textarea component</p>
+
+            <StoryHeader> Implementation</StoryHeader>
+
+            <StoryCode padded>import Textarea from {'carbon-react/lib/components/textarea'}</StoryCode>
+
+            <p>To render a Textarea:</p>
+            <StoryCode padded>{'<Textarea name="myTextarea" />'}</StoryCode>
+          </div>
+        )
+      }
+    },
+    { notes: { markdown: notes } }
+  );
