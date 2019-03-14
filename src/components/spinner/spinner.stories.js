02205dd27b471a345151602a1ba30d51f33f76c3
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 16:08:41 +0100

    make changes in folder structure

--- src/components/spinner/spinner.stories.js
+++ src/components/spinner/spinner.stories.js
@@ -3,8 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import Spinner from './spinner';
-import notes from './notes.md';
-import { StoryHeader, StoryCode } from '../../../.storybook/style/storybook-info.styles';
+import { notes, info } from './documentation';
 
 storiesOf('Spinner', module).add(
   'default',
@@ -15,29 +14,7 @@ storiesOf('Spinner', module).add(
     return <Spinner as={ as } size={ size } />;
   },
   {
-    info: {
-      text: (
-        <div>
-          <p>Spinner component.</p>
-
-          <StoryHeader> Implementation</StoryHeader>
-
-          <p>Import the component:</p>
-
-          <StoryCode padded>i{'mport Spinner from "carbon-react/lib/components/spinner"'}</StoryCode>
-
-          <p>
-            You can pass a <StoryCode> {'size'} </StoryCode> property to adjust the size of the spinner
-          </p>
-
-          <p>The default is medium </p>
-
-          <StoryCode padded>
-            {'options: extra-small, small, medium-small, medium, medium-large, large and extra-large'}
-          </StoryCode>
-        </div>
-      )
-    }
+    info: { text: info },
+    notes: { markdown: notes }
   },
-  { notes: { markdown: notes } }
 );
