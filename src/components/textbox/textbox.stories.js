f53c3e3380d35bdad0880f0f88164e75bc180cc4
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 17:13:18 +0100

    textbox storybook update

--- src/components/textbox/textbox.stories.js
+++ src/components/textbox/textbox.stories.js
@@ -6,7 +6,7 @@ import {
 } from '@storybook/addon-knobs';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import Textbox from './textbox';
-import notes from './notes.md';
+import { info, notes } from './documentation';
 
 const store = new Store({
   value: ''
@@ -16,44 +16,53 @@ const handleChange = ({ target: { value } }) => {
   store.set({ value });
 };
 
-storiesOf('Textbox', module).add(
-  'default',
-  () => {
-    const rangeOptions = {
-      range: true,
-      min: 0,
-      max: 300,
-      setp: 1
-    };
+storiesOf('Textbox', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  })
+  .add(
+    'default',
+    () => {
+      const rangeOptions = {
+        range: true,
+        min: 0,
+        max: 300,
+        setp: 1
+      };
 
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    const label = text('label', 'Example Textarea');
-    const labelInline = label ? boolean('labelInline', false) : undefined;
-    const inputWidth = number('inputWidth', 0, rangeOptions);
-    const labelWidth = labelInline ? number('labelWidth', 0, rangeOptions) : undefined;
-    const labelHelp = text('labelHelp', 'This text provides more information for the label.');
-    const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
-    const labelAlign = labelInline ? select(
-      'labelAlign',
-      OptionsHelper.alignBinary,
-      OptionsHelper.alignBinary[0]
-    ) : undefined;
+      const fieldHelpInline = boolean('fieldHelpInline', false);
+      const label = text('label', 'Example Textarea');
+      const labelInline = label ? boolean('labelInline', false) : undefined;
+      const inputWidth = number('inputWidth', 0, rangeOptions);
+      const labelWidth = labelInline ? number('labelWidth', 0, rangeOptions) : undefined;
+      const labelHelp = text('labelHelp', 'This text provides more information for the label.');
+      const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
+      const labelAlign = labelInline ? select(
+        'labelAlign',
+        OptionsHelper.alignBinary,
+        OptionsHelper.alignBinary[0]
+      ) : undefined;
 
-    return (
-      <State store={ store }>
-        <Textbox
-          labelInline={ labelInline }
-          labelAlign={ labelAlign }
-          labelWidth={ labelWidth }
-          fieldHelpInline={ fieldHelpInline }
-          label={ label }
-          labelHelp={ labelHelp }
-          fieldHelp={ fieldHelp }
-          inputWidth={ inputWidth }
-          onChange={ handleChange }
-        />
-      </State>
-    );
-  },
-  { notes: { markdown: notes } }
-);
+      return (
+        <State store={ store }>
+          <Textbox
+            labelInline={ labelInline }
+            labelAlign={ labelAlign }
+            labelWidth={ labelWidth }
+            fieldHelpInline={ fieldHelpInline }
+            label={ label }
+            labelHelp={ labelHelp }
+            fieldHelp={ fieldHelp }
+            inputWidth={ inputWidth }
+            onChange={ handleChange }
+          />
+        </State>
+      );
+    },
+    {
+      info: { text: info },
+      notes: { markdown: notes }
+    }
+  );
