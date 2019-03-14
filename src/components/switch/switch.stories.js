00abee085c9df6ad35c6abce08b1e8c5d17d49dd
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-03-14 10:58:59 +0100

    fix storybook styling in the Switch component

--- src/components/switch/switch.stories.js
+++ src/components/switch/switch.stories.js
@@ -8,6 +8,7 @@ import { action } from '@storybook/addon-actions';
 import Switch from './switch';
 import notes from './notes.md';
 import OptionsHelper from '../../utils/helpers/options-helper/options-helper';
+import { StoryHeader, StoryCode } from '../../../.storybook/style/storybook-info.styles';
 
 const store = new Store({
   value: false
@@ -25,40 +26,62 @@ const numberConfig = {
   step: 1
 };
 
-storiesOf('Switch', module).add(
-  'default',
-  () => {
-    const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    const label = text('label', 'Example Switch');
-    const labelHelp = text('labelHelp', 'This text provides more information for the label.');
-    const labelInline = label ? boolean('labelInline', false) : undefined;
-    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : undefined;
-    const labelWidth = labelInline ? number('labelWidth', 0, numberConfig) : undefined;
-    const inputWidth = number('inputWidth', 0, numberConfig);
-    const reverse = boolean('reverse', Switch.defaultProps.reverse);
-    const loading = boolean('loading', false);
-    const children = text('children');
+storiesOf('Switch', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [State]
+    }
+  })
+  .add(
+    'default',
+    () => {
+      const fieldHelp = text('fieldHelp', 'This text provides help for the input.');
+      const fieldHelpInline = boolean('fieldHelpInline', false);
+      const label = text('label', 'Example Switch');
+      const labelHelp = text('labelHelp', 'This text provides more information for the label.');
+      const labelInline = label ? boolean('labelInline', false) : undefined;
+      const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary) : undefined;
+      const labelWidth = labelInline ? number('labelWidth', 0, numberConfig) : undefined;
+      const inputWidth = number('inputWidth', 0, numberConfig);
+      const reverse = boolean('reverse', Switch.defaultProps.reverse);
+      const loading = boolean('loading', false);
+      const children = text('children');
 
-    return (
-      <State store={ store }>
-        <Switch
-          onChange={ handleChange }
-          fieldHelp={ fieldHelp }
-          label={ label }
-          labelHelp={ labelHelp }
-          reverse={ reverse }
-          labelInline={ labelInline }
-          labelWidth={ labelWidth }
-          labelAlign={ labelAlign }
-          inputWidth={ inputWidth }
-          fieldHelpInline={ fieldHelpInline }
-          loading={ loading }
-        >
-          {children}
-        </Switch>
-      </State>
-    );
-  },
-  { notes: { markdown: notes } }
-);
+      return (
+        <State store={ store }>
+          <Switch
+            onChange={ handleChange }
+            fieldHelp={ fieldHelp }
+            label={ label }
+            labelHelp={ labelHelp }
+            reverse={ reverse }
+            labelInline={ labelInline }
+            labelWidth={ labelWidth }
+            labelAlign={ labelAlign }
+            inputWidth={ inputWidth }
+            fieldHelpInline={ fieldHelpInline }
+            loading={ loading }
+          >
+            {children}
+          </Switch>
+        </State>
+      );
+    },
+    {
+      info: {
+        text: (
+          <div>
+            <p>Switch component</p>
+            <p>This widget extends Checkbox and adds a switch styling over the top.</p>
+            <StoryHeader> Implementation</StoryHeader>
+
+            <p>Import the component:</p>
+            <StoryCode padded>
+              {'import Switch from "carbon-react/lib/components/switch"'}
+            </StoryCode>
+          </div>
+        )
+      }
+    },
+    { notes: { markdown: notes } }
+  );
