f55647211a4801c02b8cb949f4d099c68710e1dd
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-03-18 13:02:42 +0100

    FE-1303 - reverts import manager

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,5 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
+import '@storybook/addon-viewport/manager';
 import '@storybook/addon-a11y/register';
-import '@storybook/addon-viewport/register';
