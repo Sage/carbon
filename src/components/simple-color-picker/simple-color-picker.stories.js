de5fd1f2889daa8faa70f0fefb8e3be45014ec0a
izakrzewska <izakrzewska@pgs-soft.com>
2019-03-19 17:38:28 +0100

    update simple color picker

--- src/components/simple-color-picker/simple-color-picker.stories.js
+++ src/components/simple-color-picker/simple-color-picker.stories.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {
-  text, array
-} from '@storybook/addon-knobs';
+import { text, array } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { State, Store } from '@sambego/storybook-state';
 import SimpleColorPicker from './simple-color-picker';
@@ -19,7 +17,6 @@ const onChange = (e) => {
   action('select')();
 };
 
-
 storiesOf('SimpleColorPicker', module)
   .addParameters({
     info: {
@@ -27,21 +24,23 @@ storiesOf('SimpleColorPicker', module)
     }
   })
 
-  .add('default', () => {
-    const name = text('name', 'basicPicker');
-    const availableColors = array('availableColors', ['#00DC00', '#255BC7', '#ED1C5F']);
+  .add(
+    'default',
+    () => {
+      const name = text('name', 'basicPicker');
+      const availableColors = array('availableColors', ['#00DC00', '#255BC7', '#ED1C5F']);
 
-    return (
-      <State store={ store }>
-        <SimpleColorPicker
-          availableColors={ availableColors }
-          name={ name }
-          onChange={ onChange }
-
-        />
-      </State>
-    );
-  }, {
-    notes: { markdown: notes },
-    info: { text: info }
-  });
+      return (
+        <State store={ store }>
+          <SimpleColorPicker
+            availableColors={ availableColors } name={ name }
+            onChange={ onChange }
+          />
+        </State>
+      );
+    },
+    {
+      notes: { markdown: notes },
+      info: { text: info }
+    }
+  );
