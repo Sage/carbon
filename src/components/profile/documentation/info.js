a38d3618a37463cdd51ef794b86f4d02569f89dc
izakrzewska <izakrzewska@pgs-soft.com>
2019-03-19 14:00:50 +0100

    add descriptions for profile

--- src/components/profile/documentation/info.js
+++ src/components/profile/documentation/info.js
@@ -1,17 +1,15 @@
-
 import React from 'react';
 import { StoryHeader, StoryCode } from '../../../../.storybook/style/storybook-info.styles';
 
 const info = (
   <div>
     <p> Profile Component </p>
-    <p>Renders a user profile, with avatar.</p>
+    <p>Represents a person with their initials or an avatar, and some text.</p>
 
     <StoryHeader> Implementation</StoryHeader>
 
     <p>Import the component:</p>
     <StoryCode padded>import Portrait from {'"react-carbon/lib/components/profile"'}</StoryCode>
-
   </div>
 );
 
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -8,37 +8,31 @@ import './profile.scss';
 
 class Profile extends React.Component {
   static propTypes = {
-
     /**
      * A custom class name for the component.
-     *
      */
     className: PropTypes.string,
 
     /**
-     * The user's name.
-     *
+     * Define the name to display.
      */
     name: PropTypes.string.isRequired,
 
     /**
-     * The user's email.
-     *
+     * Define the email to use (will check Gravatar for image).
      */
     email: PropTypes.string.isRequired,
 
     /**
-     * The user's initials.
-     *
+     * Define initials to display if there is no Gravatar image.
      */
     initials: PropTypes.string,
 
     /**
-     * Outputs a large name version.
-     *
+     * Enable a larger theme for the name.
      */
     large: PropTypes.bool
-  }
+  };
 
   static defaultProps = {
     large: false
@@ -51,12 +45,9 @@ class Profile extends React.Component {
    * @return {String}
    */
   get classes() {
-    return classNames(
-      'carbon-profile',
-      this.props.className, {
-        'carbon-profile--large': this.props.large
-      }
-    );
+    return classNames('carbon-profile', this.props.className, {
+      'carbon-profile--large': this.props.large
+    });
   }
 
   /**
@@ -66,7 +57,9 @@ class Profile extends React.Component {
    * @return {String}
    */
   get initials() {
-    if (this.props.initials) { return this.props.initials; }
+    if (this.props.initials) {
+      return this.props.initials;
+    }
     return acronymize(this.props.name);
   }
 
@@ -97,10 +90,11 @@ class Profile extends React.Component {
     return (
       <div className='carbon-profile__details'>
         <span className='carbon-profile__name' data-element='name'>
-          { this.props.name }
-        </span><br />
+          {this.props.name}
+        </span>
+        <br />
         <span className='carbon-profile__email' data-element='email'>
-          { this.props.email }
+          {this.props.email}
         </span>
       </div>
     );
@@ -112,8 +106,8 @@ class Profile extends React.Component {
   render() {
     return (
       <div className={ this.classes } { ...tagComponent('profile', this.props) }>
-        { this.avatar }
-        { this.text }
+        {this.avatar}
+        {this.text}
       </div>
     );
   }
