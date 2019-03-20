f59ac25330e5669ce78131e7cd8fcc78f46b3a60
samhinton88 <sam.hinton88@gmail.com>
2019-03-20 15:56:03 +0000

    basic spec

--- src/__experimental__/components/grouped-character/grouped-character.spec.js
+++ src/__experimental__/components/grouped-character/grouped-character.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import GroupedCharacter from '.';
 
-const mountComponent = props => mount(<GroupedCharacter { ...props } />)
+const mountComponent = props => mount(<GroupedCharacter { ...props } />);
 
 describe('GroupedCharacter', () => {
   const basicGroupConfig = [2, 2, 4],
@@ -23,15 +23,26 @@ describe('GroupedCharacter', () => {
     it('takes configuration for how text should be grouped', () => {
       expect(input.props().value).toEqual('12-34-5678');
     });
-
     it('emits an unformatted string', () => {
       input.simulate('change', { target: { value: '123456' } });
       expect(onChange).toHaveBeenCalledWith({ target: { value: '123456' } });
     });
-
-    it('does not allow value to be updated beyond the maximum character length allowed by the groups', () => {
-      input.simulate('keyPress', { key: 'p' });
+    it('does not allow values of length greater than that allowed by the group config', () => {
+      instance = mountComponent({
+        separator, groups: basicGroupConfig, value: '1234567890', onChange
+      });
+      input = instance.find('input');
       expect(input.props().value).toEqual('12-34-5678');
+    });
+    fit('formats the input value correctly when deleting characters next to separator', () => {
+      const deletePress = new KeyboardEvent('keydown', { which: '46' });
+      instance = mountComponent({
+        separator, groups: basicGroupConfig, value: '12345', onChange
+      });
+      input = instance.find('input');
+      input.simulate('click');
+      document.dispatchEvent(deletePress);
+      expect(input.props().value).toEqual('12-34');
     });
   });
 });
