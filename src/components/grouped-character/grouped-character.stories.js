ef2ffae38afc074c635bc3e8f985af6c25f79f90
dquilter <david@kyan.com>
2019-03-20 15:36:00 +0000

    Updates for Grouped Character Story

--- src/components/grouped-character/grouped-character.stories.js
+++ src/components/grouped-character/grouped-character.stories.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
   text,
-  number,
-  boolean
+  boolean,
+  select
 } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import OptionsHelper from '../../utils/helpers/options-helper';
 import GroupedCharacter from './grouped-character';
 
 storiesOf('GroupedCharacter', module)
   .add('default', () => {
     const inputWidth = text('inputWidth', '');
     const separator = text('separator', GroupedCharacter.defaultProps.separator);
-    const timeToDisappear = number('timeToDisappear', 0);
     const label = text('label', 'Example Grouped Character');
     const labelInline = boolean('labelInline', false);
     const labelWidth = labelInline ? text('labelWidth', '') : undefined;
-    const labelAlign = labelInline ? text('labelAlign', '') : undefined;
+    const labelAlign = labelInline ? select(
+      'labelAlign',
+      OptionsHelper.alignBinary,
+      OptionsHelper.alignBinary[0]
+    ) : undefined;
     const labelHelp = text('labelHelp', 'More information for the label');
     const fieldHelp = text('fieldHelp', 'More information for the input');
     const fieldHelpInline = boolean('fieldHelpInline', true);
@@ -26,7 +30,6 @@ storiesOf('GroupedCharacter', module)
         inputWidth={ inputWidth }
         separator={ separator }
         groups={ [2, 2, 2, 2] }
-        timeToDisappear={ timeToDisappear }
         label={ label }
         labelInline={ labelInline }
         labelWidth={ labelWidth }
