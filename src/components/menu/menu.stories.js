c7c71933403494b55de1e442f73a26114d3c925a
dquilter <david@kyan.com>
2019-03-21 09:13:16 +0000

    Adds Extra Menu Item Options in Storybook

--- src/components/menu/menu.stories.js
+++ src/components/menu/menu.stories.js
@@ -25,8 +25,9 @@ storiesOf('Menu', module)
         <MenuItem submenu='Item Two'>
           <MenuItem href='#'>Sub Menu Item One</MenuItem>
           <SubmenuBlock>
-            <MenuItem href='#'>Sub Menu Item Two</MenuItem>
+            <MenuItem icon="settings" href='#'>Sub Menu Item Two</MenuItem>
             <MenuItem href='#'>Sub Menu Item Three</MenuItem>
+            <MenuItem divide href='#'>Sub Menu Item Four</MenuItem>
           </SubmenuBlock>
         </MenuItem>
       </Menu>
