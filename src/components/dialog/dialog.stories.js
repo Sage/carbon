2055e5ffb0ac3492c0712e8bfe43e994e2eda5aa
dquilter <david@kyan.com>
2019-03-21 14:45:54 +0000

    Adds Form to Dialog Story

--- src/components/dialog/dialog.stories.js
+++ src/components/dialog/dialog.stories.js
@@ -6,6 +6,8 @@ import { State, Store } from '@sambego/storybook-state';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './documentation/notes.md';
 import Dialog from './dialog';
+import Form from '../form';
+import Textbox from '../textbox';
 import Button from '../button/button';
 
 const store = new Store({
@@ -62,7 +64,16 @@ storiesOf('Dialog', module)
             disableEscKey={ disableEscKey }
             ariaRole={ ariaRole }
             onClick={ handleClick }
-          />
+          >
+            <Form>
+              <Textbox label="First Name" />
+              <Textbox label="Middle Name" />
+              <Textbox label="Surname" />
+              <Textbox label="Birth Place" />
+              <Textbox label="Favourite Colour" />
+              <Textbox label="Address" />
+            </Form>
+          </Dialog>
         </State>
       </div>
     );
