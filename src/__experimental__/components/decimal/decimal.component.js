67ec8cf9b2c789fd3b63497ffd9a1f77e5c717d0
dquilter <david@kyan.com>
2019-03-25 12:04:39 +0000

    Working Regex for Experimental Decimal

--- src/__experimental__/components/decimal/decimal.component.js
+++ src/__experimental__/components/decimal/decimal.component.js
@@ -1,141 +1,84 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import InputDecoratorBridge from '../input-decorator-bridge';
+import TextBox from '../textbox';
 import I18nHelper from '../../../utils/helpers/i18n';
 
 class Decimal extends React.Component {
-  static propTypes = {
-    /**
-     * The default value alignment on the input
-     */
-    align: PropTypes.string,
-    /**
-     * The decimal precision of the value in the input
-     */
-    precision: PropTypes.number,
-    /**
-     * The width of the input as a percentage
-     */
-    inputWidth: PropTypes.number,
-    /**
-     * The value of the input
-     */
-    value: PropTypes.string,
-    formattedValue: PropTypes.string
+  state = {
+    value: '0.00'
   }
 
-  // inputWidth: PropTypes.number,
-  // fieldHelp: PropTypes.number,
-  // label: PropTypes.number,
-  // labelInline: PropTypes.number,
-  // labelWidth: PropTypes.number,
-  // labelAlign: PropTypes.number,
-  // labelHelp: PropTypes.number
-
-  static defaultProps = {
-    align: 'left',
-    precision: 2,
-    inputWidth: 100
-  };
-
-  componentDidUpdate(nextProps) {
-    console.log(nextProps)
-    if (nextProps.value !== this.props.value) {
-      
+  componentDidUpdate(prevProps, prevState) {
+    const test = this.testValue();
+    console.log(prevState.value, this.props.value)
+    console.log(prevState.value !== this.props.value, test)
+    if (prevState.value !== this.props.value && test) {
+      this.setState({
+        value: this.props.value
+      })
     }
   }
-
-  handleOnChange = (evt) => {
-    console.log('TEA PARTY')
-
-    const isValid = this.isValidDecimal(
-      evt.target.value,
-      this.verifiedPrecision(this.props.precision)
-    );
-
-    console.log(isValid)
-    console.log(this.props)
-    
-    if (isValid && this.props.onChange) {
-      this.props.onChange(evt, this.props);
-    } else {
-      evt.target.value = this.props.value || null;
-      evt.target.setSelectionRange(this.selectionStart, this.selectionEnd);
-    }
-  };
-
-  handleKeyDown = (ev) => {
-    this.selectionStart = ev.target.selectionStart;
-    this.selectionEnd = ev.target.selectionEnd;
-
-    if (this.props.onKeyDown) {
-      this.props.onKeyDown(ev, this.props);
-    }
-  };
   
-  isValidDecimal = (value, precision) => {
-    const format = I18nHelper.format();
-    const del = `\\${format.delimiter}`;
-    const sep = `\\${format.separator}`;
-    let regex;
-    if (precision > 0) {
-      regex = new RegExp(`^[-]?\\d*(?:${del}?\\d?)*${sep}?\\d{0,${precision}}$`);
-    } else {
-      regex = new RegExp(`^[-]?\\d*(?:${del}?\\d?)*$`);
-    }
-    return regex.test(value);
+  testValue = () => {
+    const { value, precision } = this.props;
+  
+    const regex = /^[\d,]*[\.{1}]?\d{0,2}?$/;
+    const isGoodDecimal = regex.test(value);
+    console.log(value)
+    console.log(isGoodDecimal)
+    if (!isGoodDecimal) { return false; }
+
+    return true;
   }
 
-  // Caps precision at 20 to prevent browser issues
-  verifiedPrecision = (precision) => {
-    if (precision > 20) {
-      console.info('Decimal precision must not be greater than 20.');
-      return 20;
-    }
-    return precision;
-  }
-
-
-  // state = {
-  //   /**
-  //    * The formatted value for display
-  //    *
-  //    * @property visibleValue
-  //    * @type {String}
-  //    */
-  //   visibleValue: I18nHelper.formatDecimal(this.value, this.verifiedPrecision(this.props.precision))
-  // };
-
-
-  // componentWillReceiveProps(newProps) {
-  //   if (this._document.activeElement !== this._input) {
-  //     let value = newProps.value || 0.00;
-  //     if (canConvertToBigNumber(value)) {
-  //       value = I18nHelper.formatDecimal(value, this.verifiedPrecision(newProps.precision));
-  //     }
-  //     this.setState({ visibleValue: value });
+  // handleChange = (evt) => {
+  //   const test = this.testValue();
+  //   const { selectionEnd } = evt.target;
+  //   const i = evt.target;
+    
+  //   if (test) {
+  //     this.setState({
+  //       value: this.props.value
+  //     });
+  //     setTimeout(() => i.setSelectionRange(selectionEnd, selectionEnd))
   //   }
   // }
 
-
-
-
-
-
-
-
-
-
-
   render() {
     return (
-      <InputDecoratorBridge
-        onChange={ this.handleOnChange }
-        onKeyDown={ this.handleKeyDown }
+      <TextBox
         {...this.props}
+        // onChange={ this.handleChange }
+        value={ this.state.value }
       />
     );
   }
+};
+
+Decimal.propTypes = {
+  /**
+   * The default value alignment on the input
+   */
+  align: PropTypes.string,
+  /**
+   * The decimal precision of the value in the input
+   */
+  precision: PropTypes.number,
+  /**
+   * The width of the input as a percentage
+   */
+  inputWidth: PropTypes.number,
+  /**
+   * The value of the input
+   */
+  value: PropTypes.string
 }
+
+Decimal.defaultProps = {
+  align: 'left',
+  precision: 2,
+  inputWidth: 100
+};
+
 
 export default Decimal;
