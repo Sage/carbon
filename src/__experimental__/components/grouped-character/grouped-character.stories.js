974f2cd496dc0598b7b195edcfd587013912dd7c
samhinton88 <sam.hinton88@gmail.com>
2019-03-25 09:52:57 +0000

    storybook fixes

--- src/__experimental__/components/grouped-character/grouped-character.stories.js
+++ src/__experimental__/components/grouped-character/grouped-character.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Store, State } from '@sambego/storybook-state';
 import { text, object } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 import GroupedCharacter from './grouped-character.component';
 
 const groupedCharacterStore = new Store({
@@ -9,15 +10,19 @@ const groupedCharacterStore = new Store({
 });
 
 storiesOf('Experimental/GroupedCharacter', module)
-  .add('Basic', () => {
-    return (
-      <State store={ groupedCharacterStore }>
-        <GroupedCharacter
-          groups={ object('groups', [2, 2, 4]) }
-          separator={ text('separator', '-') }
-          value={ groupedCharacterStore.get('value') }
-          onChange={ (ev) => { groupedCharacterStore.set({ value: ev.target.value }); } }
-        />
-      </State>
-    );
-  });
+  .addParameters({
+    info: { propTablesExclude: [State] }
+  })
+  .add('Basic',
+    () => {
+      return (
+        <State store={ groupedCharacterStore }>
+          <GroupedCharacter
+            groups={ object('groups', [2, 2, 4]) }
+            separator={ text('separator', '-') }
+            value={ groupedCharacterStore.get('value') }
+            onChange={ (ev) => { groupedCharacterStore.set({ value: ev.target.value }); } }
+          />
+        </State>
+      );
+    });
