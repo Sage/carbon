50ab94b248269ac3dc9103f0534e36c5dbf38b17
dquilter <david@kyan.com>
2019-03-26 11:08:36 +0000

    Use Correct Width Prop for Label Component

--- src/__experimental__/components/label/label.style.js
+++ src/__experimental__/components/label/label.style.js
@@ -13,13 +13,13 @@ const LabelStyle = styled.label`
   width: 100%;
 
   ${({
-    align, inline, inputSize, labelWidth
+    align, inline, inputSize, width
   }) => inline && css`
     box-sizing: border-box;
     padding-bottom: 0;
     padding-right: ${sizes[inputSize].padding};
     text-align: ${align};
-    width: ${labelWidth}%;
+    width: ${width}%;
     ${inputSize === 'small' && css`padding-top: 8px;`}
     ${inputSize === 'medium' && css`padding-top: 12px;`}
     ${inputSize === 'large' && css`padding-top: 16px;`}
@@ -41,7 +41,7 @@ LabelStyle.defaultProps = {
   align: 'left',
   inputSize: 'medium',
   theme: BaseTheme,
-  labelWidth: 30
+  width: 30
 };
 
 LabelStyle.propTypes = {
