bc691dd5e7773ad80b07b462391551ccc1958c6a
izakrzewska <izakrzewska@pgs-soft.com>
2019-03-26 14:46:08 +0100

    fix rainbow issue

--- src/components/rainbow/rainbow.stories.js
+++ src/components/rainbow/rainbow.stories.js
@@ -29,7 +29,7 @@ storiesOf('Rainbow ', module).add(
     const data = object('data', myImmutableData);
     const title = text('title', 'Rainbow chart');
 
-    return <Rainbow title={ title } data={ data } />;
+    return <Rainbow title={ title } data={ Immutable.fromJS(data) } />;
   },
   {
     notes: { markdown: notes },
