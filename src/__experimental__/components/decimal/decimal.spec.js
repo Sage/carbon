24a7a275e8bb2fc2d56cade98852182c82cd51e4
dquilter <david@kyan.com>
2019-03-27 13:43:12 +0000

    Completed Validation Specs for Decimal

--- src/__experimental__/components/decimal/decimal.spec.js
+++ src/__experimental__/components/decimal/decimal.spec.js
@@ -3,33 +3,53 @@ import { shallow, mount } from 'enzyme';
 import Decimal from './decimal.component';
 import Textbox from '../textbox/textbox.component';
 
-const input = shallow(<Decimal onChange={() => true} value='12.34' />)
+function render(props) {
+  return shallow(
+    <Decimal
+      onChange={() => true}
+      value={props.value}
+      precision={props.precision || '2'}
+    />
+  );
+}
 
 describe('Decimal', () => {
   describe('Input validation', () => {
+    it('renders the correct value', () => {
+      const input = render({ value: '0.00' });
+      input.instance().handleChange({ target: {value: '14.79'} })
+      expect(input.find('Textbox').prop('value')).toEqual('14.79');
+    });
     it('does not allow the user to enter letters or special characters', () => {
+      const input = render({ value: '12.34' });
       input.instance().handleChange({ target: {value: '1hello$1.27'} })
       expect(input.find('Textbox').prop('value')).toEqual('12.34');
     });
     it('does not allow the user to enter commas after the decimal point', () => {
-      const test = false;
-      expect(test).toEqual(true);
+      const input = render({ value: '34.56' });
+      input.instance().handleChange({ target: {value: '34.5,,,,6'} })
+      expect(input.find('Textbox').prop('value')).toEqual('34.56');
     });
-    it('formats the commas correctly on blur', () => {
-      const test = false;
-      expect(test).toEqual(true);
+    it('formats the delimiters correctly on blur', () => {
+      const input = render({ value: '1234567.00' });
+      input.instance().handleChange({ target: {value: '1,,,2345,,,67.00'} })
+      input.instance().handleBlur();
+      expect(input.find('Textbox').prop('value')).toEqual('1,234,567.00');
     });
     it('updates the value after increasing the precison', () => {
-      const test = false;
-      expect(test).toEqual(true);
+      const input = render({ value: '99.99', precision: '2' });
+      input.setProps({ precision: 4 })
+      expect(input.find('Textbox').prop('value')).toEqual('99.9900');
     });
     it('updates the value after decreasing the precison', () => {
-      const test = false;
-      expect(test).toEqual(true);
+      const input = render({ value: '1234.1234567', precision: '7' });
+      input.setProps({ precision: 4 })
+      expect(input.find('Textbox').prop('value')).toEqual('1,234.1235');
     });
-    it('does not allow the precison to be greater than 20', () => {
-      const test = false;
-      expect(test).toEqual(true);
+    it('does not allow the precison to be greater than 15', () => {
+      const input = render({ value: '4.1234', precision: '4' });
+      input.setProps({ precision: 20 })
+      expect(input.find('Textbox').prop('value')).toEqual('4.123400000000000');
     });
   });
   // DQ: DO THESE ACTUALLY GO HERE?!?!
