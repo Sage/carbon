3dadbf8039549226117001ebda2447803ab9c5fc
Ed Leeks <edward.leeks@sage.com>
2019-03-28 11:30:38 +0000

    change back to "theme" so is backwards compatible

--- src/components/button/button.d.ts
+++ src/components/button/button.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 export interface ButtonProps {
   as?: 'primary' | 'secondary' | 'tertiary' | 'destructive' | 'darkBackground';
   disabled?: boolean;
-  classicTheme?: 'blue' | 'grey' | 'magenta' | 'magenta-dull' | 'red' | 'white';
+  theme?: 'blue' | 'grey' | 'magenta' | 'magenta-dull' | 'red' | 'white';
   size?: 'small' | 'medium' | 'large';
   subtext?: string;
   children?: React.ReactNode;
