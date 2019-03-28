670409618a1668691ccd1a4d868734c272c71bb7
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-03-28 14:48:51 +0100

    FE-1223 - colors from theme

--- src/components/date/weekday/weekday-classic.style.js
+++ src/components/date/weekday/weekday-classic.style.js
@@ -2,12 +2,12 @@ import { css } from 'styled-components';
 import { THEMES } from '../../../style/themes';
 
 export default ({ theme }) => theme.name === THEMES.classic && css`
-  background-color: #fff;
-  color: rgba(0,0,0,.6);
+  background-color: ${theme.colors.white};
+  color: rgba(0, 0, 0, .6);
   font-size: 14px;
   font-weight: 700;
   text-transform: capitalize;
-  border-color: #fff;
+  border-color: ${theme.colors.white};
   border-style: solid;
   border-width: 0 3px 3px 3px;
 `;
