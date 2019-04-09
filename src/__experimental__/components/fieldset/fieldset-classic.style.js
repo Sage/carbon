ae19f7ca7cd05d63cf4d148a01102ad12e8c8df1
dquilter <david@kyan.com>
2019-04-09 11:15:19 +0100

    Updates Label and Legend Colors for Classic Theme

--- src/__experimental__/components/fieldset/fieldset-classic.style.js
+++ src/__experimental__/components/fieldset/fieldset-classic.style.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 import { THEMES } from '../../../style/themes';
 
 export default ({ theme }) => theme.name === THEMES.classic && css`
-    color: ${theme.text.color};
+    color: #003349;
     font-size: 14px;
     font-weight: bold;
     line-height: 14px;
