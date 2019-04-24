d1829d4bd6fa55b3b42bfd8df182421563101f90
grabkowski <jgrabkowski@pgs-soft.com>
2019-04-24 11:27:02 +0200

    added info for classic button

--- src/components/button/documentation/index.js
+++ src/components/button/documentation/index.js
@@ -1,2 +1,3 @@
 export { default as notes } from './notes.md';
-export { default as Info } from './Info';
+export { default as Info } from './info';
+export { default as InfoClassic } from './info-classic';
