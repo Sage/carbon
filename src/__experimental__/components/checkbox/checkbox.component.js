f6b90dccdfadcd74dec94132fc84d0a50022dc31
Steve Butler <steve.butler@kyan.com>
2019-04-24 10:14:32 +0100

    Single line prop descs

--- src/__experimental__/components/checkbox/checkbox.component.js
+++ src/__experimental__/components/checkbox/checkbox.component.js
@@ -53,41 +53,23 @@ const Checkbox = (props) => {
 };
 
 Checkbox.propTypes = {
-  /**
-   * Set the value of the checkbox
-   */
+  /** Set the value of the checkbox */
   checked: PropTypes.bool,
-  /**
-   * Toggles disabling of input
-   */
+  /** Toggles disabling of input */
   disabled: PropTypes.bool,
-  /**
-   * Toggles error styles
-   */
+  /** Toggles error styles */
   error: PropTypes.bool,
-  /**
-   * Displays fieldHelp inline with the checkbox
-   */
+  /** Displays fieldHelp inline with the checkbox */
   fieldHelpInline: PropTypes.bool,
-  /**
-   * Sets percentage-based input width
-   */
+  /** Sets percentage-based input width */
   inputWidth: PropTypes.number,
-  /**
-   * Sets label alignment - accepted values: 'left' (default), 'right'
-   */
+  /** Sets label alignment - accepted values: 'left' (default), 'right' */
   labelAlign: PropTypes.string,
-  /**
-   * Sets percentage-based label width
-   */
+  /** Sets percentage-based label width */
   labelWidth: PropTypes.number,
-  /**
-   * Accepts a callback function which can be used to update parent state on change
-   */
+  /** Accepts a callback function which can be used to update parent state on change */
   onChange: PropTypes.func,
-  /**
-   * Reverses label and checkbox display
-   */
+  /** Reverses label and checkbox display */
   reverse: PropTypes.bool,
   /**
    * Set the size of the checkbox to 'small' (16x16 - default) or 'large' (24x24).
