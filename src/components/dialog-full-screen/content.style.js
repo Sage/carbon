352cc8b3d64efd0d1f3289787dd7275568b85a89
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-04-24 13:51:02 +0200

    FE-1316 - fix for the content height style in DialogFullScreen component

--- src/components/dialog-full-screen/content.style.js
+++ src/components/dialog-full-screen/content.style.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import FullScreenHeading from './full-screen-heading/full-screen-heading.style';
 
 const StyledContent = styled.div`
   padding: 30px 0;
   overflow-y: auto;
   height: calc(100% - 60px);
 
-  .carbon-full-screen-heading + & {
+  ${FullScreenHeading} + & {
     height: calc(100% - 166px);
   }
 `;
