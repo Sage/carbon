3570a644185cd1a732b904f333895a724848e8e4
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-04-24 10:27:15 +0200

    update tests

--- src/components/sidebar/sidebar-header/sidebar-header.spec.js
+++ src/components/sidebar/sidebar-header/sidebar-header.spec.js
@@ -1,44 +1,28 @@
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
 import { shallow } from 'enzyme';
 import SidebarHeader from './sidebar-header.component';
 import Textbox from '../../textbox/textbox';
-import { rootTagTest } from '../../../utils/helpers/tags/tags-specs/tags-specs';
 
 describe('Sidebar Header', () => {
-  let instance, classInstance;
+  let wrapper;
 
   beforeEach(() => {
-    instance = TestUtils.renderIntoDocument(
+    wrapper = shallow(
       <SidebarHeader>
         <Textbox />
       </SidebarHeader>
     );
-
-    classInstance = TestUtils.renderIntoDocument(
-      <SidebarHeader className='custom-class'>
-        <Textbox />
-      </SidebarHeader>
-    );
-  });
-
-  describe('mainClasses', () => {
-    it('appends any additional passed classes', () => {
-      expect(classInstance.mainClasses).toEqual('custom-class');
-    });
   });
 
   describe('render', () => {
-    it('renders any children passed to it', () => {
-      expect(TestUtils.findRenderedComponentWithType(instance, Textbox)).toBeTruthy();
+    it('should render child', () => {
+      expect(wrapper.find(Textbox)).toBeTruthy();
     });
   });
 
   describe('tags on component', () => {
-    const wrapper = shallow(<SidebarHeader data-element='bar' data-role='baz' />);
-
     it('include correct component, element and role data tags', () => {
-      rootTagTest(wrapper, 'sidebar-header', 'bar', 'baz');
+      wrapper.setProps({ 'data-element': 'bar', 'data-role': 'baz' });
     });
   });
 });
