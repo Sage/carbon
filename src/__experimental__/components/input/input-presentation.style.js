74df4892537efb7526fb91d285a355eb3d90b15f
Michał Maciorowski <mmaciorowski@pgs-soft.com>
2019-04-26 16:15:15 +0200

    FE-1282 - white background for inputs and a textarea

--- src/__experimental__/components/input/input-presentation.style.js
+++ src/__experimental__/components/input/input-presentation.style.js
@@ -7,7 +7,7 @@ import inputClassicStyling from './input-presentation-classic.style';
 
 const InputPresentationStyle = styled.div`
   align-items: center;
-  background: transparent;
+  background: #fff;
   border: 1px solid ${({ theme }) => theme.colors.border};
   box-sizing: border-box;
   cursor: text;
