9028922f7eee794e7a6b598783b227d9390f604d
dquilter <david@kyan.com>
2019-04-30 14:43:04 +0100

    Current Page and Total Records for Pager

--- src/components/pager/pager.component.js
+++ src/components/pager/pager.component.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import I18n from 'i18n-js';
 import Immutable from 'immutable';
 import {
   PagerContainerStyles,
@@ -9,17 +10,44 @@ import {
 } from './pager.styles';
 
 const Pager = (props) => {
+  const [currentPage, setCurrentPage] = useState(props.currentPage);
+
+  useEffect(() => {
+    setCurrentPage(props.currentPage);
+  }, [props.currentPage]);
+
+  function recordsText(numberOfRecords) {
+    const count = numberOfRecords >= 0 ? numberOfRecords : 0;
+    const descriptor = I18n.t(
+      'pager.records',
+      { count: Number(numberOfRecords), defaultValue: ' records' }
+    );
+
+    return `${count} ${descriptor}`;
+  }
+
   return (
     <PagerContainerStyles>
       <PagerSizeOptionsStyles>Show 100 items</PagerSizeOptionsStyles>
-      <PagerNavigationStyles>First Prev Next Last</PagerNavigationStyles>
-      <PagerSummaryStyles>2000 items</PagerSummaryStyles>
+      <PagerNavigationStyles>First Prev Page {currentPage} Next Last</PagerNavigationStyles>
+      <PagerSummaryStyles>{recordsText(props.totalRecords)}</PagerSummaryStyles>
     </PagerContainerStyles>
   );
-}
+};
 
 Pager.propTypes = {
-
+  /** Current visible page */
+  currentPage: PropTypes.string.isRequired,
+  /** Total number of records */
+  totalRecords: PropTypes.number.isRequired,
+  /** Function called when pager changes (PageSize, Current Page) */
+  onPagination: PropTypes.func.isRequired,
+  /** Pagination page size */
+  pageSize: PropTypes.string,
+  /** Should the page size selection dropdown be shown */
+  showPageSizeSelection: PropTypes.bool,
+  /** Set of page size options */
+  pageSizeSelectionOptions: PropTypes.object
 }
 
 Pager.defaultProps = {
