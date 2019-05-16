62af6f2003fa2262972f19c9ef4783e408437085
dquilter <david@kyan.com>
2019-05-16 09:28:33 +0100

    First Pass Specs for StepSequence Components

--- src/components/step-sequence/step-sequence-item/step-sequence-item.spec.js
+++ src/components/step-sequence/step-sequence-item/step-sequence-item.spec.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import StepSequenceItem from './step-sequence-item';
+// import { shallow } from 'enzyme';
+import TestRenderer from 'react-test-renderer';
+import 'jest-styled-components';
+import StepSequenceItem from './step-sequence-item.component';
+import classicTheme from '../../../style/themes/classic';
+import smallTheme from '../../../style/themes/small';
 
-describe('<StepSequenceItem />', () => {
-  let stepSequenceItem;
-
-  beforeAll(() => {
-    stepSequenceItem = shallow(
-      <StepSequenceItem
-        aria-label='Step 1 of 5'
-        indicator='2'
-        hiddenCompleteLabel='Complete text for non visual users'
-        hiddenCurrentLabel='Current text for non visual users'
-      >
-        bar
+describe('StepSequenceItem', () => {
+  const wrapper = props => (
+    TestRenderer.create(
+      <StepSequenceItem { ...props }>
+        Item
       </StepSequenceItem>
-    );
-  });
+    )
+  );
 
-  const statusTest = status => () => {
-    stepSequenceItem.setProps({ status });
-    expect(stepSequenceItem).toMatchSnapshot();
+  const defaultProps = {
+    ariaLabel: 'Step 1 of 5',
+    indicator: '2',
+    hiddenCompleteLabel: 'Complete text for non visual users',
+    hiddenCurrentLabel: 'Current text for non visual users'
   };
 
-  test('completed render', statusTest('complete'));
-  test('current render', statusTest('current'));
-  test('incomplete render', statusTest('incomplete'));
+  it('renders correctly', () => {
+    expect(wrapper({ ...defaultProps, theme: smallTheme })).toMatchSnapshot();
+  });
+
+  describe('Classic theme', () => {
+    it('renders correctly', () => {
+      expect(wrapper({ ...defaultProps, theme: classicTheme })).toMatchSnapshot();
+    });
+  });
 });
