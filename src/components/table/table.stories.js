79633839daceb2430897dc1e5b3299d3a2147afd
Ed Leeks <edward.leeks@sage.com>
2019-05-20 08:44:06 +0100

    removes some of theme options surfaced, adds classic story

--- src/components/table/table.stories.js
+++ src/components/table/table.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { boolean, text, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
@@ -9,6 +10,7 @@ import MultiActionButton from '../multi-action-button';
 import {
   Table, TableCell, TableHeader, TableRow
 } from '.';
+import classic from '../../style/themes/classic';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import { notes, info } from './documentation';
 
@@ -72,6 +74,65 @@ storiesOf('Table', module)
       propTablesExclude: [State]
     }
   })
+  .add('classic', () => {
+    const pageSize = text('pageSize', '5');
+    const selectable = boolean('selectable', false);
+    const highlightable = boolean('highlightable', false);
+    const shrink = boolean('shrink', false);
+    const caption = text('Caption', 'Country and Country Codes');
+    const totalRecords = text('totalRecords', '50');
+    const paginate = boolean('paginate', false);
+    const showPageSizeSelection = paginate && boolean('showPageSizeSelection', false);
+    const theme = select(
+      'theme',
+      [
+        OptionsHelper.tableThemes[0],
+        OptionsHelper.tableThemes[1]
+      ],
+      Table.defaultProps.theme
+    );
+
+    return (
+      <ThemeProvider theme={ classic }>
+        <State store={ store } parseState={ state => ({ ...state, children: buildRows(pageSize, totalRecords) }) }>
+
+          <Table
+            actionToolbarChildren={ (context) => {
+              return [
+                <Button disabled={ context.disabled } key='single-action'>
+                    Test Action
+                </Button>,
+                <MultiActionButton
+                  text='Actions' disabled={ context.disabled }
+                  key='multi-actions'
+                >
+                  <Button>foo</Button>
+                  <Button>bar</Button>
+                  <Button>qux</Button>
+                </MultiActionButton>
+              ];
+            } }
+            path='/countries'
+            caption={ caption }
+            shrink={ shrink }
+            highlightable={ highlightable }
+            pageSize={ pageSize }
+            selectable={ selectable }
+            paginate={ paginate }
+            actions={ { delete: { icon: 'bin' }, settings: { icon: 'settings' } } }
+            totalRecords={ totalRecords }
+            showPageSizeSelection={ showPageSizeSelection }
+            onChange={ handleChange }
+            theme={ theme }
+          />
+
+        </State>
+      </ThemeProvider>
+    );
+  }, {
+    info: { text: info },
+    notes: { markdown: notes }
+  })
   .add(
     'default',
     () => {
@@ -83,7 +144,15 @@ storiesOf('Table', module)
       const totalRecords = text('totalRecords', '50');
       const paginate = boolean('paginate', false);
       const showPageSizeSelection = paginate && boolean('showPageSizeSelection', false);
-      const theme = select('theme', OptionsHelper.tableThemes, Table.defaultProps.theme);
+      const theme = select(
+        'theme',
+        [
+          OptionsHelper.tableThemes[0],
+          OptionsHelper.tableThemes[1],
+          OptionsHelper.tableThemes[2]
+        ],
+        Table.defaultProps.theme
+      );
       const size = select('size', OptionsHelper.tableSizes, Table.defaultProps.size);
       const isZebra = boolean('zebra striping', false);
 
