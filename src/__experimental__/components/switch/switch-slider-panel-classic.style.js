754776446d22fdffea1c26bc47fb15d1b3448b5b
Steve Butler <steve.butler@kyan.com>
2019-05-22 17:01:11 +0100

    Classic / Large margin-top fix

--- src/__experimental__/components/switch/switch-slider-panel-classic.style.js
+++ src/__experimental__/components/switch/switch-slider-panel-classic.style.js
@@ -4,6 +4,7 @@ import { THEMES } from '../../../style/themes';
 export default ({ loading, theme }) => theme.name === THEMES.classic && css`
   &[type='off'] {
     color: ${theme.colors.white};
+    margin-top: 5px;
 
     ${!loading && `
       margin-right: 9px;
