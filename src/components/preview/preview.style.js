c77ca994c2d73956dd79814e0b9ed3e3f0e73086
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-05-22 17:00:40 +0200

    IN PROGRESS, moved css to stylec components

---
+++ src/components/preview/preview.style.js
@@ -0,0 +1,22 @@
+import styled, { keyframes } from 'styled-components';
+
+const shimmer = keyframes`
+  0% { opacity:0.1 }
+  70% { opacity:0.6 }
+  100% { opacity:0.1 }
+`;
+
+const PreviewStyle = styled.span`
+  animation: ${shimmer} 2s ease infinite;
+  background: ${({ theme }) => theme.colors.previewBackground};
+  display: block;
+  height: 15px;
+  opacity: 0.6;
+  width: 100%;
+
+  & + & {
+    margin-top: 3px;
+}
+`;
+
+export default PreviewStyle;
--- src/style/themes/base/base-theme.config.js
+++ src/style/themes/base/base-theme.config.js
@@ -24,6 +24,7 @@ export default (palette) => {
       border: palette.slateTint(40),
       focusedIcon: palette.slateTint(20),
       focusedLinkBackground: palette.goldTint(50),
+      previewBackground: palette.slateTint(75),
 
       // status
       error: palette.errorRed,
