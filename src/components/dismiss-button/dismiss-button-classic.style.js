2763aad4681e67ca1355b6b668d92ee9219770c8
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-05-22 11:18:08 +0200

    remove extracted classic colors

--- src/components/dismiss-button/dismiss-button-classic.style.js
+++ src/components/dismiss-button/dismiss-button-classic.style.js
@@ -1,11 +1,21 @@
 import { css } from 'styled-components';
 import { THEMES } from '../../style/themes';
-import dismissColors from './dismiss-button-variants';
+
+const colors = {
+  error: '#c7384f',
+  warning: '#ff7d00',
+  success: '#50b848',
+  info: '#1573e6',
+  default: '#335c6d',
+  help: '#ffab00',
+  maintenance: '#ff7d00',
+  new: '#639'
+};
 
 export default ({ theme, variant }) => theme.name === THEMES.classic && css`
   .icon-close {
     &:before {
-      color: ${dismissColors[variant]};
+      color: ${colors[variant]};
     }
   }
 
