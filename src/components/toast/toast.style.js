05db868c7f89e96c5bb07de04c518443823fb89f
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-05-23 16:48:35 +0200

    update padding

--- src/components/toast/toast.style.js
+++ src/components/toast/toast.style.js
@@ -11,7 +11,6 @@ const ToastStyle = styled(MessageStyle)`
   right: 30px;
   top: 0;
   box-shadow: 0 15px 20px 0 rgba(2,18,36, 0.2);
-
   ${classicToastStyle}   
 
 `;
@@ -21,6 +20,7 @@ const ToastTypeStyle = styled(TypeIcon)`
 `;
 
 const ToastContentStyle = styled(MessageContentStyle)`
+padding: 10px 20px 11px 20px;
     ${classicToastContentStyle};
 `;
 
