979987874075d6bfff729547a768c7da6f5876f5
grabkowski <jgrabkowski@pgs-soft.com>
2019-05-28 13:48:04 +0200

    changed file names to comply with the new naming conventions

--- src/components/multi-action-button/multi-action-button.spec.js
+++ src/components/multi-action-button/multi-action-button.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
-import MultiActionButton from './multi-action-button';
+import MultiActionButton from './multi-action-button.component';
 import Icon from './../icon';
 import Button from './../button';
 import { shallow } from 'enzyme';
