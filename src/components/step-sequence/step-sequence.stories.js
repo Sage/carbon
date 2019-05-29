8b9080a0a3be9b90ed3487918dcaa68d4b526433
dquilter <david_quilter@hotmail.com>
2019-05-29 14:14:11 +0100

    FE-1414 step sequence (#2069)
    Updates the StepSequence and StepSequenceItem components to use styled-components, and adds the DLS theme styles.

--- src/components/step-sequence/step-sequence.stories.js
+++ src/components/step-sequence/step-sequence.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
-import { StepSequence, StepSequenceItem } from './step-sequence';
+import StepSequence from './step-sequence.component';
+import StepSequenceItem from './step-sequence-item/step-sequence-item.component';
 import OptionsHelper from '../../utils/helpers/options-helper';
 
 storiesOf('Step Sequence', module).add('default', () => {
@@ -11,8 +12,8 @@ storiesOf('Step Sequence', module).add('default', () => {
     <StepSequence orientation={ orientation }>
       <StepSequenceItem
         aria-label='Step 1 of 5'
-        ariaCompleteLabel='Complete'
-        ariaCurrentLabel='Current'
+        hiddenCompleteLabel='Complete'
+        hiddenCurrentLabel='Current'
         indicator='1'
         status='complete'
       >
@@ -20,8 +21,8 @@ storiesOf('Step Sequence', module).add('default', () => {
       </StepSequenceItem>
       <StepSequenceItem
         aria-label='Step 2 of 5'
-        ariaCompleteLabel='Complete'
-        ariaCurrentLabel='Current'
+        hiddenCompleteLabel='Complete'
+        hiddenCurrentLabel='Current'
         indicator='2'
         status='complete'
       >
@@ -29,8 +30,8 @@ storiesOf('Step Sequence', module).add('default', () => {
       </StepSequenceItem>
       <StepSequenceItem
         aria-label='Step 3 of 5'
-        ariaCompleteLabel='Complete'
-        ariaCurrentLabel='Current'
+        hiddenCompleteLabel='Complete'
+        hiddenCurrentLabel='Current'
         indicator='3'
         status='current'
       >
@@ -38,8 +39,8 @@ storiesOf('Step Sequence', module).add('default', () => {
       </StepSequenceItem>
       <StepSequenceItem
         aria-label='Step 4 of 5'
-        ariaCompleteLabel='Complete'
-        ariaCurrentLabel='Current'
+        hiddenCompleteLabel='Complete'
+        hiddenCurrentLabel='Current'
         indicator='4'
         status='incomplete'
       >
@@ -47,8 +48,8 @@ storiesOf('Step Sequence', module).add('default', () => {
       </StepSequenceItem>
       <StepSequenceItem
         aria-label='Step 5 of 5'
-        ariaCompleteLabel='Complete'
-        ariaCurrentLabel='Current'
+        hiddenCompleteLabel='Complete'
+        hiddenCurrentLabel='Current'
         indicator='5'
         status='incomplete'
       >
