d351d74fb8096fd192ab503fecaa697549d51288
Ian Forster <ian.forster@sage.com>
2019-06-04 13:36:40 +0100

    moved pill documentation into its own directory

--- src/components/pill/pill.stories.js
+++ src/components/pill/pill.stories.js
@@ -5,11 +5,10 @@ import { storiesOf } from '@storybook/react';
 import { text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import Pill from './pill.component';
-import notes from './notes.md';
 import OptionsHelper from '../../utils/helpers/options-helper';
-import { StoryHeader, StoryCode } from '../../../.storybook/style/storybook-info.styles';
 import classic from '../../style/themes/classic';
 import { THEMES } from '../../style/themes';
+import { notes, Info } from './documentation';
 
 const getKnobs = (theme) => {
   // eslint-disable multiline-ternary
@@ -47,34 +46,7 @@ storiesOf('Pill', module)
       </ThemeProvider>
     );
   }, {
-    info: {
-      text: (
-        <div>
-          <p>A Pill widget.</p>
-
-          <StoryHeader>Implementation</StoryHeader>
-
-          <p>In your file</p>
-
-          <StoryCode padded>
-            {'import Pill from "carbon-react/lib/components/pill"'}
-          </StoryCode>
-
-          <p>To render a Pager:</p>
-
-          <StoryCode padded>
-            {'<Pill as="warning">My warning text</Pill>'}
-          </StoryCode>
-
-          <p>Additionally you can pass optional props to the Pill component</p>
-
-          <p>
-              &ndash; as: Customizes the appearence of the pill changing the colour
-              (see the iconColorSets for possible values).
-          </p>
-        </div>
-      )
-    },
+    info: { Pill, text: Info },
     notes: { markdown: notes }
   }).add('default', () => {
     const {
@@ -93,33 +65,6 @@ storiesOf('Pill', module)
       </Pill>
     );
   }, {
-    info: {
-      text: (
-        <div>
-          <p>A Pill widget.</p>
-
-          <StoryHeader>Implementation</StoryHeader>
-
-          <p>In your file</p>
-
-          <StoryCode padded>
-            {'import Pill from "carbon-react/lib/components/pill"'}
-          </StoryCode>
-
-          <p>To render a Pager:</p>
-
-          <StoryCode padded>
-            {'<Pill as="warning">My warning text</Pill>'}
-          </StoryCode>
-
-          <p>Additionally you can pass optional props to the Pill component</p>
-
-          <p>
-                &ndash; as: Customizes the appearence of the pill changing the colour
-                (see the iconColorSets for possible values).
-          </p>
-        </div>
-      )
-    },
+    info: { Pill, text: Info },
     notes: { markdown: notes }
   });
