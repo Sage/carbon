7c30bcfe1053e5d897f2db3c116bc3c9c4c1beff
grabkowski <jgrabkowski@pgs-soft.com>
2019-06-06 15:43:58 +0200

    fixed display issues

--- src/components/multi-action-button/multi-action-button.style.js
+++ src/components/multi-action-button/multi-action-button.style.js
@@ -1,34 +1,72 @@
 import styled, { css } from 'styled-components';
 import StyledButton from '../button/button.style';
-import { classicColorsSet } from '../button/button-classic.style';
 import StyledSplitButtonChildrenContainer from '../split-button/split-button-children.style';
+import { THEMES } from '../../style/themes';
+import StyledSplitButtonToggle from '../split-button/split-button-toggle.style';
+import baseTheme from '../../style/themes/base';
 
 const StyledMultiActionButton = styled.div`
-  ${({ buttonType, legacyColorVariant }) => buttonType === 'secondary' && css`
-      background-color: transparent;
-      color: ${classicColorsSet[legacyColorVariant].default};
-  `}
+  ${StyledSplitButtonToggle} {
+    border-left-width: 2px;
+  }
+
+  ${StyledSplitButtonToggle}:focus {
+    margin: 0 -1px;
+  }
 
   ${StyledSplitButtonChildrenContainer} {
     left: 0;
     right: auto;
-    padding-top: 5px;
-    top: 29px;
-    z-index: 12;
 
-    ${StyledButton} {
-      border: none;
-      border-radius: 0;
-      color: #fff;
-      display: block;
-      margin-left: 0;
-      min-width: 100%;
-      padding-bottom: 5px;
+    ${({ align }) => align === 'right' && css`
+      left: auto;
+      right: 0;
+
+      ${StyledButton} {
+        padding-left: 25px;
+        padding-right: 25px;
+        text-align: right;
+      }
+    `}
+  }
+
+  .carbon-icon {
+    &.icon-dropdown {
+      margin-right: 0;
+      top: 0px;
+    }
+  }
+
+  ${({ theme }) => theme.name === THEMES.classic && css`
+    ${StyledSplitButtonToggle} {
+      border-left-width: 1px;
+    }
+
+    ${StyledSplitButtonToggle},
+    ${StyledSplitButtonToggle}:focus {
+      margin: 0;
+      padding: 7px 10px 7px 18px;
+    }
+
+    ${StyledSplitButtonChildrenContainer} {
       padding-top: 5px;
-      text-align: left;
+      top: 29px;
+      z-index: 12;
 
-      &:hover {
-        background-color: #163777;
+      ${StyledButton} {
+        border: none;
+        border-radius: 0;
+        color: #fff;
+        display: block;
+        margin-left: 0;
+        min-width: 100%;
+        padding-bottom: 5px;
+        padding-top: 5px;
+        text-align: left;
+
+        &:hover {
+          background-color: #163777;
+        }
       }
     }
 
@@ -53,28 +91,11 @@ const StyledMultiActionButton = styled.div`
         }
       }
     `}
-    
-    ${({ align }) => align === 'right' && css`
-      left: auto;
-      right: 0;
-
-      ${StyledButton} {
-        padding-left: 25px;
-        padding-right: 25px;
-        text-align: right;
-      }
-    `}
-  }
-
-  .carbon-icon {
-    &.icon-dropdown {
-      margin-right: 0;
-      top: 0px;
-    }
-  }
+  `}
 `;
 
 StyledMultiActionButton.defaultProps = {
+  theme: baseTheme,
   legacyColorVariant: 'blue'
 };
 
