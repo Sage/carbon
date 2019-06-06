f9447ab07045028e004a9e6614aeadafd228b4d7
Ed Leeks <edward.leeks@sage.com>
2019-06-06 15:18:19 +0100

    fixes border on bottom of wrapper

--- src/components/table/table.style.js
+++ src/components/table/table.style.js
@@ -56,6 +56,7 @@ export const StyledInternalTableWrapper = styled.div`
   border-radius: 0px;
   overflow: visible;
   position: relative;
+  border-bottom: none;
   
   ${({ onConfigure, theme }) => onConfigure && css`
     ${StyledTable} {
