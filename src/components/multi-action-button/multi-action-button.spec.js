092efb79b07e08e10d192d583ea9c54aa3289ded
grabkowski <jgrabkowski@pgs-soft.com>
2019-06-10 11:54:00 +0200

    added unit tests

--- src/components/multi-action-button/multi-action-button.spec.js
+++ src/components/multi-action-button/multi-action-button.spec.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
-import { shallow } from 'enzyme';
+import TestRenderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { ThemeProvider } from 'styled-components';
+import { shallow, mount } from 'enzyme';
 import MultiActionButton from './multi-action-button.component';
 import Button from '../button';
 import { elementsTagTest, rootTagTest } from '../../utils/helpers/tags/tags-specs';
+import { assertStyleMatch } from '../../__spec_helper__/test-utils';
+import StyledSplitButtonChildrenContainer from '../split-button/split-button-children.style';
+import StyledButton from '../button/button.style';
+import classicTheme from '../../style/themes/classic';
 
 describe('MultiActionButton', () => {
+  let wrapper;
+
   describe('tags', () => {
     describe('on component', () => {
-      const multiActionButtonSelector = '[data-component="multi-action-button"]';
-      const wrapper = shallow(
-        <MultiActionButton
-          data-element='bar'
-          data-role='baz'
-          text='Test'
-        >
-          <Button>Test</Button>
-        </MultiActionButton>
-      );
-
       it('include correct component, element and role data tags', () => {
+        const multiActionButtonSelector = '[data-component="multi-action-button"]';
+        wrapper = render({
+          'data-element': 'bar',
+          'data-role': 'baz',
+          text: 'Test'
+        });
+
         rootTagTest(wrapper.find(multiActionButtonSelector), 'multi-action-button', 'bar', 'baz');
       });
     });
 
     describe('on internal elements', () => {
-      const wrapper = shallow(
-        <MultiActionButton text='Test'>
-          <Button>Test</Button>
-        </MultiActionButton>
-      );
+      wrapper = render();
       wrapper.setState({ showAdditionalButtons: true });
 
       elementsTagTest(wrapper, [
@@ -38,4 +38,62 @@ describe('MultiActionButton', () => {
       ]);
     });
   });
+
+  describe('when rendered', () => {
+    it('should match the snapshot', () => {
+      expect(render({}, TestRenderer.create)).toMatchSnapshot();
+    });
+  });
+
+  describe('with align prop set to "right"', () => {
+    beforeEach(() => {
+      wrapper = render({ align: 'right' }, mount);
+    });
+
+    it('menu should be right positioned relative the Toggle Button', () => {
+      assertStyleMatch({
+        left: 'auto',
+        right: '0'
+      }, wrapper, { modifier: `${StyledSplitButtonChildrenContainer}` });
+    });
+
+    it('text inside child buttons should be aligned right', () => {
+      assertStyleMatch({
+        textAlign: 'right'
+      }, wrapper, { modifier: `${StyledSplitButtonChildrenContainer} ${StyledButton}` });
+    });
+  });
+
+  describe('when rendered with "classic" theme', () => {
+    it('should match the snapshot', () => {
+      expect(renderWithTheme({
+        carbonTheme: classicTheme
+      }, TestRenderer.create)).toMatchSnapshot();
+    });
+
+    it('should match the snapshot', () => {
+      expect(renderWithTheme({
+        carbonTheme: classicTheme,
+        as: 'transparent'
+      }, TestRenderer.create)).toMatchSnapshot();
+    });
+  });
 });
+
+function render(props, renderer = shallow) {
+  return renderer(
+    <MultiActionButton text='Test' { ...props }>
+      <Button>Test</Button>
+    </MultiActionButton>
+  );
+}
+
+function renderWithTheme(props = {}, renderer = shallow) {
+  return renderer(
+    <ThemeProvider theme={ props.carbonTheme }>
+      <MultiActionButton text='Test' { ...props }>
+        <Button>Test</Button>
+      </MultiActionButton>
+    </ThemeProvider>
+  );
+};
