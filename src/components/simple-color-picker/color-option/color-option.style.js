77474895f2f0345c5d12c1266bfaabf88bf1dbdd
izakrzewska <izakrzewska@pgs-soft.com>
2019-06-11 15:19:08 +0200

    remove color-option.scss file

---
+++ src/components/simple-color-picker/color-option/color-option.style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import StyledColorSampleBox from './color-sample-box.style';
+
+const StyledColorOption = styled.li`
+  float: left;
+  margin-right: 1px;
+  margin-bottom: 1px;
+  list-style: none;
+
+  &.common-input + &.common-input {
+    margin-top: 0;
+  }
+
+  .carbon-color-option__radio-button-input {
+    &:checked + ${StyledColorSampleBox} {
+      .carbon-icon {
+        display: block;
+      }
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:focus + ${StyledColorSampleBox} {
+      border-color: #003349;
+    }
+
+    position: absolute;
+    opacity: 0;
+    height: 56px;
+    width: 56px;
+  }
+`;
+
+export default StyledColorOption;
---
+++ src/components/simple-color-picker/color-option/tick-icon.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import Icon from '../../icon/icon';
+
+const StyledTickIcon = styled(Icon)`
+  height: 22px;
+  width: 22px;
+  pointer-events: none;
+
+  &.carbon-icon {
+    display: none;
+  }
+
+  &:before {
+    font-size: 22px;
+  }
+`;
+
+export default StyledTickIcon;
