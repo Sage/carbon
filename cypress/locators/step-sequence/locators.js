adb605901d3590401ef91a88b283ab905044f308
marcinstachowicz <mstachowicz@pgs-soft.com>
2019-06-11 11:14:25 +0200

    fix according to comments

--- cypress/locators/step-sequence/locators.js
+++ cypress/locators/step-sequence/locators.js
@@ -1,5 +1,6 @@
 // component preview locators
-export const STEP_SEQUENCE = '#story-root > ol';
+const STORY_ROOT = '#story-root';
+export const STEP_SEQUENCE = `${STORY_ROOT} > ol`;
 export const STEP_SEQUENCE_ELEMENT = `${STEP_SEQUENCE} > li`;
-export const STEP_SEQUENCE_ITEM = '#story-root > li';
+export const STEP_SEQUENCE_ITEM = `${STORY_ROOT} > li`;
 export const STEP_SEQUENCE_ITEM_INDICATOR = `${STEP_SEQUENCE_ITEM} > span > span`;
