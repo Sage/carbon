ed8fdcdc07c851b345a26016eb6b2dcd14d910e6
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-06-14 15:27:08 +0200

    update design of component

--- src/components/configurable-items/configurable-item-row/configurable-item-row.style.js
+++ src/components/configurable-items/configurable-item-row/configurable-item-row.style.js
@@ -5,7 +5,7 @@ import baseTheme from '../../../style/themes/base';
 
 const ConfigurableItemRowIconStyle = styled(Icon)`
   cursor: move;
-  padding-right: 10px;
+  padding-right: 12px;
 
   ${({ theme }) => theme.name === THEMES.classic && css`
       padding-right: 0;
@@ -21,10 +21,11 @@ const ConfigurableItemRowContentWrapperStyle = styled.div`
 const ConfigurableItemRowStyle = styled.li`
   border-bottom: 1px solid ${({ theme }) => theme.disabled.input};
   font-weight: bold;
-  padding: 0.5em 0.5em 0.7em;
+  padding: 5px 0.5em;
 
   ${({ theme }) => theme.name === THEMES.classic && css`
     border-bottom: 1px solid #CCD6DA;
+    padding: 0.5em 0.5em 0.7em;
   `}
 
   ${({ isDragging, isDragged }) => (isDragging || isDragged)
