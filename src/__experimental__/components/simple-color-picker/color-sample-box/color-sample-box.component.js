c3dd528f1570c4e79afa65448591531853e4c00c
izakrzewska <izakrzewska@pgs-soft.com>
2019-06-25 07:47:38 +0200

    tick icon for dls themes

--- src/__experimental__/components/simple-color-picker/color-sample-box/color-sample-box.component.js
+++ src/__experimental__/components/simple-color-picker/color-sample-box/color-sample-box.component.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withTheme } from 'styled-components';
 import StyledColorSampleBox from './style/color-sample-box.style';
 import StyledTickIcon from '../tick-icon/tick-icon.style';
-import { THEMES } from '../../../../style/themes';
-import baseTheme from '../../../../style/themes/base';
 
-const ColorSampleBox = ({ theme, color, checked }) => {
-  const tickIcon = checked && theme.name === THEMES.classic ? <StyledTickIcon checked type='tick' /> : null;
+const ColorSampleBox = ({ color, checked }) => {
+  const tickIcon = checked ? <StyledTickIcon checked type='tick' /> : null;
   return <StyledColorSampleBox color={ color }>{tickIcon}</StyledColorSampleBox>;
 };
 
 ColorSampleBox.propTypes = {
-  theme: PropTypes.object,
   color: PropTypes.string,
   checked: PropTypes.bool
 };
 
-ColorSampleBox.defaultProps = {
-  theme: baseTheme
-};
-
-export default withTheme(ColorSampleBox);
-export { ColorSampleBox };
+export default ColorSampleBox;
