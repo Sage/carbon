8e589fd4a211f5817c1ea4cdabf127b9c0387c6c
izakrzewska <izakrzewska@pgs-soft.com>
2019-06-25 14:48:07 +0200

    add getRgbValues helper

--- src/__experimental__/components/simple-color-picker/color-sample-box/color-sample-box.component.js
+++ src/__experimental__/components/simple-color-picker/color-sample-box/color-sample-box.component.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StyledColorSampleBox from './style/color-sample-box.style';
 import StyledTickIcon from '../tick-icon/tick-icon.style';
-import calculateBrightness from '../../../../style/utils/calculate-brightness';
 
 const ColorSampleBox = ({ color, checked }) => {
-  const iconColor = calculateBrightness(color);
   const tickIcon = checked ? (
     <StyledTickIcon
-      iconColor={ iconColor } checked
+      bgColor={ color } checked
       type='tick'
     />
   ) : null;
