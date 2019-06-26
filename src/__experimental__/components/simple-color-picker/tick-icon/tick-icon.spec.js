4646fc9d3278f962e448f392786f7d51147cd507
izakrzewska <izakrzewska@pgs-soft.com>
2019-06-26 12:43:37 +0200

    update tests && proptypes checking

--- src/__experimental__/components/simple-color-picker/tick-icon/tick-icon.spec.js
+++ src/__experimental__/components/simple-color-picker/tick-icon/tick-icon.spec.js
@@ -3,6 +3,7 @@ import 'jest-styled-components';
 import TestRenderer from 'react-test-renderer';
 import { assertStyleMatch } from '../../../../__spec_helper__/test-utils';
 import StyledTickIcon from './tick-icon.style';
+import baseTheme from '../../../../style/themes/base';
 
 function render(props) {
   return TestRenderer.create(<StyledTickIcon { ...props } type='tick' />);
@@ -13,13 +14,35 @@ describe('StyledTickIcon', () => {
 
   describe('when checked', () => {
     it('changes property display to block', () => {
-      wrapper = render({ checked: true });
+      wrapper = render({ checked: true, color: '#676767' });
       assertStyleMatch(
         {
           display: 'block'
         },
         wrapper.toJSON(),
         { modifier: '&.carbon-icon' }
+      );
+    });
+
+    it('applies white icon on the dark background', () => {
+      wrapper = render({ checked: true, color: '#000' });
+      assertStyleMatch(
+        {
+          color: baseTheme.colors.white
+        },
+        wrapper.toJSON(),
+        { modifier: '&.carbon-icon::before' }
+      );
+    });
+
+    it('applies black icon on the light background', () => {
+      wrapper = render({ checked: true, color: '#fff' });
+      assertStyleMatch(
+        {
+          color: baseTheme.text.color
+        },
+        wrapper.toJSON(),
+        { modifier: '&.carbon-icon::before' }
       );
     });
   });
