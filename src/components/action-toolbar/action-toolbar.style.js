2e0918a218684e954c83b5ecba0df9b34029d63f
izakrzewska <izakrzewska@pgs-soft.com>
2019-07-05 12:18:24 +0200

    change icons & remove focus border

--- src/components/action-toolbar/action-toolbar.style.js
+++ src/components/action-toolbar/action-toolbar.style.js
@@ -5,12 +5,6 @@ import { isClassic } from '../../utils/helpers/style-helper';
 const StyledActionToolbar = styled.div`
   display: inline-block;
   padding: 15px 0;
-
-  * {
-    &:focus {
-      border: 1px solid red;
-    }
-  }
 `;
 
 const StyledActionToolbarTotal = styled.div`
