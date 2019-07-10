8bda4f8a9ae5fd93e7298e5a2831241fcfba73cc
Michael Hogg <michael@kyan.com>
2019-07-10 11:12:08 +0100

    (FE-1412) FE-1767: update portrait.stories.js with new Classic story

--- src/components/portrait/portrait.stories.js
+++ src/components/portrait/portrait.stories.js
@@ -1,37 +1,59 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select, text, boolean } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import Portrait from './portrait.component';
 import { notes, info } from './documentation';
+import classic from '../../style/themes/classic';
 
-storiesOf('Portrait', module).add(
-  'default',
-  () => {
-    const alt = text('alt', Portrait.defaultProps.alt);
-    const darkBackground = boolean('darkBackground', Portrait.defaultProps.darkBackground);
-    const source = select('source', ['src', 'gravatar'], 'src');
-    const gravatar = source === 'gravatar' ? text('gravatar') : undefined;
-    const src = source === 'src' ? text('src') : undefined;
-    const initials = text('initials', 'AZ');
-    const shape = select('shape', OptionsHelper.shapesVaried, Portrait.defaultProps.shape);
-    const size = select('size', OptionsHelper.sizesFull, Portrait.defaultProps.size);
+function commonKnobs() {
+  const source = select('source', ['src', 'gravatar'], 'src');
 
-    return (
+  return ({
+    alt: text('alt', Portrait.defaultProps.alt),
+    darkBackground: boolean('darkBackground', Portrait.defaultProps.darkBackground),
+    gravatar: source === 'gravatar' ? text('gravatar') : undefined,
+    src: source === 'src' ? text('src') : undefined,
+    initials: text('initials', 'AZ'),
+    size: select('size', OptionsHelper.sizesFull, Portrait.defaultProps.size)
+  });
+}
+
+function classicKnobs() {
+  return {
+    shape: select('shape', OptionsHelper.shapesVaried, 'standard')
+  };
+}
+
+function dlsKnobs() {
+  return {
+    shape: select('shape', OptionsHelper.shapesPortrait, 'square')
+  };
+}
+
+storiesOf('Portrait', module)
+  .add('classic', () => (
+    <ThemeProvider theme={ classic }>
       <Portrait
-        alt={ alt }
-        darkBackground={ darkBackground }
-        gravatar={ gravatar }
-        initials={ initials }
-        shape={ shape }
-        size={ size }
-        src={ src }
+        { ...commonKnobs() }
+        { ...classicKnobs() }
       />
-    );
-  },
+    </ThemeProvider>
+  ),
   {
     info: { text: info },
     notes: { markdown: notes },
     knobs: { escapeHTML: false }
-  }
-);
+  })
+  .add('default', () => (
+    <Portrait
+      { ...commonKnobs() }
+      { ...dlsKnobs() }
+    />
+  ),
+  {
+    info: { text: info },
+    notes: { markdown: notes },
+    knobs: { escapeHTML: false }
+  });
