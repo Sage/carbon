31a2a1773130c408ee3d3f9080ecd0baa6d5951b
grabkowski <jgrabkowski@pgs-soft.com>
2019-07-17 14:06:28 +0200

    removed button styling from the Help Component

--- src/components/help/help.style.js
+++ src/components/help/help.style.js
@@ -3,13 +3,18 @@ import baseTheme from '../../style/themes/base';
 import { isClassic } from '../../utils/helpers/style-helper';
 
 const StyledHelp = styled.button`
+  background: none;
   color: ${({ theme }) => theme.help.color};
   cursor: default;
   border: none;
   outline: none;
   display: inline-block;
+  font-size: 14px;
+  padding: 0;
   position: relative;
+  margin-bottom: 0;
   margin-left: 8px;
+  margin-top: 0;
   top: -1px;
 
   ${({ href }) => href && css`
