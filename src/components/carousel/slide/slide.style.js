f861b58c8d22faf2b835428753d5610652829fc2
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-07-23 16:34:27 +0200

    experimental mechanism for carousel component

--- src/components/carousel/slide/slide.style.js
+++ src/components/carousel/slide/slide.style.js
@@ -3,8 +3,15 @@ import styled, { css } from 'styled-components';
 const SlideStyle = styled.div`
   box-sizing: border-box;
   display: inline-block;
-  width: 100%;
+  /* width: 100%; //legacy */
   z-index: 10;
+  //DLS
+  min-width: 680px;
+  max-width: 680px;
+  height: 400px;
+  box-shadow:0 10px 30px 0 rgba(0,20,29,0.1), 0 30px 60px 0 rgba(0,20,29,0.1);
+  transform: scale(.9);
+
 
   ${({ isPadded }) => isPadded && css`
     padding: 0 60px;
