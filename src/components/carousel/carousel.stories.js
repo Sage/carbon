ad3d68764b5786644daa8a0e25b04c131d60bb32
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-07-30 16:17:45 +0200

    split storybook stories on classic and default

--- src/components/carousel/carousel.stories.js
+++ src/components/carousel/carousel.stories.js
@@ -1,8 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { select, boolean } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
 import notes from './documentation';
-import CarouselWithoutHOC, { Carousel, Slide } from './carousel.component';
+import BaseCarousel, { Carousel, Slide } from './carousel.component';
+import classic from '../../style/themes/classic';
 
 storiesOf('Carousel', module)
   .addParameters({
@@ -10,20 +14,57 @@ storiesOf('Carousel', module)
       propTablesExclude: [Slide]
     }
   })
-  .add('default', () => {
+  .add('classic', () => {
     const indexConfig = [0, 1, 2, 3, 4];
     const transitionConfig = ['slide', 'fade'];
-
-    const initialSlideIndex = select('initialSlideIndex', indexConfig, CarouselWithoutHOC.defaultProps.initialSlideIndex);
+    const initialSlideIndex = select('initialSlideIndex', indexConfig, BaseCarousel.defaultProps.initialSlideIndex);
     const slideIndex = select('slideIndex', indexConfig, indexConfig[0]);
-    const enableSlideSelector = boolean('enableSlideSelector', CarouselWithoutHOC.defaultProps.enableSlideSelector);
-    const enablePreviousButton = boolean('enablePreviousButton', CarouselWithoutHOC.defaultProps.enablePreviousButton);
-    const enableNextButton = boolean('enableNextButton', CarouselWithoutHOC.defaultProps.enableNextButton);
+    const enableSlideSelector = boolean('enableSlideSelector', BaseCarousel.defaultProps.enableSlideSelector);
+    const enablePreviousButton = boolean('enablePreviousButton', BaseCarousel.defaultProps.enablePreviousButton);
+    const enableNextButton = boolean('enableNextButton', BaseCarousel.defaultProps.enableNextButton);
     const transition = select(
       'transition',
       transitionConfig,
-      CarouselWithoutHOC.defaultProps.transition
+      BaseCarousel.defaultProps.transition
     );
+
+    return (
+      <ThemeProvider theme={ classic }>
+        <Carousel
+          initialSlideIndex={ initialSlideIndex }
+          slideIndex={ slideIndex }
+          enableSlideSelector={ enableSlideSelector }
+          enablePreviousButton={ enablePreviousButton }
+          enableNextButton={ enableNextButton }
+          transition={ transition }
+        >
+          <Slide>
+            <h1 style={ { textAlign: 'center' } }>Slide One</h1>
+          </Slide>
+          <Slide>
+            <h1 style={ { textAlign: 'center' } }>Slide Two</h1>
+          </Slide>
+          <Slide>
+            <h1 style={ { textAlign: 'center' } }>Slide Three</h1>
+          </Slide>
+          <Slide>
+            <h1 style={ { textAlign: 'center' } }>Slide Four</h1>
+          </Slide>
+          <Slide>
+            <h1 style={ { textAlign: 'center' } }>Slide Five</h1>
+          </Slide>
+        </Carousel>
+      </ThemeProvider>
+    );
+  })
+  .add('default', () => {
+    const indexConfig = [0, 1, 2, 3, 4];
+    const initialSlideIndex = select('initialSlideIndex', indexConfig, BaseCarousel.defaultProps.initialSlideIndex);
+    const slideIndex = select('slideIndex', indexConfig, indexConfig[0]);
+
+    const handleClick = () => {
+      action('click')();
+    };
 
     const styleElement = {
       height: '400px',
@@ -46,17 +87,13 @@ storiesOf('Carousel', module)
       <Carousel
         initialSlideIndex={ initialSlideIndex }
         slideIndex={ slideIndex }
-        enableSlideSelector={ enableSlideSelector }
-        enablePreviousButton={ enablePreviousButton }
-        enableNextButton={ enableNextButton }
-        transition={ transition }
       >
         <Slide style={ { textAlign: 'center' } }>
           <ExampleCustomElement style={ { backgroundColor: '#003349' } }>
             <h1 style={ { textAlign: 'center', color: '#090' } }>Slide 1</h1>
           </ExampleCustomElement>
         </Slide>
-        <Slide onClick={ () => console.log('test') }>
+        <Slide onClick={ handleClick }>
           <ExampleCustomElement style={ { backgroundColor: '#a85a00' } }>
             <h1 style={ { textAlign: 'center', color: '#fff' } }>Full clickable slide</h1>
           </ExampleCustomElement>
