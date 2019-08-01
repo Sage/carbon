4f1f4ebf3eedcc521e65953e24d798f26ce1e23f
grabkowski <jgrabkowski@pgs-soft.com>
2019-08-01 10:06:52 +0200

    modified the extractProps method to not rely on React PropTypes

--- src/utils/helpers/extract-props/extract-props.js
+++ src/utils/helpers/extract-props/extract-props.js
@@ -1,5 +1,5 @@
 export default function extractProps(props, reactComponent) {
-  const expectedKeys = Object.keys(reactComponent.propTypes);
+  const expectedKeys = reactComponent.safeProps;
   const mainObjectKeys = Object.keys(props);
   const filteredKeys = mainObjectKeys.filter(key => expectedKeys.includes(key));
   const filteredObject = filteredKeys.reduce((obj, key) => {
