41b54aeedd4ed6dbfefb2f80c5f173e9c6c51e04
Damian Boni <damian@kyanmedia.com>
2019-08-01 15:44:54 +0100

    FE-1655 refactored isClassic back to its originalf orm.

--- src/utils/helpers/style-helper/style-helper.js
+++ src/utils/helpers/style-helper/style-helper.js
@@ -1,6 +1,6 @@
 import { THEMES } from '../../../style/themes';
 
 // eslint-disable-next-line import/prefer-default-export
-export function isClassic(theme) {
-  return theme.name === THEMES.classic;
+export function isClassic({ name }) {
+  return name === THEMES.classic;
 }
