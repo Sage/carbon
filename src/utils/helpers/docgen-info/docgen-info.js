65c9ace8746998a4cff4fb7f41c02ebbf9a311e8
Ian Forster <ian.forster@sage.com>
2019-08-02 11:07:00 +0100

    specs for docgen-info helper

--- src/utils/helpers/docgen-info/docgen-info.js
+++ src/utils/helpers/docgen-info/docgen-info.js
@@ -1,11 +1,13 @@
 // // takes object and regular expression
 const keyMatch = (obj, regex) => {
-  const matched = Object.keys(obj).find(key => (key.match(regex) ? obj[key] : null));
-  return obj[matched][0] || null;
+  const matched = Object.keys(obj).find(
+    key => (key.match(regex) ? obj[key] : null)
+  );
+  return (matched) ? obj[matched][0] : null;
 };
 
 const getDocGenInfo = (obj, regex) => {
-  return keyMatch(obj, regex);
+  return (obj) ? keyMatch(obj, regex) : null;
 };
 
 export default getDocGenInfo;
---
+++ src/utils/helpers/docgen-info/docgen-info.spec.js
@@ -0,0 +1,68 @@
+import getDocGenInfo from './docgen-info';
+
+const Aardvark = {};
+const dgiUndefined = undefined;
+const dgiInvalid = {
+  some: 'rubbish'
+};
+const docgenInfo = {
+  'src/utils/helpers/docgen-info/spec-component/aardvark.component.js': [
+    {
+      description: '',
+      displayName: 'Aardvark',
+      methods: [],
+      props: {
+        shellcolour: {
+          type: {
+            name: 'string'
+          },
+          required: false,
+          description: 'Set the shellcolour of the Aardvark',
+          defaultValue: {
+            value: "'green'",
+            computed: false
+          }
+        }
+      }
+    }
+  ]
+};
+
+describe('docgenInfo Helper', () => {
+  describe('when no docgenInfo file', () => {
+    it('__docgenInfo is null', () => {
+      Aardvark.__docgenInfo = getDocGenInfo(
+        dgiUndefined,
+        /aardvark\.component/
+      );
+      expect(Aardvark.__docgenInfo).toBeNull();
+    });
+  });
+
+  describe('when an invalid docgenInfo file', () => {
+    it('__docgenInfo is null', () => {
+      Aardvark.__docgenInfo = getDocGenInfo(
+        dgiInvalid,
+        /aardvark\.component/
+      );
+      expect(Aardvark.__docgenInfo).toBeNull();
+    });
+  });
+
+  describe('when a valid docgenInfo file', () => {
+    beforeAll(() => {
+      Aardvark.__docgenInfo = getDocGenInfo(
+        docgenInfo,
+        /aardvark\.component/
+      );
+    });
+
+    it('appends a __docgenInfo object', () => {
+      expect(Aardvark).toHaveProperty('__docgenInfo');
+    });
+
+    it('__docgenInfo has a description', () => {
+      expect(Aardvark.__docgenInfo.props.shellcolour.description).toEqual('Set the shellcolour of the Aardvark');
+    });
+  });
+});
