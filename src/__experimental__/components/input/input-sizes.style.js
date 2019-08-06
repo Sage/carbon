a8e0c441c03e26230f5dd4d9f0bce5307ab9e374
grabkowski <jgrabkowski@pgs-soft.com>
2019-08-06 18:01:33 +0200

    added offset to the validation tooltip on modern themes

--- src/__experimental__/components/input/input-sizes.style.js
+++ src/__experimental__/components/input/input-sizes.style.js
@@ -1,14 +1,20 @@
 export default {
   small: {
     height: '32px',
-    padding: '8px'
+    padding: '8px',
+    tooltipTopOffset: 11,
+    tooltipRightOffset: 13
   },
   medium: {
     height: '40px',
-    padding: '11px'
+    padding: '11px',
+    tooltipTopOffset: 14,
+    tooltipRightOffset: 16
   },
   large: {
     height: '48px',
-    padding: '13px'
+    padding: '13px',
+    tooltipTopOffset: 18,
+    tooltipRightOffset: 18
   }
 };
