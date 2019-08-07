24e6d193f6555849262cf2cc81612a0816448153
grabkowski <jgrabkowski@pgs-soft.com>
2019-08-07 09:53:03 +0200

    refactored Validation Icon to use the isClassic util

--- src/components/validations/validation-icon.style.js
+++ src/components/validations/validation-icon.style.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 import BaseTheme from '../../style/themes/base';
-import { THEMES } from '../../style/themes';
+import { isClassic } from '../../utils/helpers/style-helper';
 
 const ValidationIconStyle = styled.div`
   color: ${({ type, theme }) => theme.colors[type]};
@@ -9,19 +9,14 @@ const ValidationIconStyle = styled.div`
   align-items: center;
   padding-left: 5px;
 
-  ${({ theme }) => {
-    if (theme.name === THEMES.classic) {
-      return css`
-        margin-right: -2px;
-        margin-top: -1px;
+  ${({ theme }) => isClassic(theme) && css`
+    margin-right: -2px;
+    margin-top: -1px;
 
-        .carbon-icon:before {
-          font-size: 20px;
-        }
-      `;
+    .carbon-icon:before {
+      font-size: 20px;
     }
-    return null;
-  }}
+  `};
 `;
 
 ValidationIconStyle.defaultProps = {
