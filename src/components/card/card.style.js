a2e0795b862a7421506d04375f089129a87b939c
Ian Forster <ian.forster@sage.com>
2019-08-14 13:53:04 +0100

    card header, description, footer layout styling

--- src/components/card/card.style.js
+++ src/components/card/card.style.js
@@ -2,23 +2,69 @@ import styled, { css } from 'styled-components';
 import propTypes from 'prop-types';
 import baseTheme from '../../style/themes/base';
 
-const CardStyle = styled.div`
+const StyledCard = styled.div`
   ${({ border, theme }) => {
     return css`
-        ${border && css`
-          border: 1px solid ${theme.colors.border}
+      background-color: ${theme.colors.white};
+      box-shadow: ${theme.shadows.cards};
+      margin-bottom: 32px;
+      position: relative;
+      transition: all 0.3s ease-in-out;
+
+      ${border && css`
+        border: 1px solid ${theme.colors.border};
+      `}
+
+      ${!border && css`
+        border: none;
       `}
     `;
   }
 }`;
 
-CardStyle.defaultProps = {
+StyledCard.defaultProps = {
   border: true,
   theme: baseTheme
 };
 
-CardStyle.propTypes = {
+StyledCard.propTypes = {
   border: propTypes.bool
 };
 
-export default CardStyle;
+const StyledCardHeader = styled.div`
+  ${() => {
+    return css`
+      padding: 32px 32px 0;
+      margin-bottom: 24px;
+    `;
+  }
+}`;
+
+const StyledCardDescription = styled.div`
+${() => {
+    return css`
+      text-align: center;
+      margin-bottom: 32px;
+  `;
+  }
+}`;
+
+const StyledCardFooter = styled.div`
+${({ theme }) => {
+    return css`
+      background-color: ${theme.card.footerBackground};
+      border-top: ${theme.card.footerBorder};
+      height: 56px;
+      line-height: 56px;
+      padding: 0;
+      text-align: center;
+    `;
+  }
+}`;
+
+export {
+  StyledCard,
+  StyledCardHeader,
+  StyledCardDescription,
+  StyledCardFooter
+};
