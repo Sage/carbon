e6918e6f4c53a4ebb0c2b8d0ec540cfd8150528e
Krystian Jedrzejowski <kjedrzejowski@pgs-soft.com>
2019-08-14 10:46:25 +0200

    FIX: Fix locator for decimal input

--- cypress/locators/decimal/locators.js
+++ cypress/locators/decimal/locators.js
@@ -2,4 +2,4 @@
 export const INPUT_PRECISION_SLIDER = 'input[name="precision"][type="range"]';
 
 // component preview locators
-export const LABEL_INPUT_PREVIEW = 'div[role="presentation"][type="text"]';
+export const LABEL_INPUT_PREVIEW = 'div[role="presentation"]';
