32b19624b7599e490489858f56c0beb36d23de8f
Michael Hogg <michael@kyan.com>
2019-08-19 15:22:53 +0100

    (FE-1527) Replace `<AppLayout>` with `withThemesProvider`

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 import { withA11y } from '@storybook/addon-a11y';
-import AppLayout from 'helpers/components/app-layout/app-layout';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 import setupI18n from '../demo/i18n/config';
 import '../demo/i18n/en';
 import './style/story-root.scss';
@@ -38,8 +38,6 @@ function loadStories() {
 addDecorator(withKnobs);
 addDecorator(withInfo(infoOptions));
 addDecorator(withA11y);
-
-// give all stories access to themes
-addDecorator(story => <AppLayout>{story()}</AppLayout>);
+addDecorator(withThemesProvider());
 
 configure(loadStories, module);
