b7fdd70319fc9023e1ff51d28ab6c592e3431b26
Ian Forster <ian.forster@sage.com>
2019-08-20 14:38:28 +0100

    card drop class selectors for styled components

---
+++ src/components/card/card-position/card-position.style.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components';
+import { POSITION_HEADER, POSITION_MIDDLE, POSITION_FOOTER } from '../card.const';
+
+const StyledCardPosition = styled.div`
+  ${({ positionType, theme }) => {
+    return css`
+      ${positionType === POSITION_HEADER && css`
+          padding: 32px 32px;
+          min-height: 48px;
+      `};
+
+      ${positionType === POSITION_MIDDLE && css`
+        text-align: center;
+        margin-bottom: 32px;
+      `}
+
+      ${positionType === POSITION_FOOTER && css` 
+        background-color: ${theme.card.footerBackground};
+        border-top: ${theme.card.footerBorder};
+        height: 56px;
+        line-height: 56px;
+        padding: 0;
+        text-align: center;
+      `}
+    `;
+  }
+}`;
+
+export default StyledCardPosition;
---
+++ src/components/card/card-position/index.js
@@ -0,0 +1,1 @@
+export { default } from './card-position.component';
