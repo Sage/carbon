526983018c3dbbb89a2d07e05a2350c0299b6b9b
Josh Jimenez Bedrock <josh@kyan.com>
2019-08-21 16:28:03 +0100

    Updated story documentation

--- src/__experimental__/components/checkbox/documentation/info-validations.js
+++ src/__experimental__/components/checkbox/documentation/info-validations.js
@@ -4,9 +4,15 @@ import { StoryHeader } from '../../../../../.storybook/style/storybook-info.styl
 const info = (
   <div>
     <StoryHeader>Checkbox Component Validations</StoryHeader>
-    <p>Uncheck first checkbox to show an <strong>error</strong> message.</p>
-    <p>Uncheck second checkbox to show a <strong>warning</strong> message.</p>
-    <p>Uncheck third checkbox to show a <strong>info</strong> message.</p>
+    <strong>In Form</strong>
+    <p>Tab off the first checkbox to show a <strong>error</strong> state.</p>
+    <p>Tab off the second checkbox to show a <strong>warning</strong> state.</p>
+    <p>Tab off the third checkbox to show a <strong>info</strong> state.</p>
+
+    <strong>In Group</strong>
+    <p>Check first checkbox to see a <strong>warning</strong> state.</p>
+    <p>Check second checkbox to see a <strong>info</strong> state.</p>
+    <p>Check third checkbox to see a <strong>error</strong> state.</p>
   </div>
 );
 
