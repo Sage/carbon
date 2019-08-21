bb407e3cadf80eb4efb77e33dd02712845a5e06f
Josh Jimenez Bedrock <josh@kyan.com>
2019-08-21 13:03:01 +0100

    Updates to the checkbox and checkbox group:
    * Consistence naming when exporting vanilla component.
    * Removed prefix on child key.
    * Updated tests
    * Added icon to checkbox group
    * Updated what elements changed colour when validation runs

--- src/__experimental__/components/checkbox/checkbox-group.component.js
+++ src/__experimental__/components/checkbox/checkbox-group.component.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import tagComponent from '../../../utils/helpers/tags';
-import Label from '../label';
 import { StyledCheckboxGroup } from './checkbox.style';
+import { withValidation } from '../../../components/validations';
+import Label from '../label';
+import ValidationIcon from '../../../components/validations/validation-icon.component';
+import Help from '../../../components/help';
 
 function initialTabIndex(childIndex) {
   return (childIndex > 0) ? -1 : 0;
@@ -12,14 +15,33 @@ function checkedTabIndex(checked) {
   return checked ? 0 : -1;
 }
 
+const getValidationType = ({ hasError, hasWarning }) => {
+  let type = 'info';
+
+  if (hasError) {
+    type = 'error';
+  } else if (hasWarning) {
+    type = 'warning';
+  }
+
+  return type;
+};
+
 const CheckboxGroup = (props) => {
-  const { children, groupName, label } = props;
+  const {
+    children,
+    groupName,
+    label,
+    hasError,
+    hasWarning,
+    hasInfo,
+    labelHelp
+  } = props;
   const [selectedValue, setSelectedValue] = useState(null);
 
   const groupLabelId = `${groupName}-label`;
 
   const buttons = React.Children.map(children, (child, index) => {
-    const key = `cKey-${index}`;
     const checked = selectedValue === child.props.value;
     const tabindex = selectedValue ? checkedTabIndex(checked) : initialTabIndex(index);
 
@@ -33,21 +55,41 @@ const CheckboxGroup = (props) => {
       {
         checked,
         inputName: groupName,
-        key,
         onChange: handleChange,
         tabindex
       }
     );
   });
 
+  const tooltip = () => {
+    if (hasError || hasWarning || hasInfo) {
+      const bool = true;
+      const type = getValidationType(props);
+
+      return (
+        <ValidationIcon
+          type={ type }
+          size='small'
+          isPartOfInput={ bool }
+        />
+      );
+    }
+
+    return labelHelp && <Help>{labelHelp}</Help>;
+  };
+
   return (
     <StyledCheckboxGroup
       aria-labelledby={ groupLabelId }
       role='checkbox'
+      hasError={ hasError }
+      hasWarning={ hasWarning }
+      hasInfo={ hasInfo }
       { ...tagComponent('checkboxgroup', props) }
     >
       <Label id={ groupLabelId }>
         {label}
+        {tooltip()}
       </Label>
       {buttons}
     </StyledCheckboxGroup>
@@ -60,7 +102,21 @@ CheckboxGroup.propTypes = {
   /** Specifies the name prop to be applied to each button in the group */
   groupName: PropTypes.string.isRequired,
   /** The content for the RadioGroup Label */
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  /** Text for help tooltip */
+  labelHelp: PropTypes.string,
+  /** Validation indicators */
+  hasError: PropTypes.bool,
+  hasWarning: PropTypes.bool,
+  hasInfo: PropTypes.bool
 };
 
-export default CheckboxGroup;
+CheckboxGroup.defaultProps = {
+  hasError: false,
+  hasWarning: false,
+  hasInfo: false
+};
+
+export { CheckboxGroup as OriginalCheckboxGroup };
+
+export default withValidation(CheckboxGroup);
