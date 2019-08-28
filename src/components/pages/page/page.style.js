094f4eebc38b05897d8448bbcae5add5c3210163
Damian Boni <damian@kyanmedia.com>
2019-08-28 14:51:51 +0100

    FE-1587 fixed styling after master merge.

--- src/components/pages/page/page.style.js
+++ src/components/pages/page/page.style.js
@@ -1,9 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import BaseTheme from '../../../style/themes/base';
+import { LinkStyleAnchor } from '../../link/link.style';
+import { StyledIcon } from '../../icon/icon.style';
+import { isClassic } from '../../../utils/helpers/style-helper';
 
 const StyledPage = styled.article`
   width: 100%;
   height: 100%;
+
+  ${LinkStyleAnchor} {
+    text-decoration: none !important;
+
+    ${({ theme }) => !isClassic(theme) && css`
+      height: 25px;
+    `}
+
+    &:hover {
+      text-decoration: none !important;
+    }
+
+    ${StyledIcon} {
+      margin-top: -9px;
+      margin-right: 0px;
+      top: 11px;
+      color: #4d7080;
+
+      &:hover {
+        color: #335B6D;
+      }
+    }
+  }
 `;
 
 const StyledPageContent = styled.div`
