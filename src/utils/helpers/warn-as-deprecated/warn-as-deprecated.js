ae60ef44ae274a7ed588e8d5140542bc375eabe8
Ed Leeks <edward.leeks@sage.com>
2019-08-30 20:07:27 +0100

    uses Logger in depercation function

--- src/utils/helpers/warn-as-deprecated/warn-as-deprecated.js
+++ src/utils/helpers/warn-as-deprecated/warn-as-deprecated.js
@@ -1,12 +1,12 @@
+import Logger from '../../logger';
 /**
  * A function to display a warning in the console when a deprecated component is being used in developpment
  */
-export default (oldName, newName, env) => {
-  if (env === 'development' && oldName.length && newName.length) {
-    const carbonDeprecationLabel = '*** CARBON DEPRECATED COMPONENT WARNING *** \n';
+export default (oldName, newName) => {
+  if (oldName.length && newName.length) {
     const warnMessage = (
       `${oldName} is marked as deprecated, it will soon be moved to "carbon-graveyard" and be replaced by ${newName}`
     );
-    console.warn(carbonDeprecationLabel, warnMessage);
+    Logger.deprecate(warnMessage);
   }
 };
