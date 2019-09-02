de0781af9f3c4703d7ea08eba38928837340830f
Damian Boni <damian@kyanmedia.com>
2019-09-02 15:12:18 +0100

    FE-1587 adjusted chevron alignment

--- src/components/pages/pages.style.js
+++ src/components/pages/pages.style.js
@@ -1,14 +1,21 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { slideAnimation, fadeAnimation } from './pages.config';
 import StyledHeadingIcon from '../heading/heading.style';
+import { isClassic } from '../../utils/helpers/style-helper';
 
 const PagesContent = styled.div`
   overflow: hidden;
   position: relative;
 
-  ${StyledHeadingIcon} {
-    margin-top: -9px;
+  && ${StyledHeadingIcon} {
+    margin-top: -3px;
   }
+
+  ${({ theme }) => isClassic(theme) && css`
+    && ${StyledHeadingIcon} {
+      margin-top: -22px;
+    }
+  `}
 
   .carbon-app-wrapper {
     min-width: auto;
