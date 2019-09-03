be4ef0c132541869f6b12963feb89dcbba3bc111
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-09-03 16:15:05 +0200

    restore files

--- src/components/radio-button/radio-button.stories.js
+++ src/components/radio-button/radio-button.stories.js
@@ -1,79 +1,53 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { boolean, text, select } from '@storybook/addon-knobs';
-import { Store, State } from '@sambego/storybook-state';
-import OptionsHelper from '../../utils/helpers/options-helper';
-import { RadioButton } from '../../__experimental__/components/radio-button';
-import { notes, info } from './documentation';
-import getDocGenInfo from '../../utils/helpers/docgen-info';
-
-RadioButton.__docgenInfo = getDocGenInfo(
-  require('./docgenInfo.json'),
-  /radio-button(?!spec)/
-);
-
-const radioButtonsStore = new Store({
-  selectedButton: ''
-});
-
-function handleChange(ev) {
-  radioButtonsStore.set({ selectedButton: ev.target.value });
-}
+import {
+  boolean, text, number
+} from '@storybook/addon-knobs';
+import RadioButton from './radio-button';
+import notes from './notes.md';
 
 storiesOf('Radio Button', module).add(
   'default',
   () => {
     const fieldHelp = text('fieldHelp', 'Additional information below the input.');
-    const fieldHelpInline = boolean('fieldHelpInline', RadioButton.defaultProps.fieldHelpInline);
+    const fieldHelpInline = boolean('fieldHelpInline', false);
+    const inputWidth = number('inputWidth', 0);
     const label = text('label', 'Example RadioButton');
-    const labelInline = boolean('labelInline', false);
-    const labelWidth = labelInline ? text('labelWidth', '') : undefined;
-    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary, 'left') : undefined;
+    const labelAlign = text('labelAlign', 'left');
     const labelHelp = text('labelHelp', 'Example label help text');
-    const inputWidth = text('inputWidth', '');
-
-    const buttonOneValue = 'button-one';
-    const buttonTwoValue = 'button-two';
+    const labelInline = boolean('labelInline', false);
+    const labelWidth = number('labelWidth', 0);
+    const className = text('className', '');
 
     return (
-      <State store={ radioButtonsStore }>
-        {state => [
-          <RadioButton
-            checked={ state.selectedButton === buttonOneValue }
-            key='first-button'
-            fieldHelp={ fieldHelp }
-            fieldHelpInline={ fieldHelpInline }
-            inputWidth={ inputWidth }
-            label={ label }
-            labelAlign={ labelAlign }
-            labelHelp={ labelHelp }
-            labelInline={ labelInline }
-            labelWidth={ labelWidth }
-            name='radio-buttons-example'
-            onChange={ handleChange }
-            value={ buttonOneValue }
-          />,
-          <RadioButton
-            checked={ state.selectedButton === buttonTwoValue }
-            key='second-button'
-            fieldHelp={ fieldHelp }
-            fieldHelpInline={ fieldHelpInline }
-            inputWidth={ inputWidth }
-            label={ label }
-            labelAlign={ labelAlign }
-            labelHelp={ labelHelp }
-            labelInline={ labelInline }
-            labelWidth={ labelWidth }
-            name='radio-buttons-example'
-            onChange={ handleChange }
-            value={ buttonTwoValue }
-          />
-        ]}
-      </State>
+      [
+        <RadioButton
+          className={ className }
+          fieldHelp={ fieldHelp }
+          fieldHelpInline={ fieldHelpInline }
+          inputWidth={ inputWidth }
+          label={ label }
+          labelAlign={ labelAlign }
+          labelHelp={ labelHelp }
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          name='radio-buttons-name'
+        />,
+        <RadioButton
+          className={ className }
+          fieldHelp={ fieldHelp }
+          fieldHelpInline={ fieldHelpInline }
+          inputWidth={ inputWidth }
+          label={ label }
+          labelAlign={ labelAlign }
+          labelHelp={ labelHelp }
+          labelInline={ labelInline }
+          labelWidth={ labelWidth }
+          name='radio-buttons-name'
+        />
+      ]
     );
-  },
-  {
-    notes: { markdown: notes },
-    info: { text: info }
+  }, {
+    notes: { markdown: notes }
   }
 );
