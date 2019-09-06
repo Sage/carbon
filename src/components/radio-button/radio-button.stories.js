fdeb1a0a9d30d4df3c0f997f22cc8634851b5df3
dawidzarzycki <dzarzycki@pgs-soft.com>
2019-09-06 14:19:53 +0200

    restore files from 44edacf316ade86c47d8c202b34abbfc74f38555

--- src/components/radio-button/radio-button.stories.js
+++ src/components/radio-button/radio-button.stories.js
@@ -1,53 +1,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {
-  boolean, text, number
-} from '@storybook/addon-knobs';
+import { boolean, text, select } from '@storybook/addon-knobs';
+import OptionsHelper from '../../utils/helpers/options-helper';
 import RadioButton from './radio-button';
-import notes from './notes.md';
+import { notes, info } from './documentation';
 
 storiesOf('Radio Button', module).add(
   'default',
   () => {
     const fieldHelp = text('fieldHelp', 'Additional information below the input.');
-    const fieldHelpInline = boolean('fieldHelpInline', false);
-    const inputWidth = number('inputWidth', 0);
+    const fieldHelpInline = boolean('fieldHelpInline', RadioButton.defaultProps.fieldHelpInline);
     const label = text('label', 'Example RadioButton');
-    const labelAlign = text('labelAlign', 'left');
-    const labelHelp = text('labelHelp', 'Example label help text');
     const labelInline = boolean('labelInline', false);
-    const labelWidth = number('labelWidth', 0);
-    const className = text('className', '');
+    const labelWidth = labelInline ? text('labelWidth', '') : undefined;
+    const labelAlign = labelInline ? select('labelAlign', OptionsHelper.alignBinary, 'left') : undefined;
+    const labelHelp = text('labelHelp', 'Example label help text');
+    const inputWidth = text('inputWidth', '');
 
-    return (
-      [
-        <RadioButton
-          className={ className }
-          fieldHelp={ fieldHelp }
-          fieldHelpInline={ fieldHelpInline }
-          inputWidth={ inputWidth }
-          label={ label }
-          labelAlign={ labelAlign }
-          labelHelp={ labelHelp }
-          labelInline={ labelInline }
-          labelWidth={ labelWidth }
-          name='radio-buttons-name'
-        />,
-        <RadioButton
-          className={ className }
-          fieldHelp={ fieldHelp }
-          fieldHelpInline={ fieldHelpInline }
-          inputWidth={ inputWidth }
-          label={ label }
-          labelAlign={ labelAlign }
-          labelHelp={ labelHelp }
-          labelInline={ labelInline }
-          labelWidth={ labelWidth }
-          name='radio-buttons-name'
-        />
-      ]
-    );
-  }, {
-    notes: { markdown: notes }
+    return [
+      <RadioButton
+        key='first-button'
+        fieldHelp={ fieldHelp }
+        fieldHelpInline={ fieldHelpInline }
+        inputWidth={ inputWidth }
+        label={ label }
+        labelAlign={ labelAlign }
+        labelHelp={ labelHelp }
+        labelInline={ labelInline }
+        labelWidth={ labelWidth }
+        name='radio-buttons-example'
+      />,
+      <RadioButton
+        key='second-button'
+        fieldHelp={ fieldHelp }
+        fieldHelpInline={ fieldHelpInline }
+        inputWidth={ inputWidth }
+        label={ label }
+        labelAlign={ labelAlign }
+        labelHelp={ labelHelp }
+        labelInline={ labelInline }
+        labelWidth={ labelWidth }
+        name='radio-buttons-example'
+      />
+    ];
+  },
+  {
+    notes: { markdown: notes },
+    info: { text: info }
   }
 );
