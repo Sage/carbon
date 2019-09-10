0f3d6e57f5b9f06e928e3b04986911ea1659934a
grabkowski <jgrabkowski@pgs-soft.com>
2019-09-10 16:09:51 +0200

    added proper unit tests

--- src/components/card/card.spec.js
+++ src/components/card/card.spec.js
@@ -1,181 +1,155 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import TestRenderer from 'react-test-renderer';
 import Card from './card.component';
 import { assertStyleMatch } from '../../__spec_helper__/test-utils';
-import smallTheme from '../../style/themes/small';
-import mediumTheme from '../../style/themes/medium';
-import largeTheme from '../../style/themes/large';
 import 'jest-styled-components';
+import Icon from '../icon';
 
-const defaultThemes = [
-  ['small', smallTheme],
-  ['medium', mediumTheme],
-  ['large', largeTheme]
-];
+function render(props = {}, renderer = shallow) {
+  const children = props.children || <div />;
+
+  return renderer(
+    <Card { ...props }>
+      { children }
+    </Card>
+  );
+}
 
 describe('Card', () => {
-  describe('default themes', () => {
-    describe.each(defaultThemes)('when the Card is rendered', (name, theme) => {
-      describe(`${name} theme`, () => {
-        describe('when border is not enabled', () => {
-          it(`matches the expected styles for a default ${name} Card`, () => {
-            const wrapper = TestRenderer.create(
-              <Card
-                border={ false }
-                theme={ theme }
-              />
-            ).toJSON();
-            assertStyleMatch({
-              border: 'none'
-            }, wrapper);
-          });
+  describe('when the content is added as children', () => {
+    it('then that content should be rendered inside the component', () => {
+      const content = (
+        <div><span>content</span></div>
+      );
+      const wrapper = render({
+        children: content
+      });
+
+      expect(wrapper.containsMatchingElement(content)).toBe(true);
+    });
+
+    it('then all children should have the "spacing" prop added and set to "medium"', () => {
+      const content = [
+        <div className='mockedContent' key='content1'>content</div>,
+        <div className='mockedContent' key='content2'>content2</div>
+      ];
+      const wrapper = render({
+        children: content
+      });
+      expect(wrapper.find('.mockedContent').at(0).props().spacing).toBe('medium');
+      expect(wrapper.find('.mockedContent').at(1).props().spacing).toBe('medium');
+    });
+  });
+
+  describe('when the "draggable" prop is set to true', () => {
+    it('then a "drag" icon should be rendered', () => {
+      const wrapper = render({
+        draggable: true
+      });
+
+      expect(wrapper.find(Icon).exists()).toBe(true);
+      expect(wrapper.find(Icon).props().type).toBe('drag');
+    });
+  });
+
+  describe('when the "action" prop is set', () => {
+    describe('with the "interactive" prop set to true', () => {
+      it('then the method passed in the "action" prop should be called after click is triggered', () => {
+        const action = jest.fn();
+        const wrapper = render({
+          interactive: true,
+          action
         });
 
-        describe('when border is passed as a prop', () => {
-          it(`matches the expected styles for a default ${name} Card`, () => {
-            const wrapper = TestRenderer.create(
-              <Card
-                border
-                theme={ theme }
-              />
-            ).toJSON();
-            assertStyleMatch({
-              border: `1px solid ${theme.colors.border}`
-            }, wrapper);
+        wrapper.simulate('click');
+
+        expect(action).toHaveBeenCalled();
+      });
+
+      describe('and a key is pressed', () => {
+        it('then the method passed in the "action" prop should be called if the key is "enter"', () => {
+          const action = jest.fn();
+          const wrapper = render({
+            interactive: true,
+            action
           });
+
+          wrapper.simulate('keyDown', { which: 13 });
+
+          expect(action).toHaveBeenCalled();
         });
 
-        describe('when width is not passed as a prop', () => {
-          const wrapper = shallow(
-            <Card
-              theme={ theme }
-            />
-          );
-          const elem = wrapper.find('[data-element="card"]');
-          it('width fills containing element', () => {
-            expect(elem).not.toHaveStyleRule('width');
+        it('then the method passed in the "action" prop should be not called if the key is other than "enter"', () => {
+          const action = jest.fn();
+          const wrapper = render({
+            interactive: true,
+            action
           });
-        });
 
-        describe('when width is passed as a percentage value', () => {
-          const widthPct = '50%';
-          const wrapper = TestRenderer.create(
-            <Card
-              cardWidth={ widthPct }
-              theme={ theme }
-            />
-          ).toJSON();
+          wrapper.simulate('keyDown', { which: 10 });
 
-          it(`Card has style rule of width: ${widthPct}`, () => {
-            assertStyleMatch({
-              width: widthPct
-            }, wrapper);
-          });
-        });
-
-        describe('when width is passed as a pixel value', () => {
-          const widthPx = '500px';
-          const wrapper = TestRenderer.create(
-            <Card
-              cardWidth={ widthPx }
-              theme={ theme }
-            />
-          ).toJSON();
-
-          it(`Card has style rule of width: ${widthPx}`, () => {
-            assertStyleMatch({
-              width: widthPx
-            }, wrapper);
-          });
-        });
-
-        describe('when a header is not passed as a prop', () => {
-          const wrapper = mount(
-            <Card
-              theme={ theme }
-            />
-          );
-
-          it('does not renders a header', () => {
-            const elem = wrapper.find('[data-element="header"]');
-            expect(elem.exists()).toEqual(false);
-          });
-        });
-
-        describe('when a header is passed as a prop', () => {
-          const wrapper = mount(
-            <Card
-              header
-              headerPrimary='header is passed as a prop'
-              theme={ theme }
-            />
-          );
-          const elem = wrapper.find('[data-element="header"]');
-
-          it('renders a header', () => {
-            expect(elem.exists()).toEqual(true);
-          });
-        });
-
-        describe('when middle is not passed as a prop', () => {
-          const wrapper = mount(
-            <Card
-              theme={ theme }
-            />
-          );
-
-          it('does not render any middle', () => {
-            const elem = wrapper.find('[data-element="middle"]');
-            expect(elem.exists()).toEqual(false);
-          });
-        });
-
-        describe('when middle is passed as a prop', () => {
-          const primary = 'primary description text';
-          const wrapper = mount(
-            <Card
-              middle
-              theme={ theme }
-              middlePrimary={ primary }
-            />
-          );
-          const elem = wrapper.find('[data-element="middle"]');
-
-          it('renders a middle', () => {
-            expect(elem.exists()).toEqual(true);
-          });
-        });
-
-        describe('when footer is not passed as a prop', () => {
-          const wrapper = mount(
-            <Card
-              theme={ theme }
-            />
-          );
-
-          it('does not render a footer', () => {
-            const elem = wrapper.find('[data-element="footer"]');
-            expect(elem.exists()).toEqual(false);
-          });
-        });
-
-        describe('when footer is passed as a prop', () => {
-          const primary = 'footer is passed as a prop';
-          const wrapper = mount(
-            <Card
-              footer
-              theme={ theme }
-              footerPrimary={ primary }
-            />
-          );
-          const elem = wrapper.find('[data-element="footer"]');
-
-          it('renders a footer', () => {
-            expect(elem.exists()).toEqual(true);
-          });
+          expect(action).not.toHaveBeenCalled();
         });
       });
+
+      describe('and with the "draggable" prop set to true', () => {
+        it('then the method passed in the "action" prop should not be called', () => {
+          const action = jest.fn();
+          const wrapper = render({
+            interactive: true,
+            draggable: true,
+            action
+          });
+
+          wrapper.simulate('click');
+
+          expect(action).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('with the "interactive" prop not set', () => {
+      it('then the method passed in the "action" prop should not be called', () => {
+        const action = jest.fn();
+        const wrapper = render({
+          action
+        });
+
+        wrapper.simulate('click');
+
+        expect(action).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('when width is not passed as a prop', () => {
+    const wrapper = render();
+    const elem = wrapper.find('[data-element="card"]');
+    it('width fills containing element', () => {
+      expect(elem).not.toHaveStyleRule('width');
+    });
+  });
+
+  describe('when width is passed as a percentage value', () => {
+    const widthPct = '50%';
+    const wrapper = render({ cardWidth: widthPct }, TestRenderer.create);
+
+    it(`Card has style rule of width: ${widthPct}`, () => {
+      assertStyleMatch({
+        width: widthPct
+      }, wrapper.toJSON());
+    });
+  });
+
+  describe('when width is passed as a pixel value', () => {
+    const widthPx = '500px';
+    const wrapper = render({ cardWidth: widthPx }, TestRenderer.create);
+
+    it(`Card has style rule of width: ${widthPx}`, () => {
+      assertStyleMatch({
+        width: widthPx
+      }, wrapper.toJSON());
     });
   });
 });
