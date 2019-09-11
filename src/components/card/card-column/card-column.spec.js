0e597a49b7f68025791681b4259ed5a3fd6c599a
grabkowski <jgrabkowski@pgs-soft.com>
2019-09-11 17:34:26 +0200

    added unit tests

--- src/components/card/card-column/card-column.spec.js
+++ src/components/card/card-column/card-column.spec.js
@@ -1,73 +1,41 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import TestRenderer from 'react-test-renderer';
 import CardColumn from './card-column.component';
 import { assertStyleMatch } from '../../../__spec_helper__/test-utils';
 import 'jest-styled-components';
 
-const contentTypeVariants = [
-  ['header', ['primary', 'secondary']],
-  ['middle', ['primary', 'secondary', 'tertiary']],
-  ['footer', ['primary']]
-];
-
-const expectedStyle = {
-  header: {
-    primary: {
-      fontSize: '22px',
-      fontWeight: '700',
-      lineHeight: '26px',
-      margin: '0'
-    },
-    secondary: {
-      fontSize: '14px',
-      fontWeight: '400',
-      lineHeight: '21px',
-      margin: '0'
-    }
-  },
-  middle: {
-    primary: {
-      color: 'rgba(0,0,0,0.74)',
-      fontSize: '24px',
-      fontWeight: '700',
-      marginBottom: '10px'
-    },
-    secondary: {
-      color: 'rgba(0,0,0,0.65)',
-      fontSize: '14px',
-      fontWeight: '700',
-      marginBottom: '8px'
-    },
-    tertiary: {
-      color: 'rgba(0,0,0,0.55)',
-      fontSize: '12px'
-    }
-  },
-  footer: {
-    lineHeight: '30px',
-    margin: '0',
-    color: '#008200',
-    fontWeight: '600',
-    padding: '12px 0'
-  }
-};
-
 describe('CardColumn', () => {
-  describe.each(contentTypeVariants)('each row renders', (position, contentStyles) => {
-    describe.each(contentStyles)('to match the expected style', (style) => {
-      let wrapper;
-
-      beforeEach(() => {
-        wrapper = mount(
-          <CardColumn
-            type={ { position, contentStyle: style } }
-          />
-        );
+  describe('when the content is added as children', () => {
+    it('then that content should be rendered inside the component', () => {
+      const content = (
+        <div><span>content</span></div>
+      );
+      const wrapper = renderCardColumn({
+        children: content
       });
 
-      it(`for ${position} ${style} content`, () => {
-        assertStyleMatch({ ...expectedStyle[position][style] }, wrapper);
-      });
+      expect(wrapper.containsMatchingElement(content)).toBe(true);
+    });
+  });
+
+  fdescribe.each(['left', 'center', 'right'])('when the "align" prop is set to %s', (align) => {
+    it(`then the text-align should be set to ${align}`, () => {
+      const wrapper = renderCardColumn({ align }, TestRenderer.create);
+
+      assertStyleMatch({
+        textAlign: align
+      }, wrapper.toJSON());
     });
   });
 });
+
+function renderCardColumn(props = {}, renderer = shallow) {
+  const children = props.children || <div />;
+
+  return renderer(
+    <CardColumn { ...props }>
+      { children }
+    </CardColumn>
+  );
+}
--- src/components/card/card-row/card-row.spec.js
+++ src/components/card/card-row/card-row.spec.js
@@ -1,288 +1,45 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import TestRenderer from 'react-test-renderer';
 import CardRow from './card-row.component';
 import { assertStyleMatch } from '../../../__spec_helper__/test-utils';
-import smallTheme from '../../../style/themes/small';
-import mediumTheme from '../../../style/themes/medium';
-import largeTheme from '../../../style/themes/large';
 import 'jest-styled-components';
 
-const defaultThemes = [
-  ['small', smallTheme],
-  ['medium', mediumTheme],
-  ['large', largeTheme]
-];
-
 describe('CardRow', () => {
-  describe('default themes', () => {
-    describe.each(defaultThemes)('when the CardRow component is rendered', (name, theme) => {
-      describe(`${name} theme`, () => {
-        describe('when section is rendered as a header', () => {
-          describe('when props are not passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='header'
-                theme={ theme }
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered header', () => {
-              assertStyleMatch({
-                padding: '32px 32px',
-                minHeight: '48px'
-              }, wrapper);
-            });
-          });
-
-          describe('when a valid props are passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='header'
-                theme={ theme }
-                primary='header is passed as a prop'
-                secondary='this is a subtitle'
-                icon='/path/to/icon.svg'
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered header', () => {
-              assertStyleMatch({
-                padding: '32px 32px',
-                minHeight: '48px'
-              }, wrapper);
-            });
-          });
-
-          describe('when props do not include primary text', () => {
-            const wrapper = mount(
-              <CardRow
-                positionType='header'
-                theme={ theme }
-                secondary='this is secondary text'
-              />
-            );
-            const elem = wrapper.find('[data-element="header-primary"]');
-            it('does not render a primary element', () => {
-              expect(elem.exists()).toEqual(false);
-            });
-          });
-
-          describe('when props includes primary text', () => {
-            it('renders a primary element', () => {
-              const wrapper = mount(
-                <CardRow
-                  positionType='header'
-                  theme={ theme }
-                  primary='this is primary text'
-                />
-              );
-              const elem = wrapper.find('[data-element="header-primary"]');
-              expect(elem.exists()).toEqual(true);
-            });
-          });
-
-          describe('when props do not include secondary text', () => {
-            const wrapper = mount(
-              <CardRow
-                positionType='header'
-                theme={ theme }
-                primary='this is primary text'
-              />
-            );
-            const elem = wrapper.find('[data-element="header-secondary"]');
-            it('does not render a secondary element', () => {
-              expect(elem.exists()).toEqual(false);
-            });
-          });
-
-          describe('when props include secondary text', () => {
-            it('renders a secondary element', () => {
-              const wrapper = mount(
-                <CardRow
-                  positionType='header'
-                  theme={ theme }
-                  secondary='this is secondary text'
-                />
-              );
-              const elem = wrapper.find('[data-element="header-secondary"]');
-              expect(elem.exists()).toEqual(true);
-            });
-          });
-        });
-
-        describe('when section is rendered as a middle', () => {
-          describe('when props are not passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='middle'
-                theme={ theme }
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered middle', () => {
-              assertStyleMatch({
-                padding: '0 32px',
-                marginBottom: '32px'
-              }, wrapper);
-            });
-          });
-
-          describe('when a valid props are passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='middle'
-                theme={ theme }
-                primary='this is primary text'
-                secondary='this is secondary text'
-                tertiary='this is tertiary text'
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered middle', () => {
-              assertStyleMatch({
-                padding: '0 32px',
-                marginBottom: '32px'
-              }, wrapper);
-            });
-          });
-
-          describe('when props do not include primary text', () => {
-            const wrapper = mount(
-              <CardRow
-                positionType='middle'
-                theme={ theme }
-                secondary='this is secondary text'
-                tertiary='this is tertiary text'
-              />
-            );
-            const elem = wrapper.find('[data-element="middle-primary"]');
-            it('does not render a primary element', () => {
-              expect(elem.exists()).toEqual(false);
-            });
-          });
-
-          describe('when props includes primary text', () => {
-            it('renders a primary element', () => {
-              const wrapper = mount(
-                <CardRow
-                  positionType='middle'
-                  theme={ theme }
-                  primary='this is primary text'
-                />
-              );
-              const elem = wrapper.find('[data-element="middle-primary"]');
-              expect(elem.exists()).toEqual(true);
-            });
-          });
-
-          describe('when props do not include secondary text', () => {
-            const wrapper = mount(
-              <CardRow
-                positionType='middle'
-                theme={ theme }
-                primary='this is primary text'
-              />
-            );
-            const elem = wrapper.find('[data-element="middle-secondary"]');
-            it('does not render a secondary element', () => {
-              expect(elem.exists()).toEqual(false);
-            });
-          });
-
-          describe('when props include secondary text', () => {
-            it('renders a secondary element', () => {
-              const wrapper = mount(
-                <CardRow
-                  positionType='middle'
-                  theme={ theme }
-                  secondary='this is secondary text'
-                />
-              );
-              const elem = wrapper.find('[data-element="middle-secondary"]');
-              expect(elem.exists()).toEqual(true);
-            });
-          });
-
-          describe('when props do not include tertiary text', () => {
-            const wrapper = mount(
-              <CardRow
-                positionType='middle'
-                theme={ theme }
-                primary='this is primary text'
-                secondary='this is secondary text'
-              />
-            );
-            const elem = wrapper.find('[data-element="middle-tertiary"]');
-            it('does not render a secondary element', () => {
-              expect(elem.exists()).toEqual(false);
-            });
-          });
-
-          describe('when props include tertiary text', () => {
-            it('renders a tertiary element', () => {
-              const wrapper = mount(
-                <CardRow
-                  positionType='middle'
-                  theme={ theme }
-                  primary='this is primary text'
-                  secondary='this is secondary text'
-                  tertiary='this is tertiary text'
-                />
-              );
-              const elem = wrapper.find('[data-element="middle-tertiary"]');
-              expect(elem.exists()).toEqual(true);
-            });
-          });
-        });
-
-        describe('when section is rendered as a footer', () => {
-          describe('when props are not passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='footer'
-                theme={ theme }
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered footer', () => {
-              assertStyleMatch({
-                backgroundColor: theme.card.footerBackground,
-                borderTop: theme.card.footerBorder,
-                height: '56px',
-                lineHeight: '56px',
-                padding: '0 32px'
-              }, wrapper);
-            });
-          });
-
-          describe('when a valid props are passed', () => {
-            const wrapper = TestRenderer.create(
-              <CardRow
-                positionType='footer'
-                theme={ theme }
-                primary='this is primary text'
-              />
-            ).toJSON();
-
-            it('matches the styles for a rendered footer', () => {
-              assertStyleMatch({
-                backgroundColor: theme.card.footerBackground,
-                borderTop: theme.card.footerBorder,
-                height: '56px',
-                lineHeight: '56px',
-                padding: '0 32px'
-              }, wrapper);
-            });
-          });
-        });
-
-        xdescribe('when section is aligned', () => {
-        });
-
-        xdescribe('when children include an Icon', () => {
-        });
+  describe('when the content is added as children', () => {
+    it('then that content should be rendered inside the component', () => {
+      const content = (
+        <div><span>content</span></div>
+      );
+      const wrapper = renderCardRow({
+        children: content
       });
+
+      expect(wrapper.containsMatchingElement(content)).toBe(true);
+    });
+  });
+
+  describe.each([
+    ['small', '16px 0'],
+    ['medium', '24px 0'],
+    ['large', '32px 0']
+  ])('when the "spacing" prop is set to %s', (size, expectedVal) => {
+    const wrapper = renderCardRow({ spacing: size }, TestRenderer.create);
+
+    it(`then the margin should be set to ${expectedVal}`, () => {
+      assertStyleMatch({
+        margin: expectedVal
+      }, wrapper.toJSON());
     });
   });
 });
+
+function renderCardRow(props = {}, renderer = shallow) {
+  const children = props.children || <div />;
+
+  return renderer(
+    <CardRow { ...props }>
+      { children }
+    </CardRow>
+  );
+}
