636394b7e9a6874fb49c1a5930decc0d8d8dc7a2
Jamie Thompson <jamie.thompson@sage.com>
2019-09-12 12:22:08 +0100

    fix: convert action-popover story to use theme selector FE-1253

--- src/components/action-popover/action-popover.stories.js
+++ src/components/action-popover/action-popover.stories.js
@@ -6,6 +6,7 @@ import { notes, Info } from './documentation';
 import ActionPopover from '.';
 import { MenuButton } from './action-popover.style';
 import getDocGenInfo from '../../utils/helpers/docgen-info';
+import { dlsThemeSelector, classicThemeSelector } from '../../../.storybook/theme-selectors';
 import {
   Table, TableRow, TableCell, TableHeader
 } from '../table';
@@ -17,60 +18,65 @@ ActionPopover.__docgenInfo = getDocGenInfo(
   /action-popover\.component(?!spec)/
 );
 
-storiesOf('Action Popover', module)
-  .add('default', () => {
-    return (
-      <Table isZebra>
-        <TableRow>
-          <TableHeader>First Name</TableHeader>
-          <TableHeader>Last Name</TableHeader>
-          <TableHeader>&nbsp;</TableHeader>
-        </TableRow>
-        <TableRow>
-          <TableCell>John</TableCell>
-          <TableCell>Doe</TableCell>
-          <TableCell>
-            <ActionPopover>
-              <ActionPopover.Item icon='email' onClick={ action('email') }>Email Invoice</ActionPopover.Item>
-              <ActionPopover.Item
-                disabled icon='print'
-                onClick={ action('print') }
-              >Print Invoice
-              </ActionPopover.Item>
-              <ActionPopover.Item icon='pdf' onClick={ action('pdf') }>Download PDF</ActionPopover.Item>
-              <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
-              <ActionPopover.Divider />
-              <ActionPopover.Item icon='delete' onClick={ action('delete') }>Delete</ActionPopover.Item>
-            </ActionPopover>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Jane</TableCell>
-          <TableCell>Smith</TableCell>
-          <TableCell>
-            <ActionPopover>
-              <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
-            </ActionPopover>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Bob</TableCell>
-          <TableCell>Jones</TableCell>
-          <TableCell>
-            <ActionPopover>
-              <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
-            </ActionPopover>
-          </TableCell>
-        </TableRow>
-      </Table>
-    );
-  }, {
+function makeStory(storyName, themeSelector) {
+  const component = () => (
+    <Table isZebra>
+      <TableRow>
+        <TableHeader>First Name</TableHeader>
+        <TableHeader>Last Name</TableHeader>
+        <TableHeader>&nbsp;</TableHeader>
+      </TableRow>
+      <TableRow>
+        <TableCell>John</TableCell>
+        <TableCell>Doe</TableCell>
+        <TableCell>
+          <ActionPopover>
+            <ActionPopover.Item icon='email' onClick={ action('email') }>Email Invoice</ActionPopover.Item>
+            <ActionPopover.Item
+              disabled icon='print'
+              onClick={ action('print') }
+            >Print Invoice
+            </ActionPopover.Item>
+            <ActionPopover.Item icon='pdf' onClick={ action('pdf') }>Download PDF</ActionPopover.Item>
+            <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
+            <ActionPopover.Divider />
+            <ActionPopover.Item icon='delete' onClick={ action('delete') }>Delete</ActionPopover.Item>
+          </ActionPopover>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Jane</TableCell>
+        <TableCell>Smith</TableCell>
+        <TableCell>
+          <ActionPopover>
+            <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
+          </ActionPopover>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Bob</TableCell>
+        <TableCell>Jones</TableCell>
+        <TableCell>
+          <ActionPopover>
+            <ActionPopover.Item icon='csv' onClick={ action('csv') }>Download CSV</ActionPopover.Item>
+          </ActionPopover>
+        </TableCell>
+      </TableRow>
+    </Table>
+  );
+
+  const metadata = {
+    themeSelector,
+    notes: { markdown: notes },
     info: {
       text: Info,
       propTablesExclude: [Table, TableRow, TableCell, TableHeader, MenuButton, StyledComponent]
-    },
-    notes: { markdown: notes },
-    knobs: {
-      escapeHTML: false
     }
-  });
+  };
+
+  return [storyName, component, metadata];
+}
+
+storiesOf('Action Popover', module)
+  .add(...makeStory('default', dlsThemeSelector))
+  .add(...makeStory('classic', classicThemeSelector));
