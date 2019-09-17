76978181ef8ce5f25cafe0277767f79c1fd18afa
Josh Jimenez Bedrock <josh@kyan.com>
2019-09-17 09:49:39 +0100

    Updated change log, test, and add TypeScript definitions for the checkbox component.

---
+++ src/__experimental__/components/checkbox/checkbox-group.d.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { WithValidationProps } from '../../../components/validations/with-validation';
+
+interface CheckboxGroupProps {
+  children: React.FunctionComponent | React.ComponentClass;
+  groupName: string;
+  hasError: boolean;
+  hasWarning: boolean;
+  hasInfo: boolean;
+}
+
+declare const OriginalCheckboxGroup: React.FunctionComponent<CheckboxGroupProps>;
+
+declare const CheckboxGroup: React.ComponentClass<WithValidationProps & CheckboxGroupProps>;
+
+export { CheckboxGroup, OriginalCheckboxGroup };
