451069844f3cda377c8814ebc62dadde4d164371
Damian Boni <damian@kyanmedia.com>
2019-09-20 10:27:59 +0100

    FE-1974 updated textbox snapshot with shallow and extra dive.

--- src/__experimental__/components/textbox/textbox.spec.js
+++ src/__experimental__/components/textbox/textbox.spec.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Textbox from '.';
 
 jest.mock('../../../utils/helpers/guid', () => () => 'mocked-guid');
 
 describe('Textbox', () => {
   it('renders with InputPresentation and Input and all props passed to Input', () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <Textbox value='foobar' leftChildren='southpaw children'>
         normal children
       </Textbox>
-    ).find('Textbox');
+    ).dive().dive().dive();
     expect(wrapper).toMatchSnapshot();
   });
 });
