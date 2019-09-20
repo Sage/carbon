4f44b4dcbba87ad3b0b0341758fe8567a4d634dd
Michael Hogg <michael@kyan.com>
2019-09-20 09:39:40 +0100

    (FE-1569) custom-drag-layer.style.js: add box-shadow, make opaque

--- src/components/drag-and-drop/custom-drag-layer/custom-drag-layer.style.js
+++ src/components/drag-and-drop/custom-drag-layer/custom-drag-layer.style.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
 const StyledDragContainer = styled.div`
-  height: 100%;
   left: 0;
   list-style: none;
   margin: 0;
-  opacity: 0.7;
   padding: 0;
   pointer-events: none;
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 100;
+  box-shadow: 0 10px 20px 0 rgba(0,20,29,0.2), 0 20px 40px 0 rgba(0,20,29,0.1);
 `;
 
 const StyledDragWrapper = styled.div``;
