2bedb98d54480c5f2465686fadd040e54325ef8f
Damian Boni <damian@kyanmedia.com>
2019-09-23 14:04:02 +0100

    FE-1587 removed button on third page as its not needed anymore. added isClassic to dialog.

--- src/components/dialog-full-screen/content-classic.style.js
+++ src/components/dialog-full-screen/content-classic.style.js
@@ -1,7 +1,7 @@
 import { css } from 'styled-components';
-import { THEMES } from '../../style/themes';
+import { isClassic } from '../../utils/helpers/style-helper';
 
-export default ({ theme, hasHeader }) => theme.name === THEMES.classic && css`
+export default ({ theme, hasHeader }) => isClassic(theme) && css`
   padding-top: 30px;
   padding-bottom: 30px;
 
