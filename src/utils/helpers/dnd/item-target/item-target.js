23843dfe3796b60d5c18e0c0104d3e1f19cf0fa5
Michael Hogg <michael@kyan.com>
2019-09-24 14:28:27 +0100

    (FE-1569) dnd/item-target: remove "50% height" conditional logic
    It looks like this is old example code provided by
    ReactDnD in 2015 to handle situations where the
    draggable items don't all have the same height:
    
    https://github.com/react-dnd/react-dnd/issues/233#issuecomment-121623792
    
    https://github.com/react-dnd/react-dnd/pull/251

--- src/utils/helpers/dnd/item-target/item-target.js
+++ src/utils/helpers/dnd/item-target/item-target.js
@@ -16,33 +16,6 @@ const ItemTargetHelper = {
       return;
     }
 
-    // Determine rectangle on screen
-    const el = ReactDOM.findDOMNode(component); // eslint-disable-line react/no-find-dom-node
-    const hoverBoundingRect = el.getBoundingClientRect();
-
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-    // Only perform the move when the mouse has crossed half of the items height
-    // When dragging downwards, only move when the cursor is below 50%
-    // When dragging upwards, only move when the cursor is above 50%
-
-    // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-      return;
-    }
-
-    // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      return;
-    }
-
     // Time to actually perform the action
     const onDrag = props.onDrag || component.context.dragAndDropOnDrag;
     onDrag(dragIndex, hoverIndex);
