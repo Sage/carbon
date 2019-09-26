119edbb0a592f76b0650f0d80e817c4cc98cba88
Josh Jimenez Bedrock <josh@kyan.com>
2019-09-26 14:14:14 +0100

    Added missing TypeScript definition for RadioButtonGroup

---
+++ src/__experimental__/components/radio-button/radio-button-group.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { WithValidationProps } from '../../../components/validations/with-validation';
+
+export interface RadioButtonGroupProps {
+  children: React.ComponentClass;
+  groupName: string;
+  label: string;
+  labelHelp?: string;
+  hasError?: boolean;
+  hasWarning?: boolean;
+  hasInfo?: boolean;
+}
+
+declare const RadioButtonGroup: React.ComponentClass<WithValidationProps & RadioButtonGroupProps>;
+
+export { RadioButtonGroup as default };
