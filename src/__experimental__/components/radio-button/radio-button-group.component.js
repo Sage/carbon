1f7018f630cc334c0856dc6fdee3b5cc93fb5e32
Damian Boni <damian@kyanmedia.com>
2019-09-30 14:32:52 +0100

    FE-2060 removed unused useRef

--- src/__experimental__/components/radio-button/radio-button-group.component.js
+++ src/__experimental__/components/radio-button/radio-button-group.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import tagComponent from '../../../utils/helpers/tags';
 import { StyledRadioButtonGroup } from './radio-button.style';
