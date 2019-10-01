521c0659c98a8abaa6206e94e20c09842264528a
Damian Boni <damian@kyanmedia.com>
2019-10-01 12:24:28 +0100

    FE-2076 updated spec coverage for changes to onHoverUpDown

--- src/utils/helpers/dnd/item-target/__spec__.js
+++ src/utils/helpers/dnd/item-target/__spec__.js
@@ -9,7 +9,8 @@ describe('ItemTargetHelper', () => {
         monitor = {},
         monitorItem,
         props,
-        clientOffset;
+        clientOffset,
+        offsetDiff;
 
     beforeEach(() => {
       component = document.createElement('div');
@@ -39,12 +40,20 @@ describe('ItemTargetHelper', () => {
         index: 0
       };
 
+      offsetDiff = {
+        x: 0,
+        y: 0
+      };
+
       monitor = {
         getItem: () => {
           return monitorItem;
         },
         getClientOffset: () => {
           return clientOffset;
+        },
+        getDifferenceFromInitialOffset: () => {
+          return offsetDiff;
         }
       };
     });
@@ -53,6 +62,18 @@ describe('ItemTargetHelper', () => {
       it('item dragged downwards and below 50% of item height', () => {
         props.index = 1;
         clientOffset.y = 76; // over the halfway point when dragging downwards
+
+        spyOn(props, 'onDrag');
+        ItemTargetHelper.onHoverUpDown(props, monitor, component);
+
+        expect(props.onDrag).toHaveBeenCalled();
+      });
+
+      it('item dragged upwards and above 50% of item height', () => {
+        props.index = 0;
+
+        clientOffset.y = 51;
+        monitorItem.index = 1;
 
         spyOn(props, 'onDrag');
         ItemTargetHelper.onHoverUpDown(props, monitor, component);
@@ -86,6 +107,36 @@ describe('ItemTargetHelper', () => {
 
         ItemTargetHelper.onHoverUpDown(props, monitor, component);
         expect(contextSpy).toHaveBeenCalled();
+      });
+
+      describe('when difference between item offset y and initial offset y is less than 1', () => {
+        it('it does not run onDrag', () => {
+          props.index = 0;
+          clientOffset.y = 51;
+          monitorItem.index = 1;
+          monitorItem.offsetDiffY = 10;
+          offsetDiff.y = 11;
+
+          spyOn(props, 'onDrag');
+          ItemTargetHelper.onHoverUpDown(props, monitor, component);
+
+          expect(props.onDrag).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('when difference between item offset y and initial offset y is greater than 1', () => {
+        it('it does not run onDrag', () => {
+          props.index = 0;
+          monitorItem.index = 1;
+          clientOffset.y = 51;
+          monitorItem.offsetDiffY = 10;
+          offsetDiff.y = 12;
+
+          spyOn(props, 'onDrag');
+          ItemTargetHelper.onHoverUpDown(props, monitor, component);
+
+          expect(props.onDrag).toHaveBeenCalled();
+        });
       });
     });
 
--- src/utils/helpers/dnd/item-target/item-target.js
+++ src/utils/helpers/dnd/item-target/item-target.js
@@ -1,12 +1,12 @@
 const ItemTargetHelper = {
-
   /**
    * Helper method for when drag and drop is enabled for
    * items grouped together vertically e.g. a list, or
    * a table.
    */
   onHoverUpDown(props, monitor, component) {
-    const dragIndex = monitor.getItem().index;
+    const item = monitor.getItem();
+    const dragIndex = item.index;
     const hoverIndex = props.index;
 
     // Don't replace items with themselves
@@ -14,14 +14,23 @@ const ItemTargetHelper = {
       return;
     }
 
+    const newOffsetDiff = monitor.getDifferenceFromInitialOffset();
+
+    //prevents flickering
+    if (item.offsetDiffY && Math.abs(item.offsetDiffY - newOffsetDiff.y) <= 1) {
+      return;
+    }
+
     // Time to actually perform the action
     const onDrag = props.onDrag || component.context.dragAndDropOnDrag;
     onDrag(dragIndex, hoverIndex);
+
     // Note: we're mutating the monitor item here!
     // Generally it's better to avoid mutations,
     // but it's good here for the sake of performance
     // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
+    item.index = hoverIndex;
+    item.offsetDiffY = newOffsetDiff.y;
   }
 
 };
