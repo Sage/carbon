da2ac956fca8da2a91c337e05202ca2b4e41d20c
Jamie Thompson <jamie.thompson@sage.com>
2019-10-10 19:12:35 +0100

    Revert "FE-2115 work in progress, errors resolved, need more spec coverage."
    This reverts commit dae245539b2fe545a5d6db3aa06cfd5285f9c5cb.

--- src/__experimental__/components/checkbox/checkbox-group.spec.js
+++ src/__experimental__/components/checkbox/checkbox-group.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import 'jest-styled-components';
 import TestRenderer from 'react-test-renderer';
@@ -69,41 +68,16 @@ describe('CheckboxGroup', () => {
 
   describe('onChange', () => {
     it('should be called', () => {
-      const onChange = jest.fn();
-      const wrapper = render({ onChange });
+      const fakeFunction = jest.fn();
+      const wrapper = render({}, {
+        onChange: fakeFunction
+      });
       const checkbox = wrapper.find(Checkbox).first();
 
-      act(() => {
-        checkbox.props().onChange({ target: checkbox.getDOMNode() });
-      });
+      checkbox.prop('onChange')();
 
-      expect(onChange).toHaveBeenCalled();
+      expect(fakeFunction).toBeCalledTimes(1);
     });
-
-    describe('when value is undefined', () => {
-      it('confirms that the isController is false', () => {
-        const wrapper = render({ value: undefined });
-        const checkboxGroup = wrapper.find(CheckboxGroup).first();
-        expect(checkboxGroup.props().value).toEqual(undefined);
-        expect(checkboxGroup.props().isControled).toEqual(undefined);
-      });
-    });
-
-    describe('when isControled is true', () => {
-      it('confirms that the isController is true', () => {
-        const onChange = jest.fn();
-        const wrapper = render({ value: 'one', onChange });
-        const checkboxGroup = wrapper.find(CheckboxGroup).first();
-        const checkbox = wrapper.find(Checkbox).first();
-
-        act(() => {
-          checkbox.props().onChange({ target: checkbox.getDOMNode() });
-        });
-
-        expect(onChange).toHaveBeenCalled();
-        expect(checkboxGroup.props().value).toEqual('one');
-      });
-    });  
   });
 
   describe('styles', () => {
@@ -138,6 +112,21 @@ describe('CheckboxGroup', () => {
           assertStyleMatch({
             border: `1px solid ${validationTypes[type].color}`
           }, checkboxWrapper, { modifier: 'svg' });
+        });
+      });
+
+      describe('pass validation props', () => {
+        const wrapper = render({}, { checked: true });
+
+        it('checked === false', () => {
+          wrapper.setProps({
+            hasError: true
+          });
+
+          const checkboxWrapper = wrapper.find(Checkbox).first();
+
+          expect(checkboxWrapper.prop('checked')).toBe(true);
+          expect(checkboxWrapper.prop('hasError')).toBeUndefined();
         });
       });
     });
