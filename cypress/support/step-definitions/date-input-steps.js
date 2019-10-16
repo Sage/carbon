845d717ea4645b229740e2ccdad6230c4d142cdd
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2019-10-16 09:21:16 +0200

    added date input validations tests

--- cypress/support/step-definitions/date-input-steps.js
+++ cypress/support/step-definitions/date-input-steps.js
@@ -1,5 +1,5 @@
 import {
-  dateInput, dayPickerDay, minDate, maxDate,
+  dateInput, dayPickerDay, minDate, maxDate, dayPickerWrapper,
 } from '../../locators/date-input/index';
 import { labelPreview } from '../../locators';
 
@@ -8,7 +8,7 @@ const DAY_PICKER_PREFIX = 'DayPicker-Day--';
 const TODAY_CALENDAR = Cypress.moment().format('ddd MMM D, YYYY');
 const YESTERDAY_CALENDAR = Cypress.moment().subtract(1, 'days').format('ddd MMM D, YYYY');
 const TOMORROW_CALENDAR = Cypress.moment().add(1, 'days').format('ddd MMM D, YYYY');
-
+const VALIDATION_DATE = '/04/2019';
 const TODAY_KNOBS = Cypress.moment().format('YYYY-MM-DD');
 
 Then('Date input is disabled', () => {
@@ -65,8 +65,16 @@ When('I choose date tomorrow via DayPicker', () => {
   dayPickerDay(TOMORROW_CALENDAR).click();
 });
 
+When('I type specific date {string}', (day) => {
+  dateInput().clear().type(`${day}${VALIDATION_DATE}`);
+});
+
 Then('the date is set to today', () => {
   dayPickerDay(TODAY_CALENDAR).should('have.attr', 'aria-label').should('contains', TODAY_CALENDAR);
   dayPickerDay(TODAY_CALENDAR).should('have.attr', 'class').should('contains', `${DAY_PICKER_PREFIX}today`);
   dayPickerDay(TODAY_CALENDAR).should('have.attr', 'class').should('contains', `${DAY_PICKER_PREFIX}selected`);
 });
+
+And('I click onto specific day {string} via DayPicker for validation component', (specificDay) => {
+  dayPickerDay(`${specificDay}`).click();
+})
