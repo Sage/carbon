5d1a046417115e77043e15c711d90ab4a2686003
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2019-10-28 14:26:44 +0100

    added checkbox and radioButton validations scripts

--- cypress/locators/checkbox/index.js
+++ cypress/locators/checkbox/index.js
@@ -1,8 +1,11 @@
 import {
-  CHECKBOX_HELP, CHECKBOX_COMMON_INPUT_FIELD, CHECKBOX_LABEL,
+  CHECKBOX_HELP, CHECKBOX_COMMON_INPUT_FIELD, CHECKBOX_LABEL, CHECKBOX,
 } from './locators';
 
 // component preview locators
 export const checkboxHelpTextPreview = () => cy.iFrame(CHECKBOX_HELP);
 export const checkboxCommonInputField = () => cy.iFrame(CHECKBOX_COMMON_INPUT_FIELD);
 export const checkboxLabelPreview = () => cy.iFrame(CHECKBOX_LABEL);
+
+// component preview locators into iFrame
+export const checkbox = position => cy.get(CHECKBOX).eq(position);
