90e8075b2a225dbd4cf1c9fb8484234982f722c6
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2019-10-30 14:37:32 +0100

    added textbox scripts

--- cypress/support/step-definitions/common-steps.js
+++ cypress/support/step-definitions/common-steps.js
@@ -2,9 +2,10 @@ import {
   visitComponentUrl, setSlidebar, pressESCKey, pressTABKey,
 } from '../helper';
 import {
-  commonButtonPreview, labelPreview, helpIcon, inputWidthSlider, fieldHelpPreview,
-  labelWidthSlider, backgroundUILocator, closeIconButton, tooltipPreview, getKnobsInput,
-  icon, inputWidthPreview, label, eventInAction, getDataElementByNameAndValue, storyRoot,
+  commonButtonPreview, labelPreview, helpIcon, helpIconByPosition, inputWidthSlider,
+  fieldHelpPreview, fieldHelpPreviewByPosition, labelWidthSlider, backgroundUILocator,
+  closeIconButton, tooltipPreview, tooltipPreviewByPosition, getKnobsInput, icon,
+  inputWidthPreview, label, labelByPosition, eventInAction, getDataElementByNameAndValue, storyRoot,
   precisionSlider, storyRootNoIframe, tooltipPreviewNoIframe, getDataElementByValueNoIframe,
 } from '../../locators';
 import { dialogTitle, dialogSubtitle } from '../../locators/dialog';
@@ -12,6 +13,7 @@ import { DEBUG_FLAG } from '..';
 import { getElementNoIframe, commonButtonPreviewNoIframe } from '../../locators/build';
 
 const LABEL_INPUT_INLINE_CLASS = 'common-input__label--inline';
+const TEXT_ALIGN = 'text-align';
 const FIRST_ELEMENT = 0;
 const SECOND_ELEMENT = 1;
 const THIRD_ELEMENT = 2;
@@ -140,8 +142,60 @@ Then('label on preview is {string}', (text) => {
   labelPreview().should('have.text', text);
 });
 
+Then('label is set to {string}', (text) => {
+  label().should('have.text', text);
+});
+
+Then('{string} label is set to {string}', (position, text) => {
+  switch (position) {
+    case 'First':
+      labelByPosition(FIRST_ELEMENT).should('have.text', text);
+      break;
+    case 'Second':
+      labelByPosition(SECOND_ELEMENT).should('have.text', text);
+      break;
+    default: throw new Error('There are only two label elements on the page');
+  }
+});
+
+Then('{string} label width is set to {string}', (position, width) => {
+  switch (position) {
+    case 'First':
+      labelByPosition(FIRST_ELEMENT).should('have.attr', 'width', `${width}`);
+      break;
+    case 'Second':
+      labelByPosition(SECOND_ELEMENT).should('have.attr', 'width', `${width}`);
+      break;
+    default: throw new Error('There are only two label elements on the page');
+  }
+});
+
+Then('{string} label Align on preview is {string}', (position, direction) => {
+  switch (position) {
+    case 'First':
+      labelByPosition(FIRST_ELEMENT).should($element => expect($element).to.have.css(TEXT_ALIGN, `${direction}`));
+      break;
+    case 'Second':
+      labelByPosition(SECOND_ELEMENT).should($element => expect($element).to.have.css(TEXT_ALIGN, `${direction}`));
+      break;
+    default: throw new Error('There are only two label elements on the page');
+  }
+});
+
 When('I hover mouse onto help icon', () => {
   helpIcon().trigger('mouseover');
+});
+
+When('I hover mouse onto {string} help icon', (position) => {
+  switch (position) {
+    case 'first':
+      helpIconByPosition(FIRST_ELEMENT).trigger('mouseover');
+      break;
+    case 'second':
+      helpIconByPosition(SECOND_ELEMENT).trigger('mouseover');
+      break;
+    default: throw new Error('There are only two help icons on the page');
+  }
 });
 
 When('I hover mouse onto icon', () => {
@@ -184,6 +238,18 @@ Then('tooltipPreview on preview is set to {string}', (text) => {
   tooltipPreview().should('have.text', text);
 });
 
+Then('{string} tooltipPreview on preview is set to {string}', (position, text) => {
+  switch (position) {
+    case 'First':
+      tooltipPreviewByPosition(FIRST_ELEMENT).should('have.text', text);
+      break;
+    case 'Second':
+      tooltipPreviewByPosition(SECOND_ELEMENT).should('have.text', text);
+      break;
+    default: throw new Error('There are only two tooltipPreview elements on the page');
+  }
+});
+
 Then('tooltipPreview on preview into iFrame is set to {string}', (text) => {
   tooltipPreviewNoIframe().should('have.text', text);
 });
@@ -194,6 +260,18 @@ When('I set inputWidth slider to {int}', (width) => {
 
 Then('fieldHelp on preview is set to {string}', (text) => {
   fieldHelpPreview().should('have.text', text);
+});
+
+Then('{string} fieldHelp on preview is set to {string}', (position, text) => {
+  switch (position) {
+    case 'First':
+      fieldHelpPreviewByPosition(FIRST_ELEMENT).should('have.text', text);
+      break;
+    case 'Second':
+      fieldHelpPreviewByPosition(SECOND_ELEMENT).should('have.text', text);
+      break;
+    default: throw new Error('There are only two field helps elements on the page');
+  }
 });
 
 When('I set label width slider to {int}', (width) => {
--- cypress/support/step-definitions/textbox-steps.js
+++ cypress/support/step-definitions/textbox-steps.js
@@ -1,16 +1,46 @@
-import { textbox, textboxDataComponent } from '../../locators/textbox';
-import { label, commonDataElementInputPreview } from '../../locators';
+import { textbox, textboxByPosition, textboxDataComponent } from '../../locators/textbox';
+import { label, labelByPosition, commonDataElementInputPreview } from '../../locators';
 
 const TEXT_ALIGN = 'text-align';
+const FIRST_ELEMENT = 0;
+const SECOND_ELEMENT = 1;
 
 Then('Textbox placeholder is set to {string}', (text) => {
   textbox().children().should('have.attr', 'placeholder', text);
+});
+
+Then('{string} textbox placeholder is set to {string}', (position, text) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).children().should('have.attr', 'placeholder', text);
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).children().should('have.attr', 'placeholder', text);
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 Then('Textbox component is disabled', () => {
   textbox().children()
     .should('have.css', 'color', 'rgb(179, 194, 200)')
     .and('have.css', 'cursor', 'not-allowed');
+});
+
+Then('{string} textbox component is disabled', (position) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).children()
+        .should('have.css', 'color', 'rgba(0, 0, 0, 0.55)')
+        .and('have.css', 'cursor', 'not-allowed');
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).children()
+        .should('have.css', 'color', 'rgba(0, 0, 0, 0.55)')
+        .and('have.css', 'cursor', 'not-allowed');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 Then('Textbox component is not disabled', () => {
@@ -20,10 +50,53 @@ Then('Textbox component is not disabled', () => {
     .and('not.have.css', 'cursor', 'not-allowed');
 });
 
+Then('{string} textbox component is not disabled', (position) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).should('not.be.disabled');
+      textbox(FIRST_ELEMENT).children()
+        .should('not.have.css', 'color', 'rgba(0, 0, 0, 0.55)')
+        .and('not.have.css', 'cursor', 'not-allowed');
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).should('not.be.disabled');
+      textbox(SECOND_ELEMENT).children()
+        .should('not.have.css', 'color', 'rgba(0, 0, 0, 0.55)')
+        .and('not.have.css', 'cursor', 'not-allowed');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
+});
+
 Then('Textbox component is readOnly', () => {
-  textbox().should('have.css', 'background', 'rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box')
-    .and('have.css', 'border-color', 'rgba(0, 0, 0, 0)')
-    .and('not.have.css', 'background', 'rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box');
+  textbox().should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+    .and('have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
+    .and('have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+    .and('have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
+    .and('have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
+    .and('not.have.css', 'background-color', 'rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box');
+});
+
+Then('{string} textbox component is readOnly', (position) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'background-color', 'rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box');
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'background-color', 'rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 Then('Textbox component is not readOnly', () => {
@@ -32,20 +105,90 @@ Then('Textbox component is not readOnly', () => {
     .and('have.css', 'background', 'rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box border-box');
 });
 
+Then('{string} textbox component is not readOnly', (position) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).should('not.have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'background-color', 'rgb(255, 255, 255)');
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).should('not.have.css', 'background-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
+        .and('not.have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
+        .and('have.css', 'background-color', 'rgb(255, 255, 255)');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
+});
+
 Then('Textbox component is labelInline', () => {
   label().should('have.css', TEXT_ALIGN, 'left');
+});
+
+Then('{string} textbox component is labelInline', (position) => {
+  switch (position) {
+    case 'First':
+      labelByPosition(FIRST_ELEMENT).should('have.css', TEXT_ALIGN, 'left');
+      break;
+    case 'Second':
+      labelByPosition(SECOND_ELEMENT).should('have.css', TEXT_ALIGN, 'left');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 Then('Textbox component is not labelInline', () => {
   label().should('not.have.css', TEXT_ALIGN, 'left');
 });
 
+Then('{string} textbox component is not labelInline', (position) => {
+  switch (position) {
+    case 'First':
+      labelByPosition(FIRST_ELEMENT).should('not.have.css', TEXT_ALIGN, 'left');
+      break;
+    case 'Second':
+      labelByPosition(SECOND_ELEMENT).should('not.have.css', TEXT_ALIGN, 'left');
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
+});
+
 Then('Textbox inputWidth is set to {string}', (width) => {
   textbox().should('have.css', 'flex', `0 0 ${width}%`);
 });
 
+Then('{string} Textbox inputWidth is set to {string}', (position, width) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).should('have.css', 'flex', `0 0 ${width}%`);
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).should('have.css', 'flex', `0 0 ${width}%`);
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
+});
+
 When('I input {string} into Textbox', (text) => {
   textbox().children().clear().type(text);
+});
+
+When('I input {string} into {string} Textbox', (text, position) => {
+  switch (position) {
+    case 'first':
+      textboxByPosition(FIRST_ELEMENT).children().clear().type(text);
+      break;
+    case 'second':
+      textboxByPosition(SECOND_ELEMENT).children().clear().type(text);
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 When('I input {string} into Textbox for deprecated component', (text) => {
@@ -56,6 +199,22 @@ Then('Textbox input on preview is set to {string}', () => {
   textbox().children().invoke('text').then(((text) => {
     expect(text.trim()).to.eq(text);
   }));
+});
+
+Then('{string} textbox input on preview is set to {string}', (position) => {
+  switch (position) {
+    case 'First':
+      textbox(FIRST_ELEMENT).children().invoke('text').then(((text) => {
+        expect(text.trim()).to.eq(text);
+      }));
+      break;
+    case 'Second':
+      textbox(SECOND_ELEMENT).children().invoke('text').then(((text) => {
+        expect(text.trim()).to.eq(text);
+      }));
+      break;
+    default: throw new Error('There are only two textbox elements on the page');
+  }
 });
 
 Then('Textbox input on preview is set to {string} for deprecated component', () => {
