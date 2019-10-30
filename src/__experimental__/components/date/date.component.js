d409ef2bc8fd9f98bfb10be160bf4739f7cbd8d8
grabkowski <jgrabkowski@pgs-soft.com>
2019-10-30 11:39:07 +0000

    fixed incorrenct DateRange validation

--- src/__experimental__/components/date/date.component.js
+++ src/__experimental__/components/date/date.component.js
@@ -311,6 +311,7 @@ class BaseDateInput extends React.Component {
           { ...inputProps }
           inputIcon='calendar'
           value={ this.state.visibleValue }
+          rawValue={ DateHelper.formatDateString(this.state.selectedDate) }
           inputRef={ this.assignInput }
           { ...events }
         />
