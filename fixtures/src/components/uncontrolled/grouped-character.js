3fc5cba1ba74a8b9153b5c4598693bd990eb0ca7
Ed Leeks <edward.leeks@sage.com>
2019-11-06 10:10:02 +0000

    refactor: grouped-character fixtures

--- fixtures/src/components/uncontrolled/grouped-character.js
+++ fixtures/src/components/uncontrolled/grouped-character.js
@@ -15,8 +15,8 @@ const UncontrolledGroupedCharacter = () => {
             <div id='controlled_grouped_character'>
               <h1>Uncontrolled GroupedCharacter</h1>
               <ul>
-                <li>onChange handler should update the log when the value is changed, e.target.value should be the
-                  users input
+                <li>onChange handler should update the log when the value is changed, e.target.value should contain
+                 &#123; formattedValue, rawValue &#125; based on the user&apos;s input
                 </li>
                 <li>onBlur handler should update the log when the groupedcharacter is blurred
                 </li>
@@ -41,4 +41,4 @@ const UncontrolledGroupedCharacter = () => {
   );
 };
 
-export default React.memo(UncontrolledGroupedCharacter, () => false);
+export default UncontrolledGroupedCharacter;
