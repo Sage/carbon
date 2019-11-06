67015660e6e5ebbdf29f7ab3a3e260356b0c6e2c
Ed Leeks <edward.leeks@sage.com>
2019-11-06 10:55:57 +0000

    refactor: update index reducers

--- fixtures/src/components/index.js
+++ fixtures/src/components/index.js
@@ -5,27 +5,21 @@ import Link from 'carbon-react/lib/components/link';
 const Index = ({ route: { routes } }) => (
   <AppWrapper>
     <h1>Carbon Fixture Testbed</h1>
-    <p>This application is used to test various carbon controls. It is intended to be used during automation.</p>
+    <p>This application is used to test various Carbon controls. It is intended to be used during automation.</p>
 
     <h2>Uncontrolled Inputs</h2>
     <ol>
-      {Object.keys(routes).reduce(((acc, key) => {
-        if (routes[key].uncontrolled) {
-          const to = `/uncontrolled/${key}`;
-          acc.push(<li key={ to }><Link to={ to }>{routes[key].description}</Link></li>);
-        }
-        return acc;
-      }), [])}
+      {Object.keys(routes).filter(key => routes[key].uncontrolled).map((key) => {
+        const to = `/uncontrolled/${key}`;
+        return (<li key={ to }><Link to={ to }>{routes[key].description}</Link></li>);
+      })}
     </ol>
     <h2>Controlled Inputs</h2>
     <ol>
-      {Object.keys(routes).reduce(((acc, key) => {
-        if (routes[key].controlled) {
-          const to = `/controlled/${key}`;
-          acc.push(<li key={ to }><Link to={ to }>{routes[key].description}</Link></li>);
-        }
-        return acc;
-      }), [])}
+      {Object.keys(routes).filter(key => routes[key].controlled).map((key) => {
+        const to = `/controlled/${key}`;
+        return (<li key={ to }><Link to={ to }>{routes[key].description}</Link></li>);
+      })}
     </ol>
   </AppWrapper>
 );
