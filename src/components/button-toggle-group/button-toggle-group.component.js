67f310bfa5cf9270e2af9b669d5b2fc44f54e1d8
grabkowski <jgrabkowski@pgs-soft.com>
2019-11-07 10:57:11 +0000

    added controlled functionality to ButtonToggleGroup

--- src/components/button-toggle-group/button-toggle-group.component.js
+++ src/components/button-toggle-group/button-toggle-group.component.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import FormField from '../../__experimental__/components/form-field';
 import ButtonToggleGroupStyle from './button-toggle-group.style';
 import withValidations from '../validations/with-validation.hoc';
+import RadioButtonMapper from '../../__experimental__/components/radio-button/radio-button-mapper.component';
 
 const BaseButtonToggleGroup = ((props) => {
   const {
+    name,
     inputWidth,
     errorMessage,
     label,
     onChange,
-    children
+    onBlur,
+    children,
+    value
   } = props;
 
   return (
@@ -23,13 +27,22 @@ const BaseButtonToggleGroup = ((props) => {
         errorMessage={ errorMessage }
         onChange={ onChange }
       >
-        {children}
+        <RadioButtonMapper
+          name={ name }
+          onBlur={ onBlur }
+          onChange={ onChange }
+          value={ value }
+        >
+          {children}
+        </RadioButtonMapper>
       </ButtonToggleGroupStyle>
     </FormField>
   );
 });
 
 BaseButtonToggleGroup.propTypes = {
+  /** Specifies the name prop to be applied to each button in the group */
+  name: PropTypes.string.isRequired,
   /** Children to be rendered (ButtonToggle). */
   children: PropTypes.node.isRequired,
   /** Message displayed on error */
@@ -51,7 +64,11 @@ BaseButtonToggleGroup.propTypes = {
   /** The alignment for the text in the label. */
   labelAlign: PropTypes.string,
   /** callback to handle change event */
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  /** Callback fired when each RadioButton is blurred */
+  onBlur: PropTypes.func,
+  /** The value of the Button Toggle Group */
+  value: PropTypes.string
 };
 
 BaseButtonToggleGroup.displayName = 'BaseButtonToggleGroup';
