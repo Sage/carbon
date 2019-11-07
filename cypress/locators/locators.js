0163c72b1a5e6de77450dd8aea8073b0d7002cf6
Ihor Dluhosh <idluhosh@pgs-soft.com>
2019-11-07 12:02:59 +0100

    updated tests for Link component

--- cypress/locators/locators.js
+++ cypress/locators/locators.js
@@ -12,7 +12,7 @@ export const TOOLTIP_PREVIEW = '[data-component="tooltip"]';
 export const FIELD_HELP_PREVIEW = 'span[data-element="help"]';
 export const STORY_ROOT = '#story-root';
 export const BACKGROUND_UI_LOCATOR = 'div[data-element="modal-background"]';
-export const LINK = 'a[data-component="link"][tabindex=0]';
+export const LINK = '[data-component="link"]';
 export const ICON = '[data-component="icon"]';
 export const INPUT_WIDTH_PREVIEW = '.common-input__field';
 export const COMMMON_DATA_ELEMENT_INPUT = 'input[data-element="input"]';
