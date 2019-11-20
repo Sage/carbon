1617ad954877e0f8e1d5e711de29f42da0fb2e3e
Ihor Dluhosh <idluhosh@pgs-soft.com>
2019-11-20 16:08:56 +0100

    added actionPopover tests

--- cypress/locators/action-popover/index.js
+++ cypress/locators/action-popover/index.js
@@ -6,9 +6,11 @@ import {
 } from './locators';
 
 // component preview locators
-export const actionPopoverButton = () => cy.iFrame(ACTION_POPOVER_BUTTON);
-export const actionPopover = () => cy.iFrame(ACTION_POPOVER_DATA_COMPONENT);
-export const actionPopoverDivider = () => cy.iFrame(ACTION_POPOVER_DIVIDER);
-export const actionPopoverInnerItem = index => actionPopover().eq(0)
+export const actionPopoverButton = () => cy.get(ACTION_POPOVER_BUTTON);
+export const actionPopoverButtonNoIframe = () => cy.iFrame(ACTION_POPOVER_BUTTON);
+export const actionPopover = () => cy.get(ACTION_POPOVER_DATA_COMPONENT);
+export const actionPopoverNoIFrame = () => cy.iFrame(ACTION_POPOVER_DATA_COMPONENT);
+export const actionPopoverDivider = () => cy.get(ACTION_POPOVER_DIVIDER);
+export const actionPopoverInnerItem = index => cy.iFrame(ACTION_POPOVER_DATA_COMPONENT).eq(0)
   .find(`div:nth-child(${index})`);
-export const ellipsisIcon = () => cy.iFrame(ELLIPSIS_VERTICAL_ICON);
+export const ellipsisIcon = () => cy.get(ELLIPSIS_VERTICAL_ICON);
--- cypress/support/step-definitions/action-popover-steps.js
+++ cypress/support/step-definitions/action-popover-steps.js
@@ -1,24 +1,75 @@
-import { actionPopoverButton, actionPopover, actionPopoverInnerItem } from '../../locators/action-popover';
+import {
+  actionPopoverButton, actionPopover, actionPopoverInnerItem,
+  actionPopoverButtonNoIframe, actionPopoverNoIFrame,
+} from '../../locators/action-popover';
 
 Then('Action Popover element is visible', () => {
   actionPopover().should('be.visible');
+});
+
+Then('Action Popover element is not visible', () => {
+  actionPopover().should('not.be.visible');
 });
 
 When('I open Action Popover element', () => {
   actionPopoverButton().eq(0).click();
 });
 
+When('I open Action Popover element page with iFrame', () => {
+  actionPopoverButtonNoIframe().eq(0).click();
+});
+
+When('I hit ESC on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 16, which: 16, release: false });
+  cy.focused().trigger('keydown', { keyCode: 27, which: 27 });
+});
+
+When('I hit Tab on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 9, which: 9 });
+});
+
+When('I hit Home on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 36, which: 36 });
+});
+
+When('I hit uparrow on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 38, which: 38 });
+});
+
+When('I hit End on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 35, which: 35 });
+});
+
+When('I hit ShiftTab on Action Popover element in iFrame', () => {
+  cy.focused().trigger('keydown', { keyCode: 16, which: 16, release: false });
+  cy.focused().trigger('keydown', { keyCode: 9, which: 9 });
+});
+
+When('I click on outside dialog in iFrame', () => {
+  cy.get('#story-root').click({ force: true });
+});
+
 When('I press keyboard {string} key times {int}', (key, times) => {
   for (let i = 0; i < times; i++) {
-    actionPopover().eq(0).trigger(`{${key}}`);
+    cy.focused().type(`${key}`);
   }
 });
 
-Then('Action Popover element {string} inner context is set to {string}', (element, text) => {
-  cy.focused().should('have', actionPopoverInnerItem(element));
-  actionPopoverInnerItem(element).invoke('text').should('contain', text);
+When('I press keyboard {string} key times {int} on actionPopover open icon', (key, times) => {
+  for (let i = 0; i < times; i++) {
+    actionPopoverButton().first().type(`${key}`);
+  }
+});
+
+Then('Action Popover element {word} inner context is set to {string}', (element, text) => {
+  cy.focused().should('contain', text);
+  cy.focused().should('have.css', 'box-shadow', 'rgb(255, 181, 0) 0px 0px 0px 2px inset');
 });
 
 When('I click {int} actionPopoverInnerItem', (element) => {
   actionPopoverInnerItem(element).click();
+});
+
+When('I press {string} actionPopoverInnerItem onto {int} element', (key, element) => {
+  actionPopoverInnerItem(element).type(`{${key}}`);
 });
