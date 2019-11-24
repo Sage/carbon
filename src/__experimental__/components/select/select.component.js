0c5a2281eece52bad1f45976df53a926e0c43352
Bence Laky <bence.laky@sage.com>
2019-11-24 10:32:11 +0100

    feat: leftchildren property added to select

--- src/__experimental__/components/select/select.component.js
+++ src/__experimental__/components/select/select.component.js
@@ -231,10 +231,9 @@ class Select extends React.Component {
 
   textboxProps() {
     const {
-      typeAhead,
-      value,
-      placeholder
+      typeAhead, value, placeholder, leftChildren
     } = this.props;
+
 
     const placeholderText = this.placeholder(placeholder, value);
 
@@ -247,6 +246,10 @@ class Select extends React.Component {
       value: this.value(value),
       formattedValue: this.formattedValue(this.state.filter, value)
     };
+
+    if (leftChildren) {
+      props.leftChildren = props.leftChildren ? [leftChildren, ...props.leftChildren] : leftChildren;
+    }
 
     return props;
   }
@@ -358,7 +361,9 @@ Select.propTypes = {
   typeAhead: PropTypes.bool,
   /** Can the user type a value in the <Textbox> to filter the dropdown menu options? */
   filterable: PropTypes.bool,
-  isAnyValueSelected: PropTypes.bool
+  isAnyValueSelected: PropTypes.bool,
+  /** Add additional child elements before the input */
+  leftChildren: PropTypes.node
 };
 
 Select.defaultProps = {
