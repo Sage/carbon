a21f33fdb32cd4bfae19ad97747217c29d103d46
grabkowski <jgrabkowski@pgs-soft.com>
2019-11-27 16:11:53 +0000

    fixed RadioButton label incorrect spacing
    moved Checkbox related styling from CheckableInput to Checkbox

--- src/__experimental__/components/checkbox/checkbox.spec.js
+++ src/__experimental__/components/checkbox/checkbox.spec.js
@@ -283,7 +283,6 @@ describe('Checkbox', () => {
 
       it('applies appropriate Label styles', () => {
         assertStyleMatch({
-          paddingLeft: '6px',
           paddingBottom: '0',
           paddingTop: '0'
         }, wrapper, { modifier: css`${LabelStyle}` });
