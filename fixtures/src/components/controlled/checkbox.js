5aee4e41127c70f1c0da82eab35a0ef4c9a1277b
Michael Hogg <michael@kyan.com>
2019-11-27 14:27:49 +0000

    (FE-2248) Controlled checkbox fixture: add group example

--- fixtures/src/components/controlled/checkbox.js
+++ fixtures/src/components/controlled/checkbox.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { Checkbox } from 'carbon-react/lib/__experimental__/components/checkbox';
+import { Checkbox, CheckboxGroup } from 'carbon-react/lib/__experimental__/components/checkbox';
 import { LogConsumer } from '../log';
 
 const ControlledCheckbox = () => {
   const [checked, setChecked] = useState(false);
   const [checkedExampleTwo, setCheckedExampleTwo] = useState(true);
+  const [checkedGroupCbOne, setCheckedGroupCbOne] = useState(false);
+  const [checkedGroupCbTwo, setCheckedGroupCbTwo] = useState(false);
+  const [checkedGroupCbThree, setCheckedGroupCbThree] = useState(false);
   return (
     <LogConsumer>
       {(log) => {
@@ -45,6 +48,44 @@ const ControlledCheckbox = () => {
                 checked={ checkedExampleTwo }
                 name='controlled_checkbox_two'
               />
+            </div>
+            <div id='controlled_checkboxes_in_group'>
+              <h2>Within A Checkbox Group</h2>
+              <ul>
+                <li>each checkbox is independently controlled within the group</li>
+              </ul>
+              <CheckboxGroup
+                id='checkbox_group'
+                label='Checkbox Group Example'
+              >
+                <Checkbox
+                  id='group_checkbox_one'
+                  name='group_checkbox_one'
+                  value='value one'
+                  label='label one'
+                  checked={ checkedGroupCbOne }
+                  onChange={ onChange(setCheckedGroupCbOne) }
+                  onBlur={ onBlur }
+                />
+                <Checkbox
+                  id='group_checkbox_two'
+                  name='group_checkbox_two'
+                  value='value two'
+                  label='label two'
+                  checked={ checkedGroupCbTwo }
+                  onChange={ onChange(setCheckedGroupCbTwo) }
+                  onBlur={ onBlur }
+                />
+                <Checkbox
+                  id='group_checkbox_three'
+                  name='group_checkbox_three'
+                  value='value three'
+                  label='label three'
+                  checked={ checkedGroupCbThree }
+                  onChange={ onChange(setCheckedGroupCbThree) }
+                  onBlur={ onBlur }
+                />
+              </CheckboxGroup>
             </div>
           </React.Fragment>
         );
