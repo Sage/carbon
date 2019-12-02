4f7c65551d16a8b338533b8d80471b64983ac7ee
Ihor Dluhosh <idluhosh@pgs-soft.com>
2019-12-02 14:58:11 +0100

    updated package.json & cypress.sh

--- script/docker/cypress.sh
+++ script/docker/cypress.sh
@@ -7,11 +7,21 @@ HELP_TEXT="Options:\n\n
 --build\t\t\tRuns the build tests\n
 --accessibility\t\tRuns the accessibility tests\n
 --regression\t\t\tRuns the regression tests
+--regression-common\t\t\tRuns the regression common tests
+--regression-experimental\t\t\tRuns the regression experimental tests
+--regression-deprecated\t\t\tRuns the regression deprecated tests
+--regression-validation\t\t\tRuns the regression validation tests
+--regression-themes\t\t\tRuns the regression themes tests
 "
 
 RUN_BUILD=false
 RUN_ACCESSIBILITY=false
 RUN_REGRESSION=false
+RUN_REGRESSION_COMMON=false
+RUN_REGRESSION_EXPERIMENTAL=false
+RUN_REGRESSION_DEPRECATED=false
+RUN_REGRESSION_VALIDATION=false
+RUN_REGRESSION_THEMES=false
 
 while test $# -gt 0; do
     case "$1" in
@@ -35,6 +45,31 @@ while test $# -gt 0; do
           RUN_REGRESSION=true
           shift
           ;;
+        # run regression common tests
+        -rc|--regression-common)
+          RUN_REGRESSION_COMMON=true
+          shift
+          ;;
+       # run regression experimental tests
+       -re|--regression-experimental)
+          RUN_REGRESSION_EXPERIMENTAL=true
+          shift
+          ;;
+       # run regression deprecated tests
+        -rd|--regression-deprecated)
+          RUN_REGRESSION_DEPRECATED=true
+          shift
+          ;;
+       # run regression validation tests
+        -rv|--regression-validation)
+          RUN_REGRESSION_VALIDATION=true
+          shift
+          ;;
+       # run regression themes tests
+        -rt|--regression-themes)
+          RUN_REGRESSION_THEMES=true
+          shift
+          ;;
         *)
           break
           ;;
@@ -54,4 +89,29 @@ fi
 if $RUN_REGRESSION; then
   echo 'Running regression tests'
   docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run test-cypress-regression"
+fi
+
+if $RUN_REGRESSION_COMMON; then
+  echo 'Running regression common tests'
+  docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run cypress-group-regression-common"
+fi
+
+if $RUN_REGRESSION_EXPERIMENTAL; then
+  echo 'Running regression experimental tests'
+  docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run cypress-group-regression-experimental"
+fi
+
+if $RUN_REGRESSION_DEPRECATED; then
+  echo 'Running regression deprecated tests'
+  docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run cypress-group-regression-deprecated"
+fi
+
+if $RUN_REGRESSION_VALIDATION; then
+  echo 'Running regression validation tests'
+  docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run cypress-group-regression-validations"
+fi
+
+if $RUN_REGRESSION_THEMES; then
+  echo 'Running regression themes tests'
+  docker-compose exec -T cypress bash -c "wait-on http://storybook:9001 && npm run cypress-group-regression-themes"
 fi
