eea93029305104f7973c8720b6b5dd67171206f2
mkrds <mkordas@pgs-soft.com>
2019-12-03 14:04:30 +0100

    Added possibility to pass onKeyDown callback

--- src/__experimental__/components/date/date.component.js
+++ src/__experimental__/components/date/date.component.js
@@ -115,7 +115,9 @@ class BaseDateInput extends React.Component {
     }
   };
 
-  handleTabKeyDown = (ev) => {
+  handleKeyDown = (ev) => {
+    if (this.props.onKeyDown) this.props.onKeyDown(ev);
+
     if (Events.isTabKey(ev)) {
       this.isOpening = false;
       this.inputFocusedViaPicker = false;
@@ -315,7 +317,7 @@ class BaseDateInput extends React.Component {
       onBlur: this.handleBlur,
       onChange: this.handleVisibleInputChange,
       onFocus: this.handleFocus,
-      onKeyDown: this.handleTabKeyDown
+      onKeyDown: this.handleKeyDown
     };
 
     return (
