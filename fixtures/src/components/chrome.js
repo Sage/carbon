f320a9238bd45529d92bb12e53868c45e9f14777
Damian Boni <damian@kyanmedia.com>
2019-12-03 14:20:41 +0000

    FE-2386 fixed bug where log messages were being lost.

--- fixtures/src/components/chrome.js
+++ fixtures/src/components/chrome.js
@@ -1,25 +1,27 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import Link from 'carbon-react/lib/components/link';
 import AppWrapper from 'carbon-react/lib/components/app-wrapper';
 import Log, { LogProvider } from './log';
 
 const Chrome = ({ children }) => {
   const [history, setHistory] = useState([]);
+  const refHistory = useRef([]);
 
   const log = useCallback(({
     target: {
       name, id, value, checked
     }
   }, obj) => {
-    setHistory([...history, {
+    refHistory.current = [...refHistory.current, {
       ...obj,
       timestamp: Date.now(),
       name,
       id,
       value,
       checked
-    }]);
-  }, [history, setHistory]);
+    }];
+    setHistory(refHistory.current);
+  }, [history, refHistory, setHistory]);
 
   return (
     <AppWrapper>
