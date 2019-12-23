705adbd158e6283930d358ac6e949978fb81acb4
grabkowski <jgrabkowski@pgs-soft.com>
2019-12-23 15:38:44 +0000

    fix: typos

--- src/style/themes/medium/index.js
+++ src/style/themes/medium/index.js
@@ -1,7 +1,7 @@
 import { aegeanTheme } from '..';
 
 export default (() => {
-  console.warn(`"Medium Theme" have been renamed to "Aegean".
+  console.warn(`"Medium Theme" has been renamed to "Aegean".
 All references to that theme should be updated to:
 import { aegeanTheme } from "carbon-react/lib/style/themes"`);
 
--- src/style/themes/small/index.js
+++ src/style/themes/small/index.js
@@ -1,7 +1,7 @@
 import { mintTheme } from '..';
 
 export default (() => {
-  console.warn(`"Small Theme" have been renamed to "Mint".
+  console.warn(`"Small Theme" has been renamed to "Mint".
 All references to that theme should be updated to:
 import { mintTheme } from 'carbon-react/lib/style/themes'`);
 
