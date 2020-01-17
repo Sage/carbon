f24bfb1dcd79ca84178f4c299f3b2af7a02311b7
Ed Leeks <edward.leeks@sage.com>
2020-01-17 10:45:35 +0000

    refactor(action-popover): separates popopver-menu from popover-button
    action-popover-menu now a separate component to allow use as submenu

--- src/components/action-popover/index.js
+++ src/components/action-popover/index.js
@@ -1,3 +1,4 @@
 export { default as ActionPopover } from './action-popover.component';
+export { default as ActionPopoverMenu } from './action-popover-menu.component';
 export { default as ActionPopoverItem } from './action-popover-item.component';
 export { default as ActionPopoverDivider } from './action-popover-divider.component';
