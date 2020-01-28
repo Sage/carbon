9491dee11dfa59f2649903c3e54ac5a6af31650d
Ihor Dluhosh <idluhosh@pgs-soft.com>
2020-01-28 11:56:02 +0100

    test(splitbutton): add splitButton tests
    FE-2358

--- cypress/locators/split-button/index.js
+++ cypress/locators/split-button/index.js
@@ -1,4 +1,10 @@
-import { SPLIT_TOGGLE_BUTTON } from './locators';
+import { SPLIT_TOGGLE_BUTTON, ADDITIONAL_BUTTONS, SPLIT_MAIN_BUTTON } from './locators';
 
 // component preview locators
 export const splitToggleButton = () => cy.iFrame(SPLIT_TOGGLE_BUTTON);
+export const additionalButton = index => cy.iFrame(ADDITIONAL_BUTTONS).children().eq(index);
+export const splitMainButton = index => cy.iFrame(SPLIT_MAIN_BUTTON)
+  .find('button')
+  .find(`span:nth-child(${index})`);
+export const splitMainButtonDataComponent = index => cy.iFrame(SPLIT_MAIN_BUTTON)
+  .find(`:nth-child(${index})`);
--- cypress/locators/split-button/locators.js
+++ cypress/locators/split-button/locators.js
@@ -1,2 +1,4 @@
 // component preview locators
 export const SPLIT_TOGGLE_BUTTON = '[data-element="toggle-button"]';
+export const ADDITIONAL_BUTTONS = '[data-element="additional-buttons"]';
+export const SPLIT_MAIN_BUTTON = '[data-component="split-button"]';
