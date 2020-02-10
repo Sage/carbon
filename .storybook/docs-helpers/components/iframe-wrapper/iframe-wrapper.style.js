50bd96e1acadb3a4789c70b1346bc694b78b68b4
Ed Leeks <edward.leeks@sage.com>
2020-02-10 14:10:27 +0000

    docs: adds story docs

--- .storybook/docs-helpers/components/iframe-wrapper/iframe-wrapper.style.js
+++ .storybook/docs-helpers/components/iframe-wrapper/iframe-wrapper.style.js
@@ -1,20 +1,12 @@
 import styled from 'styled-components';
 
 const DocsWrapper = styled.iframe`
-  ${({ src, width, height }) => `
+  ${({ src, height }) => `
     src: ${src};
-    width: ${width};
     height: ${height};
   `}
-  padding: 24px;
-  display: flex;
-  position: relative;
-  flex-wrap: wrap;
-  overflow: auto;
-  flex-direction: row;
-  box-shadow: 0px 2px 3px -2px rgba(0,0,0,0.2);
-  border-radius: 5px;
-  border: 1px solid lightgray;
+  width: 100%;
+  border: none;
 `;
 
 export default DocsWrapper;
