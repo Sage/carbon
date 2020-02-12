53b2404b1bde6510958751dfefe9e69fa3971197
Damian Boni <damian@kyanmedia.com>
2020-02-12 15:57:00 +0000

    feat(icon-button): new component to be used in place of close icon

---
+++ src/components/icon-button/icon-button-classic.style.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+import { isClassic } from '../../utils/helpers/style-helper';
+
+export default ({ theme }) => isClassic(theme) && css`
+  &:focus {
+    outline: -webkit-focus-ring-color auto 5px;
+  }
+`;
