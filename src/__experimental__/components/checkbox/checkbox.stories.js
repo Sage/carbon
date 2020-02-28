e5566cfc509589cf46c30f56909daefbce8f2515
mkrds <mkordas@pgs-soft.com>
2020-02-28 13:50:48 +0100

    fix(checkbox): fix incorrect key assignment in checkbox story

--- src/__experimental__/components/checkbox/checkbox.stories.js
+++ src/__experimental__/components/checkbox/checkbox.stories.js
@@ -212,9 +212,9 @@ const checkboxGroupComponent = () => (
 
     <h3>In Group</h3>
     <State store={ groupStore }>
-      {state => [
+      {state => (
         <CheckboxGroup
-          key='checkbox-group'
+          id='checkbox-group'
           name='checkbox-group'
           groupName='checkbox-group'
           label={ text('label', 'What would you choose?', 'group') }
@@ -228,13 +228,13 @@ const checkboxGroupComponent = () => (
             <Checkbox
               checked={ state[id] }
               name={ `checkbox-input-${id}` }
-              key={ `checkbox-input-${id}` }
               onChange={ ev => handleGroupChange(ev, id) }
               { ...defaultKnobs(id) }
+              key={ `checkbox-input-${id}` }
             />
           ))}
         </CheckboxGroup>
-      ]}
+      )}
     </State>
   </div>
 );
