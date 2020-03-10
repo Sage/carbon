12d0d8873d5243c132a04a85fc0c9ed2f9962326
mkrds <mkordas@pgs-soft.com>
2020-03-10 15:32:34 +0100

    feat(anchor-navigation): create anchor-navigation component

---
+++ src/components/anchor-navigation/anchor-navigation.style.js
@@ -0,0 +1,104 @@
+import styled, { css } from 'styled-components';
+
+import { baseTheme } from '../../style/themes';
+
+const StyledAnchorNavigation = styled.div`
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+
+  ${({ styleOverride }) => styleOverride}
+`;
+
+const StyledNavigation = styled.ul`
+  position: sticky;
+  top: 32px;
+  box-shadow: inset 3px 0px 0px 0px ${({ theme }) => theme.disabled.background};
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  max-width: 240px;
+
+  ${({ styleOverride }) => styleOverride}
+`;
+
+const StyledContent = styled.div`
+  flex: 1;
+  margin-left: 32px;
+
+  ${({ styleOverride }) => styleOverride}
+`;
+
+const StyledNavigationItem = styled.li`
+  width: 100%;
+
+  a {
+    cursor: pointer;
+    display: block;
+    text-decoration: none;
+    color: ${({ theme }) => theme.text.color};
+    background-color: transparent;
+    border-left: 3px solid ${({ theme }) => theme.disabled.background};
+    font-weight: 700;
+    padding: 12px 24px;
+
+    &:focus {
+      outline: none;
+      position: relative;
+
+      &:before {
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: -3px;
+        right: 0;
+        z-index: 1;
+        box-shadow: 0 0 6px ${({ theme }) => theme.colors.focus};
+      }
+    }
+
+    &:hover {
+      background-color: ${({ isSelected, theme }) => (!isSelected && theme.anchorNavigation.navItemHoverBackground)}
+    }
+
+    ${({ isSelected, theme }) => isSelected && css`
+      background-color: ${theme.colors.white};
+      border-left-color: ${theme.colors.primary};
+    `}
+  }
+
+  ${({ styleOverride }) => styleOverride}
+`;
+
+const StyledAnchorDivider = styled.div.attrs({ 'data-element': 'anchor-navigation-divider' })`
+  background-color: ${({ theme }) => theme.anchorNavigation.divider};
+  height: 1px;
+
+  ${({ styleOverride }) => styleOverride}
+`;
+
+
+StyledAnchorNavigation.defaultProps = {
+  theme: baseTheme
+};
+StyledNavigation.defaultProps = {
+  theme: baseTheme
+};
+StyledNavigationItem.defaultProps = {
+  theme: baseTheme
+};
+StyledContent.defaultProps = {
+  theme: baseTheme
+};
+StyledAnchorDivider.defaultProps = {
+  theme: baseTheme
+};
+
+export {
+  StyledAnchorNavigation,
+  StyledNavigation,
+  StyledNavigationItem,
+  StyledContent,
+  StyledAnchorDivider
+};
---
+++ src/components/anchor-navigation/anchor-section-divider.component.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import { StyledAnchorDivider } from './anchor-navigation.style';
+
+StyledAnchorDivider.displayName = 'AnchorSectionDivider';
+
+StyledAnchorDivider.propTypes = {
+  styleOverride: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
+};
+
+export default StyledAnchorDivider;
