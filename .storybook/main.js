1ea3565bed715ba6f9801bf92973adda0d987f56
Ed Leeks <edward.leeks@sage.com>
2020-03-11 12:26:26 +0000

    chore: remove cdn font import and add local font for storybook
    BREAKING CHANGE: The Lato font import has been removed from carbon
    consuming applications are now responsible for including them, how to do so has been documented in README.md

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,6 +18,11 @@ module.exports = {
         test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']
       }
     );
+    config.module.rules.push(
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['url-loader']
+      }
+    );
     config.resolve = {
       alias: {
         helpers: path.resolve(__dirname, '__helpers__/')
