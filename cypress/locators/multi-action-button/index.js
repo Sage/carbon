1b725fdf38e4eded18754ba8c247b021dc46e40a
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2020-03-25 13:08:42 +0100

    test(mountinapp, multiactionbutton,navigationbar): add cyress tests
    add cypress tests for multiActionButton default story and remove classic story, minor refactor for
    mountInApp and NavigationBar components

--- cypress/locators/multi-action-button/index.js
+++ cypress/locators/multi-action-button/index.js
@@ -1,5 +1,10 @@
-import { MULTI_ACTION_BUTTON_PREVIEW, MULTI_ACTION_BUTTON_LIST } from './locators';
+import { MULTI_ACTION_BUTTON_LIST, MULTI_ACTION_BUTTON_SUBTEXT, MULTI_ACTION_BUTTON_TEXT, MULTI_ACTION_BUTTON_COMPONENT } from './locators';
 
 // component preview locators
-export const multiActionButtonPreview = () => cy.iFrame(MULTI_ACTION_BUTTON_PREVIEW);
+export const multiActionButtonComponent = () => cy.iFrame(MULTI_ACTION_BUTTON_COMPONENT);
 export const multiActionButtonList = () => cy.iFrame(MULTI_ACTION_BUTTON_LIST).children();
+export const multiActionButtonSubtext = () => cy.iFrame(MULTI_ACTION_BUTTON_SUBTEXT);
+export const multiActionButtonText = () => multiActionButtonComponent()
+  .find(MULTI_ACTION_BUTTON_TEXT);
+export const multiActionButton = () => multiActionButtonComponent()
+  .find('button');
--- cypress/support/step-definitions/multi-action-button-steps.js
+++ cypress/support/step-definitions/multi-action-button-steps.js
@@ -1,51 +1,34 @@
-import { multiActionButtonPreview, multiActionButtonList } from '../../locators/multi-action-button';
+import { multiActionButtonList, multiActionButtonSubtext, multiActionButtonText, multiActionButton, multiActionButtonComponent,
+} from '../../locators/multi-action-button';
 
 const MULTI_ACTION_BUTTON_INNER_TEXT = 'Example ButtonExample Button with long textShort';
 const TEXT_ALIGN = 'text-align';
 
 Then('Multi Action Button text on preview is set to {string}', (text) => {
-  multiActionButtonPreview()
-    .children().children().first()
-    .should('have.text', text);
+  multiActionButtonText().should('have.text', text);
 });
 
 Then('Multi Action Button state is disabled', () => {
-  multiActionButtonPreview()
-    .children()
-    .should('have.attr', 'disabled');
+  multiActionButton().should('have.attr', 'disabled');
 });
 
 Then('Multi Action Button state is not disabled', () => {
-  multiActionButtonPreview()
-    .children()
-    .should('not.have.attr', 'disabled');
+  multiActionButton().should('not.have.attr', 'disabled');
 });
 
-Then('Multi Action Button as on preview is {string}', (asProperty) => {
-  switch (asProperty) {
-    case 'primary':
-      multiActionButtonPreview().children()
-        .should('have.css', 'background-color', 'rgb(37, 91, 199)')
-        .and('have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)');
-      break;
-    case 'secondary':
-      multiActionButtonPreview().children()
-        .should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
-        .and('have.css', 'border-bottom-color', 'rgb(37, 91, 199)')
-        .and('have.css', 'border-top-color', 'rgb(37, 91, 199)')
-        .and('have.css', 'border-right-color', 'rgb(37, 91, 199)')
-        .and('have.css', 'border-left-color', 'rgb(37, 91, 199)');
-      break;
-    case 'transparent':
-      multiActionButtonPreview().children()
-        .should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
-        .and('have.css', 'border-bottom-color', 'rgba(0, 0, 0, 0)')
-        .and('have.css', 'border-top-color', 'rgba(0, 0, 0, 0)')
-        .and('have.css', 'border-right-color', 'rgba(0, 0, 0, 0)')
-        .and('have.css', 'border-left-color', 'rgba(0, 0, 0, 0)');
-      break;
-    default: throw new Error('Not a valid as property');
-  }
+Then('Multi Action Button has {string} background-color', (color) => {
+  multiActionButton().should('have.css', 'background-color', color);
+});
+
+Then('Multi Action Button border color is {string} border-color', (borderColor) => {
+  multiActionButton().should('have.css', 'border-bottom-color', borderColor)
+    .and('have.css', 'border-top-color', borderColor)
+    .and('have.css', 'border-right-color', borderColor)
+    .and('have.css', 'border-left-color', borderColor);
+});
+
+Then('Multi Action Button height is {string}', (height) => {
+  multiActionButton().should('have.css', 'height', `${height}px`);
 });
 
 Then('Multi Action Button align on preview is {string}', (align) => {
@@ -53,13 +36,23 @@ Then('Multi Action Button align on preview is {string}', (align) => {
     .should('have.css', TEXT_ALIGN, `${align}`);
 });
 
-When('I invoke Multi Action Button component', () => {
-  multiActionButtonPreview().trigger('mouseover');
+Then('Multi Action Button subtext on preview is {string}', (subtext) => {
+  multiActionButtonSubtext().should('have.text', subtext);
+});
+
+Then('Multi Action Button has golden border color', () => {
+  cy.focused().should('have.css', 'border', 'rgb(255, 181, 0) solid 3px');
+});
+
+When('I hoverover on Multi Action Button', () => {
+  multiActionButtonComponent().trigger('mouseover');
+});
+
+When('I click the Multi Action Button', () => {
+  multiActionButtonComponent().click();
 });
 
 Then('Multi Action Button is expanded and contains three items', () => {
-  multiActionButtonList()
-    .should('have.length', 3);
-  multiActionButtonList()
-    .invoke('text').should('contain', MULTI_ACTION_BUTTON_INNER_TEXT);
+  multiActionButtonList().should('have.length', 3);
+  multiActionButtonList().invoke('text').should('contain', MULTI_ACTION_BUTTON_INNER_TEXT);
 });
