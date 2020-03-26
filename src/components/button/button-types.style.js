5e2346b4b5c20aacc2e4d6aeb4a58390d1a38233
Jamie Thompson <jamie.thompson@sage.com>
2020-03-26 12:27:43 +0000

    feat(button): add destructive secondary buttons
    Fixes FE-2553

--- src/components/button/button-types.style.js
+++ src/components/button/button-types.style.js
@@ -54,6 +54,20 @@ export default ({ colors, disabled }, isDisabled, destructive) => ({
        ${makeColors(colors.white)}
       }
 
+      ${destructive ? `
+        border-color: ${colors.error};
+        ${makeColors(colors.error)}
+          &:focus {
+            ${makeColors(colors.white)}
+            background: ${colors.destructive.hover};
+          }
+          &:hover {
+            ${makeColors(colors.white)}
+            border-color: ${colors.destructive.hover};
+            background: ${colors.destructive.hover};
+          }
+      ` : ''}
+
       ${isDisabled ? `
         border-color: ${disabled.button};
         color: ${disabled.text};
