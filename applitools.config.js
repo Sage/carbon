6b7e435e954a7606a08aa0a85b8bfff945335e44
Sian Ford <sian.ford@sage.com>
2020-04-07 18:39:30 +0100

    test(applitools): fixes to applitools config js file
    moved some import commands and fixed some errors in the file

--- applitools.config.js
+++ applitools.config.js
@@ -1,10 +1,11 @@
 
-const applitools_settings = {
+const applitoolsSettings = {
   appName: 'Carbon Sage',
-  matchLevel: 'Strict', // Strict, Exact, Layout and Content - https://help.applitools.com/hc/en-us/articles/360007188591-Match-Levels
+  // Strict, Exact, Layout and Content - https://help.applitools.com/hc/en-us/articles/360007188591-Match-Levels
+  matchLevel: 'Strict',
   envName: 'Development',
   testName: 'my test',
   batchName: 'my batch',
-  browser: [{width: 2560, height: 1440, name: 'chrome'}]
-}
-export default applitools_settings
+  browser: [{ width: 2560, height: 1440, name: 'chrome' }]
+};
+export default applitoolsSettings;
