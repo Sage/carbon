3d6ab79102987083d87efe7e3067d05f9f331cd4
Ihor Dluhosh <idluhosh@pgs-soft.com>
2020-05-12 13:55:02 +0200

    ci(applitools): integrate applitools into cypress tests

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+
+const applitoolsSettings = {
+  appName: 'Carbon Sage',
+  // Strict, Exact, Layout and Content - https://help.applitools.com/hc/en-us/articles/360007188591-Match-Levels
+  matchLevel: 'Strict',
+  envName: 'Development',
+  browser: [{ width: 2560, height: 1440, name: 'chrome' }]
+};
+export default applitoolsSettings;
