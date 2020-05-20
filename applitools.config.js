fcc686a2b102115e8e9761825cd09a7098420aab
Ihor Dluhosh <idluhosh@pgs-soft.com>
2020-05-20 09:40:14 +0200

    docs: update testing documentation

--- applitools.config.js
+++ applitools.config.js
@@ -4,6 +4,12 @@ const applitoolsSettings = {
   // Strict, Exact, Layout and Content - https://help.applitools.com/hc/en-us/articles/360007188591-Match-Levels
   matchLevel: 'Strict',
   envName: 'Development',
-  browser: [{ width: 2560, height: 1440, name: 'chrome' }]
+  saveNewTests: true,
+  concurrency: 10,
+  browser: [
+    { width: 2560, height: 1440, name: 'chrome' },
+    { width: 2560, height: 1440, name: 'firefox' },
+    { width: 2560, height: 1440, name: 'safari' }
+  ]
 };
 export default applitoolsSettings;
