ae2833b595d0fba22b9094aa8f151915ccfb2f2a
grabkowski <jgrabkowski@pgs-soft.com>
2020-06-18 15:18:24 +0200

    feat: add new FilterableSelect Component

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,2 +1,3 @@
 export { default as Option } from './option/option.component';
 export { default as Select } from './simple-select/simple-select.component';
+export { default as FilterableSelect } from './filterable-select/filterable-select.component';
---
+++ src/components/select/utils/highlight-part-of-text.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import MatchingText from './matching-text.style';
+
+export default function highlightPartOfText (text, partToHighlight) {
+  if (!partToHighlight || !partToHighlight.length || !text) return text;
+  const lowercaseText = text.toLowerCase();
+  const lowercasePart = partToHighlight.toLowerCase();
+  const indexOfFirstMatch = lowercaseText.indexOf(lowercasePart);
+
+  if (indexOfFirstMatch === -1) {
+    return text;
+  }
+
+  const precedingText = text.substr(0, indexOfFirstMatch);
+  const matchingText = text.substr(indexOfFirstMatch, partToHighlight.length);
+  let followingText = text.substr(indexOfFirstMatch + partToHighlight.length, text.length);
+
+  if (followingText.length >= partToHighlight.length) {
+    followingText = highlightPartOfText(followingText, partToHighlight);
+  }
+
+  const newValue = [
+    <span key='preceding'>{ precedingText }</span>,
+    <MatchingText key='match'>{ matchingText }</MatchingText>,
+    <span key='following'>{ followingText }</span>
+  ];
+
+  return newValue;
+}
