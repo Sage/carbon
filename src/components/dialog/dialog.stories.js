c8bba28d270b41db8ab4f554d2798093869107a4
mkrds <mkordas@pgs-soft.com>
2020-07-01 18:35:53 +0200

    docs: remove deprecated components from dialog stories

--- src/components/dialog/dialog.stories.js
+++ src/components/dialog/dialog.stories.js
@@ -3,18 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { State, Store } from '@sambego/storybook-state';
-import { fromJS } from 'immutable';
-import { dlsThemeSelector, classicThemeSelector } from '../../../.storybook/theme-selectors';
+import { dlsThemeSelector } from '../../../.storybook/theme-selectors';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import notes from './documentation/notes.md';
 import Dialog from './dialog.component';
-import Form from '../../__deprecated__/components/form';
-import Textbox from '../../__deprecated__/components/textbox';
+import Form from '../form';
+import Textbox from '../../__experimental__/components/textbox';
 import Button from '../button';
 import Modal from '../modal';
-import Date from '../../__experimental__/components/date';
+import DateInput from '../../__experimental__/components/date';
 import { Checkbox } from '../../__experimental__/components/checkbox';
-import Dropdown from '../../__deprecated__/components/dropdown';
+import { Select, Option } from '../../__experimental__/components/select';
 import getDocGenInfo from '../../utils/helpers/docgen-info';
 
 Dialog.__docgenInfo = getDocGenInfo(
@@ -40,7 +39,7 @@ const handleClick = (evt) => {
   action('click')(evt);
 };
 
-function makeStory(name, themeSelector, disableChromatic = false) {
+function makeStory(name, themeSelector, stickyFooter) {
   const Component = () => {
     const [date, setDate] = useState('2020-06-01');
     const height = text('height', '400');
@@ -48,10 +47,33 @@ function makeStory(name, themeSelector, disableChromatic = false) {
     const subtitle = text('subtitle', 'Example Subtitle');
     const size = select('size', OptionsHelper.sizesFull, Dialog.defaultProps.size);
     const showCloseIcon = boolean('showCloseIcon', Dialog.defaultProps.showCloseIcon);
-    const stickyFormFooter = boolean('stickyFormFooter', false);
     const enableBackgroundUI = boolean('enableBackgroundUI', false);
     const disableEscKey = boolean('disableEscKey', false);
     const ariaRole = text('ariaRole', Dialog.defaultProps.ariaRole);
+
+    const selectOptions = [{
+      id: '1', name: 'Orange'
+    }, {
+      id: '2', name: 'Blue'
+    }, {
+      id: '3', name: 'Green'
+    }, {
+      id: '4', name: 'Black'
+    }, {
+      id: '5', name: 'Yellow'
+    }, {
+      id: '6', name: 'White'
+    }, {
+      id: '7', name: 'Magenta'
+    }, {
+      id: '8', name: 'Cyan'
+    }, {
+      id: '9', name: 'Red'
+    }, {
+      id: '10', name: 'Grey'
+    }, {
+      id: '11', name: 'Purple'
+    }];
 
     return (
       <div>
@@ -65,52 +87,38 @@ function makeStory(name, themeSelector, disableChromatic = false) {
             subtitle={ subtitle }
             size={ size }
             showCloseIcon={ showCloseIcon }
-            stickyFormFooter={ stickyFormFooter }
             enableBackgroundUI={ enableBackgroundUI }
             disableEscKey={ disableEscKey }
             ariaRole={ ariaRole }
             onClick={ handleClick }
           >
-            <Form>
+            <Form
+              stickyFooter={ stickyFooter }
+              leftSideButtons={ <Button onClick={ handleCancel }>Cancel</Button> }
+              saveButton={ <Button buttonType='primary' type='submit'>Save</Button> }
+            >
               <Textbox label='First Name' />
               <Textbox label='Middle Name' />
               <Textbox label='Surname' />
               <Textbox label='Birth Place' />
               <Textbox label='Favourite Colour' />
               <Textbox label='Address' />
-              <Date
+              <DateInput
                 name='date' label='Birthday'
                 value={ date }
                 onChange={ e => setDate(e.target.value) }
               />
-              <Dropdown
-                name='foo' options={ fromJS([{
-                  id: '1', name: 'Orange'
-                }, {
-                  id: '2', name: 'Blue'
-                }, {
-                  id: '3', name: 'Green'
-                }, {
-                  id: '4', name: 'Black'
-                }, {
-                  id: '5', name: 'Yellow'
-                }, {
-                  id: '6', name: 'White'
-                }, {
-                  id: '7', name: 'Magenta'
-                }, {
-                  id: '8', name: 'Cyan'
-                }, {
-                  id: '9', name: 'Red'
-                }, {
-                  id: '10', name: 'Grey'
-                }, {
-                  id: '11', name: 'Purple'
-                }]) }
-                value='1'
-              />
+              <Select label='Color'>
+                {selectOptions.map(option => (
+                  <Option
+                    key={ option.name }
+                    value={ option }
+                    text={ option.name }
+                  />
+                ))}
+              </Select>
               <Textbox label='Pet Name' />
-              <Date
+              <DateInput
                 name='date' label="Pet's birthday"
                 value={ date }
                 onChange={ e => setDate(e.target.value) }
@@ -133,16 +141,14 @@ function makeStory(name, themeSelector, disableChromatic = false) {
         Form,
         Textbox,
         Checkbox,
-        Date,
-        Dropdown,
+        DateInput,
+        Select,
+        Option,
         Modal
       ]
     },
     notes: { markdown: notes },
-    knobs: { escapeHTML: false },
-    chromatic: {
-      disable: disableChromatic
-    }
+    knobs: { escapeHTML: false }
   };
 
   return [name, Component, metadata];
@@ -150,4 +156,4 @@ function makeStory(name, themeSelector, disableChromatic = false) {
 
 storiesOf('Dialog', module)
   .add(...makeStory('default', dlsThemeSelector))
-  .add(...makeStory('classic', classicThemeSelector, true));
+  .add(...makeStory('with sticky Form footer', dlsThemeSelector, true));
