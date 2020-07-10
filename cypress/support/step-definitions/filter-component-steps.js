ced84b759826993c63de1a3aa36aa5db0d5ff18c
mkrds <mkordas@pgs-soft.com>
2020-07-10 13:50:16 +0200

    chore: remove deprecated filter component BREAKING CHANGE: The deprecated Filter component has been removed

--- cypress/support/step-definitions/filter-component-steps.js
+++
@@ -1,7 +0,0 @@
-import { filterPreview } from '../../locators/filter';
-
-const ALIGN_PREFIX = 'carbon-filter--align-';
-
-Then('Filter label align property is set to {string}', (align) => {
-  filterPreview().should('have.class', `${ALIGN_PREFIX}${align}`);
-});
