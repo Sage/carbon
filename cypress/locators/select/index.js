24f48ab605fb9bb525fe70f36253343f8eeac072
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2020-07-31 13:01:10 +0200

    test(multiselect): add a new cypress tests

--- cypress/locators/select/index.js
+++ cypress/locators/select/index.js
@@ -7,6 +7,8 @@ import {
   CONTROLLED_LABEL,
   SELECT_LIST,
   SELECT_BASIC,
+  OPEN_ON_FOCUS_ID,
+  MULTI_SELECT,
 } from './locators';
 import { PILL_PREVIEW } from '../pill/locators';
 
@@ -24,10 +26,14 @@ export const selectInputNoIframe = () => cy.get(SELECT_INPUT);
 // locators into DS
 export const simpleSelectID = () => cy.iFrame(CONTROLLED_SELECT_ID);
 export const selectDataComponent = (index, component) => cy.iFrame(`[data-component="${component}-select"]`).eq(index);
+export const multiSelectDataComponent = index => cy.iFrame(MULTI_SELECT).eq(index);
 export const selectList = () => cy.iFrame(SELECT_LIST);
 export const selectOption = index => cy.iFrame(SELECT_OPTIONS).eq(index);
 export const dropdownButton = index => cy.iFrame(DROPDOWN_BUTTON).eq(index);
 export const controlledLabel = () => cy.iFrame(CONTROLLED_LABEL);
+export const openOnFocusID = () => cy.iFrame(OPEN_ON_FOCUS_ID);
+export const multiSelectPill = () => cy.iFrame(PILL_PREVIEW);
+export const multiSelectPillByPosition = index => cy.iFrame(PILL_PREVIEW).eq(index);
 
 export const simpleSelectNoIframe = () => cy.get(SELECT_BASIC);
 export const simpleSelectIframe = () => cy.iFrame(SELECT_BASIC);
--- cypress/locators/select/locators.js
+++ cypress/locators/select/locators.js
@@ -1,5 +1,6 @@
 // component preview locators
 export const SELECT = '[data-component="carbon-select"]';
+export const MULTI_SELECT = '[data-component="multiselect"]';
 export const SELECT_INPUT = '[data-element="input"]';
 
 export const CONTROLLED_SELECT_ID = '[id="controlled"]';
@@ -8,3 +9,4 @@ export const DROPDOWN_BUTTON = '[data-element="dropdown"]';
 export const CONTROLLED_LABEL = '[id="controlled-usage"]';
 export const SELECT_LIST = '[data-element="select-list"]';
 export const SELECT_BASIC = '[id="simple"]';
+export const OPEN_ON_FOCUS_ID = '[id="openOnFocus"]';
