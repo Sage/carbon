24df79c903de453e17595c2de56f7cabe2e3181d
Jamie Thompson <jamie.thompson@sage.com>
2020-09-01 14:55:24 +0100

    refactor: clean sidebar story
    - remove themeSelector
    - remove disableChromatic
    - remove storybook-state

--- src/components/sidebar/sidebar.stories.js
+++ src/components/sidebar/sidebar.stories.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
-import { State, Store } from '@sambego/storybook-state';
-import { dlsThemeSelector } from '../../../.storybook/theme-selectors';
 import OptionsHelper from '../../utils/helpers/options-helper';
 import { Sidebar, SidebarHeader } from '.';
 import Button from '../button';
@@ -21,64 +19,56 @@ SidebarHeader.__docgenInfo = getDocGenInfo(
   /sidebar-header\.component(?!spec)/
 );
 
-const store = new Store({
-  open: false
-});
 
-const onCancel = () => {
-  store.set({ open: false });
-  action('cancel')();
-};
-
-const openSidebar = () => {
-  store.set({ open: true });
-};
-
-function makeStory(name, themeSelector, disableChromatic = false) {
-  const component = () => {
+storiesOf('Sidebar', module)
+  .addParameters({
+    info: {
+      propTablesExclude: [Button],
+      text: info
+    },
+    notes: {
+      markdown: notes
+    }
+  })
+  .add('default', () => {
     const enableBackgroundUI = boolean('enableBackgroundUI', Sidebar.defaultProps.enableBackgroundUI);
     const position = select('position', OptionsHelper.alignBinary, Sidebar.defaultProps.position);
     const size = select('size', OptionsHelper.sizesFull, Sidebar.defaultProps.size);
     const open = boolean('open', Sidebar.defaultProps.open);
-
     return (
       <Sidebar
         enableBackgroundUI={ enableBackgroundUI } open={ open }
         position={ position } size={ size }
-        onCancel={ onCancel }
+        onCancel={ action('cancel') }
       >
         <SidebarHeader>Header Content</SidebarHeader>
         Main Content
       </Sidebar>
     );
-  };
-
-  const metadata = {
-    themeSelector,
-    chromatic: {
-      disable: disableChromatic
-    }
-  };
-
-  return [name, component, metadata];
-}
-
-function makeButtonStory(name, themeSelector, disableChromatic = false) {
-  const component = () => {
+  })
+  .add('with button', () => {
     const enableBackgroundUI = boolean('enableBackgroundUI', Sidebar.defaultProps.enableBackgroundUI);
     const position = select('position', OptionsHelper.alignBinary, Sidebar.defaultProps.position);
     const size = select('size', OptionsHelper.sizesFull, Sidebar.defaultProps.size);
+    const [open, setOpen] = useState(false);
 
     return (
-      <State store={ store }>
-        <Button key='button' onClick={ openSidebar }>Open sidebar</Button>
+      <>
+        <Button onClick={ () => {
+          setOpen(true);
+        } }
+        >Open sidebar
+        </Button>
         <Sidebar
           key='sidebar'
           enableBackgroundUI={ enableBackgroundUI }
-          open={ store.get('open') }
+          open={ open }
           position={ position }
           size={ size }
-          onCancel={ onCancel }
+          onCancel={ (e) => {
+            setOpen(false);
+            action('cancel')(e);
+          } }
         >
           <SidebarHeader>Header Content</SidebarHeader>
           <div>
@@ -86,31 +76,7 @@ function makeButtonStory(name, themeSelector, disableChromatic = false) {
             <Button as='secondary'>Last</Button>
           </div>
           Main Content
-
         </Sidebar>
-      </State>
+      </>
     );
-  };
-
-  const metadata = {
-    themeSelector,
-    chromatic: {
-      disable: disableChromatic
-    }
-  };
-
-  return [name, component, metadata];
-}
-
-storiesOf('Sidebar', module)
-  .addParameters({
-    info: {
-      propTablesExclude: [Button, State],
-      text: info
-    },
-    notes: {
-      markdown: notes
-    }
-  })
-  .add(...makeStory('default', dlsThemeSelector))
-  .add(...makeButtonStory('with button', dlsThemeSelector));
+  });
