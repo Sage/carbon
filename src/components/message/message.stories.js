df952c740b209294a0b6e1010100640336bec948
mkrds <mkordas@pgs-soft.com>
2020-09-04 10:35:58 +0200

    docs(message): refactor stories to csf and mdx formats

--- src/components/message/message.stories.js
+++ src/components/message/message.stories.js
@@ -1,54 +1,50 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React, { useState } from 'react';
 import { text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
-import { dlsThemeSelector, classicThemeSelector } from '../../../.storybook/theme-selectors';
+import { dlsThemeSelector } from '../../../.storybook/theme-selectors';
 import OptionsHelper from '../../utils/helpers/options-helper';
-import notes from './documentation';
 import Message from './message.component';
-import getDocGenInfo from '../../utils/helpers/docgen-info';
 
-Message.__docgenInfo = getDocGenInfo(
-  require('./docgenInfo.json'),
-  /message\.component(?!spec)/
-);
-
-function makeStory(name, themeSelector, disableChromatic = false) {
-  const component = () => {
-    const variant = select('type', OptionsHelper.messages, Message.defaultProps.variant);
-    const open = boolean('open', Message.defaultProps.open);
-    const title = text('title');
-    const id = text('id', 'custom-id');
-    const transparent = boolean('transparent', Message.defaultProps.transparent);
-    const children = text('children', 'This is some information from the Message Component.');
-    const showCloseIcon = boolean('showCloseIcon', Message.defaultProps.showCloseIcon);
-    const onDismiss = showCloseIcon ? (evt) => { action('click')(evt); } : undefined;
-
-    return (
-      <Message
-        variant={ variant } open={ open }
-        title={ title } transparent={ transparent }
-        onDismiss={ onDismiss }
-        id={ id }
-        showCloseIcon={ showCloseIcon }
-      >
-        {children}
-      </Message>
-    );
-  };
-
-  const metadata = {
-    themeSelector,
-    notes: { markdown: notes },
-    knobs: { escapeHTML: false },
+export default {
+  title: 'Message/Test',
+  component: Message,
+  parameters: {
+    themeSelector: dlsThemeSelector,
+    info: {
+      disable: true
+    },
     chromatic: {
-      disable: disableChromatic
+      disable: true
     }
+  }
+};
+
+export const Default = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const variant = select('type', OptionsHelper.messages, Message.defaultProps.variant);
+  const title = text('title');
+  const id = text('id', 'custom-id');
+  const transparent = boolean('transparent', Message.defaultProps.transparent);
+  const children = text('children', 'This is some information from the Message Component.');
+  const showCloseIcon = boolean('showCloseIcon', Message.defaultProps.showCloseIcon);
+  const roundedCorners = boolean('roundedCorners', Message.defaultProps.roundedCorners);
+  const onDismiss = (evt) => {
+    setIsOpen(false);
+    action('click')(evt);
   };
 
-  return [name, component, metadata];
-}
-
-storiesOf('Message', module)
-  .add(...makeStory('default', dlsThemeSelector))
-  .add(...makeStory('classic', classicThemeSelector, true));
+  return (
+    <Message
+      variant={ variant }
+      open={ isOpen }
+      title={ title }
+      transparent={ transparent }
+      onDismiss={ onDismiss }
+      id={ id }
+      showCloseIcon={ showCloseIcon }
+      roundedCorners={ roundedCorners }
+    >
+      {children}
+    </Message>
+  );
+};
