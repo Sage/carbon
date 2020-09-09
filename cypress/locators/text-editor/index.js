4f83ebbb1c0e588748ede6fe09c27f1738746d8d
Katarzyna Kusiak <kkusiak@pgs-soft.com>
2020-09-09 14:42:47 +0200

    test(texteditor): remove not used methods

--- cypress/locators/text-editor/index.js
+++ cypress/locators/text-editor/index.js
@@ -1,12 +1,10 @@
 import {
-  TEXT_EDITOR_COMPONENT,
   TEXT_EDITOR_COUNTER,
   TEXT_EDITOR_INPUT,
   TEXT_EDITOR_TOOLBAR,
 } from './locators';
 
 // component preview locators in NoIFrame
-export const textEditorComponent = () => cy.get(TEXT_EDITOR_COMPONENT);
 export const textEditorCounter = () => cy.get(TEXT_EDITOR_COUNTER);
 export const textEditorInput = () => cy.get(TEXT_EDITOR_INPUT);
 export const textEditorToolbar = buttonType => cy.get(TEXT_EDITOR_TOOLBAR)
