1144f9a49e78446bf9f09de77f9fcf1d5d1603dc
grabkowski <jgrabkowski@pgs-soft.com>
2020-09-14 16:00:29 +0200

    feat(grid): add styled-system cusom padding and margin props
    grid-container now accepts m, ml, mr, mt, mb and gridGap props
    grid-item now accepts p, pl, pr, pt, pb, gridColumn and gridRow props
    
    BREAKING CHANGE: gridColumnStart and gridColumnEnd props in GridItem
    and GridItem responsiveSettings replaced with gridColumn styled-system compatible prop,
    gridRowStart and gridRowEnd props in GridItem and GridItem responsiveSettings
    replaced with gridRow styled-system compatible prop

--- src/components/grid/grid-container/grid-container.component.js
+++ src/components/grid/grid-container/grid-container.component.js
@@ -5,10 +5,39 @@ import GridItem from '../grid-item';
 
 const GridContainer = (props) => {
   const {
-    children
+    children,
+    m,
+    ml,
+    mr,
+    mt,
+    mb,
+    mx,
+    my,
+    gridGap
   } = props;
+
+  const styledSystemProps = {
+    m,
+    ml,
+    mr,
+    mt,
+    mb,
+    gridGap
+  };
+
+  if (mx) {
+    styledSystemProps.mx = mx;
+  }
+
+  if (my) {
+    styledSystemProps.my = my;
+  }
+
   return (
-    <GridContainerStyle data-component='grid'>
+    <GridContainerStyle
+      data-component='grid'
+      { ...styledSystemProps }
+    >
       {children}
     </GridContainerStyle>
   );
@@ -16,6 +45,22 @@ const GridContainer = (props) => {
 
 GridContainer.propTypes = {
   /** Defines the Components to be rendered within the GridContainer. Requires a GridItem */
-  children: PropTypes.oneOfType([GridItem, PropTypes.arrayOf(GridItem)]).isRequired
+  children: PropTypes.oneOfType([GridItem, PropTypes.arrayOf(GridItem)]).isRequired,
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default margin */
+  m: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default margin-left */
+  ml: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default margin-right */
+  mr: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default margin-top */
+  mt: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default margin-bottom */
+  mb: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default horizontal margins */
+  mx: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value or a number to be multiplied by base spacing unit (8). Overrides default vertical margins */
+  my: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Any valid CSS value to override default grid-gap */
+  gridGap: PropTypes.string
 };
 export default GridContainer;
