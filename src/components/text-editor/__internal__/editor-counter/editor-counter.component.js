9c6f114faf2afb2ccc9fe4720efa23642739097c
Ed Leeks <edward.leeks@sage.com>
2020-10-19 17:12:06 +0100

    feat(text-editor): add support for validation icon and error styles
    The `ValidationIcon` is rendered when a string value is passed to either the `error`, `warning` or
    `info` prop. THe component has been slightly refactored to use outlines instead of borders to
    prevent the content jumping when the styling is updated based on the validation state. When the
    component is in an error state, the outline and counter text colour are updated to reflect it.

--- src/components/text-editor/__internal__/editor-counter/editor-counter.component.js
+++ src/components/text-editor/__internal__/editor-counter/editor-counter.component.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StyledCounter from './editor-counter.style';
+import { StyledCounterWrapper, StyledCounter } from './editor-counter.style';
+import ValidationIcon from '../../../validations';
 
-const Counter = ({ count = 0, limit = 3000 }) => (
-  <StyledCounter data-component='text-editor-counter'>
-    { `${limit - count}` }
-  </StyledCounter>
+const Counter = ({
+  count = 0,
+  limit = 3000,
+  error,
+  warning,
+  info
+}) => (
+  <StyledCounterWrapper
+    hasIcon={ !!(error || warning || info) }
+    data-component='text-editor-counter'
+  >
+    { !!(error || warning || info) && (
+      <ValidationIcon
+        error={ error }
+        warning={ warning }
+        info={ info }
+        tooltipPosition='top'
+        tabIndex={ 0 }
+      />
+    )}
+    <StyledCounter hasError={ !!error }>{ `${limit - count}` }</StyledCounter>
+  </StyledCounterWrapper>
 );
 
 Counter.propTypes = {
   /** Sets the current count value */
   count: PropTypes.number,
   /** Sets the current limit value */
-  limit: PropTypes.number
+  limit: PropTypes.number,
+  /** Message to be displayed when there is an error */
+  error: PropTypes.string,
+  /** Message to be displayed when there is a warning */
+  warning: PropTypes.string,
+  /** Message to be displayed when there is an info */
+  info: PropTypes.string
 };
 
 export default Counter;
