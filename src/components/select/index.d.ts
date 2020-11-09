53e7a3376d7329166530b7522c1f3ca88d7352e0
grabkowski <jgrabkowski@pgs-soft.com>
2020-11-09 17:43:37 +0100

    fix(select): typescript definitions not exported

---
+++ src/components/select/index.d.ts
@@ -0,0 +1,4 @@
+export { default as Option } from './option';
+export { default as Select } from './simple-select';
+export { default as FilterableSelect } from './filterable-select';
+export { default as MultiSelect } from './multi-select';
