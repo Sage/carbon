43cd1a00dbcf86f395527d562ac5dfd2a459a394
mkrds <mkordas@pgs-soft.com>
2020-11-26 18:20:32 +0100

    chore(field-help): remove classic theme support

--- src/__experimental__/components/field-help/field-help.style.js
+++ src/__experimental__/components/field-help/field-help.style.js
@@ -1,7 +1,6 @@
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import BaseTheme from "../../../style/themes/base";
-import { isClassic } from "../../../utils/helpers/style-helper";
 
 const FieldHelpStyle = styled.span`
   display: block;
@@ -14,20 +13,6 @@ const FieldHelpStyle = styled.span`
     css`
       margin-left: ${labelWidth}%;
       padding-left: 0;
-    `}
-
-  ${({ labelInline, labelWidth, theme }) =>
-    isClassic(theme) &&
-    css`
-      color: #335c6d;
-      margin-left: 6px;
-      margin-right: 6px;
-      margin-top: 5px;
-
-      ${labelInline &&
-      css`
-        margin-left: calc(${labelWidth}% + 6px);
-      `}
     `}
 `;
 
