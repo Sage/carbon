ef51f0f8b26cd7c0b831cb55928cb45a56ea7c3b
mkrds <mkordas@pgs-soft.com>
2020-11-27 16:02:55 +0100

    chore(grouped-character): remove classic theme support BREAKING CHANGE: GroupedCharacter no longer supports classic theme

--- src/__experimental__/components/grouped-character/grouped-character.stories.js
+++ src/__experimental__/components/grouped-character/grouped-character.stories.js
@@ -1,142 +1,45 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Store, State } from "@sambego/storybook-state";
-import { text, object, boolean } from "@storybook/addon-knobs";
+import React, { useState } from "react";
+import { text, object } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import {
-  dlsThemeSelector,
-  classicThemeSelector,
-} from "../../../../.storybook/theme-selectors";
+
 import GroupedCharacter from "./grouped-character.component";
 import { getCommonTextboxProps } from "../textbox/textbox.stories";
-import { OriginalTextbox } from "../textbox";
-import { info } from "./documentation";
-import getDocGenInfo from "../../../utils/helpers/docgen-info";
 
-GroupedCharacter.__docgenInfo = getDocGenInfo(
-  require("./docgenInfo.json"),
-  /grouped-character\.component(?!spec)/
-);
-
-const groupedCharacterStore = new Store({
-  value: "",
-});
-
-const onChange = (ev) => {
-  groupedCharacterStore.set({ value: ev.target.value.rawValue });
-  action("change")(ev);
-};
-
-const requiredComponent = () => {
-  const groups = object("groups", [2, 2, 4]);
-  const separator = text("separator", "-");
-
-  return (
-    <State store={groupedCharacterStore}>
-      <GroupedCharacter
-        {...getCommonTextboxProps({ requiredKnob: false })}
-        groups={groups}
-        separator={separator}
-        value={groupedCharacterStore.get("value")}
-        onChange={onChange}
-        required
-      />
-    </State>
-  );
-};
-
-const defaultComponent = () => {
-  const groups = object("groups", [2, 2, 4]);
-  const separator = text("separator", "-");
-
-  return (
-    <State store={groupedCharacterStore}>
-      <GroupedCharacter
-        {...getCommonTextboxProps()}
-        groups={groups}
-        separator={separator}
-        value={groupedCharacterStore.get("value")}
-        onChange={onChange}
-      />
-    </State>
-  );
-};
-
-const autoFocusComponent = () => {
-  boolean("autoFocus", true);
-  return defaultComponent();
-};
-
-const validationsComponent = () => {
-  const validationTypes = ["error", "warning", "info"];
-  const groups = object("groups", [2, 2, 4]);
-  const separator = text("separator", "-");
-
-  return (
-    <>
-      <h4>Validation as string</h4>
-      <h6>On component</h6>
-      {validationTypes.map((validation) => (
-        <GroupedCharacter
-          {...getCommonTextboxProps()}
-          key={`${validation}-string-component`}
-          groups={groups}
-          separator={separator}
-          onChange={() => {}}
-          {...{ [validation]: "Message" }}
-        />
-      ))}
-      <h6>On label</h6>
-      {validationTypes.map((validation) => (
-        <GroupedCharacter
-          {...getCommonTextboxProps()}
-          key={`${validation}-string-label`}
-          groups={groups}
-          separator={separator}
-          onChange={() => {}}
-          validationOnLabel
-          {...{ [validation]: "Message" }}
-        />
-      ))}
-
-      <h4>Validation as boolean</h4>
-      {validationTypes.map((validation) => (
-        <GroupedCharacter
-          {...getCommonTextboxProps()}
-          key={`${validation}-boolean`}
-          groups={groups}
-          separator={separator}
-          onChange={() => {}}
-          {...{ [validation]: true }}
-        />
-      ))}
-    </>
-  );
-};
-
-function makeStory(name, themeSelector, component, disableChromatic = false) {
-  const metadata = {
-    themeSelector,
+export default {
+  title: "Experimental/GroupedCharacter/Test",
+  component: GroupedCharacter,
+  parameters: {
     info: {
-      text: info,
-      propTables: [GroupedCharacter, OriginalTextbox],
+      disable: true,
     },
     chromatic: {
-      disable: disableChromatic,
+      disable: true,
     },
     knobs: { escapeHTML: false },
+  },
+};
+
+export const Default = (props) => {
+  const [state, setState] = useState("");
+  const groups = object("groups", [2, 2, 4]);
+  const separator = text("separator", "-");
+  const onChange = (ev) => {
+    setState(ev.target.value.rawValue);
+    action("change")(ev);
   };
 
-  return [name, component, metadata];
-}
+  return (
+    <GroupedCharacter
+      {...getCommonTextboxProps()}
+      groups={groups}
+      separator={separator}
+      value={state}
+      onChange={onChange}
+      {...props}
+    />
+  );
+};
 
-storiesOf("Experimental/GroupedCharacter", module)
-  .addParameters({
-    info: { text: info, propTables: [GroupedCharacter, OriginalTextbox] },
-    knobs: { escapeHTML: false },
-  })
-  .add(...makeStory("default", dlsThemeSelector, defaultComponent))
-  .add(...makeStory("classic", classicThemeSelector, defaultComponent, true))
-  .add(...makeStory("validations", dlsThemeSelector, validationsComponent))
-  .add(...makeStory("autoFocus", dlsThemeSelector, autoFocusComponent))
-  .add(...makeStory("required", dlsThemeSelector, requiredComponent));
+Default.story = {
+  name: "default",
+};
