7a3a7176cb9a0d757412fce11370ef532549a25e
mkrds <mkordas@pgs-soft.com>
2020-11-27 13:10:27 +0100

    chore(form-field): remove classic theme support

--- src/__experimental__/components/form-field/form-field.style.js
+++ src/__experimental__/components/form-field/form-field.style.js
@@ -1,10 +1,9 @@
 import styled, { css } from "styled-components";
 import BaseTheme from "../../../style/themes/base";
-import { isClassic } from "../../../utils/helpers/style-helper";
 
 const FormFieldStyle = styled.div`
   & + & {
-    margin-top: ${({ theme }) => (isClassic(theme) ? "10px" : "16px")};
+    margin-top: 16px;
   }
 
   &&& {
