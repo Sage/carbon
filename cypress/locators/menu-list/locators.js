e6db65c0a36cebeccc1b0fb5c98b93b99e1e41b2
Sam Johnson <sam.johnson@sage.com>
2021-01-06 13:47:01 +0000

    test(cypress): fix cypress test

--- cypress/locators/menu-list/locators.js
+++ cypress/locators/menu-list/locators.js
@@ -1,5 +1,5 @@
 // component preview locators
-export const MENU_LIST_CHILDREN = 'div[data-component="menu-list"][class="carbon-menu-list"]';
+export const MENU_LIST_CHILDREN = 'div[data-component="menu-list"]';
 export const MENU_LIST_SEARCH = '[data-element="input"]';
 export const MENU_LIST = 'ul[class="carbon-menu-list__list"]';
 export const MENU_LIST_ITEMS = '#root > div[data-component="menu-list"] > ul[class="carbon-menu-list__list"] > li:nth-child(2) > div[data-component="menu-list"] > ul[class="carbon-menu-list__list"]';
