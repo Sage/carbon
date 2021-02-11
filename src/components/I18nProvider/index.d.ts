346c4eb679c59d07e1da17b7e52e29af53a9fe1a
Michał Maciorowski <michal.maciorowski2@sage.com>
2021-02-11 13:41:54 +0100

    refactor(i18next): add I18nProvider types

--- src/components/I18nProvider/index.d.ts
+++ src/components/I18nProvider/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export interface I18nProviderProps {
     children: React.ReactNode;
-    t: (string) => string
+    t: (key: string) => string;
 }
 
 declare const I18nProvider: React.FunctionComponent<I18nProviderProps>;
