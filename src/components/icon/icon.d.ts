fdc90d25920999dbbfa44b68a0ab896b0a7a0990
grabkowski <jgrabkowski@pgs-soft.com>
2021-02-23 18:04:51 +0100

    refactor(icon): add descriptions to type definitions

--- src/components/icon/icon.d.ts
+++ src/components/icon/icon.d.ts
@@ -1,23 +1,39 @@
 import * as React from 'react';
 
 export interface IconProps {
-  className?: string;
+  /** Icon type */
   type: string;
+  /** Background size */
   bgSize?: 'small' | 'medium' | 'large';
+  /** Background shape */
   bgShape?: 'circle' | 'rounded-rect' | 'square';
+  /** Background color theme */
   bgTheme?: 'info' | 'error' | 'success' | 'warning' | 'business' | 'none';
+  /** Icon font size */
   fontSize?: 'small' | 'large';
+  /** Icon color */
   iconColor?: 'default' | 'on-light-background' | 'on-dark-background' | 'business-color';
+  /** Override iconColor, provide any color from palette or any valid css color value. */
   color?: string;
+  /** Override bgTheme, provide any color from palette or any valid css color value. */
   bg?: string;
+  /** Sets the icon in the disabled state */
   disabled?: boolean;
+  /** Margin right, given number will be multiplied by base spacing unit (8) */
   mr?: number;
+  /** Margin left, given number will be multiplied by base spacing unit (8) */
   ml?: number;
+  /** Aria label for accessibility purposes */
   ariaLabel?: string;
+  /** The message string to be displayed in the tooltip */
   tooltipMessage?: string;
+  /** The position to display the tooltip */
   tooltipPosition?: "top" | "bottom" | "left" | "right";
+  /** Control whether the tooltip is visible */
   tooltipVisible?: boolean;
+  /** Override background color of the Tooltip, provide any color from palette or any valid css color value. */
   tooltipBgColor?: string;
+  /** Override font color of the Tooltip, provide any color from palette or any valid css color value. */
   tooltipFontColor?: string;
 }
 
