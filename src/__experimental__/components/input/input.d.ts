59c92b8b76ac94785151e8d0dbe3f53930ad3c9b
grabkowski <jakub.grabkowski@sage.com>
2021-03-01 15:52:37 +0100

    refactor(input): add prop documentation to TS type definitions
    add missing autoFocus, disabled, placeholder, readOnly, equired and value prop
    add prop documentation to all input type definitions
    add proper event type definitions
    add ValidationProps type definitions
    add InputPresentation type definitions

--- src/__experimental__/components/input/input.d.ts
+++ src/__experimental__/components/input/input.d.ts
@@ -1,18 +1,44 @@
 import * as React from "react";
-export interface InputProps {
-  className?: string;
+export interface CommonInputProps {
+  /** If true the Component will be focused when rendered */
+  autoFocus?: boolean;
+  /** If true, the component will be disabled */
+  disabled?: boolean;
+  /** HTML id attribute of the input */
   id?: string;
-  inputRef?: () => void; // a callback to retrieve the input reference
+  /** A callback to retrieve the input reference */
+  inputRef?: () => void;
+  /** Name of the input */
   name?: string;
-  onBlur?: () => void;
-  onClick?: () => void;
-  onKeyDown?: () => void;
-  onFocus?: () => void;
-  autoFocus?: () => void;
-  onChange?: () => void;
+  /** Specify a callback triggered on blur */
+  onBlur?: (ev: React.ChangeEvent<HTMLElement>) => void;
+  /** Specify a callback triggered on change */
+  onChange?: (ev: React.ChangeEvent<HTMLElement>) => void;
+  /** pecify a callback triggered on click */
+  onClick?: (ev: React.MouseEvent<HTMLElement>) => void;
+  /** Specify a callback triggered on focus */
+  onFocus?: (ev: React.ChangeEvent<HTMLElement>) => void;
+  /** pecify a callback triggered on keuyDown */
+  onKeyDown?: (ev: React.KeyboardEvent<HTMLInputElement>) => void;
+  /** Placeholder string to be displayed in input */
+  placeholder?: string;
+  /** If true, the component will be read-only */
+  readOnly?: boolean;
+  /** Flag to configure component as mandatory */
+  required?: boolean;
+  /** The value of the Input */
+  value?: string | string[] | object | object[];
+}
+
+export interface InputProps extends CommonInputProps {
+  /** Integer to determine a timeout for the defered callback */
+  deferTimeout?: number;
+  /** Defered callback to be called after the onChange event */
   onChangeDeferred?: () => void;
-  deferTimeout?: () => void;
+  /** HTML type attribute of the input */
   type?: string;
 }
+
 declare const Input: React.ComponentType<InputProps & React.HTMLProps<HTMLInputElement>>;
+
 export default Input;
