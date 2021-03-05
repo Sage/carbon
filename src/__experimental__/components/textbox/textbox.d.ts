fe1e7e19c99c0de5b1b998cfef3bdd9715447cc7
grabkowski <jakub.grabkowski@sage.com>
2021-03-05 12:33:23 +0100

    refactor(textbox): clean up type definitions

--- src/__experimental__/components/textbox/textbox.d.ts
+++ src/__experimental__/components/textbox/textbox.d.ts
@@ -1,87 +1,80 @@
 import * as React from "react";
-import { SpacingProps } from "../../../utils/helpers/options-helper";
+import { SpaceProps } from "styled-system";
+import { ValidationPropTypes } from "../../../components/validations";
 
-export interface TextboxProps extends SpacingProps {
+export interface CommonTextboxProps extends ValidationPropTypes {
+  /** Breakpoint for adaptive label (inline labels change to top aligned). Enables the adaptive behaviour when set */
+  adaptiveLabelBreakpoint?: number;
+  /** Flag to indicate that component is used in a Form */
+  childOfForm?: boolean;
+  /** Integer to determine a timeout for the defered callback */
+  deferTimeout?: number;
+  /** If true, the component will be disabled */
+  disabled?: boolean;
+  /** Help content to be displayed under an input */
+  fieldHelp?: React.ReactNode;
   /**
    * An optional alternative for props.value, this is useful if the
    * real value is an ID but you want to show a human-readable version.
    */
   formattedValue?: string;
-  /** The value of the Textbox */
-  value?: string | object | string[] | object[];
-  /** The unformatted value  */
-  rawValue?: string;
-  /** If true, the component will be disabled */
-  disabled?: boolean;
-  /** If true, the component will be read-only */
-  readOnly?: boolean;
-  /** Event handler for the change event */
-  onChange?: (ev: React.ChangeEvent<HTMLInputElement>) => void;
-  /** Event handler for the keyDown event */
-  onKeyDown?: (ev: React.KeyboardEvent<HTMLInputElement>) => void;
-  /** Defered callback called after the onChange event */
-  onChangeDeferred?: () => void;
-  /** Integer to determine timeout for defered callback */
-  deferTimeout?: number;
-  /** Label */
-  label?: string;
-  /** Text applied to label help tooltip */
-  labelHelp?: React.ReactNode;
-  /** When true, label is placed in line an input */
-  labelInline?: boolean;
-  /** Spacing between label and a field for inline label, given number will be multiplied by base spacing unit (8) */
-  labelSpacing?: 1 | 2;
-  /** Width of a label in percentage. Works only when labelInline is true */
-  labelWidth?: number;
-  /** Width of an input in percentage. Works only when labelInline is true */
-  inputWidth?: number;
-  /** Help content to be displayed under an input */
-  fieldHelp?: React.ReactNode;
-  /** Type of the icon that will be rendered next to the input */
-  children?: React.ReactNode;
   /** Type of the icon that will be rendered next to the input */
   inputIcon?: string;
-  /** Additional child elements to display before the input */
-  leftChildren?: React.ReactNode;
-  /** Flag to configure component when in a Form */
-  childOfForm?: boolean;
-  /** Flag to configure component as optional in Form */
-  isOptional?: boolean;
-  /** Indicate that error has occurred
-   * Pass string to display icon, tooltip and red border
-   * Pass true boolean to only display red border
-   */
-  error?: boolean | string;
-  /** Indicate that warning has occurred
-   * Pass string to display icon, tooltip and orange border
-   * Pass true boolean to only display orange border
-   */
-  warning?: boolean | string;
-  /** Indicate additional information
-   * Pass string to display icon, tooltip and blue border
-   * Pass true boolean to only display blue border
-   */
-  info?: boolean | string;
-  /** When true, validation icon will be placed on label instead of being placed on the input */
-  validationOnLabel?: boolean;
-  /** Size of an input */
-  size?: "small" | "medium" | "large";
-  /** Placeholder string to be displayed in input */
-  placeholder?: string;
   /** Optional handler for click event on Textbox icon */
   iconOnClick?: (ev: React.MouseEvent<HTMLElement>) => void;
   /** Overrides the default tabindex of the component */
   iconTabIndex?: string | number;
-  /** Handler for onClick events */
+  /** The width of the input as a percentage */
+  inputWidth?: number;
+  /** Additional child elements to display before the input */
+  leftChildren?: React.ReactNode;
+  /** Label content */
+  label?: string;
+  /** A message that the Help component will display */
+  labelHelp?: React.ReactNode;
+  /** When true label is inline */
+  labelInline?: boolean;
+  /** Spacing between label and a field for inline label, given number will be multiplied by base spacing unit (8) */
+  labelSpacing?: 1 | 2;
+  /** Label width */
+  labelWidth?: number;
+  /** Specify a callback triggered on change */
+  onChange?: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  /** Defered callback to be called after the onChange event */
+  onChangeDeferred?: () => void;
+  /** Specify a callback triggered on click */
   onClick?: (ev: React.MouseEvent<HTMLElement>) => void;
+  /** Specify a callback triggered on keuyDown */
+  onKeyDown?: (ev: React.KeyboardEvent<HTMLInputElement>) => void;
+  /** Placeholder string to be displayed in input */
+  placeholder?: string;
   /** Emphasized part of the displayed text */
   prefix?: string;
-  /** Margin bottom, given number will be multiplied by base spacing unit (8) */
-  mb?: 0 | 1 | 2 | 3 | 4 | 5 | 7;
-  /** Breakpoint for adaptive label (inline labels change to top aligned). Enables the adaptive behaviour when set */
-  adaptiveLabelBreakpoint?: number;
+  /** The unformatted value  */
+  rawValue?: string;
+  /** If true, the component will be read-only */
+  readOnly?: boolean;
   /** Flag to configure component as mandatory */
   required?: boolean;
+  /** Size of an input */
+  size?: "small" | "medium" | "large";
+  /** When true, validation icon will be placed on label instead of being placed on the input */
+  validationOnLabel?: boolean;
+  /** The value of the Input */
+  value?: string | string[] | object | object[];
 }
+
+export interface TextboxProps extends CommonTextboxProps, SpaceProps {
+  /** Content to be rendered next to the input */
+  children?: React.ReactNode;
+  /** [Legacy] Flag to configure component as optional in Form */
+  isOptional?: boolean;
+  /** Margin bottom, given number will be multiplied by base spacing unit (8) */
+  mb?: 0 | 1 | 2 | 3 | 4 | 5 | 7;
+  /** Container for DatePicker or SelectList components */
+  positionedChildren?: React.ReactNode;
+}
+
 declare const Textbox: React.ComponentType<TextboxProps>;
+
 export default Textbox;
