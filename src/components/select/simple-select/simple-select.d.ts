b9ee9817071e0a9ffa8860e2ef9cea6b2c3b53b1
grabkowski <jakub.grabkowski@sage.com>
2021-03-09 10:20:35 +0100

    refactor(simple-select): extend select textbox props and remove redundant type definitions
    Fixes #3810
    Fixes #3791

---
+++ src/components/select/simple-select/simple-select.d.ts
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { OptionProps } from "../option";
+import { OptionRowProps } from "../option-row";
+import { OptionGroupHeaderProps } from "../option-group-header";
+import { FormInputPropTypes } from "../select-textbox/select-textbox";
+
+type SelectListChild = React.ReactElement<OptionProps | OptionRowProps | OptionGroupHeaderProps>;
+
+export interface SimpleSelectProps extends FormInputPropTypes {
+  /** Child components (such as Option or OptionRow) for the SelectList */
+  children: SelectListChild[] | SelectListChild;
+  /** The default selected value(s), when the component is operating in uncontrolled mode */
+  defaultValue?: string | object;
+  /** Boolean to toggle where SelectList is rendered in relation to the Select Input */
+  disablePortal?: boolean;
+  /** If true the loader animation is displayed in the option list */
+  isLoading?: boolean;
+  /** When true component will work in multi column mode.
+   * Children should consist of OptionRow components in this mode
+   */
+  multiColumn?: boolean;
+  /** A callback that is triggered when a user scrolls to the bottom of the list */
+  onListScrollBottom?: () => void;
+  /** A custom callback for when the dropdown menu opens */
+  onOpen?: () => void;
+  /** If true the Component opens on focus */
+  openOnFocus?: boolean;
+  /** A custom message to be displayed when any option does not match the filter text */
+  noResultsMessage?: string;
+  /** SelectList table header, should consist of multiple th elements.
+   * Works only in multiColumn mode
+   */
+  tableHeader?: React.ReactNode;
+  /** If true the component input has no border and is transparent */
+  transparent?: boolean;
+  /** The selected value(s), when the component is operating in controlled mode */
+  value?: string | object;
+}
+
+declare const SimpleSelect: React.ComponentType<SimpleSelectProps>;
+
+export default SimpleSelect;
