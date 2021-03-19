78b33d5f59ed0e838eab2ecac6aeebfeae837db8
grabkowski <jakub.grabkowski@sage.com>
2021-03-19 11:07:42 +0100

    fix(checkbox-group): incorrect children type in typescript
    change children type to accept a list of checkboxes
    refactor prop types to extend ValidationPropTypes

--- src/__experimental__/components/checkbox/checkbox-group.d.ts
+++ src/__experimental__/components/checkbox/checkbox-group.d.ts
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { ValidationPropTypes } from "../../../components/validations";
+import { CheckboxProps } from "./checkbox";
 
-interface CheckboxGroupProps {
+interface CheckboxGroupProps extends ValidationPropTypes {
   /** The content for the CheckboxGroup Legend */
   legend?: string;
   /** When true, legend is placed in line with the checkboxes */
@@ -12,21 +14,9 @@ interface CheckboxGroupProps {
   /** Spacing between legend and field for inline legend, number multiplied by base spacing unit (8) */
   legendSpacing?: 1 | 2;
   /** The Checkboxes to be rendered in the group */
-  children: React.FunctionComponent | React.ComponentClass;
+  children: Array<React.ReactElement<CheckboxProps>> | React.ReactElement<CheckboxProps>;
   /** Specifies the name prop to be applied to each button in the group */
   groupName: string;
-  /* Indicate that error has occurred
-    Pass string to display icon, tooltip and red border
-    Pass true boolean to only display red border */
-  error?: boolean | string;
-  /* Indicate that warning has occurred
-    Pass string to display icon, tooltip and orange border
-    Pass true boolean to only display orange border */
-  warning?: boolean | string;
-  /* Indicate additional information
-    Pass string to display icon, tooltip and blue border
-    Pass true boolean to only display blue border */
-  info?: boolean | string;
   /** Margin bottom, given number will be multiplied by base spacing unit (8) */
   mb?: 0 | 1 | 2 | 3 | 4 | 5 | 7;
   /** Spacing between label and a field for inline label, given number will be multiplied by base spacing unit (8) */
