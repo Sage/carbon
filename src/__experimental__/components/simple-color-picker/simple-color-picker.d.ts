325f0c3051d10bfd4f762225bb38e63cbb1f7721
grabkowski <jakub.grabkowski@sage.com>
2021-03-22 17:44:41 +0100

    refactor(simple-color-picker): ts type definitions
    extend validation type definitions
    sort type definitions

--- src/__experimental__/components/simple-color-picker/simple-color-picker.d.ts
+++ src/__experimental__/components/simple-color-picker/simple-color-picker.d.ts
@@ -1,32 +1,18 @@
 import * as React from "react";
+import { ValidationPropTypes } from "../../../components/validations";
 import { SimpleColorProps } from "./simple-color/simple-color";
 
-export interface SimpleColorPickerProps {
+export interface SimpleColorPickerProps extends ValidationPropTypes {
   /** The SimpleColor components to be rendered in the group */
-  children: Array<React.ReactElement<SimpleColorProps>>;
+  children: React.ReactElement<SimpleColorProps> | Array<React.ReactElement<SimpleColorProps>>;
+  /** prop that represents childWidth */
+  childWidth?: string;
   /** Should the onBlur callback prop be initially blocked? */
   isBlurBlocked?: boolean;
-  /** Indicate that error has occurred
-   * Pass string to display icon, tooltip and red border
-   * Pass true boolean to only display red border
-   */
-  error?: boolean | string;
-  /** Indicate that warning has occurred
-   * Pass string to display icon, tooltip and orange border
-   * Pass true boolean to only display orange border
-   */
-  warning?: boolean | string;
-  /** Indicate additional information
-   * Pass string to display icon, tooltip and blue border
-   * Pass true boolean to only display blue border
-   */
-  info?: boolean | string;
-  /** When true, validation icon will be placed on legend instead of being placed by the input */
-  validationOnLegend?: boolean;
   /** The content for the Legend */
   legend: string;
-  /** The currently selected color. */
-  value?: string;
+  /** prop that sets max-width in css */
+  maxWidth?: string;
   /** The name to apply to the input. */
   name?: string;
   /** Prop for `onChange` events */
@@ -35,13 +21,14 @@ export interface SimpleColorPickerProps {
   onKeyDown?: (ev: React.SyntheticEvent) => void;
   /** Prop for `onBlur` events */
   onBlur?: (ev: React.ChangeEvent<HTMLElement>) => void;
-  /** prop that sets max-width in css */
-  maxWidth?: string;
-  /** prop that represents childWidth */
-  childWidth?: string;
   /** Flag to configure component as mandatory */
   required?: boolean;
+  /** When true, validation icon will be placed on legend instead of being placed by the input */
+  validationOnLegend?: boolean;
+  /** The currently selected color. */
+  value?: string;
 }
+
 declare const SimpleColorPicker: React.ComponentType<SimpleColorPickerProps>;
 
 export default SimpleColorPicker;
