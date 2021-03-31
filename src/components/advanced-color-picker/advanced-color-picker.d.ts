076e3acde426e0c9791e229a94ad4d1d6463d9e9
grabkowski <jakub.grabkowski@sage.com>
2021-03-31 17:46:44 +0200

    fix(advanced-color-picker): incorrect ts type definitions
    fix for type definitions not exported
    fix for availableColors, defaultColor, name prop not required in ts
    copy js prop documentation
    sort type definitions

--- src/components/advanced-color-picker/advanced-color-picker.d.ts
+++ src/components/advanced-color-picker/advanced-color-picker.d.ts
@@ -1,15 +1,29 @@
 import * as React from "react";
 
+export interface AdvancedColor {
+  label: string;
+  value: string;
+}
+
 export interface AdvancedColorPickerPropTypes {
-  name?: string;
+  /** Prop for `availableColors` containing array of objects of colors */
+  availableColors: AdvancedColor[];
+  /** Prop for `defaultColor` containing the default color for `uncontrolled` use */
+  defaultColor: string;
+  /** Specifies the name prop to be applied to each color in the group */
+  name: string;
+  /** Prop for `onBlur` event */
+  onBlur?: (ev: React.SyntheticEvent<HTMLElement>) => void;
+  /** Prop for `onChange` event */
+  onChange?: (ev: React.ChangeEvent<HTMLElement>) => void;
+  /** Prop for `onClose` event */
+  onClose?: (ev: React.MouseEvent<HTMLElement>) => void;
+  /** Prop for `onOpen` event */
+  onOpen?: (ev: React.MouseEvent<HTMLElement>) => void;
+  /** Prop for `open` status */
   open?: boolean;
-  availableColors?: [];
+  /** Prop for `selectedColor` containing pre-selected color for `controlled` use */
   selectedColor?: string;
-  defaultColor?: string;
-  onChange?: () => void;
-  onOpen?: (ev: React.ChangeEvent<HTMLElement>) => void;
-  onClose?: (ev: React.ChangeEvent<HTMLElement>) => void;
-  onBlur?: (ev: React.ChangeEvent<HTMLElement>) => void;
 }
 
 declare function AdvancedColorPicker(props: AdvancedColorPickerPropTypes): JSX.Element;
