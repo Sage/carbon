7b59608f4858048d01c07b4b65ca87b07e73a36a
grabkowski <jakub.grabkowski@sage.com>
2021-04-01 16:59:30 +0200

    fix(batch-selection): ts type definitions not properly exported
    add missing index.d.ts file
    add missing "hidden" type definition
    fix incorrect "colorTheme" types
    sort ts type definitions

--- src/components/batch-selection/batch-selection.d.ts
+++ src/components/batch-selection/batch-selection.d.ts
@@ -1,12 +1,14 @@
 import * as React from "react";
 
 export interface BatchSelectionProps {
-  /** Color of the background, transparent if not defined */
-  colorTheme?: "dark" | "light" | "transparent-base" | "transparent-white";
   /** Content to be rendered after selected count */
   children: React.ReactNode;
+  /** Color of the background, transparent if not defined */
+  colorTheme?: "dark" | "light" | "white" | "transparent";
   /** If true disables all user interaction */
   disabled?: boolean;
+  /** Hidden if true */
+  hidden?: boolean;
   /** Number of selected elements */
   selectedCount: number;
 }
