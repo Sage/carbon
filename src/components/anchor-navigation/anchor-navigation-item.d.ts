fc68fdd619c9a7bdd767dcad485d8b15ad30eae0
grabkowski <jakub.grabkowski@sage.com>
2021-04-01 15:25:12 +0200

    fix(anchor-navigation): incorrect ts type definitions
    fix incorrect event type definitions in anchor navigation item
    change children type to optional to match js
    sort props in d.ts file

--- src/components/anchor-navigation/anchor-navigation-item.d.ts
+++ src/components/anchor-navigation/anchor-navigation-item.d.ts
@@ -1,21 +1,21 @@
 import * as React from "react";
 
 export interface AnchorNavigationItemProps {
-  children: React.ReactNode;
-  /** OnKeyDown handler */
-  onKeyDown?: () => void;
-  /** onClick handler */
-  onClick?: () => void;
+  children?: React.ReactNode;
   /** href to be passed to the anchor element, can be linked with id passed to the scrollable section */
   href?: string;
-  /** tabIndex passed to the anchor element */
-  tabIndex?: number;
   /** Indicates if component is selected */
   isSelected?: boolean;
+  /** onClick handler */
+  onClick?: (ev: React.MouseEvent<HTMLAnchorElement>) => void;
+  /** OnKeyDown handler */
+  onKeyDown?: (ev: React.KeyboardEvent<HTMLAnchorElement>) => void;
   /** Allows to override existing component styles */
   styleOverride?: () => object | object;
+  /** tabIndex passed to the anchor element */
+  tabIndex?: number;
   /** Reference to the section html element meant to be shown   */
-  target?: React.RefObject<HTMLElement>;
+  target?: React.Ref<HTMLElement>;
 }
 
 declare function AnchorNavigationItem(
--- src/components/anchor-navigation/anchor-section-divider.d.ts
+++ src/components/anchor-navigation/anchor-section-divider.d.ts
@@ -1,12 +1,7 @@
 import { StyledComponentProps } from "styled-components";
 
-export interface AnchorSectionDividerProps {
-  /** @default "anchor-navigation-divider" */
-  "data-element"?: string;
-  /** Allows to override existing component styles */
-  styleOverride?: () => object | object;
-}
-
-declare function AnchorSectionDivider(attrs: StyledComponentProps<"div", {}, AnchorSectionDividerProps, "">): JSX.Element;
+declare function AnchorSectionDivider(
+  attrs: StyledComponentProps<"div", {}, {}, "">
+): JSX.Element;
 
 export default AnchorSectionDivider;
