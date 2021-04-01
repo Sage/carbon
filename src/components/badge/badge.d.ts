b3cdc6e46eb12b1258f6737b1a3e686f5cd4292f
grabkowski <jakub.grabkowski@sage.com>
2021-04-01 15:58:03 +0200

    fix(badge): incorrect ts type definitions
    change counter and onClick prop to be optional to match js proptypes
    fix missing onClick event type

--- src/components/badge/badge.d.ts
+++ src/components/badge/badge.d.ts
@@ -4,9 +4,9 @@ export interface BadgeProps {
   /** The badge will be added to this element */
   children: React.ReactNode;
   /** The number rendered in the badge component */
-  counter: string | number;
+  counter?: string | number;
   /** Callback fired when badge is clicked */
-  onClick: () => void;
+  onClick?: (ev: React.MouseEvent<HTMLElement>) => void;
 }
 
 declare function Badge(props: BadgeProps): JSX.Element;
