3a2f73c7b64198d613146136b6f6c2435c3341af
grabkowski <jakub.grabkowski@sage.com>
2021-04-06 18:16:45 +0200

    fix(confirm): type definitions not exported
    add index.d.ts file to export type definitions
    extend Dialog type definitions
    sort props type definitions
    add event argument to onConfirm type definition
    
    Fixes #3710
    Fixes #3809

--- src/components/confirm/confirm.d.ts
+++ src/components/confirm/confirm.d.ts
@@ -1,24 +1,25 @@
 import * as React from "react";
+import { DialogProps } from "../dialog/dialog";
 
-export interface ConfirmProps {
-  // ** A custom event handler when a confirmation takes place */
-  onConfirm: () => void;
-  // ** Customise the confirm button label */
-  confirmLabel?: string;
+export interface ConfirmProps extends DialogProps {
+  /** Allows to setup buttonType into cancel button */
+  cancelButtonType?: "primary" | "secondary" | "tertiary";
   // ** Customise the cancel button label */
   cancelLabel?: string;
+  // ** Customise the confirm button label */
+  confirmLabel?: string;
   /** Apply destructive style to the buttons */
   destructive?: boolean;
-  /** Defines an Icon type within the button (see Icon for options) */
-  iconType?: "error" | "warning";
   /** Makes cancel button disabled */
   disableCancel?: boolean;
   /** Makes confirm button disabled */
   disableConfirm?: boolean;
-  /** Allows to setup buttonType into cancel button */
-  cancelButtonType?: "primary" | "secondary" | "tertiary";
+  /** Defines an Icon type within the button (see Icon for options) */
+  iconType?: "error" | "warning";
   /** Adds isLoading state into confirm button */
   isLoadingConfirm?: boolean;
+  // ** A custom event handler when a confirmation takes place */
+  onConfirm: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 declare class Confirm extends React.Component<ConfirmProps> {}
