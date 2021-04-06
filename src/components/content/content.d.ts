01c08ad0e2949f46091d710b765b804de52cb7d9
grabkowski <jakub.grabkowski@sage.com>
2021-04-06 18:22:19 +0200

    refactor(content): add documentation to type definitions
    add ts type definitions documentation
    sort props type definitions

--- src/components/content/content.d.ts
+++ src/components/content/content.d.ts
@@ -1,13 +1,23 @@
 import * as React from "react";
 
 export interface ContentProps {
+  /** Aligns the content (left, center or right) */
   align?: "left" | "center" | "right";
-  variant?: "primary" | "secondary";
+  /**
+   * Over-rides the calculation of body width based on titleWidth.
+   * Sometimes we need the body to be full width while keeping a title width similar to other widths
+   */
   bodyFullWidth?: boolean;
+  /** The body of the content component */
   children?: React.ReactNode;
+  /** Displays the content inline with the title */
   inline?: boolean;
+  /** The title of the content component */
   title?: string;
+  /** Sets a custom width for the title element */
   titleWidth?: string;
+  /** Applies a theme to the Content Value: primary, secondary */
+  variant?: "primary" | "secondary";
 }
 
 declare function Content(props: ContentProps): JSX.Element;
