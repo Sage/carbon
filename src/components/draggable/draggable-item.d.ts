e85597664ac2ab74a22214f3d9ee58c3967da363
grabkowski <jakub.grabkowski@sage.com>
2021-04-07 18:19:34 +0200

    fix(draggable): incorrect ts type definitions
    fix incorrect ts type definitions
    add ts prop documentation

--- src/components/draggable/draggable-item.d.ts
+++ src/components/draggable/draggable-item.d.ts
@@ -1,8 +1,16 @@
 import * as React from "react";
 
 export interface DraggableItemProps {
+  /**
+   * The id of the `DraggableItem`.
+   *
+   * Use this prop to make `Draggable` works
+   */
   id: number | string;
+  /** The content of the component. */
   children: React.ReactNode;
+  findItem?: () => void;
+  moveItem?: () => void;
 }
 
 declare function DraggableItem(props: DraggableItemProps): JSX.Element;
