87ffe7e6da7ed5595f0f8bcb0be9d2ccb6a1eb25
grabkowski <jakub.grabkowski@sage.com>
2021-04-08 16:42:17 +0200

    fix(pill): ts type definitions not properly exported
    add index.d.ts file
    add documentation to type definitions
    sort type definitions
    
    Fixes #3708

--- src/components/pill/pill.d.ts
+++ src/components/pill/pill.d.ts
@@ -1,14 +1,21 @@
 import * as React from "react";
-import { MarginSpacingProps } from "../../utils/helpers/options-helper";
+import { MarginProps } from "styled-system";
 
-export interface PillProps extends MarginSpacingProps {
-  colorVariant?: "neutral" | "negative" | "positive" | "warning";
+export interface PillProps extends MarginProps {
+  /** Override color variant, provide any color from palette or any valid css color value. */
   borderColor?: string;
+  /** Change the color of a status pill. */
+  colorVariant?: "neutral" | "negative" | "positive" | "warning";
+  /** The content to display inside of the pill.  */
   children: string;
+  /** Fills the pill background with colour. When fill is false only the border is coloured. */
   fill?: boolean;
-  pillRole?: "tag" | "status";
+  /** Callback function for when the pill is clicked. */
   onClick?: React.MouseEventHandler<HTMLSpanElement>;
+  /** Callback function for when the remove icon is clicked. */
   onDelete?: React.MouseEventHandler<HTMLButtonElement>;
+  /** Sets the type of pill in use. */
+  pillRole?: "tag" | "status";
   size?: "S" | "M" | "L" | "XL";
 }
 
