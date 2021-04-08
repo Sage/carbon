01c07ed8aea5d51fd1577c3a466d551ace99c6ef
grabkowski <jakub.grabkowski@sage.com>
2021-04-08 16:11:45 +0200

    fix(form): type definitions not properly exported
    fix type definitions export
    fix incorrect "buttonAlignment" type definition from required to optional
    sort type definitions
    
    Fixes #3690

--- src/components/form/form.d.ts
+++ src/components/form/form.d.ts
@@ -3,37 +3,27 @@ import { FormFieldSpacing } from "../../utils/helpers/options-helper/options-hel
 
 export interface FormProps {
   /** Alignment of buttons */
-  buttonAlignment: "left" | "right";
-
-  /** Enables the sticky footer. */
-  stickyFooter?: boolean;
-
-  /** Additional buttons rendered on the left side of the save button */
-  leftSideButtons?: React.ReactNode;
-
-  /** Additional buttons rendered on the right side of the save button */
-  rightSideButtons?: React.ReactNode;
-
-  /** Callback passed to the form element */
-  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => any;
-
+  buttonAlignment?: "left" | "right";
   /** Child elements */
   children?: React.ReactNode;
-
-  /** Save button to be rendered */
-  saveButton?: React.ReactNode;
-
   /** The total number of errors present in the form */
   errorCount?: number;
-
-  /** The total number of warnings present in the form */
-  warningCount?: number;
-
   /** Spacing between form fields, given number will be multiplied by base spacing unit (8) */
   fieldSpacing?: FormFieldSpacing;
-
+  /** Additional buttons rendered on the left side of the save button */
+  leftSideButtons?: React.ReactNode;
   /** Disable HTML5 validation */
   noValidate?: boolean;
+  /** Callback passed to the form element */
+  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => any;
+  /** Additional buttons rendered on the right side of the save button */
+  rightSideButtons?: React.ReactNode;
+  /** Save button to be rendered */
+  saveButton?: React.ReactNode;
+  /** Enables the sticky footer. */
+  stickyFooter?: boolean;
+  /** The total number of warnings present in the form */
+  warningCount?: number;
 }
 
 declare function Form(props: FormProps): JSX.Element;
