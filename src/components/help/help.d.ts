eec3e482d89a0b4bccc9c50e9967290a656d479b
grabkowski <jakub.grabkowski@sage.com>
2021-04-09 12:59:49 +0200

    refactor(help): clean up ts type definitions

--- src/components/help/help.d.ts
+++ src/components/help/help.d.ts
@@ -1,14 +1,12 @@
-import * as React from "react";
-import {
-  IconTypes,
-  Positions,
-} from "../../utils/helpers/options-helper/options-helper";
+import * as OptionsHelper from "../../utils/helpers/options-helper/options-helper";
 
 export interface HelpProps {
   /** Overrides the default 'as' attribute of the Help component */
   as?: string;
   /** Message to display in tooltip */
   children?: string;
+  /** [Legacy] A custom class name for the component. */
+  className?: string;
   /** The unique id of the component (used with aria-describedby for accessibility) */
   helpId?: string;
   /** A path for the anchor */
@@ -17,8 +15,6 @@ export interface HelpProps {
   isFocused?: boolean;
   /** Overrides the default tabindex of the component */
   tabIndex?: number | string;
-  /** Help Icon type */
-  type?: IconTypes;
   /** Override background color of the Tooltip, provide any color from palette or any valid css color value. */
   tooltipBgColor?: string;
   /** Override font color of the Tooltip, provide any color from palette or any valid css color value. */
@@ -27,11 +23,11 @@ export interface HelpProps {
    *  must be an array containing some or all of ["top", "bottom", "left", "right"]
    * (see https://popper.js.org/docs/v2/modifiers/flip/#fallbackplacements)
    */
-  tooltipFlipOverrides?: Positions[];
+  tooltipFlipOverrides?: OptionsHelper.Positions[];
   /** Position of tooltip relative to target */
-  tooltipPosition?: Positions;
-  /** [Legacy] A custom class name for the component. */
-  className?: string;
+  tooltipPosition?: OptionsHelper.Positions;
+  /** Help Icon type */
+  type?: OptionsHelper.IconTypes;
 }
 
 declare function Help(props: HelpProps): JSX.Element;
