348f91ee27752666368216479d877dd842556344
grabkowski <jakub.grabkowski@sage.com>
2021-04-09 14:27:56 +0200

    feat(menu): add scrollableblock type definitions

--- src/components/menu/index.d.ts
+++ src/components/menu/index.d.ts
@@ -3,3 +3,4 @@ export { default as MenuItem } from "./menu-item/menu-item";
 export { default as SubmenuBlock } from "./submenu-block/submenu-block";
 export { default as MenuDivider } from "./menu-divider/menu-divider";
 export { default as MenuSegmentTitle } from "./menu-segment-title/menu-segment-title";
+export { default as ScrollableBlock } from "./scrollable-block/scrollable-block";
