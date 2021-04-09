adda43b647f7d7dd6afb09a28007f950356e76c3
grabkowski <jakub.grabkowski@sage.com>
2021-04-09 18:35:05 +0200

    feat(pages): add ts type definitions

---
+++ src/components/pages/pages.d.ts
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Page from "./page/page";
+import { ThemeObject } from "../../style/themes/base";
+
+export interface PagesProps {
+  /** [legacy] Custom className */
+  className?: string;
+  /** The selected tab on page load */
+  initialpageIndex?: number | string;
+  pageIndex?: number | string;
+  /** Individual tabs */
+  children?: React.ReactNode | object;
+  /** Controls which transition to use. */
+  transition?: string;
+}
+
+declare class Pages extends React.Component<PagesProps> {}
+declare function PagesWithTheme(props: PagesProps & { theme: ThemeObject }): JSX.Element;
+
+export default Pages;
+export { PagesWithTheme as Pages, Page };
