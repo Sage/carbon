75ac1d57d274623f27cdc597a16d0063e5f9ffb7
grabkowski <jakub.grabkowski@sage.com>
2021-04-13 11:54:08 +0200

    fix(toast): type definitions not properly exported
    add index.d.ts file to export type definitions
    add prop documentation for type definitions
    fix incorrect type definitions
    
    Fixes #3712

--- src/components/toast/toast.d.ts
+++ src/components/toast/toast.d.ts
@@ -1,17 +1,30 @@
 import * as React from "react";
+import * as OptionsHelper from "../../utils/helpers/options-helper";
 
 export interface ToastPropTypes {
-  children: React.FunctionComponent | React.ComponentClass;
-  variant?: string;
-  as?: string;
+  /** The rendered children of the component. */
+  children: React.ReactNode;
+  /** Customizes the appearance in the DLS theme */
+  variant?: OptionsHelper.ColorTypes;
+  /** Customizes the appearance in a legacy theme through colour (see the 'iconColorSets' for possible values) */
+  as?: OptionsHelper.ColorTypes;
+  /** Custom className */
   className?: string;
+  /** Custom id  */
   id?: string;
+  /** Component name */
   "data-component"?: string;
+  /** Determines if the Toast is open. */
   open?: boolean;
+  /** Callback for when dismissed. */
   onDismiss?: () => void;
+  /** Time for Toast to remain on screen */
   timeout?: string | number;
+  /** Centers the Toast on the screen */
   isCenter?: boolean;
+  /** Target Portal ID where the Toast will render */
   targetPortalId?: string;
+  /** Maximum toast width */
   maxWidth?: string;
 }
 
