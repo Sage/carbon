5d3960604469479c9864cb1764aa84fe220f0396
grabkowski <jakub.grabkowski@sage.com>
2021-04-13 11:14:10 +0200

    chore(text-editor): add ts prop documentation

--- src/components/text-editor/text-editor.d.ts
+++ src/components/text-editor/text-editor.d.ts
@@ -1,19 +1,27 @@
 import * as React from "react";
-import { MarginSpacingProps } from "../../utils/helpers/options-helper";
+import { MarginProps } from "styled-system";
 import { Editor, EditorState, ContentState } from "draft-js";
 
-export interface TextEditorProps extends MarginSpacingProps {
+export interface TextEditorProps extends MarginProps {
+  /** The maximum characters that the input will accept */
   characterLimit?: number;
+  /** The text for the editor's label */
   labelText: string;
+  /** onChange callback to control value updates */
   onChange: (event: object) => void;
   /** Additional elements to be rendered in the Editor Toolbar, e.g. Save and Cancel Button */
   toolbarElements?: React.ReactNode;
+  /** The value of the input, this is an EditorState immutable object */
   value: object;
   /** Flag to configure component as mandatory */
   required?: boolean;
+  /** Message to be displayed when there is an error */
   error?: string;
+  /** Message to be displayed when there is a warning */
   warning?: string;
+  /** Message to be displayed when there is an info */
   info?: string;
+  /** Number greater than 2 multiplied by line-height (21px) to override the default min-height of the editor */
   rows?: number;
 }
 
