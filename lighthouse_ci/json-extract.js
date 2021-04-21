89e990a5ef7fc92380b800815e3435c063954bd2
Jamie Thompson <jamie.thompson@sage.com>
2021-04-21 11:19:55 +0100

    refactor: test using built version

--- lighthouse_ci/json-extract.js
+++ lighthouse_ci/json-extract.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const path = require("path");
 
 const data = require("../storybook-static/stories.json");
 
-const lighthouseCiJson = "./lighthouserc.json";
-const json = require("../lighthouserc.json");
+const output = path.resolve("./lighthouserc.json");
+const defaults = require("../lighthouserc.json");
 
 const allUrls = [];
 const urls = data.stories;
@@ -14,13 +15,18 @@ for (let el in urls) {
     !el.startsWith("documentation") &&
     !el.startsWith("icon-test--all")
   ) {
-    el = `http://localhost:9001/?path=/story/${el}`;
+    el = `http://localhost:8080/?path=/story/${el}`;
     allUrls.push(el);
   }
 }
 
-json.ci.collect.url = allUrls;
+defaults.ci.collect.url = allUrls;
 
-fs.writeFile(lighthouseCiJson, JSON.stringify(json), (err) => {
-  return err ? console.log(err) : null;
+fs.writeFile(output, JSON.stringify(defaults, null, 2), (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Wrote ${allUrls.length} URL's to ${output}`);
 });
