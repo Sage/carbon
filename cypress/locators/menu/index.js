a9cb150b4d8c026ebea92996c056922d79eff131
Sian Ford <sian.ford@sage.com>
2021-04-28 12:13:15 +0100

    test(menu): add new tests for submenu options story
    adding new tests to check function of With clickToOpen menu item

--- cypress/locators/menu/index.js
+++ cypress/locators/menu/index.js
@@ -1,4 +1,10 @@
-import { SUBMENU, SCROLL_BLOCK, MENU_DIVIDER, SEGMENT_TITLE } from "./locators";
+import {
+  SUBMENU,
+  SCROLL_BLOCK,
+  MENU_DIVIDER,
+  SEGMENT_TITLE,
+  MENU,
+} from "./locators";
 
 // component preview locators
 export const submenu = () => cy.get(SUBMENU);
@@ -9,3 +15,7 @@ export const scrollBlock = () => cy.get(SUBMENU).find(SCROLL_BLOCK);
 export const lastSubmenuElement = () => submenuBlock().find("li div").last();
 export const menuDivider = () => cy.get(MENU_DIVIDER);
 export const segmentTitle = () => cy.get(SEGMENT_TITLE);
+export const menuComponent = (index) =>
+  cy.get(MENU).first().find(`li:nth-child(${index})`);
+export const submenuItem = (index) =>
+  menuComponent(index).find(SUBMENU).find("ul > li");
