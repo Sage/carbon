f1500cfe87e9b1c606354cd1cf30a62ecdfa652f
Ed Leeks <edward.leeks@sage.com>
2021-04-30 16:47:55 +0100

    test(simple-color-picker): ensure correct props are used in test

--- src/__experimental__/components/simple-color-picker/simple-color/simple-color.spec.js
+++ src/__experimental__/components/simple-color-picker/simple-color/simple-color.spec.js
@@ -7,7 +7,7 @@ import { rootTagTest } from "../../../../utils/helpers/tags/tags-specs/tags-spec
 
 function render(props) {
   return shallow(
-    <SimpleColor name="color-picker" color="#0073C2" {...props} />
+    <SimpleColor name="color-picker" value="#0073C2" {...props} />
   );
 }
 
@@ -23,7 +23,9 @@ describe("ColorOption", () => {
 
   describe("tags on component", () => {
     it("include correct component, element and role data tags", () => {
-      wrapper = shallow(<SimpleColor data-element="bar" data-role="baz" />);
+      wrapper = shallow(
+        <SimpleColor data-element="bar" data-role="baz" value="#0073C2" />
+      );
       rootTagTest(wrapper, "simple-color", "bar", "baz");
     });
   });
