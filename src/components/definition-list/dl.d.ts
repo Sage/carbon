2a22f6fb6e90f5064cd19d90d86b18b4f7ea4456
grabkowski <jakub.grabkowski@sage.com>
2021-04-30 16:48:26 +0200

    refactor(definition-list):  extend styled system space typings

--- src/components/definition-list/dl.d.ts
+++ src/components/definition-list/dl.d.ts
@@ -1,15 +1,12 @@
 import * as React from "react";
-import { SpacingProps } from "../../utils/helpers/options-helper";
-export interface DlProps extends SpacingProps {
+import { SpaceProps } from "styled-system";
+export interface DlProps extends SpaceProps {
   /** This string will specify the text align styling of the `<dt></dt>`. */
   dtTextAlign?: "left" | "center" | "right";
-
   /** This string will specify the text align styling of the `<dd></dd>`. */
   ddTextAlign?: "left" | "center" | "right";
-
   /** This value will specify the width of the `StyledDtDiv` as a percentage. */
   w?: number;
-
   /** prop to render children. */
   children: React.ReactNode;
 }
