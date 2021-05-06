aa40bdf2f59acf437cca6ceb5a4e8f87d8e07cc8
grabkowski <jakub.grabkowski@sage.com>
2021-05-06 11:08:00 +0200

    refactor(vertical-divider): ts extend styled-system space props

--- src/components/vertical-divider/vertical-divider.d.ts
+++ src/components/vertical-divider/vertical-divider.d.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { SpacingProps, TintValueType} from "../../utils/helpers/options-helper";
+import { SpaceProps } from "styled-system";
+import * as OptionsHelper from "../../utils/helpers/options-helper";
 
-export interface VerticalDividerPropTypes extends SpacingProps {
+export interface VerticalDividerPropTypes extends SpaceProps {
   h?: number | string;
   displayInline?: boolean;
-  tint?: TintValueType;
+  tint?: OptionsHelper.TintValueType;
 }
 
 declare function VerticalDivider(props: VerticalDividerPropTypes): JSX.Element;
