fde11cbbdba4766923434404916314f90ebcfcbc
grabkowski <jakub.grabkowski@sage.com>
2021-05-06 10:17:30 +0200

    refactor(navigation-bar): extend styled-system space props in ts

--- src/components/navigation-bar/navigation-bar.d.ts
+++ src/components/navigation-bar/navigation-bar.d.ts
@@ -1,8 +1,8 @@
 import * as React from "react";
 import NavigationBar from "./navigation-bar.component";
-import { SpacingProps } from "../../utils/helpers/options-helper";
+import { SpaceProps } from "styled-system";
 
-export interface NavigationBarProp extends SpacingProps {
+export interface NavigationBarProp extends SpaceProps {
   children?: React.ReactNode;
   ariaLabel?: string;
   navigationType?: "light" | "dark";
