3e7343b6c1c646f72d843f41dd886c6f6dcf5a3d
grabkowski <jakub.grabkowski@sage.com>
2021-05-06 11:06:05 +0200

    fix(tile-select): ts type prop should not be required

--- src/components/tile-select/tile-select-group.d.ts
+++ src/components/tile-select/tile-select-group.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { MarginSpacingProps } from "../../utils/helpers/options-helper";
+import { MarginProps } from "styled-system";
 
-export interface TileSelectGroupProps extends MarginSpacingProps {
+export interface TileSelectGroupProps extends MarginProps {
   /** The TileSelect components to be rendered in the group */
   children: React.ReactNode;
   /** The content for the TileSelectGroup Legend */
