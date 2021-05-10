70507ba053215e541f32ed9918709ab63643e110
mkrds <mkordas@pgs-soft.com>
2021-05-10 14:56:06 +0200

    fix(date-range): fix incorrect gaps between date fields

--- src/__experimental__/components/date-range/date-range.style.js
+++ src/__experimental__/components/date-range/date-range.style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import StyledDateInput from "../date/date.style";
+import { StyledLabelContainer } from "../label/label.style.js";
+import { StyledInputPresentationContainer } from "../input/input-presentation.style.js";
 
 const StyledDateRange = styled.div`
   & ${StyledDateInput} {
@@ -10,6 +12,14 @@ const StyledDateRange = styled.div`
 
   & ${StyledDateInput}:first-child {
     margin-right: 15px;
+  }
+
+  ${StyledLabelContainer} {
+    width: auto;
+  }
+
+  ${StyledInputPresentationContainer} {
+    flex: auto;
   }
 `;
 
