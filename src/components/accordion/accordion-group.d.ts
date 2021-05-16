d925be9d1080e03b17174361e661fe06cabda0d6
mkrds <mkordas@pgs-soft.com>
2021-05-16 22:51:00 +0200

    feat(accordion-group): allow empty children

--- src/components/accordion/accordion-group.d.ts
+++ src/components/accordion/accordion-group.d.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 import { MarginProps } from "styled-system";
 import { AccordionProps } from "./accordion";
 
-type AccordionElement = React.ReactElement<AccordionProps>;
+type AccordionElement = React.ReactElement<AccordionProps> | boolean | null | undefined;
 
 export interface AccordionGroupProps extends MarginProps {
   children?: AccordionElement | AccordionElement[];
