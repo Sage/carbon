c209ea81fffee37e35416561355bf376ff067e2d
Michał Maciorowski <michal.maciorowski2@sage.com>
2021-06-11 18:09:53 +0200

    feat(confirm): more flexible button usage

--- src/components/confirm/confirm.d.ts
+++ src/components/confirm/confirm.d.ts
@@ -2,14 +2,30 @@ import * as React from "react";
 import { DialogProps } from "../dialog/dialog";
 
 export interface ConfirmProps extends DialogProps {
-  /** Allows to setup buttonType into cancel button */
-  cancelButtonType?: "primary" | "secondary" | "tertiary";
-  // ** Customise the cancel button label */
+  /** Color variants for new business themes: "primary" | "secondary" | "tertiary" | "dashed" | "darkBackground" */
+  cancelButtonType?: "primary" | "secondary" | "tertiary" | "dashed" | "darkBackground";
+  /** Color variants for new business themes: "primary" | "secondary" | "tertiary" | "dashed" | "darkBackground" */
+  confirmButtonType?: "primary" | "secondary" | "tertiary" | "dashed" | "darkBackground";
+  /** Customise the cancel button label */
   cancelLabel?: string;
-  // ** Customise the confirm button label */
+  /** Customise the confirm button label */
   confirmLabel?: string;
   /** Apply destructive style to the buttons */
   destructive?: boolean;
+  /** Apply destructive style to the cancel button */
+  cancelButtonDestructive?: boolean;
+  /** Apply destructive style to the confirm button */
+  confirmButtonDestructive?: boolean;
+  /** Defines a cancel button Icon position related to the children: "before" | "after" */
+  cancelButtonIconPosition?: "before" | "after";
+  /* FIXME use import { IconTypes } from "components/icon/icon-types" when #4134 is merged */
+  /** Defines an Icon type within the cancel button (see Icon for options) */
+  cancelButtonIconType?: string;
+  /** Defines a cancel button Icon position related to the children: "before" | "after" */
+  confirmButtonIconPosition?: "before" | "after";
+  /* FIXME use import { IconTypes } from "components/icon/icon-types" when #4134 is merged */
+  /** Defines an Icon type within the confirm button (see Icon for options) */
+  confirmButtonIconType?: string;
   /** Makes cancel button disabled */
   disableCancel?: boolean;
   /** Makes confirm button disabled */
@@ -18,7 +34,7 @@ export interface ConfirmProps extends DialogProps {
   iconType?: "error" | "warning";
   /** Adds isLoading state into confirm button */
   isLoadingConfirm?: boolean;
-  // ** A custom event handler when a confirmation takes place */
+  /** A custom event handler when a confirmation takes place */
   onConfirm: (ev: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
