68def5369fd0d91d31bd05389be6d11aecbc38fc
Ed Leeks <edward.leeks@sage.com>
2021-08-12 15:30:25 +0100

    feat(date-range): add tooltipPosition prop to interface
    Adds `tooltipPosition` prop to support overriding the default tooltip position for icons rendered in
    `DateRange`

--- src/components/date-range/date-range.d.ts
+++ src/components/date-range/date-range.d.ts
@@ -77,6 +77,8 @@ export interface DateRangeProps extends MarginProps {
   value?: string[];
   /** When true, validation icons will be placed on labels instead of being placed on the inputs */
   validationOnLabel?: boolean;
+  /** Overrides the default tooltip position */
+  tooltipPosition?: "top" | "bottom" | "left" | "right";
 }
 
 declare class DateRange extends React.Component<DateRangeProps> {}
