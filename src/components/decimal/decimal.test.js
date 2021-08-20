5c5e6ea8159529a681aacd6d6e629c21e196c6ea
Ihor Dluhosh <idluhosh@pgs-soft.com>
2021-08-20 08:43:31 +0300

    test: add events tests

--- src/components/decimal/decimal.test.js
+++ src/components/decimal/decimal.test.js
@@ -5,107 +5,123 @@ import Decimal from "./decimal.component";
 // Decimal locator
 const decimalComponent = '[data-component="decimal"]';
 
-describe("Tests for precision for Decimal component", () => {
-  // beforeEach(() => {
-  //   // before we are inside a hook executing as part of the test
-  //   // we can use cy.on methods and create stubs, something
-  //   // we could not do from Cypress.on callbacks
-  //   const returnValueStub = cy.stub().as('returnValue')
+context("Tests for Decimal component", () => {
+  describe("check props for Decimal component", () => {
+    it("should input value and get proper output", () => {
+      // and mount the story using @cypress/react library
+      mount(<Decimal />);
 
-  //   cy.on('window:before:load', (win) => {
-  //     let userCallback, ourCallback
-  //     Object.defineProperty(win, 'onbeforeunload', {
-  //       get() {
-  //         return ourCallback
-  //       },
-  //       set(cb) {
-  //         userCallback = cb
-  //         console.log('user callback', cb)
+      // test data
+      const iterable = new Map([
+        [2, "2.00"],
+        [2.1, "2.10"],
+        [2.123, "2.123"],
+      ]);
 
-  //         ourCallback = (e) => {
-  //           console.log('proxy beforeunload event', e)
+      // then run our tests
+      for (const [inputValue, outputValue] of iterable) {
+        cy.get(decimalComponent)
+          .type(inputValue)
+          .find("input")
+          .blur({ force: true });
+        cy.get(decimalComponent)
+          .find("input")
+          .should("have.value", outputValue);
+      }
+    });
 
-  //           // prevent the application code from assigning value
-  //           Object.defineProperty(e, 'returnValue', {
-  //             get() {
-  //               return ''
-  //             },
-  //             set: returnValueStub,
-  //           })
+    it("should input value and get proper output with precision set to 5", () => {
+      // and mount the story using @cypress/react library
+      mount(<Decimal precision={5} />);
 
-  //           const result = userCallback(e)
-  //           return result
-  //         }
+      // test data
+      const iterable = new Map([
+        [2, "2.00000"],
+        [2.1, "2.10000"],
+        [2.123, "2.12300"],
+      ]);
 
-  //         // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-  //         win.addEventListener('beforeunload', ourCallback)
-  //       },
-  //     })
-  //   })
-  // })
+      // then run our tests
+      for (const [inputValue, outputValue] of iterable) {
+        cy.get(decimalComponent)
+          .type(inputValue)
+          .find("input")
+          .blur({ force: true });
+        cy.get(decimalComponent)
+          .find("input")
+          .should("have.value", outputValue);
+      }
+    });
 
-  it("should input value and get proper output", () => {
-    // and mount the story using @cypress/react library
-    mount(<Decimal />);
-    
-    // test data
-    const iterable = new Map([
-      [2, "2.00"],
-      [2.1, "2.10"],
-      [2.123, "2.123"],
-    ]);
+    it("should render Decimal with readOnly prop", () => {
+      // and mount the story using @cypress/react library
+      mount(<Decimal readOnly />);
 
-    // then run our tests
-    for (const [inputValue, outputValue] of iterable) {
+      const inputValue = "test";
+
+      // then run our tests
       cy.get(decimalComponent)
         .type(inputValue)
         .find("input")
         .blur({ force: true });
-      cy.get(decimalComponent).find("input").should("have.value", outputValue);
-
-      // cy.get('@returnValue')
-      //   .should('have.been.called')
-      //   .and('be.calledWithExactly', 'onChange');
-    }
+      cy.get(decimalComponent)
+        .find("input")
+        .should("not.have.value", inputValue)
+        .and("have.attr", "readOnly");
+    });
   });
 
-  it("should input value and get proper output with precision set to 5", () => {
-    // and mount the story using @cypress/react library
-    mount(<Decimal precision={5} />);
+  describe("check events for Decimal component", () => {
+    it("should return onChange event", () => {
+      // use cy.stub for mock the onChange callback
+      const callback = cy.stub().as("eventCallback");
+      // and mount the story using @cypress/react library
+      mount(<Decimal onChange={callback} />);
 
-    // test data
-    const iterable = new Map([
-      [2, "2.00000"],
-      [2.1, "2.10000"],
-      [2.123, "2.12300"],
-    ]);
+      const inputValue = "123";
 
-    // then run our tests
-    for (const [inputValue, outputValue] of iterable) {
+      // then run our tests
       cy.get(decimalComponent)
         .type(inputValue)
         .find("input")
         .blur({ force: true });
-      cy.get(decimalComponent).find("input").should("have.value", outputValue);
-      // cy.storyAction("onChange").should("have.been.called");
-    }
-  });
 
-  it("should render Decimal with readOnly prop", () => {
-    // and mount the story using @cypress/react library
-    mount(<Decimal readOnly />);
+      // first approach with .then
+      // .then(() => {
+      //   expect(callback).to.have.been.calledThrice
+      // });
 
-    const inputValue = "test";
+      // second approach for Events
+      cy.get("@eventCallback").should("be.calledThrice");
+      // cy.get('@eventCallback').should('be.calledWith', [
+      //   {
+      //     "target": {
+      //       "value": {
+      //         "rawValue": "1",
+      //         "formattedValue": "1.00"
+      //       }
+      //     }
+      //   }
+      // ])
+    });
 
-    // then run our tests
-    cy.get(decimalComponent)
-      .type(inputValue)
-      .find("input")
-      .blur({ force: true });
-    cy.get(decimalComponent)
-      .find("input")
-      .should("not.have.value", inputValue)
-      .and("have.attr", "readOnly");
-    // cy.storyAction("onChange").should("have.been.called");
+    it("should return onBlur event", () => {
+      // use cy.stub for mock the onChange callback
+      const callback = cy.stub();
+      // and mount the story using @cypress/react library
+      mount(<Decimal onBlur={callback} />);
+
+      const inputValue = "test";
+
+      // then run our tests
+      cy.get(decimalComponent)
+        .type(inputValue)
+        .find("input")
+        .blur({ force: true })
+        .then(() => {
+          // eslint-disable-next-line no-unused-expressions
+          expect(callback).to.have.been.calledOnce;
+        });
+    });
   });
 });
