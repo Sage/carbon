1e61160486d38cd3c71f8c8567ff91db7f80d39c
Ihor Dluhosh <idluhosh@pgs-soft.com>
2021-08-23 16:52:59 +0300

    test: add check for inner object in events

--- src/components/decimal/decimal.test.js
+++ src/components/decimal/decimal.test.js
@@ -72,27 +72,43 @@ context("Tests for Decimal component", () => {
   });
 
   describe("check events for Decimal component", () => {
-    it("should return onChange event", () => {
+    it.only("should return onChange event", () => {
       // use cy.stub for mock the onChange callback
       const callback = cy.stub().as("eventCallback");
       // and mount the story using @cypress/react library
       mount(<Decimal onChange={callback} />);
 
       const inputValue = "123";
+      const iterable = new Map([
+        ["1", "1.00"],
+        ["12", "12.00"],
+        ["123", "123.00"],
+      ]);
 
       // then run our tests
       cy.get(decimalComponent)
         .type(inputValue)
         .find("input")
-        .blur({ force: true });
+        .blur({ force: true })
 
-      // first approach with .then
-      // .then(() => {
-      //   expect(callback).to.have.been.calledThrice
-      // });
+        // first approach with .then
+        .then(() => {
+          // eslint-disable-next-line no-unused-expressions
+          expect(callback).to.have.been.calledThrice;
+          for (const [rawValue, formattedValue] of iterable) {
+            expect(
+              callback.getCalls()[iterable.entries()].args[0].target.value
+                .rawValue
+            ).to.equals(rawValue);
+            expect(
+              callback.getCalls()[iterable.entries()].args[0].target.value
+                .formattedValue
+            ).to.equals(formattedValue);
+          }
+        });
 
       // second approach for Events
-      cy.get("@eventCallback").should("be.calledThrice");
+      // cy.get("@eventCallback").should("be.calledThrice");
       // cy.get('@eventCallback').should('be.calledWith', [
       //   {
       //     "target": {
@@ -121,6 +137,13 @@ context("Tests for Decimal component", () => {
         .then(() => {
           // eslint-disable-next-line no-unused-expressions
           expect(callback).to.have.been.calledOnce;
+          // console.log(callback.getCalls()[0].args)
+          expect(
+            callback.getCalls()[0].args[0].target.value.rawValue
+          ).to.equals(inputValue);
+          expect(
+            callback.getCalls()[0].args[0].target.value.formattedValue
+          ).to.equals(inputValue);
         });
     });
   });
