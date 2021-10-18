0347f3b6ecf6a01a3271c900ede53244b7ea4f1a
Ihor Dluhosh <idluhosh@pgs-soft.com>
2021-10-18 13:49:51 +0200

    test: apply each for react tests

--- src/components/decimal/decimal.test.js
+++ src/components/decimal/decimal.test.js
@@ -13,130 +13,133 @@ const decimalComponent = '[data-component="decimal"]';
 
 context("Tests for Decimal component", () => {
   describe("check props for Decimal component", () => {
-    it("should input value and get proper output", () => {
-      const input = [
-        [0, "1", "1"],
-        [0, "134^", "134^"],
-        [0, "1234567789", "1,234,567,789"],
-        [0, ",,,,,", ",,,,,"],
-        [0, ".....", "....."],
-        [0, "abc,.123,.!@.00", "abc,.123,.!@.00"],
-        [0, "1,234$", "1,234$"],
+    const input = [
+      [0, "1", "1"],
+      [0, "134^", "134^"],
+      [0, "1234567789", "1,234,567,789"],
+      [0, ",,,,,", ",,,,,"],
+      [0, ".....", "....."],
+      [0, "abc,.123,.!@.00", "abc,.123,.!@.00"],
+      [0, "1,234$", "1,234$"],
 
-        [1, "1", "1.0"],
-        [1, "1.2", "1.2"],
-        [1, "1.23", "1.23"],
+      [1, "1", "1.0"],
+      [1, "1.2", "1.2"],
+      [1, "1.23", "1.23"],
 
-        [2, "2", "2.00"],
-        [2, "2.1", "2.10"],
-        [2, "2.12", "2.12"],
-        [2, "2.123", "2.123"],
+      [2, "2", "2.00"],
+      [2, "2.1", "2.10"],
+      [2, "2.12", "2.12"],
+      [2, "2.123", "2.123"],
 
-        [3, "2.1", "2.100"],
-        [3, "2.12", "2.120"],
-        [3, "2.123", "2.123"],
-        [3, "2.1234", "2.1234"],
+      [3, "2.1", "2.100"],
+      [3, "2.12", "2.120"],
+      [3, "2.123", "2.123"],
+      [3, "2.1234", "2.1234"],
 
-        [4, "1.1234", "1.1234"],
-        [4, "2", "2.0000"],
-        [4, "234556654", "234,556,654.0000"],
-        [4, "%^%^%<<,,,", "%^%^%<<,,,"],
+      [4, "1.1234", "1.1234"],
+      [4, "2", "2.0000"],
+      [4, "234556654", "234,556,654.0000"],
+      [4, "%^%^%<<,,,", "%^%^%<<,,,"],
 
-        [5, "1", "1.00000"],
-        [5, "1.12345", "1.12345"],
-        [5, "1a.23", "1a.23"],
+      [5, "1", "1.00000"],
+      [5, "1.12345", "1.12345"],
+      [5, "1a.23", "1a.23"],
 
-        [6, "2.123456", "2.123456"],
-        [6, "2.1", "2.100000"],
-        [6, "2a.12", "2a.12"],
-        [6, "1,232.123", "1,232.123000"],
+      [6, "2.123456", "2.123456"],
+      [6, "2.1", "2.100000"],
+      [6, "2a.12", "2a.12"],
+      [6, "1,232.123", "1,232.123000"],
 
-        [7, "1", "1.0000000"],
-        [7, "2344.1234567", "2,344.1234567"],
-        [7, "88652344.1234567", "88,652,344.1234567"],
+      [7, "1", "1.0000000"],
+      [7, "2344.1234567", "2,344.1234567"],
+      [7, "88652344.1234567", "88,652,344.1234567"],
 
-        [8, "1", "1.00000000"],
-        [8, "1.2", "1.20000000"],
-        [8, "1.23", "1.23000000"],
+      [8, "1", "1.00000000"],
+      [8, "1.2", "1.20000000"],
+      [8, "1.23", "1.23000000"],
 
-        [9, "2", "2.000000000"],
-        [9, "2.1", "2.100000000"],
-        [9, "1222.12", "1,222.120000000"],
-        [9, "2.123000000", "2.123000000"],
+      [9, "2", "2.000000000"],
+      [9, "2.1", "2.100000000"],
+      [9, "1222.12", "1,222.120000000"],
+      [9, "2.123000000", "2.123000000"],
 
-        [10, "2.1", "2.1000000000"],
-        [10, "2.12", "2.1200000000"],
-        [10, "1222.123", "1,222.1230000000"],
-        [10, "2.12345", "2.1234500000"],
+      [10, "2.1", "2.1000000000"],
+      [10, "2.12", "2.1200000000"],
+      [10, "1222.123", "1,222.1230000000"],
+      [10, "2.12345", "2.1234500000"],
 
-        [11, "1", "1.00000000000"],
-        [11, "2345", "2,345.00000000000"],
-        [11, "17899536472345", "17,899,536,472,345.00000000000"],
+      [11, "1", "1.00000000000"],
+      [11, "2345", "2,345.00000000000"],
+      [11, "17899536472345", "17,899,536,472,345.00000000000"],
 
-        [12, "1", "1.000000000000"],
-        [12, "1.12345", "1.123450000000"],
-        [12, "1a.23", "1a.23"],
+      [12, "1", "1.000000000000"],
+      [12, "1.12345", "1.123450000000"],
+      [12, "1a.23", "1a.23"],
 
-        [13, "2", "2.0000000000000"],
-        [13, "2.1", "2.1000000000000"],
-        [13, "2a.12", "2a.12"],
-        [13, "1232.123", "1,232.1230000000000"],
+      [13, "2", "2.0000000000000"],
+      [13, "2.1", "2.1000000000000"],
+      [13, "2a.12", "2a.12"],
+      [13, "1232.123", "1,232.1230000000000"],
 
-        [14, "2.1", "2.10000000000000"],
-        [14, "2.12", "2.12000000000000"],
-        [14, "2.123", "2.12300000000000"],
-        [14, "1222.1234", "1,222.12340000000000"],
+      [14, "2.1", "2.10000000000000"],
+      [14, "2.12", "2.12000000000000"],
+      [14, "2.123", "2.12300000000000"],
+      [14, "1222.1234", "1,222.12340000000000"],
 
-        [15, "1", "1.000000000000000"],
-        [15, "2332.78", "2,332.780000000000000"],
-        [15, "1a3.55", "1a3.55"],
-        [15, "1.12345", "1.123450000000000"],
-        [15, "1a.23", "1a.23"],
-      ];
+      [15, "1", "1.000000000000000"],
+      [15, "2332.78", "2,332.780000000000000"],
+      [15, "1a3.55", "1a3.55"],
+      [15, "1.12345", "1.123450000000000"],
+      [15, "1a.23", "1a.23"],
+    ];
 
-      input.map((item) => {
-        // and mount the story using @cypress/react library
-        mount(<Decimal precision={item[0]} />);
+    it.each(input)(
+      "should use %s as precision and %s as input value to produce %s output value",
+      (precision, inputValue, outputValue) => {
+        mount(<Decimal precision={precision} />);
 
         // then run our tests
         cy.get(decimalComponent)
-          .type(item[1])
+          .type(inputValue)
           .find("input")
           .blur({ force: true });
-        cy.get(decimalComponent).find("input").should("have.value", item[2]);
+        cy.get(decimalComponent)
+          .find("input")
+          .should("have.value", outputValue);
         return this;
-      });
-    });
+      }
+    );
 
-    it("should input value and get proper output for different locales", () => {
-      const input = [
-        ["en", "1,1,1,1,1.1", "11,111.100"],
-        ["en", "1,1,1222,12,1.1", "111,222,121.100"],
-        ["en", "1,,1,,1", "1,,1,,1"],
+    const inputLocale = [
+      ["en", "1,1,1,1,1.1", "11,111.100"],
+      ["en", "1,1,1222,12,1.1", "111,222,121.100"],
+      ["en", "1,,1,,1", "1,,1,,1"],
 
-        ["es-ES", "1.1.1.1.1.1,1", "111.111,100"],
-        ["es-ES", "2.123", "2123,000"],
-        ["es-ES", "21.21.111.1,013", "21.211.111,013"],
-        ["es-ES", "2.,12.,1", "2.,12.,1"],
+      ["es-ES", "1.1.1.1.1.1,1", "111.111,100"],
+      ["es-ES", "2.123", "2123,000"],
+      ["es-ES", "21.21.111.1,013", "21.211.111,013"],
+      ["es-ES", "2.,12.,1", "2.,12.,1"],
 
-        ["fr", "11111,25", "11 111,250"],
-        ["fr", "1  1  1  1  1,25", "1  1  1  1  1,25"],
+      ["fr", "11111,25", "11 111,250"],
+      ["fr", "1  1  1  1  1,25", "1  1  1  1  1,25"],
 
-        ["pt-PT", "1111,2", "1111,200"],
-        ["pt-PT", "111 11,25", "11 111,250"],
+      ["pt-PT", "1111,2", "1111,200"],
+      ["pt-PT", "111 11,25", "11 111,250"],
 
-        ["no-NO", "1 1 11,21", "1 111,210"],
-        ["no-No", "111 1 1,2", "11 111,250"],
-        ["no-NO", "1  1  1  1  1,25", "1  1  1  1  1,25"],
-      ];
+      ["no-NO", "1 1 11,21", "1 111,210"],
+      ["no-No", "111 1 1,2", "11 111,250"],
+      ["no-NO", "1  1  1  1  1,25", "1  1  1  1  1,25"],
+    ];
 
-      input.map((item) => {
+    it.each(inputLocale)(
+      "should use %s locale and %s input value to produce %s output value",
+      (locale, inputValue, outputValue) => {
         // and mount the story using @cypress/react library
-        mount(<Decimal locale={item[0]} precision={3} />);
+        mount(<Decimal locale={locale} precision={3} />);
 
         // then run our tests
         cy.get(decimalComponent)
-          .type(item[1])
+          .type(inputValue)
           .find("input")
           .blur({ force: true });
         cy.get(decimalComponent)
@@ -145,12 +148,12 @@ context("Tests for Decimal component", () => {
             for (let number = 0; number < $el.length; number++) {
               expect(
                 $el.replace(/(\s)|(&nbsp;)|(\u00a0)/g, " ").charCodeAt(number)
-              ).to.equals(input.charCodeAt(number));
+              ).to.equals(outputValue.charCodeAt(number));
             }
           });
         return this;
-      });
-    });
+      }
+    );
 
     it("should render Decimal with readOnly prop", () => {
       // and mount the story using @cypress/react library
@@ -173,45 +176,49 @@ context("Tests for Decimal component", () => {
   describe("check Decimal input", () => {
     const testData = ["mp150ú¿¡üßä", "!@#$%^*()_+-=~[];:.,?{}&\"'<>"];
 
-    it("check label renders proper specific values", () => {
-      // and mount the story using @cypress/react library
-      testData.forEach((element) => {
-        mount(<Decimal fieldHelp={element} />);
+    it.each(testData)(
+      "check label renders properly with %s as specific value",
+      (specificValue) => {
+        // and mount the story using @cypress/react library
+        mount(<Decimal fieldHelp={specificValue} />);
 
         // then run our tests
-        fieldHelpPreview().should("have.text", element);
-      });
-    });
+        fieldHelpPreview().should("have.text", specificValue);
+      }
+    );
 
-    it("check fieldHelp renders proper specific values", () => {
-      // and mount the story using @cypress/react library
-      testData.forEach((element) => {
-        mount(<Decimal label={element} />);
+    it.each(testData)(
+      "check fieldHelp renders properly with %s specific value",
+      (specificValue) => {
+        // and mount the story using @cypress/react library
+        mount(<Decimal label={specificValue} />);
 
         // then run our tests
-        getDataElementByValue("label").should("have.text", element);
-      });
-    });
+        getDataElementByValue("label").should("have.text", specificValue);
+      }
+    );
 
-    it("check tooltip renders proper specific values", () => {
-      // and mount the story using @cypress/react library
-      testData.forEach((element) => {
-        mount(<Decimal label="Label" labelHelp={element} />);
+    it.each(testData)(
+      "check tooltip renders properly with %s specific values",
+      (specificValue) => {
+        // and mount the story using @cypress/react library
+        mount(<Decimal label="Label" labelHelp={specificValue} />);
 
         // then run our tests
         getDataElementByValue("question").trigger("mouseover");
-        tooltipPreview().should("have.text", element);
-      });
-    });
+        tooltipPreview().should("have.text", specificValue);
+      }
+    );
 
-    it("check Decimal component accepts specific values", () => {
-      // and mount the story using @cypress/react library
-      mount(<Decimal />);
+    it.each(testData)(
+      "check Decimal component accepts %s as specific value",
+      (specificValue) => {
+        // and mount the story using @cypress/react library
+        mount(<Decimal />);
 
-      testData.forEach((element) => {
         // then run our tests
         cy.get(decimalComponent)
-          .type(element)
+          .type(specificValue)
           .find("input")
           .blur({ force: true });
         cy.get(decimalComponent)
@@ -220,11 +227,11 @@ context("Tests for Decimal component", () => {
             for (let number = 0; number < $el.length; number++) {
               expect(
                 $el.replace(/(\s)|(&nbsp;)|(\u00a0)/g, " ").charCodeAt(number)
-              ).to.equals(element.charCodeAt(number));
+              ).to.equals(specificValue.charCodeAt(number));
             }
           });
-      });
-    });
+      }
+    );
 
     it("check Decimal component accepts white spaces", () => {
       // and mount the story using @cypress/react library
@@ -239,18 +246,18 @@ context("Tests for Decimal component", () => {
   });
 
   describe("check events for Decimal component", () => {
-    it("should return onChange event", () => {
+    const inputValue = "123";
+    const iterable = [
+      ["1", "1.00"],
+      ["12", "12.00"],
+      ["123", "123.00"],
+    ];
+
+    it("should return onChange event for %s value", () => {
       // use cy.stub for mock the onChange callback
-      const callback = cy.stub().as("eventCallback");
+      const callback = cy.stub();
       // and mount the story using @cypress/react library
       mount(<Decimal onChange={callback} />);
-
-      const inputValue = "123";
-      const iterable = [
-        ["1", "1.00"],
-        ["12", "12.00"],
-        ["123", "123.00"],
-      ];
 
       // then run our tests
       cy.get(decimalComponent)
@@ -258,7 +265,6 @@ context("Tests for Decimal component", () => {
         .find("input")
         .blur({ force: true })
 
-        // first approach with .then
         .then(() => {
           // eslint-disable-next-line no-unused-expressions
           expect(callback).to.have.been.calledThrice;
@@ -280,8 +286,6 @@ context("Tests for Decimal component", () => {
       // and mount the story using @cypress/react library
       mount(<Decimal onBlur={callback} />);
 
-      const inputValue = "test";
-
       // then run our tests
       cy.get(decimalComponent)
         .type(inputValue)
@@ -295,7 +299,7 @@ context("Tests for Decimal component", () => {
           ).to.equals(inputValue);
           expect(
             callback.getCalls()[0].args[0].target.value.formattedValue
-          ).to.equals(inputValue);
+          ).to.equals("123.00");
         });
     });
   });
