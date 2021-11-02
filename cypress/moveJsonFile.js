36ed09b3d3ee6d8373924171934180b9386e0e4c
Ihor Dluhosh <idluhosh@pgs-soft.com>
2021-11-02 13:53:45 +0100

    test: convert bdd to it

--- cypress/moveJsonFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs-extra");
-
-const oldPathFile = "./storybook-static/stories.json";
-const newPathFile = "./cypress/fixtures/stories/stories.json";
-
-fs.renameSync(oldPathFile, newPathFile);
