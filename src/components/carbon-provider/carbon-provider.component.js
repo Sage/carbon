de42a45fb36e0637d84400010036bc7f639b95e9
Ed Leeks <edward.leeks@sage.com>
2021-11-11 13:53:23 +0000

    feat(carbon-provider): add support for passing new validation flag via context

--- src/components/carbon-provider/carbon-provider.component.js
+++ src/components/carbon-provider/carbon-provider.component.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import PropTypes from "prop-types";
 import { ThemeProvider } from "styled-components";
 
 import mintTheme from "../../style/themes/mint";
 import CarbonScopedTokensProvider from "../../style/design-tokens/carbon-scoped-tokens-provider";
 
-const CarbonProvider = ({ children, theme = mintTheme }) => (
-  <ThemeProvider theme={theme}>
-    <CarbonScopedTokensProvider>{children}</CarbonScopedTokensProvider>
-  </ThemeProvider>
-);
+export const NewValidationContext = createContext({
+  validationRedesignOptIn: false,
+});
+
+const CarbonProvider = ({
+  children,
+  validationRedesignOptIn,
+  theme = mintTheme,
+}) => {
+  const { validationRedesignOptIn: parentValidationRedesignOptIn } = useContext(
+    NewValidationContext
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CarbonScopedTokensProvider>
+        <NewValidationContext.Provider
+          value={{
+            validationRedesignOptIn:
+              parentValidationRedesignOptIn || validationRedesignOptIn,
+          }}
+        >
+          {children}
+        </NewValidationContext.Provider>
+      </CarbonScopedTokensProvider>
+    </ThemeProvider>
+  );
+};
 
 CarbonProvider.propTypes = {
   children: PropTypes.node.isRequired,
   theme: PropTypes.object,
+  validationRedesignOptIn: PropTypes.bool,
 };
 
 export default CarbonProvider;
