d9c8eda6a2bf3aca1a33278f23ab26444d5faa7c
Ed Leeks <edward.leeks@sage.com>
2021-11-11 13:53:23 +0000

    feat(carbon-provider): add support for passing new validation flag via context

--- src/components/carbon-provider/carbon-provider.d.ts
+++ src/components/carbon-provider/carbon-provider.d.ts
@@ -5,6 +5,7 @@ import { ThemeObject } from "../../style/themes/base";
 export interface CarbonProviderProps {
   theme?: ThemeObject;
   children: React.ReactNode;
+  validationRedesignOptIn?: boolean;
 }
 
 declare function CarbonProvider(props: CarbonProviderProps): JSX.Element;
