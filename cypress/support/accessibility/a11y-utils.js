0f2fdec78961dbeabdd00987779ab0f6b0f54ac4
Jamie Thompson <jamie.thompson@sage.com>
2021-11-12 16:26:35 +0000

    test(a11y): run all test suites

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -12,11 +12,11 @@ const A11YOptions = {
   runOnly: {
     type: "tag",
     values: [
-      // "wcag2a", // WCAG 2.0 & WCAG 2.1 Level A
-      // "wcag2aa", // WCAG 2.0 & WCAG 2.1 Level AA
+      "wcag2a", // WCAG 2.0 & WCAG 2.1 Level A
+      "wcag2aa", // WCAG 2.0 & WCAG 2.1 Level AA
       "wcag21a", // WCAG 2.1 Level A
       "wcag21aa", // WCAG 2.1 Level AA
-      // "best-practice", // Best practices endorsed by Deque
+      "best-practice", // Best practices endorsed by Deque
     ],
   },
 };
