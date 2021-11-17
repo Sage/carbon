a242e976fd8c244fa1328f4b8ae44fa23ef16877
Ed Leeks <edward.leeks@sage.com>
2021-11-17 16:39:34 +0000

    feat(icon-button): add TooltipProvider and event handles to toggle Tooltip if child has one
    Integrates `TooltipProvider` into `IconButton` and adds event handles to support showing/ hiding
    tooltip when button has focus/ mouseenter/ blur/ mouseleave event
    
    fix #4542

--- src/components/icon-button/icon-button.component.js
+++ src/components/icon-button/icon-button.component.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styledSystemPropTypes from "@styled-system/prop-types";
 import Events from "../../__internal__/utils/helpers/events";
 import StyledIconButton from "./icon-button.style";
 import Icon from "../icon";
 import { filterStyledSystemMarginProps } from "../../style/utils";
+import { TooltipProvider } from "../../__internal__/tooltip-provider";
 
 const marginPropTypes = filterStyledSystemMarginProps(
   styledSystemPropTypes.space
 );
 
 const IconButton = React.forwardRef(
-  (
-    { "aria-label": ariaLabel, onAction, children, ...rest },
-
-    ref
-  ) => {
+  ({ "aria-label": ariaLabel, onAction, children, disabled, ...rest }, ref) => {
     const marginProps = filterStyledSystemMarginProps(rest);
+    const hasIconWithTooltip = !!React.Children.toArray(children).find(
+      (child) => child?.props?.tooltipMessage
+    );
+    const [tooltipVisible, setTooltipVisible] = useState(undefined);
+
     const onKeyDown = (e) => {
       if (Events.isEnterKey(e) || Events.isSpaceKey(e)) {
         e.preventDefault();
@@ -30,6 +32,11 @@ const IconButton = React.forwardRef(
       onAction(e);
     };
 
+    const handleTooltipVisibility = (ev, callbackName) => {
+      setTooltipVisible(["onFocus", "onMouseEnter"].includes(callbackName));
+      if (rest[callbackName]) rest[callbackName](ev);
+    };
+
     return (
       <StyledIconButton
         {...rest}
@@ -37,9 +44,23 @@ const IconButton = React.forwardRef(
         onKeyDown={onKeyDown}
         onClick={handleOnAction}
         ref={ref}
+        disabled={disabled}
+        {...(!disabled &&
+          hasIconWithTooltip && {
+            onFocus: (ev) => handleTooltipVisibility(ev, "onFocus"),
+            onBlur: (ev) => handleTooltipVisibility(ev, "onBlur"),
+            onMouseEnter: (ev) => handleTooltipVisibility(ev, "onMouseEnter"),
+            onMouseLeave: (ev) => handleTooltipVisibility(ev, "onMouseLeave"),
+          })}
         {...marginProps}
       >
-        {children}
+        <TooltipProvider
+          disabled={disabled}
+          tooltipVisible={tooltipVisible}
+          focusable={false}
+        >
+          {children}
+        </TooltipProvider>
       </StyledIconButton>
     );
   }
@@ -55,6 +76,8 @@ IconButton.propTypes = {
   onAction: PropTypes.func.isRequired,
   /** Prop to specify the aria-label of the icon-button component */
   "aria-label": PropTypes.string,
+  /** Set the button to disabled */
+  disabled: PropTypes.bool,
 };
 
 export default IconButton;
