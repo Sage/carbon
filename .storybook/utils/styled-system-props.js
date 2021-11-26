e69340055e8a519418d69f6c0e587b478b971393
Sam Johnson <sam.johnson@sage.com>
2021-11-26 12:08:24 +0000

    docs: replace deprecated props api with argstable

--- .storybook/utils/styled-system-props.js
+++ .storybook/utils/styled-system-props.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ArgsTable } from "@storybook/components";
-import { Props } from "@storybook/addon-docs";
+import { ArgsTable as Props } from "@storybook/addon-docs";
 
 const generateStyledSystemMarginProps = (defaults) => {
   return [
