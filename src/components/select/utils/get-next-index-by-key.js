f65f3ff2fbbf7b2b6dbc9ed0fd986ed94c515ea6
Robin Zigmond <robin.zigmond@sage.com>
2022-04-07 11:32:41 +0100

    refactor(get-next-index-by-key): use switch instead of if-else

--- src/components/select/utils/get-next-index-by-key.js
+++ src/components/select/utils/get-next-index-by-key.js
@@ -7,26 +7,33 @@ export default function getNextIndexByKey(
   const isNoOptionSelected = currentIndex === -1;
   let newIndex = currentIndex;
 
-  if (key === "Home") {
-    newIndex = 0;
-  } else if (key === "End") {
-    newIndex = lastIndex;
-  } else if (key === "ArrowDown") {
-    if (isLoading && currentIndex === lastIndex) {
-      return lastIndex;
-    }
-
-    if (currentIndex === lastIndex || isNoOptionSelected) {
+  switch (key) {
+    case "Home":
       newIndex = 0;
-    } else {
-      newIndex = currentIndex + 1;
-    }
-  } else if (key === "ArrowUp") {
-    if (currentIndex === 0 || isNoOptionSelected) {
+      break;
+    case "End":
       newIndex = lastIndex;
-    } else {
-      newIndex = currentIndex - 1;
-    }
+      break;
+    case "ArrowDown":
+      if (isLoading && currentIndex === lastIndex) {
+        return lastIndex;
+      }
+
+      if (currentIndex === lastIndex || isNoOptionSelected) {
+        newIndex = 0;
+      } else {
+        newIndex = currentIndex + 1;
+      }
+      break;
+    case "ArrowUp":
+      if (currentIndex === 0 || isNoOptionSelected) {
+        newIndex = lastIndex;
+      } else {
+        newIndex = currentIndex - 1;
+      }
+      break;
+    default:
+      break;
   }
 
   return newIndex;
