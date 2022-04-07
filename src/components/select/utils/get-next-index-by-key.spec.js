9ce68b7e59223d2301b7a448dc92aaee16bbf406
Robin Zigmond <robin.zigmond@sage.com>
2022-04-07 11:54:57 +0100

    test(get-next-index-by-key): fix tests to use length-1 as last index

--- src/components/select/utils/get-next-index-by-key.spec.js
+++ src/components/select/utils/get-next-index-by-key.spec.js
@@ -7,46 +7,47 @@ describe("getNextIndexByKey", () => {
   const downKey = "ArrowDown";
   const aKey = "a";
   const list = ["a", "b", "c", "d", "e", "f"];
+  const lastIndex = list.length - 1;
 
   describe("when the homeKey is passed as the first attribute", () => {
     it("then the first index should be returned", () => {
-      expect(getNextIndexByKey(homeKey, 2, list.length)).toBe(0);
+      expect(getNextIndexByKey(homeKey, 2, lastIndex)).toBe(0);
     });
   });
 
   describe("when the endKey is passed as the first attribute", () => {
     it("then the last index should be returned", () => {
-      expect(getNextIndexByKey(endKey, 0, list.length)).toBe(list.length);
+      expect(getNextIndexByKey(endKey, 0, lastIndex)).toBe(lastIndex);
     });
   });
 
   describe("when the upKey is passed as the first attribute", () => {
     it("then the last index should be returned", () => {
-      expect(getNextIndexByKey(upKey, 0, list.length)).toBe(list.length);
+      expect(getNextIndexByKey(upKey, 0, lastIndex)).toBe(lastIndex);
     });
   });
 
   describe("when the upKey is passed as the first attribute", () => {
     it("then the preceding index should be returned", () => {
-      expect(getNextIndexByKey(upKey, 2, list.length)).toBe(1);
+      expect(getNextIndexByKey(upKey, 2, lastIndex)).toBe(1);
     });
   });
 
   describe("when the downKey is passed as the first attribute", () => {
     it("then the following index should be returned", () => {
-      expect(getNextIndexByKey(downKey, 0, list.length)).toBe(1);
+      expect(getNextIndexByKey(downKey, 0, lastIndex)).toBe(1);
     });
   });
 
   describe("when the downKey is passed as the first attribute with the last index as the second", () => {
     it("then the first index should be returned", () => {
-      expect(getNextIndexByKey(downKey, list.length, list.length)).toBe(0);
+      expect(getNextIndexByKey(downKey, lastIndex, lastIndex)).toBe(0);
     });
 
     describe("and the last argument is set to true", () => {
       it("then the last index should be returned", () => {
-        expect(getNextIndexByKey(downKey, list.length, list.length, true)).toBe(
-          list.length
+        expect(getNextIndexByKey(downKey, lastIndex, lastIndex, true)).toBe(
+          lastIndex
         );
       });
     });
@@ -54,7 +55,7 @@ describe("getNextIndexByKey", () => {
 
   describe("when a non valid key is passed as the first attribute", () => {
     it("then the index passed in the second attribute should be returned", () => {
-      expect(getNextIndexByKey(aKey, 1, list.length)).toBe(1);
+      expect(getNextIndexByKey(aKey, 1, lastIndex)).toBe(1);
     });
   });
 });
