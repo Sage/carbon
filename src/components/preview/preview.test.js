2668f475ff50231de753433b2e13ba9455f6ed1c
divya jindel <divya.jindel@sage.com>
2022-05-06 11:21:37 +0100

    test(preview): add cypress refactored tests

---
+++ src/components/preview/preview.test.js
@@ -0,0 +1,62 @@
+import * as React from "react";
+import Preview from "./preview.component";
+import CypressMountWithProviders from "../../../cypress/support/component-helper/cypress-mount";
+import { cyRoot } from "../../../cypress/locators/index";
+import {
+  previewComponent,
+  lineComponent,
+} from "../../../cypress/locators/preview/index";
+
+const testData = ["mp150ú¿¡üßä", "!@#$%^*()_+-=~[];:.,?{}&\"'<>"];
+const pixelsData = [256, 275, 300];
+
+const PreviewComponent = ({ ...props }) => {
+  return <Preview loading {...props} />;
+};
+
+context("Tests for Preview component", () => {
+  describe("should check Preview component properties", () => {
+    it.each(pixelsData)(
+      "should check height as %spx for Preview component",
+      (height) => {
+        CypressMountWithProviders(<PreviewComponent height={height} />);
+        previewComponent().should("have.css", "height", `${height}px`);
+      }
+    );
+
+    it.each(pixelsData)(
+      "should check width as %spx for Preview component",
+      (width) => {
+        CypressMountWithProviders(<PreviewComponent width={width} />);
+        previewComponent().should("have.css", "width", `${width}px`);
+      }
+    );
+
+    it.each(testData)(
+      "should check children as %s for Preview component",
+      (children) => {
+        CypressMountWithProviders(<Preview>{children}</Preview>);
+        cyRoot().should("have.text", children);
+      }
+    );
+
+    it.each([
+      [true, "be.visible"],
+      [false, "not.exist"],
+    ])(
+      "should check when loading is %s for Preview component",
+      (bool, state) => {
+        CypressMountWithProviders(<PreviewComponent loading={bool} />);
+        previewComponent().should(state);
+      }
+    );
+
+    it.each([5, 6, 8, 10])(
+      "should check %s loading lines for Preview component",
+      (line) => {
+        CypressMountWithProviders(<PreviewComponent lines={line} />);
+        lineComponent().should("have.length", line);
+      }
+    );
+  });
+});
