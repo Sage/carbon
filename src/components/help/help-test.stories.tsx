bee66c303ebaf79bedb44980fd3a96bddf4431dc
edleeks87 <edward.leeks@sage.com>
2022-06-10 16:34:41 +0100

    refactor(help): refactor files to typescript

--- src/components/help/help-test.stories.tsx
+++ src/components/help/help-test.stories.tsx
@@ -1,19 +1,21 @@
-import { Meta, Story, Canvas } from "@storybook/addon-docs";
+import React from "react";
+import { IconType } from "../icon";
+import { TooltipPositions } from "../tooltip/tooltip.config";
 
 import specialCharacters from "../../__internal__/utils/argTypes/specialCharacters";
 import { HELP_POSITIONS } from "./help.config";
 import { ICONS } from "../icon/icon-config";
 import Help from "./help.component";
 
-<Meta
-  title="Help/Test"
-  parameters={{
+export default {
+  title: "Help/Test",
+  parameters: {
     info: { disable: true },
     chromatic: {
       disable: true,
     },
-  }}
-  argTypes={{
+  },
+  argTypes: {
     tooltipFlipOverrides: {
       options: HELP_POSITIONS,
       control: {
@@ -34,17 +36,27 @@ import Help from "./help.component";
     },
     childrenSpecialCharacters: specialCharacters,
     hrefSpecialCharacters: specialCharacters,
-  }}
-/>
+  },
+};
 
-export const HelpStory = ({
+interface HelpStoryProps {
+  children?: string;
+  childrenSpecialCharacters: string;
+  hrefSpecialCharacters: string;
+  href?: string;
+  tooltipFlipOverrides?: TooltipPositions;
+  tooltipPosition?: TooltipPositions;
+  type?: IconType;
+}
+
+export const Default = ({
   children,
   childrenSpecialCharacters,
   href,
   hrefSpecialCharacters,
   tooltipFlipOverrides,
   ...args
-}) => {
+}: HelpStoryProps) => {
   const flipOverrides = tooltipFlipOverrides
     ? [tooltipFlipOverrides]
     : undefined;
@@ -52,9 +64,10 @@ export const HelpStory = ({
     <div style={{ margin: "200px" }}>
       <Help
         tooltipFlipOverrides={flipOverrides}
+        // eslint-disable-next-line react/no-children-prop
         children={children || childrenSpecialCharacters}
         href={href || hrefSpecialCharacters}
-        ariaLabel={children}
+        ariaLabel={children as string}
         value={children}
         {...args}
       />
@@ -62,25 +75,17 @@ export const HelpStory = ({
   );
 };
 
-# Help
-
-### Default
-
-<Canvas>
-  <Story
-    name="default"
-    args={{
-      children: "This is help text",
-      tooltipPosition: Help.defaultProps.tooltipPosition,
-      href: "http://www.sage.com",
-      type: "help",
-      tooltipBgColor: "",
-      tooltipFontColor: "",
-      tooltipFlipOverrides: "left",
-      childrenSpecialCharacters: undefined,
-      hrefSpecialCharacters: undefined,
-    }}
-  >
-    {HelpStory.bind({})}
-  </Story>
-</Canvas>
+Default.story = {
+  name: "default",
+  args: {
+    children: "This is help text",
+    tooltipPosition: "top",
+    href: "http://www.sage.com",
+    type: "help",
+    tooltipBgColor: "",
+    tooltipFontColor: "",
+    tooltipFlipOverrides: "left",
+    childrenSpecialCharacters: undefined,
+    hrefSpecialCharacters: undefined,
+  },
+};
