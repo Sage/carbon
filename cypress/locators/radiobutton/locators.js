b36c6d98fcf321ab6e88d199ff7deec8e2da85f7
DianeGit22 <diane.fleming@sage.com>
2022-07-08 16:48:30 +0100

    test: radiobutton
    Update src/components/radio-button/radiobutton.test.js
    
    Co-authored-by: Igor Dlugosh <dlugosh.igor@gmail.com>

---
+++ cypress/locators/radiobutton/locators.js
@@ -0,0 +1,4 @@
+// component preview locators
+export const RADIOBUTTONGROUP = '[data-component="radiogroup"]';
+export const RADIOBUTTONCOMPONENT = '[data-component="radio-button"]';
+export const RADIOBUTTONROLE = '[role="radio"]';
