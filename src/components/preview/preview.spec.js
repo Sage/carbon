9e781ef0f68f92ef1aaeaf0d01ef997d41c5e4a5
grabkowski <jakub.grabkowski@sage.com>
2022-09-09 18:19:34 +0200

    refactor(preview): rewrite unit tests to replace snapshots

--- src/components/preview/preview.spec.js
+++ src/components/preview/preview.spec.js
@@ -1,52 +1,78 @@
 import React from "react";
-import TestRenderer from "react-test-renderer";
+import { mount } from "enzyme";
 import Preview from "./preview.component";
-import { testStyledSystemMargin } from "../../__spec_helper__/test-utils";
+import {
+  assertStyleMatch,
+  testStyledSystemMargin,
+} from "../../__spec_helper__/test-utils";
+import { StyledPreviewPlaceholder } from "./preview.style";
 
-const render = (children, props) => {
-  return TestRenderer.create(<Preview {...props}>{children}</Preview>);
+const render = (props) => {
+  return mount(<Preview {...props} />);
 };
 
 describe("Preview", () => {
-  describe("when given children", () => {
-    it("will render the children", () => {
-      const wrapper = render("This is some text");
-      expect(wrapper).toMatchSnapshot();
+  describe("when given children and no loading prop", () => {
+    const childrenContent = "This is some text";
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = render({ children: childrenContent });
+    });
+
+    it("will render the text passed as children", () => {
+      expect(wrapper.text()).toBe(childrenContent);
+    });
+
+    it("will not render a placeholder", () => {
+      expect(wrapper.find(StyledPreviewPlaceholder).exists()).toBe(false);
     });
   });
 
-  describe("when given no children", () => {
-    it("will render the placeholder", () => {
-      const wrapper = render();
-      expect(wrapper).toMatchSnapshot();
+  describe("when given children and the 'loading' prop set to true", () => {
+    it("will render a placeholder", () => {
+      const childrenContent = "This is some text";
+      const wrapper = render({ children: childrenContent, loading: true });
+
+      expect(wrapper.find(StyledPreviewPlaceholder).exists()).toBe(true);
     });
   });
 
   describe("when given no children and a falsy loading prop", () => {
-    it("will render nothing", () => {
-      const wrapper = render(null, { loading: false });
-      expect(wrapper).toMatchSnapshot();
+    it("will not render a placeholder", () => {
+      const wrapper = render({ loading: false });
+
+      expect(wrapper.find(StyledPreviewPlaceholder).exists()).toBe(false);
     });
   });
 
-  describe("when given children but a truthy loading prop", () => {
+  describe("when given no children and no loading prop", () => {
     it("will render the placeholder", () => {
-      const wrapper = render("This is some text", { loading: true });
-      expect(wrapper).toMatchSnapshot();
+      const wrapper = render();
+
+      expect(wrapper.find(StyledPreviewPlaceholder).exists()).toBe(true);
     });
   });
 
-  describe("when multi line", () => {
-    it("renders multi previews, the last being 80% width", () => {
-      const wrapper = render(null, { lines: 3 });
-      expect(wrapper).toMatchSnapshot();
+  describe("when the 'lines' prop is set", () => {
+    it("renders the number of placeholders equal to that prop value", () => {
+      const wrapper = render({ lines: 3 });
+
+      expect(wrapper.find(StyledPreviewPlaceholder).length).toBe(3);
     });
   });
 
   describe("when given a width and height", () => {
-    it("renders with the given width and height", () => {
-      const wrapper = render(null, { width: "10px", height: "20px" });
-      expect(wrapper).toMatchSnapshot();
+    it("renders a placeholder with that width and height", () => {
+      const customSizes = { width: "10px", height: "20px" };
+      const wrapper = render(customSizes);
+
+      assertStyleMatch(
+        {
+          ...customSizes,
+        },
+        wrapper.find(StyledPreviewPlaceholder)
+      );
     });
   });
 
