8b68af3d5fcadeade9bca42440077f2065956e9d
Ihor Dluhosh <idlugosh@pgs-soft.com>
2022-10-07 10:34:34 +0200

    test(navigation-bar): refactor for cypress tests

--- cypress/support/step-definitions/navigation-bar-steps.js
+++
@@ -1,6 +0,0 @@
-import { Then } from "@badeball/cypress-cucumber-preprocessor";
-import navigationBar from "../../locators/navigation-bar";
-
-Then("Navigation Bar children on preview is set to {word}", (text) => {
-  navigationBar().should("have.text", text);
-});
