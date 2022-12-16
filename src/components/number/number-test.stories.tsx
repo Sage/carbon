3147f2d6cdefce479faa5b4c7e3ffe79f6b0d6b5
grabkowski <jakub.grabkowski@sage.com>
2022-12-16 14:05:57 +0100

    docs(number): move validation stories to test folder

--- src/components/number/number-test.stories.tsx
+++ src/components/number/number-test.stories.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { action } from "@storybook/addon-actions";
 import { ComponentMeta } from "@storybook/react";
 
-import CarbonProvider from "../carbon-provider/carbon-provider.component";
 import Number from "./number.component";
 import {
   CommonTextboxArgs,
@@ -57,43 +56,6 @@ export const Default = ({
 
 Default.storyName = "default";
 Default.args = {
-  onChangeDeferredEnabled: false,
-  onKeyDownEnabled: false,
-  deferTimeout: undefined,
-  ...getCommonTextboxArgs(),
-};
-
-export const NewValidationStory = ({
-  onKeyDownEnabled,
-  onChangeDeferredEnabled,
-  ...args
-}: CommonTextboxArgs & {
-  onKeyDownEnabled: boolean;
-  onChangeDeferredEnabled: boolean;
-}) => {
-  const [state, setState] = useState("");
-  const setValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    action("onChange")(event);
-    setState(event.target.value);
-  };
-  return (
-    <CarbonProvider validationRedesignOptIn>
-      <Number
-        value={state}
-        onChange={setValue}
-        onKeyDown={onKeyDownEnabled ? action("onKeyDown") : undefined}
-        onChangeDeferred={
-          onChangeDeferredEnabled ? action("onChangeDeferred") : undefined
-        }
-        m={2}
-        {...getCommonTextboxArgsWithSpecialCaracters(args)}
-      />
-    </CarbonProvider>
-  );
-};
-
-NewValidationStory.storyName = "new validation";
-NewValidationStory.args = {
   onChangeDeferredEnabled: false,
   onKeyDownEnabled: false,
   deferTimeout: undefined,
---
+++ src/components/number/number-validation-test.stories.tsx
@@ -0,0 +1,184 @@
+import React from "react";
+import { ComponentMeta } from "@storybook/react";
+
+import CarbonProvider from "../carbon-provider/carbon-provider.component";
+import Number, { NumberProps } from "./number.component";
+import {
+  CommonTextboxArgs,
+  commonTextboxArgTypes,
+  getCommonTextboxArgs,
+} from "../textbox/textbox-test.stories";
+
+export default {
+  title: "Number Input/Test",
+  parameters: {
+    info: { disable: true },
+  },
+  argTypes: {
+    deferTimeout: {
+      control: {
+        type: "number",
+      },
+    },
+    ...commonTextboxArgTypes(),
+  },
+} as ComponentMeta<typeof Number>;
+
+export const StringValidation = () => (
+  <>
+    {["error", "warning", "info"].map((validationType) => (
+      <div key={`${validationType}-string-component`}>
+        <Number
+          label="Number"
+          value="123456"
+          {...{ [validationType]: "Message" }}
+          mb={2}
+        />
+        <Number
+          label="Number - readOnly"
+          value="123456"
+          readOnly
+          {...{ [validationType]: "Message" }}
+          mb={2}
+        />
+      </div>
+    ))}
+  </>
+);
+
+StringValidation.storyName = "string validation";
+
+export const StringValidationWithTooltipPositionOverriden = () => (
+  <>
+    {["error", "warning", "info"].map((validationType) => (
+      <div key={`${validationType}-string-component`}>
+        <Number
+          label="Number"
+          value="123456"
+          {...{ [validationType]: "Message" }}
+          mb={2}
+          tooltipPosition="bottom"
+        />
+      </div>
+    ))}
+  </>
+);
+
+StringValidationWithTooltipPositionOverriden.storyName =
+  "string validation with tooltipPosition overriden";
+
+export const StringValidationOnLabel = () => (
+  <>
+    {["error", "warning", "info"].map((validationType) => (
+      <div key={`${validationType}-string-label`}>
+        <Number
+          label="Number"
+          value="123456"
+          validationOnLabel
+          {...{ [validationType]: "Message" }}
+          mb={2}
+        />
+        <Number
+          label="Number - readOnly"
+          value="123456"
+          validationOnLabel
+          readOnly
+          {...{ [validationType]: "Message" }}
+          mb={2}
+        />
+      </div>
+    ))}
+  </>
+);
+
+StringValidationOnLabel.storyName = "string validation on label";
+
+export const StringValidationOnLabelWithTooltipPositionOverriden = () => (
+  <>
+    {["error", "warning", "info"].map((validationType) => (
+      <div key={`${validationType}-string-label`}>
+        <Number
+          label="Number"
+          value="123456"
+          validationOnLabel
+          {...{ [validationType]: "Message" }}
+          mb={2}
+          tooltipPosition="top"
+        />
+      </div>
+    ))}
+  </>
+);
+
+StringValidationOnLabel.storyName =
+  "string validation on label with tooltipPosition overriden";
+
+export const BooleanValidation = () => (
+  <>
+    {["error", "warning", "info"].map((validationType) => (
+      <div key={`${validationType}-boolean-component`}>
+        <Number
+          label="Number"
+          value="123456"
+          {...{ [validationType]: true }}
+          mb={2}
+        />
+        <Number
+          label="Number - readOnly"
+          value="123456"
+          readOnly
+          {...{ [validationType]: true }}
+          mb={2}
+        />
+      </div>
+    ))}
+  </>
+);
+
+BooleanValidation.storyName = "boolean validation";
+
+export const NewValidation = ({
+  onKeyDownEnabled,
+  onChangeDeferredEnabled,
+  ...args
+}: CommonTextboxArgs & {
+  onKeyDownEnabled: boolean;
+  onChangeDeferredEnabled: boolean;
+}) => {
+  return (
+    <CarbonProvider validationRedesignOptIn>
+      {["error", "warning"].map((validationType) =>
+        ["small", "medium", "large"].map((size) => (
+          <div
+            style={{ width: "296px" }}
+            key={`${size}-${validationType}-string-component`}
+          >
+            <Number
+              label={`${size} - ${validationType}`}
+              value="123456"
+              size={size as NumberProps["size"]}
+              {...{ [validationType]: "Message" }}
+              m={4}
+            />
+            <Number
+              label={`readOnly - ${size} - ${validationType}`}
+              value="123456"
+              readOnly
+              size={size as NumberProps["size"]}
+              {...{ [validationType]: "Message" }}
+              m={4}
+            />
+          </div>
+        ))
+      )}
+    </CarbonProvider>
+  );
+};
+
+NewValidation.storyName = "new validation";
+NewValidation.args = {
+  onChangeDeferredEnabled: false,
+  onKeyDownEnabled: false,
+  deferTimeout: undefined,
+  ...getCommonTextboxArgs(),
+};
