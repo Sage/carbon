fdaf54066c62c7b6b01295417f94d00c7cab68d1
Robin Zigmond <robin.zigmond@sage.com>
2022-12-20 13:24:59 +0000

    feat(date): use React.forwardRef and deprecate inputRef prop
    allow a `ref` to be passed directly to the component, removing the need for the `inputRef` prop
    which is now deprecated
    
    re #5564

--- src/components/date/date.component.js
+++ src/components/date/date.component.js
@@ -31,353 +31,384 @@ import Textbox from "../textbox";
 import DatePicker from "./__internal__/date-picker";
 import DateRangeContext from "../date-range/date-range.context";
 import useClickAwayListener from "../../hooks/__internal__/useClickAwayListener";
+import Logger from "../../__internal__/utils/logger";
 
 const marginPropTypes = filterStyledSystemMarginProps(
   styledSystemPropTypes.space
 );
 
-const DateInput = ({
-  adaptiveLabelBreakpoint,
-  allowEmptyValue,
-  autoFocus,
-  "data-component": dataComponent,
-  "data-element": dataElement,
-  "data-role": dataRole,
-  disabled,
-  disablePortal = false,
-  helpAriaLabel,
-  labelInline,
-  minDate,
-  maxDate,
-  onBlur,
-  onChange,
-  onClick,
-  onFocus,
-  onKeyDown,
-  pickerProps = {},
-  readOnly,
-  size = "medium",
-  tooltipPosition,
-  value,
-  ...rest
-}) => {
-  const wrapperRef = useRef();
-  const parentRef = useRef();
-  const inputRef = useRef();
-  const alreadyFocused = useRef(false);
-  const isBlurBlocked = useRef(false);
-  const focusedViaPicker = useRef(false);
-  const l = useLocale();
-  const { dateFnsLocale } = l.date;
-  const { format, formats } = useMemo(() => getFormatData(dateFnsLocale()), [
-    dateFnsLocale,
-  ]);
-  const { inputRefMap, setInputRefMap } = useContext(DateRangeContext);
-  const inputName = dataElement?.split("-")[0];
-  const [open, setOpen] = useState(false);
-  const [selectedDays, setSelectedDays] = useState(
-    checkISOFormatAndLength(value)
-      ? parseISODate(value)
-      : parseDate(format, value)
-  );
-  const isInitialValue = useRef(true);
+let deprecateInputRefWarnTriggered = false;
 
-  const computeInvalidRawValue = (inputValue) =>
-    allowEmptyValue && !inputValue.length ? inputValue : null;
-
-  const buildCustomEvent = (ev) => {
-    const { id, name } = ev.target;
-
-    const [matchedFormat, matchedValue] = findMatchedFormatAndValue(
-      ev.target.value,
-      formats
+const DateInput = React.forwardRef(
+  (
+    {
+      adaptiveLabelBreakpoint,
+      allowEmptyValue,
+      autoFocus,
+      "data-component": dataComponent,
+      "data-element": dataElement,
+      "data-role": dataRole,
+      disabled,
+      disablePortal = false,
+      helpAriaLabel,
+      labelInline,
+      minDate,
+      maxDate,
+      onBlur,
+      onChange,
+      onClick,
+      onFocus,
+      onKeyDown,
+      pickerProps = {},
+      readOnly,
+      size = "medium",
+      tooltipPosition,
+      value,
+      inputRef,
+      ...rest
+    },
+    ref
+  ) => {
+    const wrapperRef = useRef();
+    const parentRef = useRef();
+    const internalInputRef = useRef();
+    const alreadyFocused = useRef(false);
+    const isBlurBlocked = useRef(false);
+    const focusedViaPicker = useRef(false);
+    const l = useLocale();
+    const { dateFnsLocale } = l.date;
+    const { format, formats } = useMemo(() => getFormatData(dateFnsLocale()), [
+      dateFnsLocale,
+    ]);
+    const { inputRefMap, setInputRefMap } = useContext(DateRangeContext);
+    const inputName = dataElement?.split("-")[0];
+    const [open, setOpen] = useState(false);
+    const [selectedDays, setSelectedDays] = useState(
+      checkISOFormatAndLength(value)
+        ? parseISODate(value)
+        : parseDate(format, value)
     );
+    const isInitialValue = useRef(true);
 
-    const formattedValueString =
-      ev.type === "blur"
-        ? formattedValue(format, selectedDays)
-        : ev.target.value;
-    const rawValue = isDateValid(parseDate(matchedFormat, matchedValue))
-      ? formatToISO(...additionalYears(matchedFormat, matchedValue))
-      : computeInvalidRawValue(ev.target.value);
-
-    ev.target = {
-      ...(name && { name }),
-      ...(id && { id }),
-      value: {
-        formattedValue: formattedValueString,
-        rawValue,
-      },
-    };
-
-    return ev;
-  };
-
-  const handleClickAway = () => {
-    if (open) {
-      alreadyFocused.current = true;
-      inputRef.current.focus();
-      isBlurBlocked.current = false;
-      inputRef.current.blur();
-      setOpen(false);
-      alreadyFocused.current = false;
-    }
-  };
-
-  const handleClickInside = useClickAwayListener(handleClickAway, "mousedown");
-
-  const handleChange = (ev) => {
-    isInitialValue.current = false;
-    onChange(buildCustomEvent(ev));
-  };
-
-  const focusInput = () => {
-    focusedViaPicker.current = true;
-    inputRef.current?.focus();
-  };
-
-  const handleDayClick = (day, ev) => {
-    setSelectedDays(day);
-    onChange(
-      buildCustomEvent({
-        ...ev,
-        target: {
-          ...ev.target,
-          value: formattedValue(format, day),
-        },
-      })
-    );
-    focusInput();
-    setOpen(false);
-  };
-
-  const handleBlur = (ev) => {
-    if (disabled || readOnly) {
-      return;
+    if (!deprecateInputRefWarnTriggered && inputRef) {
+      deprecateInputRefWarnTriggered = true;
+      Logger.deprecate(
+        "The `inputRef` prop in `DateInput` component is deprecated and will soon be removed. Please use `ref` instead."
+      );
     }
 
-    let event;
+    const computeInvalidRawValue = (inputValue) =>
+      allowEmptyValue && !inputValue.length ? inputValue : null;
 
-    if (isDateValid(selectedDays)) {
-      event = buildCustomEvent(ev);
-
-      const currentValue = checkISOFormatAndLength(value)
-        ? formattedValue(format, parseISODate(value))
-        : value;
-      const [, matchedValue] = findMatchedFormatAndValue(currentValue, formats);
-
-      if (formattedValue(format, selectedDays) !== matchedValue) {
-        onChange(event);
-      }
-    } else {
+    const buildCustomEvent = (ev) => {
       const { id, name } = ev.target;
+
+      const [matchedFormat, matchedValue] = findMatchedFormatAndValue(
+        ev.target.value,
+        formats
+      );
+
+      const formattedValueString =
+        ev.type === "blur"
+          ? formattedValue(format, selectedDays)
+          : ev.target.value;
+      const rawValue = isDateValid(parseDate(matchedFormat, matchedValue))
+        ? formatToISO(...additionalYears(matchedFormat, matchedValue))
+        : computeInvalidRawValue(ev.target.value);
 
       ev.target = {
         ...(name && { name }),
         ...(id && { id }),
         value: {
-          formattedValue: ev.target.value,
-          rawValue: computeInvalidRawValue(ev.target.value),
+          formattedValue: formattedValueString,
+          rawValue,
         },
       };
 
-      event = ev;
-    }
+      return ev;
+    };
 
-    if (isBlurBlocked.current) {
-      return;
-    }
-
-    if (onBlur) {
-      onBlur(event);
-    }
-  };
-
-  const handleFocus = (ev) => {
-    if (disabled || readOnly) {
-      return;
-    }
-
-    isBlurBlocked.current = false;
-
-    if (!open && !alreadyFocused.current) {
-      setOpen(true);
-    } else {
-      alreadyFocused.current = false;
-    }
-
-    if (onFocus) {
-      onFocus(ev);
-    }
-  };
-
-  const handleKeyDown = (ev) => {
-    if (onKeyDown) {
-      onKeyDown(ev);
-    }
-
-    if (Events.isTabKey(ev)) {
-      setOpen(false);
-      alreadyFocused.current = false;
-    }
-  };
-
-  const handleClick = (ev) => {
-    if (disabled || readOnly) {
-      return;
-    }
-
-    if (onClick) {
-      onClick(ev);
-    }
-  };
-
-  const handleMouseDown = (ev) => {
-    handleClickInside(ev);
-
-    if (disabled || readOnly) {
-      return;
-    }
-
-    if (setInputRefMap) {
-      isBlurBlocked.current = true;
-    }
-
-    if (ev.target.type === "text" && !open) {
-      setOpen(true);
-    } else if (ev.target.type !== "text") {
-      alreadyFocused.current = true;
-      setOpen((prev) => !prev);
-    }
-  };
-
-  const handleIconMouseDown = (e) => {
-    isBlurBlocked.current = true;
-    handleMouseDown(e);
-  };
-
-  const handlePickerMouseDown = (ev) => {
-    isBlurBlocked.current = true;
-    handleClickInside(ev);
-  };
-
-  const assignInput = useCallback(
-    (inputElement) => {
-      inputRef.current = inputElement;
-      parentRef.current = inputElement?.parentElement;
-
-      if (inputRefMap && inputRefMap[inputName]?.setOpen !== setOpen) {
-        setInputRefMap({
-          [inputName]: { isBlurBlocked, setOpen },
-        });
+    const handleClickAway = () => {
+      if (open) {
+        alreadyFocused.current = true;
+        internalInputRef.current.focus();
+        isBlurBlocked.current = false;
+        internalInputRef.current.blur();
+        setOpen(false);
+        alreadyFocused.current = false;
       }
-    },
-    [inputName, inputRefMap, setInputRefMap]
-  );
+    };
 
-  useEffect(() => {
-    const [matchedFormat, matchedValue] = findMatchedFormatAndValue(
-      value,
-      formats
+    const handleClickInside = useClickAwayListener(
+      handleClickAway,
+      "mousedown"
     );
 
-    if (
-      matchedFormat &&
-      matchedValue &&
-      isDateValid(parseDate(matchedFormat, matchedValue))
-    ) {
-      setSelectedDays(
-        parseDate(...additionalYears(matchedFormat, matchedValue))
-      );
-    } else if (checkISOFormatAndLength(value) && isInitialValue.current) {
-      setSelectedDays(parseISODate(value));
-    } else {
-      setSelectedDays(undefined);
-    }
-  }, [value, formats]);
-
-  const computedValue = () => {
-    if (checkISOFormatAndLength(value) && isInitialValue.current) {
-      return formattedValue(format, parseISODate(value));
-    }
-
-    const valueSeparator = getSeparator(value);
-    const formatSeparator = getSeparator(format);
-    const replaceSeparators = () =>
-      value
-        .split("")
-        .map((char) => (char === valueSeparator ? formatSeparator : char))
-        .join("");
-
-    if (
-      isInitialValue.current &&
-      valueSeparator !== formatSeparator &&
-      isDateValid(parseDate(format, replaceSeparators()))
-    ) {
+    const handleChange = (ev) => {
       isInitialValue.current = false;
+      onChange(buildCustomEvent(ev));
+    };
 
+    const focusInput = () => {
+      focusedViaPicker.current = true;
+      internalInputRef.current?.focus();
+    };
+
+    const handleDayClick = (day, ev) => {
+      setSelectedDays(day);
+      onChange(
+        buildCustomEvent({
+          ...ev,
+          target: {
+            ...ev.target,
+            value: formattedValue(format, day),
+          },
+        })
+      );
+      focusInput();
+      setOpen(false);
+    };
+
+    const handleBlur = (ev) => {
+      if (disabled || readOnly) {
+        return;
+      }
+
+      let event;
+
+      if (isDateValid(selectedDays)) {
+        event = buildCustomEvent(ev);
+
+        const currentValue = checkISOFormatAndLength(value)
+          ? formattedValue(format, parseISODate(value))
+          : value;
+        const [, matchedValue] = findMatchedFormatAndValue(
+          currentValue,
+          formats
+        );
+
+        if (formattedValue(format, selectedDays) !== matchedValue) {
+          onChange(event);
+        }
+      } else {
+        const { id, name } = ev.target;
+
+        ev.target = {
+          ...(name && { name }),
+          ...(id && { id }),
+          value: {
+            formattedValue: ev.target.value,
+            rawValue: computeInvalidRawValue(ev.target.value),
+          },
+        };
+
+        event = ev;
+      }
+
+      if (isBlurBlocked.current) {
+        return;
+      }
+
+      if (onBlur) {
+        onBlur(event);
+      }
+    };
+
+    const handleFocus = (ev) => {
+      if (disabled || readOnly) {
+        return;
+      }
+
+      isBlurBlocked.current = false;
+
+      if (!open && !alreadyFocused.current) {
+        setOpen(true);
+      } else {
+        alreadyFocused.current = false;
+      }
+
+      if (onFocus) {
+        onFocus(ev);
+      }
+    };
+
+    const handleKeyDown = (ev) => {
+      if (onKeyDown) {
+        onKeyDown(ev);
+      }
+
+      if (Events.isTabKey(ev)) {
+        setOpen(false);
+        alreadyFocused.current = false;
+      }
+    };
+
+    const handleClick = (ev) => {
+      if (disabled || readOnly) {
+        return;
+      }
+
+      if (onClick) {
+        onClick(ev);
+      }
+    };
+
+    const handleMouseDown = (ev) => {
+      handleClickInside(ev);
+
+      if (disabled || readOnly) {
+        return;
+      }
+
+      if (setInputRefMap) {
+        isBlurBlocked.current = true;
+      }
+
+      if (ev.target.type === "text" && !open) {
+        setOpen(true);
+      } else if (ev.target.type !== "text") {
+        alreadyFocused.current = true;
+        setOpen((prev) => !prev);
+      }
+    };
+
+    const handleIconMouseDown = (e) => {
+      isBlurBlocked.current = true;
+      handleMouseDown(e);
+    };
+
+    const handlePickerMouseDown = (ev) => {
+      isBlurBlocked.current = true;
+      handleClickInside(ev);
+    };
+
+    const assignInput = useCallback(
+      (inputElement) => {
+        internalInputRef.current = inputElement;
+        parentRef.current = inputElement?.parentElement;
+
+        if (ref) {
+          if (typeof ref === "function") {
+            ref(inputElement);
+          } else {
+            ref.current = inputElement;
+          }
+        }
+
+        if (inputRefMap && inputRefMap[inputName]?.setOpen !== setOpen) {
+          setInputRefMap({
+            [inputName]: { isBlurBlocked, setOpen },
+          });
+        }
+      },
+      [inputName, inputRefMap, setInputRefMap, ref]
+    );
+
+    useEffect(() => {
       const [matchedFormat, matchedValue] = findMatchedFormatAndValue(
-        replaceSeparators(),
+        value,
         formats
       );
-      return formattedValue(
-        format,
-        parseDate(...additionalYears(matchedFormat, matchedValue))
-      );
-    }
 
-    return value;
-  };
+      if (
+        matchedFormat &&
+        matchedValue &&
+        isDateValid(parseDate(matchedFormat, matchedValue))
+      ) {
+        setSelectedDays(
+          parseDate(...additionalYears(matchedFormat, matchedValue))
+        );
+      } else if (checkISOFormatAndLength(value) && isInitialValue.current) {
+        setSelectedDays(parseISODate(value));
+      } else {
+        setSelectedDays(undefined);
+      }
+    }, [value, formats]);
 
-  return (
-    <StyledDateInput
-      ref={wrapperRef}
-      role="presentation"
-      size={size}
-      labelInline={labelInline}
-      data-component={dataComponent || "date"}
-      data-element={dataElement}
-      data-role={dataRole}
-      {...filterStyledSystemMarginProps(rest)}
-      applyDateRangeStyling={!!inputRefMap}
-    >
-      <Textbox
-        {...filterOutStyledSystemSpacingProps(rest)}
-        value={computedValue()}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        onClick={handleClick}
-        onFocus={handleFocus}
-        onKeyDown={handleKeyDown}
-        iconOnClick={handleClick}
-        onMouseDown={handleMouseDown}
-        iconOnMouseDown={handleIconMouseDown}
-        inputIcon="calendar"
-        labelInline={labelInline}
-        ref={assignInput}
-        adaptiveLabelBreakpoint={adaptiveLabelBreakpoint}
-        tooltipPosition={tooltipPosition}
-        helpAriaLabel={helpAriaLabel}
-        autoFocus={autoFocus}
+    const computedValue = () => {
+      if (checkISOFormatAndLength(value) && isInitialValue.current) {
+        return formattedValue(format, parseISODate(value));
+      }
+
+      const valueSeparator = getSeparator(value);
+      const formatSeparator = getSeparator(format);
+      const replaceSeparators = () =>
+        value
+          .split("")
+          .map((char) => (char === valueSeparator ? formatSeparator : char))
+          .join("");
+
+      if (
+        isInitialValue.current &&
+        valueSeparator !== formatSeparator &&
+        isDateValid(parseDate(format, replaceSeparators()))
+      ) {
+        isInitialValue.current = false;
+
+        const [matchedFormat, matchedValue] = findMatchedFormatAndValue(
+          replaceSeparators(),
+          formats
+        );
+        return formattedValue(
+          format,
+          parseDate(...additionalYears(matchedFormat, matchedValue))
+        );
+      }
+
+      return value;
+    };
+
+    return (
+      <StyledDateInput
+        ref={wrapperRef}
+        role="presentation"
         size={size}
-        disabled={disabled}
-        readOnly={readOnly}
-      />
-      <DatePicker
-        disablePortal={disablePortal}
-        inputElement={parentRef}
-        pickerProps={pickerProps}
-        selectedDays={selectedDays}
-        setSelectedDays={setSelectedDays}
-        onDayClick={handleDayClick}
-        minDate={minDate}
-        maxDate={maxDate}
-        pickerMouseDown={handlePickerMouseDown}
-        open={open}
-      />
-    </StyledDateInput>
-  );
-};
+        labelInline={labelInline}
+        data-component={dataComponent || "date"}
+        data-element={dataElement}
+        data-role={dataRole}
+        {...filterStyledSystemMarginProps(rest)}
+        applyDateRangeStyling={!!inputRefMap}
+      >
+        <Textbox
+          {...filterOutStyledSystemSpacingProps(rest)}
+          inputRef={inputRef}
+          value={computedValue()}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          onClick={handleClick}
+          onFocus={handleFocus}
+          onKeyDown={handleKeyDown}
+          iconOnClick={handleClick}
+          onMouseDown={handleMouseDown}
+          iconOnMouseDown={handleIconMouseDown}
+          inputIcon="calendar"
+          labelInline={labelInline}
+          ref={assignInput}
+          adaptiveLabelBreakpoint={adaptiveLabelBreakpoint}
+          tooltipPosition={tooltipPosition}
+          helpAriaLabel={helpAriaLabel}
+          autoFocus={autoFocus}
+          size={size}
+          disabled={disabled}
+          readOnly={readOnly}
+        />
+        <DatePicker
+          disablePortal={disablePortal}
+          inputElement={parentRef}
+          pickerProps={pickerProps}
+          selectedDays={selectedDays}
+          setSelectedDays={setSelectedDays}
+          onDayClick={handleDayClick}
+          minDate={minDate}
+          maxDate={maxDate}
+          pickerMouseDown={handlePickerMouseDown}
+          open={open}
+        />
+      </StyledDateInput>
+    );
+  }
+);
 
 DateInput.propTypes = {
   ...Textbox.propTypes,
@@ -399,5 +430,7 @@ DateInput.propTypes = {
   /** Boolean to allow the input to have an empty value */
   allowEmptyValue: PropTypes.bool,
 };
+
+DateInput.displayName = "DateInput";
 
 export default DateInput;
