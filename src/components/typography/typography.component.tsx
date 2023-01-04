5c12f183612c50ffb1f74b73c1516270fdbadc95
Will Seabrook <will.seabrook@sage.com>
2023-01-04 16:17:26 +0000

    refactor(typography): convert from styled component to a standard component
    - create a seperate Styled Component as typography.style.ts
    - refactor Typography component to wrap new styled component
    
    BREAKING CHANGE: types for the Typography component are now properly listed and therefore
    undocumented props will no longer be supported

--- src/components/typography/typography.component.tsx
+++ src/components/typography/typography.component.tsx
@@ -1,8 +1,11 @@
-import styled, { css } from "styled-components";
-import { ColorProps, space, SpaceProps } from "styled-system";
-import styledColor from "../../style/utils/color";
-import baseTheme from "../../style/themes/base";
-import Logger from "../../__internal__/utils/logger";
+import React from "react";
+import { SpaceProps } from "styled-system";
+import tagComponent, { TagProps } from "../../__internal__/utils/helpers/tags";
+import {
+  filterStyledSystemMarginProps,
+  filterStyledSystemPaddingProps,
+} from "../../style/utils";
+import StyledTypography from "./typography.style";
 
 const VARIANT_TYPES = [
   "h1-large",
@@ -26,10 +29,14 @@ const VARIANT_TYPES = [
   "ul",
   "ol",
 ] as const;
-type VariantTypes = typeof VARIANT_TYPES[number];
-export interface TypographyProps extends SpaceProps, ColorProps {
+export type VariantTypes = typeof VARIANT_TYPES[number];
+export interface TypographyProps extends SpaceProps, TagProps {
   /** Override the variant component */
   as?: React.ElementType;
+  /** Set the ID attribute of the Typography component */
+  id?: string;
+  /** Content to be rendered inside the Typography component */
+  children?: React.ReactNode;
   /** The visual style to apply to the component */
   variant?: VariantTypes;
   /** Override the variant font-size */
@@ -46,214 +53,80 @@ export interface TypographyProps extends SpaceProps, ColorProps {
   display?: string;
   /** Override the list-style-type */
   listStyleType?: string;
-  /** Override the white-space type */
+  /** Override the white-space */
   whiteSpace?: string;
-  /** Override the word-wrap type */
+  /** Override the word-wrap */
   wordWrap?: string;
-  /** Override the text-overflow type */
+  /** Override the text-overflow */
   textOverflow?: string;
   /** Apply truncation */
   truncate?: boolean;
+  /** Override the color style */
+  color?: string;
+  /** Override the backgroundColor style */
+  backgroundColor?: string;
+  /** Override the bg value shorthand for backgroundColor */
+  bg?: string;
+  /** Override the opacity value */
+  opacity?: string | number;
+  /** @private @ignore */
+  className?: string;
 }
 
-const getAs = (variant?: VariantTypes) => {
-  switch (variant) {
-    case "h1-large":
-      return "h1";
-    case "segment-header":
-    case "segment-header-small":
-    case "segment-subheader":
-    case "segment-subheader-alt":
-      return "h5";
-    case "big":
-      return "p";
-    default:
-      return variant;
-  }
-};
-
-const getSize = (variant?: VariantTypes) => {
-  switch (variant) {
-    case "h1-large":
-      return "32px";
-    case "h1":
-      return "24px";
-    case "h2":
-      return "22px";
-    case "h3":
-    case "segment-header":
-      return "20px";
-    case "h4":
-    case "segment-header-small":
-      return "18px";
-    case "h5":
-    case "segment-subheader":
-    case "big":
-      return "16px";
-    case "small":
-    case "sub":
-    case "sup":
-      return "13px";
-    case "segment-subheader-alt":
-    case "p":
-    case "b":
-    case "strong":
-    case "em":
-    default:
-      return "14px";
-  }
-};
-
-const getLineHeight = (variant?: VariantTypes) => {
-  switch (variant) {
-    case "h1-large":
-      return "40px";
-    case "h1":
-    case "segment-subheader":
-      return "31px";
-    case "h2":
-      return "29px";
-    case "h3":
-    case "segment-header":
-      return "26px";
-    case "big":
-      return "24px";
-    case "h4":
-    case "segment-header-small":
-      return "23px";
-    case "small":
-    case "sub":
-    case "sup":
-      return "20px";
-    case "h5":
-    case "segment-subheader-alt":
-    case "p":
-    case "b":
-    case "strong":
-    case "em":
-    default:
-      return "21px";
-  }
-};
-
-const getWeight = (variant?: VariantTypes) => {
-  switch (variant) {
-    case "h1-large":
-    case "h1":
-    case "segment-header":
-    case "segment-header-small":
-      return "900";
-    case "h2":
-    case "h3":
-    case "segment-subheader":
-    case "segment-subheader-alt":
-    case "b":
-    case "em":
-    case "strong":
-      return "700";
-    case "h4":
-    case "h5":
-    case "p":
-    case "small":
-    case "big":
-    case "sub":
-    case "sup":
-    default:
-      return "400";
-  }
-};
-
-const getTransform = (variant?: VariantTypes) => {
-  if (variant === "segment-subheader-alt") {
-    return "uppercase";
-  }
-  return "none";
-};
-
-const getDecoration = (variant?: VariantTypes) => {
-  if (variant === "em") {
-    return "underline";
-  }
-  return "none";
-};
-
-let isDeprecationWarningTriggered = false;
-
-const Typography = styled.span.attrs(
-  ({
-    variant,
-    as,
-    fontSize,
-    fontWeight,
-    textTransform,
-    lineHeight,
-    textDecoration,
-  }: TypographyProps) => {
-    return {
-      as: as || getAs(variant),
-      size: fontSize || getSize(variant),
-      weight: fontWeight || getWeight(variant),
-      textTransform: textTransform || getTransform(variant),
-      textDecoration: textDecoration || getDecoration(variant),
-      lineHeight: lineHeight || getLineHeight(variant),
-      defaultMargin: variant === "p" ? "0 0 16px" : "0",
-    };
-  }
-)<TypographyProps>`
-  ${() => {
-    if (!isDeprecationWarningTriggered) {
-      isDeprecationWarningTriggered = true;
-      Logger.deprecate(
-        "Previous props that could be spread to the `Typography` component are being removed. Only props documented in the intefaces will be supported."
-      );
-    }
-    return css``;
-  }}
-  ${({
-    size,
-    weight,
-    textTransform,
-    lineHeight,
-    defaultMargin,
-    textDecoration,
-    display,
-    variant,
-    listStyleType,
-    whiteSpace,
-    wordWrap,
-    textOverflow,
-    truncate,
-  }) => css`
-    font-style: normal;
-    font-size: ${size};
-    font-weight: ${weight};
-    text-transform: ${textTransform};
-    text-decoration: ${textDecoration};
-    line-height: ${lineHeight};
-    margin: ${defaultMargin};
-    padding: 0;
-    white-space: ${truncate ? "nowrap" : whiteSpace};
-    word-wrap: ${wordWrap};
-    text-overflow: ${truncate ? "ellipsis" : textOverflow};
-    ${truncate &&
-    css`
-      overflow: hidden;
-    `};
-    ${variant === "sup" && "vertical-align: super;"};
-    ${variant === "sub" && "vertical-align: sub;"};
-    ${display && `display: ${display};`};
-    ${listStyleType && `list-style-type: ${listStyleType};`}; ;
-  `}
-  ${space}
-  ${({ color, bg, backgroundColor, ...rest }) =>
-    styledColor({ color, bg, backgroundColor, ...rest })}
-`;
-
-Typography.defaultProps = {
-  color: "blackOpacity90",
-  variant: "p",
-  theme: baseTheme,
+export const Typography = ({
+  "data-component": dataComponent,
+  variant = "p",
+  as,
+  id,
+  fontSize,
+  fontWeight,
+  textTransform,
+  lineHeight,
+  textDecoration,
+  display,
+  listStyleType,
+  whiteSpace,
+  wordWrap,
+  textOverflow,
+  truncate,
+  color = "blackOpacity90",
+  backgroundColor,
+  bg,
+  opacity,
+  children,
+  className,
+  ...rest
+}: TypographyProps) => {
+  return (
+    <StyledTypography
+      variant={variant}
+      as={as}
+      id={id}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      textTransform={textTransform}
+      lineHeight={lineHeight}
+      textDecoration={textDecoration}
+      display={display}
+      listStyleType={listStyleType}
+      whiteSpace={whiteSpace}
+      wordWrap={wordWrap}
+      textOverflow={textOverflow}
+      truncate={truncate}
+      color={color}
+      backgroundColor={backgroundColor}
+      bg={bg}
+      opacity={opacity}
+      className={className}
+      {...tagComponent(dataComponent, rest)}
+      {...filterStyledSystemMarginProps(rest)}
+      {...filterStyledSystemPaddingProps(rest)}
+    >
+      {children}
+    </StyledTypography>
+  );
 };
 
 Typography.displayName = "Typography";
+
 export default Typography;
