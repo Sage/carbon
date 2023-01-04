ffa6e2ab80ce0a3f14936f28f87b697f1f0b17e6
divya jindel <divya.jindel@sage.com>
2023-01-04 13:55:34 +0000

    test(badge): add accessibility tests

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -58,6 +58,7 @@ export default (from, end) => {
       !prepareUrl[0].startsWith("verticaldivider") &&
       !prepareUrl[0].startsWith("button-bar") &&
       !prepareUrl[0].startsWith("batch-selection") &&
+      !prepareUrl[0].startsWith("badge") &&
       !prepareUrl[0].endsWith("test")
     ) {
       urlList.push([prepareUrl[0], prepareUrl[1]]);
