90fce2e80efc46976bdec2b784c77e3336e55abb
Ihor Dluhosh <idlugosh@pgs-soft.com>
2023-01-23 10:03:28 +0100

    test(toast): add accessibility tests

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -66,6 +66,7 @@ export default (from, end) => {
       !prepareUrl[0].startsWith("preview") &&
       !prepareUrl[0].startsWith("detail") &&
       !prepareUrl[0].startsWith("help") &&
+      !prepareUrl[0].startsWith("toast") &&
       !prepareUrl[0].endsWith("test")
     ) {
       urlList.push([prepareUrl[0], prepareUrl[1]]);
