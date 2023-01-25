78213f040ebc2862d7c0b7964ea20f62a24f858d
ZhuoyuJin <zhuoyu.jin@sage.com>
2023-01-25 14:47:07 +0000

    test(sidebar): refactor for accessibility test

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -67,6 +67,7 @@ export default (from, end) => {
       !prepareUrl[0].startsWith("detail") &&
       !prepareUrl[0].startsWith("help") &&
       !prepareUrl[0].startsWith("toast") &&
+      !prepareUrl[0].startsWith("sidebar") &&
       !prepareUrl[0].startsWith("dialog-full-screen") &&
       !prepareUrl[0].startsWith("verticalmenu") &&
       !prepareUrl[0].endsWith("test")
