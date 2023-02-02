6bc77a161f8a59b032604a174ded228f7873f604
Ihor Dluhosh <idlugosh@pgs-soft.com>
2023-02-02 09:52:54 +0100

    test(date-range): add accessibility tests

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -73,6 +73,7 @@ export default (from, end) => {
       !prepareUrl[0].startsWith("step-sequence") &&
       !prepareUrl[0].startsWith("button-toggle") &&
       !prepareUrl[0].startsWith("profile") &&
+      !prepareUrl[0].startsWith("date-range") &&
       !prepareUrl[0].endsWith("test")
     ) {
       urlList.push([prepareUrl[0], prepareUrl[1]]);
