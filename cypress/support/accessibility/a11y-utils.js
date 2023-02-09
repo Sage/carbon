89757ca88d54d90b199140052fb2663cb3f01dce
divya jindel <divya.jindel@sage.com>
2023-02-09 15:15:16 +0000

    test(note): add accessibility tests

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -88,6 +88,7 @@ export default (from, end) => {
       !prepareUrl[0].startsWith("carbon-provider") &&
       !prepareUrl[0].startsWith("pill") &&
       !prepareUrl[0].startsWith("checkbox") &&
+      !prepareUrl[0].startsWith("note") &&
       !prepareUrl[0].endsWith("test")
     ) {
       urlList.push([prepareUrl[0], prepareUrl[1]]);
