65a9cef560723f9c23220ca8db0be69b7bf6b77c
mkrds <mkordas@pgs-soft.com>
2023-02-16 18:37:40 +0100

    fix(filterable-select): resolve issue with multiple execution of onOpen and onFilterChange callbacks

--- src/components/select/filterable-select/filterable-select.component.js
+++ src/components/select/filterable-select/filterable-select.component.js
@@ -16,6 +16,7 @@ import SelectList from "../select-list/select-list.component";
 import isExpectedOption from "../utils/is-expected-option";
 import isNavigationKey from "../utils/is-navigation-key";
 import Logger from "../../../__internal__/utils/logger";
+import useStableCallback from "../../../hooks/__internal__/useStableCallback";
 
 let deprecateInputRefWarnTriggered = false;
 
@@ -30,9 +31,9 @@ const FilterableSelect = React.forwardRef(
       name,
       label,
       children,
-      onOpen,
+      onOpen: onOpenProp,
       onChange,
-      onFilterChange,
+      onFilterChange: onFilterChangeProp,
       onClick,
       onKeyDown,
       onFocus,
@@ -281,13 +282,14 @@ const FilterableSelect = React.forwardRef(
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value, onChange, children]);
 
+    const onOpen = useStableCallback(onOpenProp);
     useEffect(() => {
       if (!isOpen) {
         setFilterText("");
       } else if (onOpen) {
         onOpen();
       }
-    }, [isOpen, onOpen]);
+    }, [onOpen, isOpen]);
 
     useEffect(() => {
       const hasListActionButton = listActionButton !== undefined;
@@ -308,11 +310,17 @@ const FilterableSelect = React.forwardRef(
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value, children]);
 
+    const onFilterChange = useStableCallback(onFilterChangeProp);
+    const isFirstRender = useRef(true);
     useEffect(() => {
-      if (onFilterChange) {
+      if (onFilterChange && !isFirstRender.current) {
         onFilterChange(filterText);
       }
-    }, [filterText, onFilterChange]);
+    }, [onFilterChange, filterText]);
+
+    useEffect(() => {
+      isFirstRender.current = false;
+    }, []);
 
     useEffect(() => {
       const clickEvent = "click";
@@ -409,9 +417,6 @@ const FilterableSelect = React.forwardRef(
             return true;
           }
 
-          if (onOpen) {
-            onOpen();
-          }
           if (onFocus && !isInputFocused.current) {
             triggerFocus();
             isInputFocused.current = true;
