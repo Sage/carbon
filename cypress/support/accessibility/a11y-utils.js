1a272a44890901c6010287e9c62e3aabc78f3489
Ihor Dluhosh <idlugosh@pgs-soft.com>
2023-04-03 08:44:10 +0200

    test(accessibility): update for urlList creation

--- cypress/support/accessibility/a11y-utils.js
+++ cypress/support/accessibility/a11y-utils.js
@@ -41,78 +41,18 @@ export default (from, end) => {
   for (const story in storiesJSON.stories) {
     const prepareUrl = story.split("--");
     if (
-      !prepareUrl[0].startsWith("welcome") &&
-      !prepareUrl[0].startsWith("contributing") &&
-      !prepareUrl[0].startsWith("documentation") &&
-      !prepareUrl[0].startsWith("accordion") &&
-      !prepareUrl[0].startsWith("confirm") &&
-      !prepareUrl[0].startsWith("content") &&
-      !prepareUrl[0].startsWith("alert") &&
-      !prepareUrl[0].startsWith("action-popover") &&
-      !prepareUrl[0].startsWith("anchor-navigation") &&
-      !prepareUrl[0].startsWith("loader") &&
-      !prepareUrl[0].startsWith("loader-bar") &&
-      !prepareUrl[0].startsWith("link") &&
-      !prepareUrl[0].startsWith("link-preview") &&
-      !prepareUrl[0].startsWith("verticaldivider") &&
-      !prepareUrl[0].startsWith("button-bar") &&
-      !prepareUrl[0].startsWith("button-minor") &&
-      !prepareUrl[0].startsWith("batch-selection") &&
-      !prepareUrl[0].startsWith("carousel") &&
-      !prepareUrl[0].startsWith("badge") &&
-      !prepareUrl[0].startsWith("advanced-color-picker") &&
-      !prepareUrl[0].startsWith("preview") &&
-      !prepareUrl[0].startsWith("detail") &&
-      !prepareUrl[0].startsWith("help") &&
-      !prepareUrl[0].startsWith("heading") &&
-      !prepareUrl[0].startsWith("toast") &&
-      !prepareUrl[0].startsWith("sidebar") &&
-      !prepareUrl[0].startsWith("search") &&
-      !prepareUrl[0].startsWith("switch") &&
-      !prepareUrl[0].startsWith("dialog-full-screen") &&
-      !prepareUrl[0].startsWith("verticalmenu") &&
-      !prepareUrl[0].startsWith("message") &&
-      !prepareUrl[0].startsWith("card") &&
-      !prepareUrl[0].startsWith("date-input") &&
-      !prepareUrl[0].startsWith("step-sequence") &&
-      !prepareUrl[0].startsWith("button-toggle") &&
-      !prepareUrl[0].startsWith("profile") &&
-      !prepareUrl[0].startsWith("date-range") &&
-      !prepareUrl[0].startsWith("pages") &&
-      !prepareUrl[0].startsWith("pod") &&
-      !prepareUrl[0].startsWith("button-toggle-group") &&
-      !prepareUrl[0].startsWith("progress-tracker") &&
-      !prepareUrl[0].startsWith("portrait") &&
-      !prepareUrl[0].startsWith("draggable") &&
-      !prepareUrl[0].startsWith("definition-list") &&
-      !prepareUrl[0].startsWith("decimal") &&
-      !prepareUrl[0].startsWith("box") &&
-      !prepareUrl[0].startsWith("carbon-provider") &&
-      !prepareUrl[0].startsWith("pill") &&
-      !prepareUrl[0].startsWith("checkbox") &&
-      !prepareUrl[0].startsWith("note") &&
-      !prepareUrl[0].startsWith("textbox") &&
-      !prepareUrl[0].startsWith("textarea") &&
-      !prepareUrl[0].startsWith("dismissible-box") &&
-      !prepareUrl[0].startsWith("dialog") &&
-      !prepareUrl[0].startsWith("number-input") &&
-      !prepareUrl[0].startsWith("button") &&
-      !prepareUrl[0].startsWith("icon") &&
-      !prepareUrl[0].startsWith("pager") &&
-      !prepareUrl[0].startsWith("fieldset") &&
-      !prepareUrl[0].startsWith("form") &&
-      !prepareUrl[0].startsWith("drawer") &&
-      !prepareUrl[0].startsWith("group-character") &&
-      !prepareUrl[0].startsWith("duelling-picklist") &&
-      !prepareUrl[0].startsWith("multi-action-button") &&
-      !prepareUrl[0].startsWith("settings-row") &&
-      !prepareUrl[0].startsWith("numeral-date") &&
-      !prepareUrl[0].startsWith("global-header") &&
-      !prepareUrl[0].startsWith("grid") &&
-      !prepareUrl[0].startsWith("image") &&
-      !prepareUrl[0].startsWith("navigation-bar") &&
-      !prepareUrl[0].startsWith("popover-container") &&
-      !prepareUrl[0].startsWith("menu") &&
+      (prepareUrl[0].startsWith("flat-table") ||
+        prepareUrl[0].startsWith("hr") ||
+        prepareUrl[0].startsWith("inlile-inputs") ||
+        prepareUrl[0].startsWith("radiobutton") ||
+        prepareUrl[0].startsWith("select") ||
+        prepareUrl[0].startsWith("showeditpod") ||
+        prepareUrl[0].startsWith("simple-color-picker") ||
+        prepareUrl[0].startsWith("split-button") ||
+        prepareUrl[0].startsWith("tabs") ||
+        prepareUrl[0].startsWith("text-editor") ||
+        prepareUrl[0].startsWith("tile") ||
+        prepareUrl[0].startsWith("tooltip")) &&
       !prepareUrl[0].endsWith("test")
     ) {
       urlList.push([prepareUrl[0], prepareUrl[1]]);
@@ -127,6 +67,7 @@ export default (from, end) => {
       it.each(urlList)(
         "should render %s component with %s story and have no accessibility violations",
         (componentName, storyName) => {
+          console.log(urlList);
           visitComponentUrl(componentName, storyName);
 
           // open the popover-container component
