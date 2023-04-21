b21debdcb3185b1e4d2ecbf6c4ba72c561fbf433
edleeks87 <edward.leeks@sage.com>
2023-04-21 15:50:43 +0100

    feat(anchor-navigation): add rounded corner styling to component

--- cypress/locators/anchor-navigation/index.js
+++ cypress/locators/anchor-navigation/index.js
@@ -1,6 +1,7 @@
 import {
   ANCHOR_NAVIGATION,
   ANCHOR_NAVIGATION_STICKY_NAVIGATION,
+  ANCHOR_NAVIGATION_ITEM,
 } from "./locators";
 
 // component preview locators
@@ -9,3 +10,5 @@ export const anchorNavigationStickyNavigation = (text) =>
   anchorNavigation().find(ANCHOR_NAVIGATION_STICKY_NAVIGATION).contains(text);
 export const anchorNavigationStickyMainPage = (text) =>
   anchorNavigation().find("div > h2").contains(text);
+export const anchorNavigationItem = (index) =>
+  cy.get(ANCHOR_NAVIGATION_ITEM).eq(index);
