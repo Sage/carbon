76e33ef546068b670aa5fd9dcd453a9ae7466907
ZhuoyuJin <zhuoyu.jin@sage.com>
2023-05-18 12:12:23 +0100

    test(detail): typescript refactor

--- cypress/components/detail/detail.cy.tsx
+++ cypress/components/detail/detail.cy.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Detail from "../../../src/components/detail";
 import CypressMountWithProviders from "../../support/component-helper/cypress-mount";
