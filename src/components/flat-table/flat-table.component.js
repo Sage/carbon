209af5eef7ae4475224155ea9d45308f0c90fd9f
DipperTheDan <daniel.dipper@sage.com>
2023-06-14 09:10:27 +0100

    fix(flat-table): fix sticky footer no longer at the foot of the table
    This work reverts back to the code before we had this bug introduced. This will reintroduce
    https://github.com/Sage/carbon/issues/5704 but this will be re-opened and fixed seperately.
    
    fixes FE-6018

--- src/components/flat-table/flat-table.component.js
+++ src/components/flat-table/flat-table.component.js
@@ -228,12 +228,12 @@ const FlatTable = ({
                 {children}
               </FlatTableThemeContext.Provider>
             </StyledFlatTable>
-            {footer && (
-              <StyledFlatTableFooter hasStickyFooter={hasStickyFooter}>
-                {footer}
-              </StyledFlatTableFooter>
-            )}
           </StyledTableContainer>
+          {footer && (
+            <StyledFlatTableFooter hasStickyFooter={hasStickyFooter}>
+              {footer}
+            </StyledFlatTableFooter>
+          )}
         </StyledFlatTableWrapper>
       )}
     </DrawerSidebarContext.Consumer>
