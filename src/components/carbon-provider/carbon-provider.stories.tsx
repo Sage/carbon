3a83e82f523e4fe8f2adffb0ee0ca2423c5a34ca
James Parslow <james.parslow@sage.com>
2023-06-14 18:04:10 +0100

    docs(carbon-provider): update prop descriptions and docs

--- src/components/carbon-provider/carbon-provider.stories.tsx
+++ src/components/carbon-provider/carbon-provider.stories.tsx
@@ -1,44 +1,20 @@
 import React from "react";
-import { ComponentStory } from "@storybook/react";
+import type { StoryFn } from "@storybook/react";
 
-import aegeanTheme from "../../style/themes/aegean";
-import baseTheme from "../../style/themes/base";
-import sageTheme from "../../style/themes/sage";
-import Box from "../box";
 import Button from "../button";
 import CarbonProvider from ".";
+import { sageTheme, mintTheme } from "../../style/themes";
 
-export const Default: ComponentStory<typeof CarbonProvider> = () => {
-  return (
-    <CarbonProvider>
-      <Button buttonType="primary">Button</Button>
-    </CarbonProvider>
-  );
-};
+export const SageTheme: StoryFn = () => (
+  <CarbonProvider theme={sageTheme}>
+    <Button buttonType="primary">Button</Button>
+  </CarbonProvider>
+);
+SageTheme.storyName = "using latest sage theme";
 
-export const Theming: ComponentStory<typeof CarbonProvider> = () => {
-  return (
-    <CarbonProvider theme={aegeanTheme}>
-      <Button buttonType="primary">Button</Button>
-    </CarbonProvider>
-  );
-};
-
-export const Mixing: ComponentStory<typeof CarbonProvider> = () => {
-  return (
-    <Box>
-      <CarbonProvider>
-        <Button buttonType="primary">Button</Button>
-      </CarbonProvider>
-      <CarbonProvider theme={aegeanTheme}>
-        <Button buttonType="primary">Button</Button>
-        <CarbonProvider theme={baseTheme}>
-          <Button buttonType="primary">Button</Button>
-        </CarbonProvider>
-      </CarbonProvider>
-      <CarbonProvider theme={sageTheme}>
-        <Button buttonType="primary">Button</Button>
-      </CarbonProvider>
-    </Box>
-  );
-};
+export const MintTheme: StoryFn = () => (
+  <CarbonProvider theme={mintTheme}>
+    <Button buttonType="primary">Button</Button>
+  </CarbonProvider>
+);
+MintTheme.storyName = "using mint theme";
