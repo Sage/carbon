57701fca574bd797da2697834d2c598af29690d3
Stephen O'Gorman <stephen.ogorman@sage.com>
2023-06-15 16:19:54 +0100

    test(icon): typescript refactor

--- cypress/components/icon/icon.cy.tsx
+++ cypress/components/icon/icon.cy.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IconProps } from "components/icon";
 import {
   IconComponent,
   IconTooltipComponent,
@@ -17,7 +18,7 @@ const colorData = [COLOR.ORANGE, COLOR.RED, COLOR.BLACK, COLOR.BROWN];
 
 context("Tests for Icon component", () => {
   describe("should check Icon component properties", () => {
-    it.each(["error", "add", "admin", "alert"])(
+    it.each(["error", "add", "admin", "alert"] as IconProps["type"][])(
       "should check %s type for Icon component",
       (iconType) => {
         CypressMountWithProviders(<IconComponent type={iconType} />);
@@ -31,39 +32,48 @@ context("Tests for Icon component", () => {
       [SIZE.MEDIUM, 32],
       [SIZE.LARGE, 40],
       [SIZE.EXTRALARGE, 56],
-    ])("should check %s bgSize for Icon component", (size, pixelSize) => {
-      CypressMountWithProviders(<IconComponent bgSize={size} />);
-      icon().then(($el) => {
-        assertCssValueIsApproximately($el, "height", pixelSize);
-        assertCssValueIsApproximately($el, "width", pixelSize);
-      });
-    });
+    ] as [IconProps["bgSize"], number][])(
+      "should check %s bgSize for Icon component",
+      (size, pixelSize) => {
+        CypressMountWithProviders(<IconComponent bgSize={size} />);
+        icon().then(($el) => {
+          assertCssValueIsApproximately($el, "height", pixelSize);
+          assertCssValueIsApproximately($el, "width", pixelSize);
+        });
+      }
+    );
 
     it.each([
       ["circle", 50],
       ["rounded-rect", 20],
       ["square", 0],
-    ])("should check bgShape as %s for Icon component", (bgShape, radius) => {
-      CypressMountWithProviders(<IconComponent bgShape={bgShape} />);
-      icon()
-        .should("have.css", "border-bottom-left-radius", `${radius}%`)
-        .and("have.css", "border-bottom-right-radius", `${radius}%`)
-        .and("have.css", "border-top-left-radius", `${radius}%`)
-        .and("have.css", "border-top-right-radius", `${radius}%`);
-    });
+    ] as [IconProps["bgShape"], number][])(
+      "should check bgShape as %s for Icon component",
+      (bgShape, radius) => {
+        CypressMountWithProviders(<IconComponent bgShape={bgShape} />);
+        icon()
+          .should("have.css", "border-bottom-left-radius", `${radius}%`)
+          .and("have.css", "border-bottom-right-radius", `${radius}%`)
+          .and("have.css", "border-top-left-radius", `${radius}%`)
+          .and("have.css", "border-top-right-radius", `${radius}%`);
+      }
+    );
 
     it.each([
       [SIZE.SMALL, 24],
       [SIZE.MEDIUM, 32],
       [SIZE.LARGE, 40],
       [SIZE.EXTRALARGE, 56],
-    ])("should check %s fontSize for Icon component", (fontSize, pixelSize) => {
-      CypressMountWithProviders(<IconComponent fontSize={fontSize} />);
-      icon().then(($el) => {
-        assertCssValueIsApproximately($el, "height", pixelSize);
-        assertCssValueIsApproximately($el, "width", pixelSize);
-      });
-    });
+    ] as [IconProps["fontSize"], number][])(
+      "should check %s fontSize for Icon component",
+      (fontSize, pixelSize) => {
+        CypressMountWithProviders(<IconComponent fontSize={fontSize} />);
+        icon().then(($el) => {
+          assertCssValueIsApproximately($el, "height", pixelSize);
+          assertCssValueIsApproximately($el, "width", pixelSize);
+        });
+      }
+    );
 
     it.each(colorData)(
       "should check %s background color for Icon component",
@@ -97,11 +107,14 @@ context("Tests for Icon component", () => {
       }
     );
 
-    it.each(["true", "false"])(
+    it.each([
+      [true, "true"],
+      [false, "false"],
+    ])(
       "should check when aria-hidden is %s for Icon component",
-      (bool) => {
+      (bool, hidden) => {
         CypressMountWithProviders(<IconComponent aria-hidden={bool} />);
-        icon().should("have.attr", "aria-hidden", bool);
+        icon().should("have.attr", "aria-hidden", hidden);
       }
     );
 
@@ -119,7 +132,12 @@ context("Tests for Icon component", () => {
       }
     );
 
-    it.each(["bottom", "left", "right", "top"])(
+    it.each([
+      "bottom",
+      "left",
+      "right",
+      "top",
+    ] as IconProps["tooltipPosition"][])(
       "should check %s position of tooltip for Icon component",
       (position) => {
         CypressMountWithProviders(
@@ -181,23 +199,23 @@ context("Tests for Icon component", () => {
     );
 
     it.each([
-      ["left", "bottom", "top"],
-      ["top", "bottom", "top"],
-      ["left", "top", "bottom"],
-      ["bottom", "top", "bottom"],
-      ["bottom", "left", "bottom"],
-      ["bottom", "right", "bottom"],
-      ["top", "left", "top"],
-      ["top", "right", "top"],
-      ["right", "bottom", "right"],
-      ["right", "top", "right"],
-    ])(
+      [["left"], "left", "bottom", "top"],
+      [["top"], "top", "bottom", "top"],
+      [["left"], "left", "top", "bottom"],
+      [["bottom"], "bottom", "top", "bottom"],
+      [["bottom"], "bottom", "left", "bottom"],
+      [["bottom"], "bottom", "right", "bottom"],
+      [["top"], "top", "left", "top"],
+      [["top"], "top", "right", "top"],
+      [["right"], "right", "bottom", "right"],
+      [["right"], "right", "top", "right"],
+    ] as [IconProps["tooltipFlipOverrides"], string, IconProps["tooltipPosition"], "top" | "bottom" | "right"][])(
       "should check flip position to the %s when tooltip position is %s and scrolling to the %s side for Icon component",
-      (flipPosition, tooltipPosition, scrollPosition) => {
+      (flipPosition, expectedPosition, tooltipPosition, scrollPosition) => {
         CypressMountWithProviders(
           <div style={{ padding: "60px 60px 60px 60px" }}>
             <IconTooltipComponent
-              tooltipFlipOverrides={[flipPosition]}
+              tooltipFlipOverrides={flipPosition}
               tooltipPosition={tooltipPosition}
             />
           </div>
@@ -207,7 +225,7 @@ context("Tests for Icon component", () => {
         getDataElementByValue("tooltip").should(
           "have.attr",
           "data-placement",
-          flipPosition
+          expectedPosition
         );
       }
     );
@@ -219,7 +237,7 @@ context("Tests for Icon component", () => {
   });
 
   describe("Accessibility tests for Icon component", () => {
-    it.each(["error", "add", "admin", "alert"])(
+    it.each(["error", "add", "admin", "alert"] as IconProps["type"][])(
       "should check %s type for accessibilty tests",
       (iconType) => {
         CypressMountWithProviders(<IconComponent type={iconType} />);
@@ -233,12 +251,15 @@ context("Tests for Icon component", () => {
       SIZE.MEDIUM,
       SIZE.LARGE,
       SIZE.EXTRALARGE,
-    ])("should check %s bgSize for accessibilty tests", (size) => {
-      CypressMountWithProviders(<IconComponent bgSize={size} />);
-      cy.checkAccessibility();
-    });
+    ] as IconProps["bgSize"][])(
+      "should check %s bgSize for accessibilty tests",
+      (size) => {
+        CypressMountWithProviders(<IconComponent bgSize={size} />);
+        cy.checkAccessibility();
+      }
+    );
 
-    it.each(["circle", "rounded-rect", "square"])(
+    it.each(["circle", "rounded-rect", "square"] as IconProps["bgShape"][])(
       "should check bgShape as %s for accessibilty tests",
       (bgShape) => {
         CypressMountWithProviders(<IconComponent bgShape={bgShape} />);
@@ -246,7 +267,12 @@ context("Tests for Icon component", () => {
       }
     );
 
-    it.each([SIZE.SMALL, SIZE.MEDIUM, SIZE.LARGE, SIZE.EXTRALARGE])(
+    it.each([
+      SIZE.SMALL,
+      SIZE.MEDIUM,
+      SIZE.LARGE,
+      SIZE.EXTRALARGE,
+    ] as IconProps["fontSize"][])(
       "should check %s fontSize for accessibilty tests",
       (fontSize) => {
         CypressMountWithProviders(<IconComponent fontSize={fontSize} />);
@@ -278,7 +304,7 @@ context("Tests for Icon component", () => {
       }
     );
 
-    it.each(["true", "false"])(
+    it.each([true, false])(
       "should check when aria-hidden is %s for accessibilty tests",
       (bool) => {
         CypressMountWithProviders(<IconComponent aria-hidden={bool} />);
@@ -296,7 +322,12 @@ context("Tests for Icon component", () => {
       }
     );
 
-    it.each(["bottom", "left", "right", "top"])(
+    it.each([
+      "bottom",
+      "left",
+      "right",
+      "top",
+    ] as IconProps["tooltipPosition"][])(
       "should check %s position of tooltip for accessibilty tests",
       (position) => {
         CypressMountWithProviders(
@@ -347,23 +378,23 @@ context("Tests for Icon component", () => {
     );
 
     it.each([
-      ["left", "bottom"],
-      ["top", "bottom"],
-      ["left", "top"],
-      ["bottom", "top"],
-      ["bottom", "left"],
-      ["bottom", "right"],
-      ["top", "left"],
-      ["top", "right"],
-      ["right", "bottom"],
-      ["right", "top"],
-    ])(
+      [["left"], "bottom"],
+      [["top"], "bottom"],
+      [["left"], "top"],
+      [["bottom"], "top"],
+      [["bottom"], "left"],
+      [["bottom"], "right"],
+      [["top"], "left"],
+      [["top"], "right"],
+      [["right"], "bottom"],
+      [["right"], "top"],
+    ] as [IconProps["tooltipFlipOverrides"], IconProps["tooltipPosition"]][])(
       "should check flip position to the %s when tooltip position is %s and scrolling to the %s side for accessibilty tests",
       (flipPosition, tooltipPosition) => {
         CypressMountWithProviders(
           <div style={{ padding: "60px 60px 60px 60px" }}>
             <IconTooltipComponent
-              tooltipFlipOverrides={[flipPosition]}
+              tooltipFlipOverrides={flipPosition}
               tooltipPosition={tooltipPosition}
             />
           </div>
