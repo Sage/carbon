76d00a2e90b5d6501c5026cdc7174a3a75076add
DipperTheDan <daniel.dipper@sage.com>
2023-08-22 09:25:06 +0100

    test(anchor-navigation): migrate from cypress to playwright

---
+++ playwright/components/anchor-navigation/index.ts
@@ -0,0 +1,17 @@
+import { Page } from "playwright-core";
+import {
+  ANCHOR_NAVIGATION,
+  ANCHOR_NAVIGATION_STICKY_NAVIGATION,
+  ANCHOR_NAVIGATION_ITEM,
+} from "./locators";
+
+// component preview locators
+export const anchorNavigation = (page: Page) => page.locator(ANCHOR_NAVIGATION);
+export const anchorNavigationStickyNavigation = (page: Page, text: string) =>
+  anchorNavigation(page)
+    .locator(ANCHOR_NAVIGATION_STICKY_NAVIGATION)
+    .getByText(text);
+export const anchorNavigationStickyMainPage = (page: Page, text: string) =>
+  anchorNavigation(page).locator("div > h2").getByText(text);
+export const anchorNavigationItem = (page: Page, index: number) =>
+  page.locator(ANCHOR_NAVIGATION_ITEM).nth(index);
---
+++ playwright/components/anchor-navigation/locators.ts
@@ -0,0 +1,5 @@
+// component preview locators
+export const ANCHOR_NAVIGATION = '[data-component="anchor-navigation"]';
+export const ANCHOR_NAVIGATION_STICKY_NAVIGATION =
+  '[data-element="anchor-sticky-navigation"]';
+export const ANCHOR_NAVIGATION_ITEM = '[data-element="anchor-navigation-item"]';
