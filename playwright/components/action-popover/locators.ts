a10b6e7fab7b78370a3b94cf2e12e8d64711d561
Sian <sian.ford@sage.com>
2023-09-07 11:38:28 +0100

    test(note): refactor cypress to playwright

---
+++ playwright/components/action-popover/locators.ts
@@ -0,0 +1,13 @@
+// component preview locators
+export const ACTION_POPOVER_BUTTON = '[data-element="action-popover-button"]';
+export const ACTION_POPOVER_DATA_COMPONENT =
+  '[data-component="action-popover"]';
+export const ACTION_POPOVER_SUBMENU = '[data-element="action-popover-submenu"]';
+export const ACTION_POPOVER_MENU_ITEM_ICON =
+  '[data-element="action-popover-menu-item-icon"]';
+export const ACTION_POPOVER_MENU_ITEM_INNER_TEXT =
+  '[data-element="action-popover-menu-item-inner-text"]';
+export const ACTION_POPOVER_MENU_ITEM_CHEVRON =
+  '[data-element="action-popover-menu-item-chevron"]';
+export const ACTION_POPOVER_WRAPPER =
+  '[data-component="action-popover-wrapper"]';
