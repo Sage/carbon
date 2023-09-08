3d26c0e5d59ab96ac5d9e912d72ef9ac57209f2f
Robin Zigmond <robin.zigmond@sage.com>
2023-09-08 10:57:38 +0100

    test(box): convert cypress tests to playwright

--- src/components/box/box-test.stories.tsx
+++ src/components/box/box-test.stories.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import Box, { BoxProps } from ".";
-import RadioButton, { RadioButtonGroup } from "../radio-button";
 
 export default {
   title: "Box/Test",
@@ -78,63 +76,3 @@ export const Default = (props: Partial<BoxProps>) => {
 };
 
 Default.storyName = "default";
-
-export const BoxComponentMulti = (props: Partial<BoxProps>) => {
-  return (
-    <div>
-      <Box display="flex" data-element="box" bg="blue" {...props}>
-        <Box
-          width="100px"
-          height="100px"
-          bg="primary"
-          color="yellow"
-          data-element="boxone"
-          {...props}
-        >
-          {" "}
-          Supercalifrajilisticexpialidocious Word{" "}
-        </Box>
-        <Box
-          width="100px"
-          height="100px"
-          bg="primary"
-          color="yellow"
-          data-element="boxtwo"
-          {...props}
-        >
-          {" "}
-          Box Two Box Two Box Two Box Two Box Two{" "}
-        </Box>
-        <Box
-          width="100px"
-          height="100px"
-          bg="primary"
-          color="yellow"
-          data-element="boxthree"
-          {...props}
-        >
-          {" "}
-          Box Three Box Three Box Three Box Three{" "}
-        </Box>
-      </Box>
-    </div>
-  );
-};
-
-export const BoxComponentSticky = () => {
-  return (
-    <Box data-element="scrollable-box" height="95vh" overflowY="auto">
-      <Box position="sticky" top="0px" height="100px" bg="black">
-        foo
-      </Box>
-      <Box>
-        <Box height="100px">Foo</Box>
-        <RadioButtonGroup inline legend="test" name="test-group">
-          <RadioButton id="test-1" value="1" label="first" size="large" />
-          <RadioButton id="test-2" value="2" label="second" size="large" />
-        </RadioButtonGroup>
-        <Box height="110vh">Foo</Box>
-      </Box>
-    </Box>
-  );
-};
