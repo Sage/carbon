10eaa2c936822609209508164671bdd942a8edaa
Stephen O'Gorman <stephen.ogorman@sage.com>
2023-10-18 10:37:40 +0100

    test(duelling-picklist): playwright refactor

---
+++ playwright/components/search/locators.ts
@@ -0,0 +1,4 @@
+// component preview locators
+export const SEARCH_COMPONENT = '[data-component="search"]';
+export const CROSS_ICON = '[data-element="cross"]';
+export const SEARCH_ICON = "search";
