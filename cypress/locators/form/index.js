f3576dd2eb8ba6e82580a29470e1114f8d0ce1cb
Stephen O'Gorman <stephen.ogorman@sage.com>
2023-10-25 09:53:30 +0100

    test(form): playwright refactor

--- cypress/locators/form/index.js
+++
@@ -1,6 +0,0 @@
-import { FORM_COMPONENT, FORM_FOOTER_COMPONENT } from "./locators";
-// component preview locators
-export const formPreview = () => cy.get(FORM_COMPONENT);
-export const formFooterComponent = () => cy.get(FORM_FOOTER_COMPONENT);
-export const formButtonComponent = () =>
-  formFooterComponent().find("div > button");
