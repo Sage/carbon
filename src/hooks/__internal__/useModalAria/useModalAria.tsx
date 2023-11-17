40ba27a6c5e67d6eb49440e7108541dc2f0c972c
Robin Zigmond <robin.zigmond@sage.com>
2023-11-17 15:29:16 +0000

    fix(input): ensure focus styles removed when becoming inert
    Our input components manage focus styles using react state rather than CSS, due to limitations in
    their implementation. This state is changed in the input's focus and blur events. However when
    opening a modal, we add the `inert` attribute to all parts of the DOM tree outside the modal - this
    causes the input to lose focus, as inert elements are not supposed to receive focus in any
    descendants. In this situation, not all browsers fire the `blur` event on the input that's losing
    focus - in particular, Firefox does not. This results in the focus style remaining on the
    now-unfocused input. To fix this we need to manually call the `blur` method on any currently-focused
    element before adding the `inert` attribute.
    
    fix #6415

--- src/hooks/__internal__/useModalAria/useModalAria.tsx
+++ src/hooks/__internal__/useModalAria/useModalAria.tsx
@@ -20,6 +20,12 @@ export default function useModalAria(
           "aria-hidden": rootElement.getAttribute("aria-hidden"),
           inert: rootElement.getAttribute("inert"),
         });
+        // need to manually call the blur event on any currently-focused element that might be inside the element
+        // we're making inert, since Firefox fails to do this, which can result in the focus styles remaining on
+        // an input that is no longer focused
+        if (rootElement.contains(document.activeElement)) {
+          (document.activeElement as HTMLElement | null)?.blur();
+        }
         rootElement.setAttribute("aria-hidden", "true");
         rootElement.setAttribute("inert", "");
       } else if (rootElement !== topModal) {
