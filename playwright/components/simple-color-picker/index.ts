133c28ea416d95de1f349158f01871c9dcb66f7b
Robin Zigmond <robin.zigmond@sage.com>
2024-01-09 15:38:30 +0000

    chore: address pr comments

--- playwright/components/simple-color-picker/index.ts
+++ playwright/components/simple-color-picker/index.ts
@@ -1,10 +1,8 @@
 import type { Page } from "@playwright/test";
-import { SIMPLE_COLOR_PICKER } from "./locators";
+import SIMPLE_COLOR_PICKER from "./locators";
 
 // component preview locators
 export const simpleColorPickerLegend = (page: Page) =>
   page.locator(SIMPLE_COLOR_PICKER).locator("legend");
 
-export default {
-  simpleColorPickerLegend,
-};
+export default simpleColorPickerLegend;
--- playwright/components/simple-color-picker/locators.ts
+++ playwright/components/simple-color-picker/locators.ts
@@ -1,4 +1,4 @@
 // component preview locators
 export const SIMPLE_COLOR_PICKER = '[data-component="simple-color-picker"]';
 
-export default { SIMPLE_COLOR_PICKER };
+export default SIMPLE_COLOR_PICKER;
