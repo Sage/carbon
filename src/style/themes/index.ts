737876cc0e5a10d9503603ce75c5628a2a76d0ad
edleeks87 <edward.leeks@sage.com>
2024-03-20 11:11:41 +0000

    chore: tidy up some theme based tests and utils
    Removes carbon theme array as we only have `sageTheme` and refactors some tests that previously
    iterated  over the  theme array

--- src/style/themes/index.ts
+++ src/style/themes/index.ts
@@ -5,4 +5,3 @@ export { default as baseTheme } from "./base";
 export { default as noTheme } from "./none";
 
 export type { ThemeObject } from "./base";
-export const carbonThemeList = [sageTheme];
