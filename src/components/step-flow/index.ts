4d5d808982e1557a7b0f2c964e302a7c30715cb6
tom.davies <thomas.davies@sage.com>
2024-06-27 13:27:17 +0100

    feat(step-flow): allow component title to be compositional
    the sub component `StepFlowTitle` has been created to enable consumers to use a compositional
    approach, and render custom title content. Consumers can now pass a node to the `title` prop
    and pass any relevant elements to achieve designs and meet requirements - as well as the
    `StepFlowTitle` within that node, to ensure the component still functions correctly.
    
    Within the `StepFlowTitle` sub component, consumers can pass the `titleString` prop to override
    the text children, `titleVariant` to change the heading level between h1 and h2 and the
    `screenReaderOnlyTitle` to provide an alternative screen reader only title. This is useful
    if added information or context needs to be added to the title, especially if other elements
    have now been rendered in the node passed to the `title` prop.
    
    fix #6732

--- src/components/step-flow/index.ts
+++ src/components/step-flow/index.ts
@@ -1,4 +1,5 @@
 export { default as StepFlow } from "./step-flow.component";
+export { default as StepFlowTitle } from "./step-flow-title/step-flow-title.component";
 export type {
   StepFlowProps,
   StepFlowHandle,
--- src/components/step-flow/step-flow.stories.tsx
+++ src/components/step-flow/step-flow.stories.tsx
@@ -9,8 +9,12 @@ import Form from "../form";
 import Dialog from "../dialog";
 import Typography from "../typography";
 import Textarea from "../textarea";
+import Box from "../box";
+import Icon from "../icon";
+import Image from "../image";
+import pointSvg from "../../../.assets/point.svg";
 
-import { StepFlow, StepFlowHandle, Steps } from ".";
+import { StepFlow, StepFlowTitle, StepFlowHandle, Steps } from ".";
 
 const styledSystemProps = generateStyledSystemProps({
   margin: true,
@@ -32,6 +36,37 @@ export const DefaultStory: Story = () => {
   return <StepFlow title="Step title" currentStep={1} totalSteps={6} />;
 };
 DefaultStory.storyName = "Default";
+
+export const TitleNodeStory: Story = () => {
+  const titleNode = (
+    <Box display="flex" alignItems="center">
+      <Icon type="bin" />
+      <StepFlowTitle titleString="Step title" />
+    </Box>
+  );
+
+  return <StepFlow title={titleNode} currentStep={1} totalSteps={6} />;
+};
+TitleNodeStory.storyName = "Title Node";
+
+export const TitleNodeStoryWithScreenReaderOnlyTitle: Story = () => {
+  const titleNode = (
+    <Box display="flex" alignItems="center">
+      <StepFlowTitle
+        titleString="Step title"
+        screenReaderOnlyTitle="Step Title with a pointer image"
+      />
+      <Image alt="" src={pointSvg} decorative size={50} />
+    </Box>
+  );
+
+  return <StepFlow title={titleNode} currentStep={1} totalSteps={6} />;
+};
+TitleNodeStoryWithScreenReaderOnlyTitle.storyName =
+  "Title Node with Screen Reader Only Title";
+TitleNodeStoryWithScreenReaderOnlyTitle.parameters = {
+  chromatic: { disableSnapshot: true },
+};
 
 export const CategoryStory: Story = () => {
   return (
@@ -165,6 +200,81 @@ export const ExampleImplementation: Story = () => {
   );
 };
 ExampleImplementation.storyName = "Example Implementation";
+
+export const ExampleImplementationWithTitleNode: Story = () => {
+  const lowestStep = 1;
+  const highestStep = 3;
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(lowestStep);
+  const stepFlowHandle = useRef<StepFlowHandle>(null);
+
+  const stepTitles = ["Transaction Type", "Add refund", "Refund details"];
+
+  function handleClick(clickType: string) {
+    stepFlowHandle.current?.focus();
+
+    if (clickType === "Back") {
+      setStep(step > lowestStep ? step - 1 : step);
+    } else {
+      setStep(step < highestStep ? step + 1 : step);
+    }
+  }
+
+  const titleNode = (
+    <Box display="flex" alignItems="center">
+      <Icon type="bin" />
+      <StepFlowTitle titleString={stepTitles[step - 1]} />
+    </Box>
+  );
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Dialog</Button>
+      <Dialog
+        open={isOpen}
+        showCloseIcon={false}
+        title={
+          <StepFlow
+            category="Add client"
+            title={titleNode}
+            currentStep={step as Steps}
+            totalSteps={highestStep}
+            ref={stepFlowHandle}
+            showProgressIndicator
+            showCloseIcon
+            onDismiss={() => setIsOpen(false)}
+            mb="20px"
+          />
+        }
+      >
+        <Form
+          stickyFooter
+          leftSideButtons={
+            <Button buttonType="tertiary" onClick={() => handleClick("Back")}>
+              Back
+            </Button>
+          }
+          rightSideButtons={
+            <Button
+              buttonType="primary"
+              onClick={() => handleClick("Continue")}
+            >
+              Continue
+            </Button>
+          }
+        >
+          <Typography>
+            This is an example of a Dialog with a Form as content, with a Step
+            Flow to help users complete tasks in a specific order.
+          </Typography>
+          <Textarea label="Reason For Refund" />
+        </Form>
+      </Dialog>
+    </>
+  );
+};
+ExampleImplementationWithTitleNode.storyName =
+  "Example Implementation with title node";
 
 export const ExampleImplementationWithTranslations: Story = () => {
   const lowestStep = 1;
