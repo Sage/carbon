d5fd27c17ee66b2389c068c7f5253f295031ec77
James Parslow <james.parslow@sage.com>
2024-07-08 14:24:23 +0100

    refactor(select): clean up tests for internal utils

---
+++ src/components/select/__internal__/utils/is-expected-option.test.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Option from "../../option";
+import isExpectedOption from "./is-expected-option";
+
+test("returns false when the element is not an Option", () => {
+  expect(isExpectedOption(<div>mock</div>, "foo")).toBeFalsy();
+});
+
+test("returns true when the element is an Option whose value property matches expectedValue", () => {
+  expect(
+    isExpectedOption(<Option value="foo" text="bar" />, "foo")
+  ).toBeTruthy();
+});
+
+test("returns false when the element is an Option whose value property does not match expectedValue", () => {
+  expect(
+    isExpectedOption(<Option value="foo" text="bar" />, "bar")
+  ).toBeFalsy();
+});
+
+test("returns true when the element is an Option whose value property is an object whose id property matches that of expectedValue", () => {
+  expect(
+    isExpectedOption(
+      <Option value={{ id: "foo", value: "foo" }} text="bar" />,
+      { id: "foo", value: "foo" }
+    )
+  ).toBeTruthy();
+});
+
+test("returns false when the element is an Option whose value property is an object whose id property does not match that of expectedValue", () => {
+  expect(
+    isExpectedOption(
+      <Option value={{ id: "foo", value: "foo" }} text="bar" />,
+      { id: "bar", value: "foo" }
+    )
+  ).toBeFalsy();
+});
+
+it.each([
+  ["null", null],
+  ["undefined", undefined],
+  ["empty string", ""],
+  ["empty object", {}],
+])("returns false when expectedValue is `%s`", (_, expectedValue) => {
+  expect(
+    isExpectedOption(
+      <Option text="bar" value={{ id: "foo", value: "foo" }} />,
+      expectedValue
+    )
+  ).toBe(false);
+});
---
+++ src/components/select/__internal__/utils/is-expected-value.test.ts
@@ -0,0 +1,21 @@
+import isExpectedValue from "./is-expected-value";
+
+test("returns true when currentValue is a string and matches expectedValue", () => {
+  expect(isExpectedValue("foo", "foo")).toBeTruthy();
+});
+
+test("returns false when currentValue is a string and does match expectedValue", () => {
+  expect(isExpectedValue("foo", "bar")).toBeFalsy();
+});
+
+test("returns true when currentValue is an object whose id property matches that of expectedValue", () => {
+  expect(
+    isExpectedValue({ id: "foo", value: "foo" }, { id: "foo", value: "foo" })
+  ).toBeTruthy();
+});
+
+test("returns false when currentValue is an object whose id property does not match that of expectedValue", () => {
+  expect(
+    isExpectedValue({ id: "foo", value: "foo" }, { id: "bar", value: "foo" })
+  ).toBeFalsy();
+});
