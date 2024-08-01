4a3269a0ba37eff8779fbbf145401b8be05e065e
James Parslow <james.parslow@sage.com>
2024-08-01 13:57:39 +0100

    Revert "chore(storybook): disable version picker addon"
    This reverts commit 4aa66ea480d9e76da8f97464513706b5f3fcc6b1.

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,20 @@
 import { addons } from "@storybook/manager-api";
+import { types } from "@storybook/addons";
 import sageTheme from "./sageTheme";
+import { ADDON_ID, TOOL_ID } from "./version-picker/constants";
+import { VersionPicker } from "./version-picker";
 import { API_PreparedIndexEntry, API_StatusObject } from "@storybook/types";
+
+if (process.env.NODE_ENV === "production") {
+  addons.register(ADDON_ID, () => {
+    addons.add(TOOL_ID, {
+      type: types.TOOL,
+      title: "Version picker",
+      match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
+      render: VersionPicker,
+    });
+  });
+}
 
 addons.setConfig({
   theme: sageTheme,
