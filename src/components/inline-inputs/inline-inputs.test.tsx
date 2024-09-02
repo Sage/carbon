ce9e38c7a5295c8f08d7b9174ed660eb967f312d
nuria1110 <nuria.torresramon@sage.com>
2024-09-02 16:24:07 +0100

    refactor(inline-inputs): convert enzyme tests to RTL

---
+++ src/components/inline-inputs/inline-inputs.test.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import InlineInputs from ".";
+import Textbox from "../textbox";
+import {
+  mockMatchMedia,
+  testStyledSystemMargin,
+} from "../../__spec_helper__/__internal__/test-utils";
+
+test("renders single child", () => {
+  render(
+    <InlineInputs>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByRole("textbox")).toBeVisible();
+});
+
+test("renders multiple children", () => {
+  render(
+    <InlineInputs>
+      <Textbox onChange={() => {}} />
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getAllByRole("textbox")).toHaveLength(2);
+});
+
+test("renders with provided label", () => {
+  render(
+    <InlineInputs label="Inputs Label">
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByText("Inputs Label")).toBeVisible();
+});
+
+test("renders with provided labelId", () => {
+  render(
+    <InlineInputs label="Inputs Label" labelId="inputs-label">
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByText("Inputs Label")).toHaveAttribute(
+    "id",
+    "inputs-label"
+  );
+});
+
+test("renders with provided htmlFor", () => {
+  render(
+    <InlineInputs label="Inputs Label" htmlFor="inputs">
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByText("Inputs Label")).toHaveAttribute("for", "inputs");
+});
+
+test("renders required children when required prop is true", () => {
+  render(
+    <InlineInputs required>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByRole("textbox")).toBeRequired();
+});
+
+// coverage
+test("should render with expected styles when adaptiveLabelBreakpoint set and screen is smaller than the breakpoint", () => {
+  mockMatchMedia(false);
+  render(
+    <InlineInputs label="Inputs Label" adaptiveLabelBreakpoint={1000}>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByTestId("inline-inputs")).toHaveStyle({
+    display: "block",
+  });
+  expect(screen.getByTestId("label-container")).toHaveStyle({
+    marginBottom: "8px",
+  });
+});
+
+// coverage
+test("should render with expected styles when adaptiveLabelBreakpoint set and screen is larger than the breakpoint", () => {
+  mockMatchMedia(true);
+  render(
+    <InlineInputs label="Inputs Label" adaptiveLabelBreakpoint={1000}>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByTestId("inline-inputs")).toHaveStyle({
+    display: "flex",
+  });
+});
+
+// coverage
+test("renders with expected styles when labelWidth is provided", () => {
+  render(
+    <InlineInputs label="Inputs Label" labelWidth={50}>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByTestId("label-container")).toHaveStyle({
+    flex: "0 0 50%",
+  });
+});
+
+// coverage
+test("renders with expected styles when inputWidth is provided", () => {
+  render(
+    <InlineInputs inputWidth={50}>
+      <Textbox onChange={() => {}} />
+    </InlineInputs>
+  );
+
+  expect(screen.getByTestId("inline-inputs-container")).toHaveStyle({
+    flex: "0 0 50%",
+  });
+});
+
+testStyledSystemMargin((props) => <InlineInputs {...props} />);
