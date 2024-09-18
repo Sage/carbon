866137a13ddcad40be0ec2ca2da124eac400dd06
James Parslow <james.parslow@sage.com>
2024-09-18 09:19:53 +0100

    feat(flat-table): add `hasOuterVerticalBorders` and `bottomBorderRadius` props
    `hasOuterVerticalBorders` allows for the visibility of a table's outer left and right
    borders to be toggled.
    
    `bottomBorderRadius` - allows customisation of the table's bottom border radius.

--- src/components/flat-table/flat-table-test.stories.tsx
+++ src/components/flat-table/flat-table-test.stories.tsx
@@ -36,6 +36,7 @@ export default {
     "FlatTableSizeFocus",
     "FlatTableInsideDrawer",
     "FlatRowHeaderWithNoPaddingAndButtons",
+    "MinimalDesign",
   ],
   parameters: {
     info: { disable: true },
