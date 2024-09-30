a42855a089b1f132ad51e299e52a8a419f352c82
DipperTheDan <daniel.dipper@sage.com>
2024-09-30 15:41:01 +0100

    test(usemodalaria): tidy up RTL tests

--- src/hooks/__internal__/useModalAria/useModalAria.test.tsx
+++ src/hooks/__internal__/useModalAria/useModalAria.test.tsx
@@ -96,227 +96,223 @@ const ModalWithButtonInPortal = ({
   );
 };
 
-describe("useModalAria", () => {
-  describe("with one dialog", () => {
-    it("the open button is in the accessibility tree", () => {
-      render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
-        wrapper: CarbonProvider,
-      });
-
-      expect(screen.getByRole("button", { name: "open" })).toBeInTheDocument();
+describe("with one dialog", () => {
+  it("the open button is in the accessibility tree", () => {
+    render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
+      wrapper: CarbonProvider,
     });
 
-    it("after opening the modal, the open button is no longer in the accessibility tree", async () => {
-      const user = userEvent.setup();
-      render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
-        wrapper: CarbonProvider,
-      });
-
-      await user.click(screen.getByRole("button", { name: "open" }));
-
-      expect(
-        screen.queryByRole("button", { name: "open" })
-      ).not.toBeInTheDocument();
-    });
-
-    it("after opening and then closing the modal, the open button is in the accessibility tree", async () => {
-      const user = userEvent.setup();
-      render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
-        wrapper: CarbonProvider,
-      });
-
-      await user.click(screen.getByRole("button", { name: "open" }));
-      await user.click(screen.getByRole("button", { name: "close" }));
-
-      expect(screen.getByRole("button", { name: "open" })).toBeInTheDocument();
-    });
-
-    it("adds the aria-modal attribute to the dialog when open", async () => {
-      const user = userEvent.setup();
-      render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
-        wrapper: CarbonProvider,
-      });
-
-      await user.click(screen.getByRole("button", { name: "open" }));
-
-      expect(screen.queryByRole("dialog")).toHaveAttribute(
-        "aria-modal",
-        "true"
-      );
-    });
+    expect(screen.getByRole("button", { name: "open" })).toBeInTheDocument();
   });
 
-  describe("with nested dialogs", () => {
-    it("verifies only the first dialog open button is accessible when no dialogs are open", () => {
-      render(<NestedModals />, { wrapper: CarbonProvider });
-      expect(
-        screen.getByRole("button", { name: "Open modal 1" })
-      ).toBeInTheDocument();
-      expect(
-        screen.queryByRole("button", { name: "Open modal 2" })
-      ).not.toBeInTheDocument();
-    });
-
-    it("verifies only the first dialog and its contents are accessible when opened", async () => {
-      const user = userEvent.setup();
-      render(<NestedModals />, { wrapper: CarbonProvider });
-
-      await user.click(screen.getByRole("button", { name: "Open modal 1" }));
-
-      expect(screen.getByRole("dialog", { name: "dialog 1" })).toHaveAttribute(
-        "aria-modal",
-        "true"
-      );
-      expect(
-        screen.getByRole("button", { name: "Open modal 2" })
-      ).toBeInTheDocument();
-      expect(
-        screen.queryByRole("button", { name: "Open modal 1" })
-      ).not.toBeInTheDocument();
-    });
-
-    it("verifies only the second dialog and its contents are accessible when opened", async () => {
-      const user = userEvent.setup();
-      render(<NestedModals />, { wrapper: CarbonProvider });
-
-      await user.click(screen.getByRole("button", { name: "Open modal 1" }));
-      await user.click(screen.getByRole("button", { name: "Open modal 2" }));
-
-      expect(screen.getByRole("dialog", { name: "dialog 2" })).toHaveAttribute(
-        "aria-modal",
-        "true"
-      );
-      expect(
-        screen.getByRole("button", { name: "Close modal 2" })
-      ).toBeInTheDocument();
-      expect(
-        screen.queryByRole("dialog", { name: "dialog 1" })
-      ).toHaveAttribute("aria-modal", "false");
-      expect(
-        screen.queryByRole("button", { name: "Open modal 1" })
-      ).not.toBeInTheDocument();
-      expect(
-        screen.queryByRole("button", { name: "Open modal 2" })
-      ).not.toBeInTheDocument();
-    });
-
-    it("verifies only first modal and its contents are accessible after closing the second modal", async () => {
-      const user = userEvent.setup();
-      render(<NestedModals />, { wrapper: CarbonProvider });
-
-      await user.click(screen.getByRole("button", { name: "Open modal 1" }));
-      await user.click(screen.getByRole("button", { name: "Open modal 2" }));
-      await user.click(screen.getByRole("button", { name: "Close modal 2" }));
-
-      expect(screen.getByRole("dialog", { name: "dialog 1" })).toHaveAttribute(
-        "aria-modal",
-        "true"
-      );
-      expect(
-        screen.getByRole("button", { name: "Open modal 2" })
-      ).toBeInTheDocument();
-      expect(
-        screen.queryByRole("dialog", { name: "dialog 2" })
-      ).not.toBeInTheDocument();
-      expect(
-        screen.queryByRole("button", { name: "Open modal 1" })
-      ).not.toBeInTheDocument();
-    });
-
-    it("verifies only the first dialog open button is accessible after closing both modals", async () => {
-      const user = userEvent.setup();
-      render(<NestedModals />, { wrapper: CarbonProvider });
-
-      await user.click(screen.getByRole("button", { name: "Open modal 1" }));
-      await user.click(screen.getByRole("button", { name: "Open modal 2" }));
-      await user.click(screen.getByRole("button", { name: "Close modal 2" }));
-      await user.click(screen.getByRole("button", { name: "Close modal 1" }));
-
-      expect(
-        screen.getByRole("button", { name: "Open modal 1" })
-      ).toBeInTheDocument();
-      await waitFor(() =>
-        expect(
-          screen.queryByRole("button", { name: "Open modal 2" })
-        ).not.toBeInTheDocument()
-      );
-    });
-  });
-
-  it("overrides any pre-existing aria-hidden and inert properties when modal is opened", async () => {
+  it("after opening the modal, the open button is no longer in the accessibility tree", async () => {
     const user = userEvent.setup();
-    render(
-      <>
-        <div data-role="old-aria-hidden" aria-hidden="false" />
-        <ModalComponent openButtonText="open" closeButtonText="close" />
-        {/* @ts-expect-error inert property not recognised by React. Support to be added in React 19 https://github.com/facebook/react/pull/24730 */}
-        <div data-role="old-inert" inert="foo" />
-      </>,
-      { wrapper: CarbonProvider }
-    );
+    render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
+      wrapper: CarbonProvider,
+    });
 
     await user.click(screen.getByRole("button", { name: "open" }));
 
-    await waitFor(() =>
-      expect(screen.getByTestId("old-aria-hidden")).toHaveAttribute(
-        "aria-hidden",
-        "true"
-      )
-    );
-    await waitFor(() =>
-      expect(screen.getByTestId("old-inert")).toHaveAttribute("inert", "")
-    );
+    expect(
+      screen.queryByRole("button", { name: "open" })
+    ).not.toBeInTheDocument();
   });
 
-  it("restores any previously-overridden aria-hidden and inert properties when modal is closed", async () => {
+  it("after opening and then closing the modal, the open button is in the accessibility tree", async () => {
     const user = userEvent.setup();
-    render(
-      <>
-        <div data-role="old-aria-hidden" aria-hidden="false" />
-        <ModalComponent openButtonText="open" closeButtonText="close" />
-        {/* @ts-expect-error inert property not recognised by React. Support to be added in React 19 https://github.com/facebook/react/pull/24730 */}
-        <div data-role="old-inert" inert="foo" />
-      </>,
-      { wrapper: CarbonProvider }
-    );
+    render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
+      wrapper: CarbonProvider,
+    });
 
     await user.click(screen.getByRole("button", { name: "open" }));
     await user.click(screen.getByRole("button", { name: "close" }));
 
-    await waitFor(() =>
-      expect(screen.getByTestId("old-aria-hidden")).toHaveAttribute(
-        "aria-hidden",
-        "false"
-      )
-    );
-    await waitFor(() =>
-      expect(screen.getByTestId("old-inert")).toHaveAttribute("inert", "foo")
-    );
+    expect(screen.getByRole("button", { name: "open" })).toBeInTheDocument();
   });
 
-  describe("with additional content in a portal", () => {
-    it("without the inertOptOut flag, the portal content is not in the accessibility tree", async () => {
-      const user = userEvent.setup();
-      render(<ModalWithButtonInPortal />, { wrapper: CarbonProvider });
-
-      await user.click(screen.getByRole("button", { name: "open modal" }));
-
-      expect(
-        screen.queryByRole("button", { name: "button inside portal" })
-      ).not.toBeInTheDocument();
+  it("adds the aria-modal attribute to the dialog when open", async () => {
+    const user = userEvent.setup();
+    render(<ModalComponent openButtonText="open" closeButtonText="close" />, {
+      wrapper: CarbonProvider,
     });
 
-    it("with the inertOptOut flag, the portal content is in the accessibility tree", async () => {
-      const user = userEvent.setup();
-      render(<ModalWithButtonInPortal inertOptOut />, {
-        wrapper: CarbonProvider,
-      });
+    await user.click(screen.getByRole("button", { name: "open" }));
 
-      await user.click(screen.getByRole("button", { name: "open modal" }));
+    expect(screen.queryByRole("dialog")).toHaveAttribute("aria-modal", "true");
+  });
+});
 
+describe("with nested dialogs", () => {
+  it("verifies only the first dialog open button is accessible when no dialogs are open", () => {
+    render(<NestedModals />, { wrapper: CarbonProvider });
+    expect(
+      screen.getByRole("button", { name: "Open modal 1" })
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Open modal 2" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("verifies only the first dialog and its contents are accessible when opened", async () => {
+    const user = userEvent.setup();
+    render(<NestedModals />, { wrapper: CarbonProvider });
+
+    await user.click(screen.getByRole("button", { name: "Open modal 1" }));
+
+    expect(screen.getByRole("dialog", { name: "dialog 1" })).toHaveAttribute(
+      "aria-modal",
+      "true"
+    );
+    expect(
+      screen.getByRole("button", { name: "Open modal 2" })
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Open modal 1" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("verifies only the second dialog and its contents are accessible when opened", async () => {
+    const user = userEvent.setup();
+    render(<NestedModals />, { wrapper: CarbonProvider });
+
+    await user.click(screen.getByRole("button", { name: "Open modal 1" }));
+    await user.click(screen.getByRole("button", { name: "Open modal 2" }));
+
+    expect(screen.getByRole("dialog", { name: "dialog 2" })).toHaveAttribute(
+      "aria-modal",
+      "true"
+    );
+    expect(
+      screen.getByRole("button", { name: "Close modal 2" })
+    ).toBeInTheDocument();
+    expect(screen.queryByRole("dialog", { name: "dialog 1" })).toHaveAttribute(
+      "aria-modal",
+      "false"
+    );
+    expect(
+      screen.queryByRole("button", { name: "Open modal 1" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Open modal 2" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("verifies only first modal and its contents are accessible after closing the second modal", async () => {
+    const user = userEvent.setup();
+    render(<NestedModals />, { wrapper: CarbonProvider });
+
+    await user.click(screen.getByRole("button", { name: "Open modal 1" }));
+    await user.click(screen.getByRole("button", { name: "Open modal 2" }));
+    await user.click(screen.getByRole("button", { name: "Close modal 2" }));
+
+    expect(screen.getByRole("dialog", { name: "dialog 1" })).toHaveAttribute(
+      "aria-modal",
+      "true"
+    );
+    expect(
+      screen.getByRole("button", { name: "Open modal 2" })
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByRole("dialog", { name: "dialog 2" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Open modal 1" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("verifies only the first dialog open button is accessible after closing both modals", async () => {
+    const user = userEvent.setup();
+    render(<NestedModals />, { wrapper: CarbonProvider });
+
+    await user.click(screen.getByRole("button", { name: "Open modal 1" }));
+    await user.click(screen.getByRole("button", { name: "Open modal 2" }));
+    await user.click(screen.getByRole("button", { name: "Close modal 2" }));
+    await user.click(screen.getByRole("button", { name: "Close modal 1" }));
+
+    expect(
+      screen.getByRole("button", { name: "Open modal 1" })
+    ).toBeInTheDocument();
+    await waitFor(() =>
       expect(
-        screen.getByRole("button", { name: "button inside portal" })
-      ).toBeInTheDocument();
+        screen.queryByRole("button", { name: "Open modal 2" })
+      ).not.toBeInTheDocument()
+    );
+  });
+});
+
+it("overrides any pre-existing aria-hidden and inert properties when modal is opened", async () => {
+  const user = userEvent.setup();
+  render(
+    <>
+      <div data-role="old-aria-hidden" aria-hidden="false" />
+      <ModalComponent openButtonText="open" closeButtonText="close" />
+      {/* @ts-expect-error inert property not recognised by React. Support to be added in React 19 https://github.com/facebook/react/pull/24730 */}
+      <div data-role="old-inert" inert="foo" />
+    </>,
+    { wrapper: CarbonProvider }
+  );
+
+  await user.click(screen.getByRole("button", { name: "open" }));
+
+  await waitFor(() =>
+    expect(screen.getByTestId("old-aria-hidden")).toHaveAttribute(
+      "aria-hidden",
+      "true"
+    )
+  );
+  await waitFor(() =>
+    expect(screen.getByTestId("old-inert")).toHaveAttribute("inert", "")
+  );
+});
+
+it("restores any previously-overridden aria-hidden and inert properties when modal is closed", async () => {
+  const user = userEvent.setup();
+  render(
+    <>
+      <div data-role="old-aria-hidden" aria-hidden="false" />
+      <ModalComponent openButtonText="open" closeButtonText="close" />
+      {/* @ts-expect-error inert property not recognised by React. Support to be added in React 19 https://github.com/facebook/react/pull/24730 */}
+      <div data-role="old-inert" inert="foo" />
+    </>,
+    { wrapper: CarbonProvider }
+  );
+
+  await user.click(screen.getByRole("button", { name: "open" }));
+  await user.click(screen.getByRole("button", { name: "close" }));
+
+  await waitFor(() =>
+    expect(screen.getByTestId("old-aria-hidden")).toHaveAttribute(
+      "aria-hidden",
+      "false"
+    )
+  );
+  await waitFor(() =>
+    expect(screen.getByTestId("old-inert")).toHaveAttribute("inert", "foo")
+  );
+});
+
+describe("with additional content in a portal", () => {
+  it("without the inertOptOut flag, the portal content is not in the accessibility tree", async () => {
+    const user = userEvent.setup();
+    render(<ModalWithButtonInPortal />, { wrapper: CarbonProvider });
+
+    await user.click(screen.getByRole("button", { name: "open modal" }));
+
+    expect(
+      screen.queryByRole("button", { name: "button inside portal" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("with the inertOptOut flag, the portal content is in the accessibility tree", async () => {
+    const user = userEvent.setup();
+    render(<ModalWithButtonInPortal inertOptOut />, {
+      wrapper: CarbonProvider,
     });
+
+    await user.click(screen.getByRole("button", { name: "open modal" }));
+
+    expect(
+      screen.getByRole("button", { name: "button inside portal" })
+    ).toBeInTheDocument();
   });
 });
