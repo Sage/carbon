38aaed9f7abaa7c5fe7750d9f9a61b60c3a3b0f3
DipperTheDan <daniel.dipper@sage.com>
2024-10-08 09:35:09 +0100

    fix(action-popover): ensure that opening using the up arrow focuses last element in the menu
    Currently we have a bug whereby when a user presses the up arrow key, the first focusable item in
    the menu is focused. This appears to be a bug as we have code that specifically handles this
    behaviour but is overridden elsewhere in the code. This fix ensures that the last focusable item is
    focused.
    
    fixes #6826

---
+++ src/components/action-popover/__internal__/action-popover-utils.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ActionPopoverItem } from "../action-popover-item/action-popover-item.component";
+
+// Reusable type alias for item types
+type ReactItem = React.ReactChild | React.ReactFragment | React.ReactPortal;
+
+export const getItems = (
+  children: React.ReactNode | React.ReactNode[]
+): ReactItem[] =>
+  React.Children.toArray(children).filter(
+    (child) => React.isValidElement(child) && child.type === ActionPopoverItem
+  );
+
+export const isItemDisabled = (item: ReactItem) =>
+  React.isValidElement(item) && !!item.props?.disabled;
+
+export const findFirstFocusableItem = (items: ReactItem[]): number =>
+  items.findIndex((_, index) => !isItemDisabled(items[index]));
+
+// FIX-ME: FE-6248
+// Once we no longer support Node 16, this function can be removed and `findLastIndex()` can be used in its place.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findLastIndex
+export const findLastFocusableItem = (items: ReactItem[]): number => {
+  for (let i = items.length - 1; i >= 0; i--) {
+    if (!isItemDisabled(items[i])) {
+      return i;
+    }
+  }
+  return -1;
+};
