6dad65378312de9fa40d90113b84380c76dc3de6
James Parslow <james.parslow@sage.com>
2024-11-13 17:19:44 +0000

    chore: replace playwright wrapper with react 18 counterpart
    BREAKING CHANGE: `axe-core` is no longer a public dependency. Its
    previous exposure as one was unintentional, and has now been corrected.

--- src/components/select/multi-select/multi-select.pw.tsx
+++ src/components/select/multi-select/multi-select.pw.tsx
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/experimental-ct-react17";
+import { expect, test } from "@playwright/experimental-ct-react";
 import React from "react";
 import MultiSelect, { MultiSelectProps } from ".";
 import Option from "../option";
