5b8096ab2a5a9e446758ff89b320de183135b1c4
James Parslow <james.parslow@sage.com>
2024-11-14 14:55:04 +0000

    chore(date): suppress typescript errors for component code (not for release)
    to be removed once FE-6648 has been completed

--- src/components/date/__internal__/date-picker/date-picker.component.tsx
+++ src/components/date/__internal__/date-picker/date-picker.component.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import DayPicker, {
   DayPickerProps,
--- src/components/date/__internal__/utils.ts
+++ src/components/date/__internal__/utils.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { Modifier } from "react-day-picker";
 import { format, formatISO, isMatch, parse, parseISO } from "./date-fns-fp";
 
