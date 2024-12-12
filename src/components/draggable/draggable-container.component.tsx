7272a563a7049b38b1ab9c14ed9057e9bbc5638f
tom.davies <thomas.davies@sage.com>
2024-12-12 11:28:07 +0000

    feat: add new useDraggable hook

--- src/components/draggable/draggable-container.component.tsx
+++ src/components/draggable/draggable-container.component.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import React, { forwardRef, useMemo } from "react";
 import { MarginProps } from "styled-system";
 
 import invariant from "invariant";
 import { filterStyledSystemMarginProps } from "../../style/utils";
 import DraggableItem from "./draggable-item/draggable-item.component";
-import DropTarget from "./__internal__/drop-target.component";
+
 import { TagProps } from "../../__internal__/utils/helpers/tags";
+import { StyledDraggableContainer } from "./draggable-item/draggable-item.style";
+import { UseDraggableHandle } from "../../hooks/useDraggable/useDraggable";
+import useDraggable from "../../hooks/useDraggable/useDraggable";
+
+type FlexDirection = "row-reverse" | "row";
 
 export interface DraggableContainerProps extends MarginProps, TagProps {
   /** Callback fired when order is changed */
@@ -25,20 +28,19 @@ export interface DraggableContainerProps extends MarginProps, TagProps {
    * Defines the direction in which the draggable items contents are placed.
    * Can be either "row" or "row-reverse".
    */
-  flexDirection?: "row" | "row-reverse";
+  flexDirection?: FlexDirection;
+  id?: string | number;
 }
 
-const DraggableContainer = ({
+const DraggableContainer = forwardRef<UseDraggableHandle, DraggableContainerProps>(({
   "data-element": dataElement,
   "data-role": dataRole = "draggable-container",
   children,
   getOrder,
   flexDirection = "row",
+  id,
   ...rest
-}: DraggableContainerProps): JSX.Element => {
-  const [draggableItems, setDraggableItems] = useState(
-    React.Children.toArray(children),
-  );
+}: DraggableContainerProps, ref): JSX.Element => {
 
   const hasProperChildren = useMemo(() => {
     const invalidChild = React.Children.toArray(children).find(
@@ -55,80 +57,24 @@ const DraggableContainer = ({
     `\`${DraggableContainer.displayName}\` only accepts children of type \`${DraggableItem.displayName}\`.`,
   );
 
-  const isFirstRender = useRef(true);
-
-  useEffect(() => {
-    if (!isFirstRender.current) {
-      setDraggableItems(React.Children.toArray(children));
-    } else {
-      isFirstRender.current = false;
-    }
-  }, [children]);
-
-  const findItem = (id: string | number) => {
-    const draggableItem = draggableItems.filter((item) => {
-      return React.isValidElement(item) && `${item.props.id}` === id;
-    })[0];
-
-    return {
-      draggableItem,
-      index: draggableItems.indexOf(draggableItem),
-    };
-  };
-
-  const moveItem = (id: string, atIndex: number) => {
-    const { draggableItem, index } = findItem(id);
-
-    // istanbul ignore if
-    if (!draggableItem) return;
-
-    const copyOfDraggableItems = [...draggableItems];
-    copyOfDraggableItems.splice(index, 1);
-    copyOfDraggableItems.splice(atIndex, 0, draggableItem);
-    setDraggableItems(copyOfDraggableItems);
-  };
-
-  const getItemsId = (item?: string | number) => {
-    if (!getOrder) {
-      return;
-    }
-
-    const draggableItemIds = draggableItems.map(
-      (draggableItem) => (draggableItem as { props: { id: string } }).props.id,
-    );
-
-    getOrder(draggableItemIds, item);
-  };
-
   const marginProps = filterStyledSystemMarginProps(rest);
 
+  const [draggableHookContainer, dragState ] = useDraggable({draggableItems: children, id, ref});
+
   return (
-    <DndProvider backend={HTML5Backend}>
-      <DropTarget
-        data-element={dataElement}
-        data-role={dataRole}
-        getOrder={getItemsId}
-        {...marginProps}
-      >
-        {draggableItems.map((item) => {
-          return (
-            React.isValidElement(item) &&
-            React.cloneElement(
-              item as React.ReactElement,
-              {
-                id: `${item.props.id}`,
-                findItem,
-                moveItem,
-                flexDirection,
-              },
-              [item.props.children],
-            )
-          );
-        })}
-      </DropTarget>
-    </DndProvider>
+    <StyledDraggableContainer
+      data-component="draggable-container"
+      data-element={dataElement}
+      data-role={dataRole}
+      flexDirection={flexDirection}
+      isDragging={dragState.type === "is-dragging-over"}
+      childId={dragState.id}
+      {...marginProps}
+    >
+          {draggableHookContainer}
+    </StyledDraggableContainer>
   );
-};
+});
 
 DraggableContainer.displayName = "DraggableContainer";
 
---
+++ src/hooks/useDraggable/__internal__/draggable-container.tsx
@@ -0,0 +1,138 @@
+import React, { useContext, useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback, useMemo, CSSProperties } from "react";
+
+import { monitorForElements, dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+import { isDraggableItemData, DragState } from "./draggable-utils";
+
+import { UseDraggableHandle } from "../useDraggable";
+import DraggableProviderContext from "../draggable-provider-context";
+import guid from "../../../../src/__internal__/utils/helpers/guid";
+import DraggableContainerContext from "../draggable-container-context";
+import DraggableItemContext from "../draggable-item-context";
+
+
+export interface DraggableContainerProps {
+  children?: React.ReactNode;
+  id?: string | number;
+  containerStyle?: CSSProperties;
+  containerNode?: string;
+  setDragState?: React.Dispatch<React.SetStateAction<DragState>>;
+}
+
+const DraggableContainer = forwardRef<UseDraggableHandle, DraggableContainerProps>(({
+  children,
+  id,
+  containerStyle,
+  containerNode ="div",
+  setDragState,
+}: DraggableContainerProps, ref): JSX.Element => {
+  const [list, setList] = useState<React.ReactNode[]>([]);
+
+  const localRegister = (items: React.ReactNode | React.ReactNode[]) => {
+    setList((prevList) => Array.isArray(items) ? [...prevList, ...items] : [...prevList, items]);
+  };
+
+const localMove = useCallback((itemId: string | number, toIndex: number) => {
+    // currently relies on the children of draggable item having a unique id 
+    const fromIndex = list.findIndex((item) => React.isValidElement(item) && item.props.children.props.id === itemId);    
+
+    const copy = [...list]; 
+
+    const [nodeToMove] = copy.splice(fromIndex, 1);
+    copy.splice(toIndex, 0, nodeToMove);
+    setList(copy);
+  }, [list]);
+
+  const { register, lists, move } = useContext(DraggableProviderContext);
+
+  const uniqueId = useRef(guid());
+
+  const effectiveList = useMemo(() => {
+    return list.length > 0 ? list : (lists?.[id || uniqueId.current] || []);
+  }, [list, lists, id]);
+
+  const hasMounted = useRef(false);
+
+  useEffect(() => {
+    if (!hasMounted.current) {
+      if(register){
+        register(id || uniqueId.current, React.Children.toArray(children));
+      } else {
+        localRegister(React.Children.toArray(children));
+      }
+      hasMounted.current = true;
+    }
+  }, [children, id, register]);
+ 
+  useImperativeHandle(ref, () => ({
+    reOrder: (itemId: number | string, toIndex: number) => {
+      localMove(itemId, toIndex);
+    },
+  }));
+
+  const itemRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const element = itemRef.current;
+    if (!element) {
+      return;
+    }
+    combine(
+    dropTargetForElements({
+      element,
+    }),
+    monitorForElements({
+      canMonitor({ source }) {
+        return isDraggableItemData(source.data);
+      },
+      onDrop({ location, source }) {
+
+        const target = location.current.dropTargets[0];
+        if (!target) {
+          return;
+        }
+
+        const sourceData = source.data;
+        const targetData = target.data;
+        if (
+          !isDraggableItemData(sourceData) ||
+          !isDraggableItemData(targetData)
+        ) {
+          return;
+        }
+        const indexOfSource = Number(sourceData.itemIndex);
+        const indexOfTarget = Number(targetData.itemIndex);
+        const destinationId = Number(sourceData.itemId);
+
+        if (indexOfTarget < 0 || indexOfSource < 0) {
+          return;
+        }
+
+        // if move is not passed via context, use local move func instead
+        if(!move){localMove(destinationId, indexOfTarget)}        
+      },
+    })
+  )
+  }, [effectiveList, id, localMove, move]);
+
+  return (
+    <DraggableContainerContext.Provider value={{ columnId: id || uniqueId.current }}>
+      {React.createElement(
+        containerNode,
+        {
+          'data-element': 'use-draggable-container',
+          id: id || uniqueId.current,
+          style: containerStyle,
+          ref: itemRef,
+        },
+          (effectiveList || []).map((child: React.ReactNode, index: number) => (
+            <DraggableItemContext.Provider value={{ index }}>
+              {child}
+            </DraggableItemContext.Provider>
+          ))
+      )}
+    </DraggableContainerContext.Provider>
+  );
+});
+
+export default DraggableContainer;
