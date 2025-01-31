590e8b6a8f11777ec5d4e7a815716ee2b539241d
tom.davies <thomas.davies@sage.com>
2025-01-31 10:50:20 +0000

    temp 1 - backup before trying to move toward on drop target change approach

--- src/hooks/useDraggable/__internal__/draggable-container.tsx
+++ src/hooks/useDraggable/__internal__/draggable-container.tsx
@@ -1,6 +1,19 @@
-import React, { useContext, useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback, useMemo, CSSProperties } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useCallback,
+  useMemo,
+  CSSProperties,
+} from "react";
 
-import { monitorForElements, dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import {
+  monitorForElements,
+  dropTargetForElements,
+} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 import { isDraggableItemData, DragState } from "./draggable-utils";
 
@@ -10,129 +23,152 @@ import guid from "../../../../src/__internal__/utils/helpers/guid";
 import DraggableContainerContext from "../draggable-container-context";
 import DraggableItemContext from "../draggable-item-context";
 
-
 export interface DraggableContainerProps {
   children?: React.ReactNode;
   id?: string | number;
   containerStyle?: CSSProperties;
   containerNode?: string;
-  setDragState?: React.Dispatch<React.SetStateAction<DragState>>;
 }
 
-const DraggableContainer = forwardRef<UseDraggableHandle, DraggableContainerProps>(({
-  children,
-  id,
-  containerStyle,
-  containerNode ="div",
-  setDragState,
-}: DraggableContainerProps, ref): JSX.Element => {
-  const [list, setList] = useState<React.ReactNode[]>([]);
+const DraggableContainer = forwardRef<
+  UseDraggableHandle,
+  DraggableContainerProps
+>(
+  (
+    {
+      children,
+      id,
+      containerStyle,
+      containerNode = "div",
+    }: DraggableContainerProps,
+    ref,
+  ): JSX.Element => {
+    const [list, setList] = useState<React.ReactNode[]>([]);
 
-  const localRegister = (items: React.ReactNode | React.ReactNode[]) => {
-    setList((prevList) => Array.isArray(items) ? [...prevList, ...items] : [...prevList, items]);
-  };
+    const localRegister = (items: React.ReactNode | React.ReactNode[]) => {
+      setList((prevList) =>
+        Array.isArray(items) ? [...prevList, ...items] : [...prevList, items],
+      );
+    };
 
-const localMove = useCallback((itemId: string | number, toIndex: number) => {
-    // currently relies on the children of draggable item having a unique id 
-    const fromIndex = list.findIndex((item) => React.isValidElement(item) && item.props.children.props.id === itemId);    
+    const localMove = useCallback(
+      (itemId: string | number, toIndex: number) => {
+        // currently relies on the children of draggable item having a unique id
+        const fromIndex = list.findIndex(
+          (item) =>
+            React.isValidElement(item) &&
+            item.props.children.props.id === itemId,
+        );
 
-    const copy = [...list]; 
+        const copy = [...list];
 
-    const [nodeToMove] = copy.splice(fromIndex, 1);
-    copy.splice(toIndex, 0, nodeToMove);
-    setList(copy);
-  }, [list]);
+        const [nodeToMove] = copy.splice(fromIndex, 1);
+        copy.splice(toIndex, 0, nodeToMove);
+        setList(copy);
+      },
+      [list],
+    );
 
-  const { register, lists, move } = useContext(DraggableProviderContext);
+    const { register, lists, move } = useContext(DraggableProviderContext);
 
-  const uniqueId = useRef(guid());
+    const uniqueId = useRef(guid());
 
-  const effectiveList = useMemo(() => {
-    return list.length > 0 ? list : (lists?.[id || uniqueId.current] || []);
-  }, [list, lists, id]);
+    const effectiveList = useMemo(() => {
+      return list.length > 0 ? list : lists?.[id || uniqueId.current] || [];
+    }, [list, lists, id]);
 
-  const hasMounted = useRef(false);
+    const hasMounted = useRef(false);
 
-  useEffect(() => {
-    if (!hasMounted.current) {
-      if(register){
-        register(id || uniqueId.current, React.Children.toArray(children));
-      } else {
-        localRegister(React.Children.toArray(children));
+    useEffect(() => {
+      if (!hasMounted.current) {
+        if (register) {
+          register(id || uniqueId.current, React.Children.toArray(children));
+        } else {
+          localRegister(React.Children.toArray(children));
+        }
+        hasMounted.current = true;
       }
-      hasMounted.current = true;
-    }
-  }, [children, id, register]);
- 
-  useImperativeHandle(ref, () => ({
-    reOrder: (itemId: number | string, toIndex: number) => {
-      localMove(itemId, toIndex);
-    },
-  }));
+    }, [children, id, register]);
 
-  const itemRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const element = itemRef.current;
-    if (!element) {
-      return;
-    }
-    combine(
-    dropTargetForElements({
-      element,
-    }),
-    monitorForElements({
-      canMonitor({ source }) {
-        return isDraggableItemData(source.data);
+    useImperativeHandle(ref, () => ({
+      reOrder: (itemId: number | string, toIndex: number) => {
+        localMove(itemId, toIndex);
       },
-      onDrop({ location, source }) {
+    }));
 
-        const target = location.current.dropTargets[0];
-        if (!target) {
-          return;
-        }
+    const itemRef = useRef<HTMLDivElement | null>(null);
 
-        const sourceData = source.data;
-        const targetData = target.data;
-        if (
-          !isDraggableItemData(sourceData) ||
-          !isDraggableItemData(targetData)
-        ) {
-          return;
-        }
-        const indexOfSource = Number(sourceData.itemIndex);
-        const indexOfTarget = Number(targetData.itemIndex);
-        const destinationId = Number(sourceData.itemId);
+    useEffect(() => {
+      const element = itemRef.current;
+      if (!element) {
+        return;
+      }
+    
+      const cleanup = combine(
+        dropTargetForElements({
+          element,
+        }),
+        monitorForElements({
+          canMonitor({ source }) {
+            return isDraggableItemData(source.data);
+          },
+          onDropTargetChange({ location, source }) {
+            const target = location.current.dropTargets[0];
+            if (!target) {
+              return;
+            }
+    
+            const sourceData = source.data;
+            const targetData = target.data;
+            if (
+              !isDraggableItemData(sourceData) ||
+              !isDraggableItemData(targetData)
+            ) {
+              return;
+            }
+            const indexOfSource = Number(sourceData.itemIndex);
+            const indexOfTarget = Number(targetData.itemIndex);
+            const destinationId = Number(sourceData.itemId);
+    
+            if (indexOfTarget < 0 || indexOfSource < 0) {
+              return;
+            }
+    
+            if (!move) {
+              localMove(destinationId, indexOfTarget);
+              return;
+            }
+            return;
+          },
+        })
+      );
+    
+      return () => {
+        cleanup();
+      };
+    }, [move, localMove]);
 
-        if (indexOfTarget < 0 || indexOfSource < 0) {
-          return;
-        }
-
-        // if move is not passed via context, use local move func instead
-        if(!move){localMove(destinationId, indexOfTarget)}        
-      },
-    })
-  )
-  }, [effectiveList, id, localMove, move]);
-
-  return (
-    <DraggableContainerContext.Provider value={{ columnId: id || uniqueId.current }}>
-      {React.createElement(
-        containerNode,
-        {
-          'data-element': 'use-draggable-container',
-          id: id || uniqueId.current,
-          style: containerStyle,
-          ref: itemRef,
-        },
+    return (
+      <DraggableContainerContext.Provider
+        value={{ columnId: id || uniqueId.current }}
+      >
+        {React.createElement(
+          containerNode,
+          {
+            "data-element": "use-draggable-container",
+            id: id || uniqueId.current,
+            style: containerStyle,
+            ref: itemRef,
+          },
           (effectiveList || []).map((child: React.ReactNode, index: number) => (
             <DraggableItemContext.Provider value={{ index }}>
               {child}
             </DraggableItemContext.Provider>
-          ))
-      )}
-    </DraggableContainerContext.Provider>
-  );
-});
+          )),
+        )}
+      </DraggableContainerContext.Provider>
+    );
+  },
+);
 
 export default DraggableContainer;
--- src/hooks/useDraggable/draggable-item-context.tsx
+++ src/hooks/useDraggable/draggable-item-context.tsx
@@ -1,9 +1,11 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
 interface DraggableItemContextType {
   index: number;
 }
 
-const DraggableItemContext = createContext<DraggableItemContextType | null>(null);
+const DraggableItemContext = createContext<DraggableItemContextType | null>(
+  null,
+);
 
 export default DraggableItemContext;
