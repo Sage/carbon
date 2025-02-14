96929ccbc15d3b851b90e547331c716a767b8dd7
edleeks87 <edward.leeks@sage.com>
2025-02-14 10:24:44 +0000

    chore: add post build stuff

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,48 +7,10 @@ import glob from "glob";
 import postcss from "rollup-plugin-postcss";
 import swc from "rollup-plugin-swc3";
 import { fileURLToPath } from "url";
-import url from "@rollup/plugin-url";
 
 // Convert import.meta.url to a file path and get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const getSvgFileName = (filePath) => {
-  const baseDir = path.join(__dirname, "src/components"); // Base directory
-  const searchPattern = path.join(baseDir, `**/${filePath}`); // Search for the file in subdirectories
-
-  const matches = glob.sync(searchPattern, { absolute: true });
-
-  if (matches.length > 0) {
-    return matches[0]; // Return the first match
-  }
-  // Ensure fileName is always a string, not an object
-
-  // try {
-  //   const stats = fs.statSync(filePath);
-  //   if (stats.isFile()) {
-  //     console.log(`${filePath} is a file.`);
-  //   } else {
-  //     console.log(`${filePath} is not a file.`);
-  //   }
-  // } catch (error) {
-  //   console.error(`Error checking file: ${error.message}`);
-  // }
-  // // If `filePath` exists, process it; otherwise, return the default name
-  // if (filePath) {
-  //   // Get the relative path from the 'src' directory (adjust accordingly if needed)
-  //   const relativePath = path.relative(
-  //     path.join(__dirname, 'src'),
-  //     filePath
-  //   );
-
-  //   // Construct the output path based on the relative path, including the directory structure
-  //   return path.join(path.dirname(relativePath), '[name][extname]');
-  // }
-
-  // Return default filename if filePath is not available
-  return `assets/${filePath}`;
-};
 
 export default {
   input: Object.fromEntries(
@@ -65,6 +27,8 @@ export default {
           "**/*.pw.tsx",
           "**/*.test-pw.tsx",
           "**/*.d.ts",
+          "**/*.svg", // Ignore SVGs and move them in post-build
+          "**/test-utils.ts", // Ignore test-utils files
         ],
       })
       .map((file) => [
@@ -82,6 +46,7 @@ export default {
   external: [
     /node_modules/,
     /^[^./]|^\.[^./]|^\.\.[^/]/,
+    /\.svg$/, // Treat all SVGs as external
   ],
   plugins: [
     resolve({
@@ -94,13 +59,6 @@ export default {
       requireReturnsDefault: "auto",
     }),
     json(),
-    url({
-      include: ["**/*.svg"],
-      limit: 0,
-      // fileName: "components/[dirname]/[name][extname]",
-
-      fileName: getSvgFileName("[dirname]/[name][extname]")
-    }),
     postcss({
       extract: true,
       minimize: true,
@@ -123,28 +81,34 @@ export default {
   ],
   output: [
     {
-      dir: path.join(__dirname, "dist/esm"),
+      dir: path.join(__dirname, "dist/lib"),
       format: "cjs",
       // sourcemap: true,
-      entryFileNames: (chunkInfo) => {
+      entryFileNames: ({name}) => {
         // Exclude specific files from the root (like action-popover-utils.js)
-        if (chunkInfo.name.includes("-utils") || chunkInfo.name.includes(".style")) {
-          return "components/[name].js"; // Move to the relevant component folder
+        if (/utils/.test(name) || /.style/.test(name)) {
+          return `components/${name}.js`; // Move to the relevant component folder
         }
         return "[name].js"; // Otherwise, default to the usual naming convention
       },
     },
     {
-      dir: path.join(__dirname, "dist/lib"),
+      dir: path.join(__dirname, "dist/esm"),
       format: "esm",
       // sourcemap: true,
-      entryFileNames: (chunkInfo) => {
+      entryFileNames: ({name}) => {
         // Exclude specific files from the root (like action-popover-utils.js)
-        if (chunkInfo.name.includes("-utils") || chunkInfo.name.includes(".style")) {
-          return "components/[name].js"; // Move to the relevant component folder
+        if (/utils/.test(name) || /style/.test(name)) {
+          return `components/${name}.js`; // Move to the relevant component folder
         }
         return "[name].js"; // Otherwise, default to the usual naming convention
       },
     },
   ],
+  // manualChunks(id) {
+  //   if (/utils/.test(id)) {
+  //     return id; // Force it into a named chunk
+  //   }
+  //   return null; // Let Rollup decide for other files
+  // }
 };
