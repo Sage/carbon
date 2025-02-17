2d9f491f3968781a83d897b15d43fff582767e19
nuria1110 <nuria.torresramon@sage.com>
2025-02-17 17:18:53 +0000

    fix(vertical-divider): ensure default data-component attribute cannot be overridden
    The default data-component value could be overridden due to rest being spread after it had been set.
    
    BREAKING CHANGE: Remove support to override data-component attribute.

--- src/components/vertical-divider/vertical-divider.component.tsx
+++ src/components/vertical-divider/vertical-divider.component.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { SpaceProps } from "styled-system";
 import MenuContext from "../menu/__internal__/menu.context";
 import { StyledVerticalWrapper, StyledDivider } from "./vertical-divider.style";
+import tagComponent, { TagProps } from "../../__internal__/utils/helpers/tags";
 
 type TintRange =
   | 1
@@ -105,7 +106,9 @@ type TintRange =
   | 99
   | 100;
 
-export interface VerticalDividerProps extends SpaceProps {
+export interface VerticalDividerProps
+  extends SpaceProps,
+    Omit<TagProps, "data-component"> {
   /** Shorthand for the height attribute */
   h?: number | string;
   /** Height attribute of the component */
@@ -136,7 +139,6 @@ export const VerticalDivider = ({
 
   return (
     <StyledVerticalWrapper
-      data-component="vertical-divider"
       data-role="vertical-divider"
       p={props.p || 3}
       height={h || height}
@@ -144,6 +146,7 @@ export const VerticalDivider = ({
       {...props}
       as={inMenu ? "li" : "div"}
       aria-hidden={inMenu ?? ariaHidden}
+      {...tagComponent("vertical-divider", props)}
     >
       <StyledDivider data-role="divider" tint={tint} />
     </StyledVerticalWrapper>
