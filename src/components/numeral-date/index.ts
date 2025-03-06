b2a0dcef671e5b30eb9aa2a4d36829a07c3dc3bc
Daniel Dipper <daniel.dipper@sage.com>
2025-03-06 15:12:32 +0000

    feat(numeral-date): allow component to be focused programmatically
    Currently it is not possible to focus the numeral date component programmatically. This changeallows
    consumers to now do that.
    
    BREAKING CHANGE: In order to allow NumeralDate to support a forward ref the typescript interface for
    the component has had to be changed.
    
    fixes #7180

--- src/components/numeral-date/index.ts
+++ src/components/numeral-date/index.ts
@@ -1,2 +1,5 @@
 export { default } from "./numeral-date.component";
-export type { NumeralDateProps } from "./numeral-date.component";
+export type {
+  NumeralDateProps,
+  NumeralDateHandle,
+} from "./numeral-date.component";
