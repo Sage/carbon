d5b4a1e5635c4edf0b3346eba5f2b469ef1b39ce
tom.davies <thomas.davies@sage.com>
2025-03-27 09:22:34 +0000

    temp new

--- src/hooks/useDraggable/index.ts
+++ src/hooks/useDraggable/index.ts
@@ -1,3 +1,4 @@
 export { default } from "./useDraggable";
 export { default as DraggableProvider } from "./draggable-provider";
+export type { DraggableProviderHandle } from "./draggable-provider";
 export type { UseDraggableHandle } from "./useDraggable";
