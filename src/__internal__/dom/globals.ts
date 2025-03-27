dca466eed9f9814452a64b66b4aa06f0f1423a74
edleeks87 <edward.leeks@sage.com>
2025-03-27 13:42:04 +0000

    build: upgrade node to v22

--- src/__internal__/dom/globals.ts
+++ src/__internal__/dom/globals.ts
@@ -55,5 +55,7 @@ export function getDocument(): Document | undefined {
  * @returns The global `navigator` object, if it exists.
  */
 export function getNavigator(): Navigator | undefined {
-  return typeof navigator !== "undefined" ? navigator : undefined;
+  return typeof navigator !== "undefined"
+    ? navigator
+    : /* istanbul ignore next: node versions >=21 have a partial implementation of navigatior */ undefined;
 }
