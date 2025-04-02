a92e54c561fd2b417746bc09706f7ca9d0849092
edleeks87 <edward.leeks@sage.com>
2025-04-02 17:40:43 +0100

    feat(text-editor): add support for `onChange` callback being called with json and html values
    Adds a new argument to the `onChange` callback type so that it can be called with the string value
    and the HTML and JSON formatted values
    
    fix #7285

--- src/components/text-editor/index.ts
+++ src/components/text-editor/index.ts
@@ -1,3 +1,6 @@
 export { default } from "./text-editor.component";
 export { createEmpty, createFromHTML } from "./__internal__";
-export type { TextEditorProps } from "./text-editor.component";
+export type {
+  TextEditorProps,
+  EditorFormattedValues,
+} from "./text-editor.component";
