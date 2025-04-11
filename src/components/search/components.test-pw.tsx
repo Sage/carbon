c0627abebe3d05fcdf3cbe7826b923ddc0d2fd1e
Daniel Dipper <daniel.dipper@sage.com>
2025-04-11 11:59:29 +0100

    test(search): enable skipped accessibility tests

--- src/components/search/components.test-pw.tsx
+++ src/components/search/components.test-pw.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Search, { SearchProps } from ".";
+import Box from "../box";
 
 // eslint-disable-next-line import/prefer-default-export
 export const SearchComponent = (props: SearchProps) => {
@@ -11,5 +12,36 @@ export const SearchComponent = (props: SearchProps) => {
       value={value}
       {...props}
     />
+  );
+};
+
+export const SearchComponentLightBackground = (props: SearchProps) => {
+  const [value, setValue] = React.useState("");
+  return (
+    <Box width="700px" height="108px" bg="#FFFFFF">
+      <Search
+        placeholder="Search..."
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        searchButton
+        {...props}
+      />
+    </Box>
+  );
+};
+
+export const SearchComponentDarkBackground = (props: SearchProps) => {
+  const [value, setValue] = React.useState("");
+  return (
+    <Box width="700px" height="108px" bg="#003349">
+      <Search
+        placeholder="Search..."
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        variant="dark"
+        searchButton
+        {...props}
+      />
+    </Box>
   );
 };
