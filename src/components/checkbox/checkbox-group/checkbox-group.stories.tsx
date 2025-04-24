7a2e10ae000ce73afd2d487e1ba00d0432056345
James Parslow <james.parslow@sage.com>
2025-04-24 17:32:12 +0100

    docs(checkbox-group): separate docs from checkbox
    in order to make clear that Checkbox can be used independently to CheckboxGroup

--- src/components/checkbox/checkbox-group/checkbox-group.stories.tsx
+++ src/components/checkbox/checkbox-group/checkbox-group.stories.tsx
@@ -1,33 +1,130 @@
+import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 import generateStyledSystemProps from "../../../../.storybook/utils/styled-system-props";
-import { CheckboxGroup } from "..";
+import CarbonProvider from "../../carbon-provider";
 
-/**
- * This file is used primarily as a means to generate the props table.
- * It contains the tag: ["hideInSidebar"] so that it is not included in the sidebar.
- */
+import { CheckboxGroup, Checkbox } from "..";
 
 const styledSystemProps = generateStyledSystemProps({
   margin: true,
 });
 
-const meta: Meta<typeof CheckboxGroup> = {
-  title: "CheckboxGroup",
+const meta = {
+  title: "Checkbox Group",
   component: CheckboxGroup,
-  tags: ["hideInSidebar"],
   argTypes: {
     ...styledSystemProps,
   },
   parameters: {
     chromatic: { disableSnapshot: true },
   },
-};
+} satisfies Meta<typeof CheckboxGroup>;
 
 export default meta;
 type Story = StoryObj<typeof CheckboxGroup>;
 
 export const Default: Story = {
+  render: (args) => (
+    <CheckboxGroup {...args}>
+      {["Apple", "Banana", "Cherry", "Date"].map((label) => (
+        <Checkbox key={label} name="fruits" label={label} />
+      ))}
+    </CheckboxGroup>
+  ),
   args: {
-    children: [],
+    legend: "What fruits do you have?",
+  },
+};
+
+export const RequiredGroup: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    required: true,
+  },
+};
+
+export const OptionalGroup: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    isOptional: true,
+  },
+};
+
+export const Inline: Story = {
+  render: (args) => (
+    <CarbonProvider validationRedesignOptIn>
+      <CheckboxGroup {...args}>
+        {["Apple", "Banana", "Cherry", "Date"].map((label) => (
+          <Checkbox key={label} name="fruits" label={label} />
+        ))}
+      </CheckboxGroup>
+    </CarbonProvider>
+  ),
+  args: {
+    inline: true,
+    required: true,
+    legend: "What fruits do you have?",
+  },
+};
+
+export const ErrorState: Story = {
+  render: (args) => (
+    <CarbonProvider validationRedesignOptIn>
+      <CheckboxGroup {...args}>
+        <Checkbox label="Apple" name="Apple" />
+        <Checkbox label="Banana" name="Banana" />
+        <Checkbox label="Cherry" name="Cherry" />
+      </CheckboxGroup>
+    </CarbonProvider>
+  ),
+  args: {
+    error: "One or more fruits must be selected",
+    legend: "What fruits do you have?",
+    required: true,
+  },
+};
+
+export const WarningState: Story = {
+  render: (args) => (
+    <CarbonProvider validationRedesignOptIn>
+      <CheckboxGroup {...args}>
+        <Checkbox label="Apple" name="Apple" />
+        <Checkbox label="Banana" name="Banana" />
+        <Checkbox label="Cherry" name="Cherry" />
+      </CheckboxGroup>
+    </CarbonProvider>
+  ),
+  args: {
+    warning: "One or more fruits are recommended",
+    legend: "What fruits do you have?",
+  },
+};
+
+export const LegacyErrorState: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    error: "One or more fruits must be selected",
+  },
+};
+
+export const LegacyWarningState: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    warning: "One or more fruits are recommended",
+  },
+};
+
+export const LegacyInlineLegend: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    legendInline: true,
+    legendWidth: 20,
+    legendAlign: "left",
+    legendSpacing: 2,
   },
 };
