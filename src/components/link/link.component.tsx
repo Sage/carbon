aaaa4b5f9f533d7953071f70d52b2ca0c168463e
Debra Toranska <dobroslawa.toranska@gmail.com>
2025-05-15 17:36:51 +0100

    refactor(link, menu-item): reverse

--- src/components/link/link.component.tsx
+++ src/components/link/link.component.tsx
@@ -1,118 +1,92 @@
-import React, { forwardRef, useContext, useState } from "react";
-import styled, { css } from "styled-components";
-import LinkBase, { LinkBaseProps } from "./link-base.component";
-import Icon, { IconType } from "../icon";
+import React, { useContext, useEffect, useState } from "react";
+import { StyledLink } from "./link.style";
+import type { Variants } from "./link.style";
 import MenuContext from "../menu/__internal__/menu.context";
+import BatchSelectionContext from "../batch-selection/__internal__/batch-selection.context";
+import { IconType } from "../icon";
+import tagComponent, {
+  TagProps,
+} from "../../__internal__/utils/helpers/tags/tags";
+import BaseLink from "./link-base.component";
 
-export interface LinkProps extends LinkBaseProps {
-  icon?: IconType;
+export interface LinkProps extends React.AriaAttributes, TagProps {
+  href?: string;
   iconAlign?: "left" | "right";
   tooltipMessage?: string;
-  tooltipPosition?: "top" | "right" | "bottom" | "left";
-  removeAriaLabelOnIcon?: boolean;
+  tooltipPosition?: "bottom" | "left" | "right" | "top";
+  children?: React.ReactNode;
+  target?: string;
+  rel?: string;
   ariaLabel?: string;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement | HTMLButtonElement>;
+  onKeyDown?: React.KeyboardEventHandler<HTMLAnchorElement | HTMLButtonElement>;
+  onMouseDown?: React.MouseEventHandler<HTMLAnchorElement | HTMLButtonElement>;
+  removeAriaLabelOnIcon?: boolean;
+  isSkipLink?: boolean;
+  icon?: IconType;
+  disabled?: boolean;
+  className?: string;
+  variant?: Variants;
   isDarkBackground?: boolean;
-  maxWidth?: boolean;
 }
 
-const StyledLinkContent = styled.span<{ $maxWidth?: boolean }>`
-  display: inline-flex;
-  align-items: center;
-
-  ${({ $maxWidth }) =>
-    $maxWidth &&
-    css`
-      max-width: inherit;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      vertical-align: bottom;
-    `}
-`;
-
-const IconWrapper = styled.span<{
-  iconAlign: "left" | "right";
-  hasChildren: boolean;
-}>`
-  position: relative;
-
-  ${({ iconAlign, hasChildren }) =>
-    !hasChildren
-      ? css`
-          margin: 0;
-        `
-      : iconAlign === "left"
-      ? css`
-          margin-right: var(--spacing100);
-        `
-      : css`
-          margin-left: var(--spacing100);
-          margin-right: 0;
-        `}
-`;
-
-const Link = forwardRef<HTMLAnchorElement | HTMLButtonElement, LinkProps>(
+const Link = React.forwardRef<HTMLAnchorElement | HTMLButtonElement, LinkProps>(
   (
     {
-      icon,
-      iconAlign = "left",
-      tooltipMessage,
-      tooltipPosition,
-      removeAriaLabelOnIcon,
-      ariaLabel,
       children,
-      maxWidth,
+      className,
+      iconAlign,
+      variant = "default",
+      isDarkBackground,
+      isSkipLink,
+      href,
+      onClick,
+      disabled,
       ...rest
     },
-    ref
+    ref,
   ) => {
-    const { inMenu } = useContext(MenuContext);
     const [hasFocus, setHasFocus] = useState(false);
-    const hasChildren = !!children;
-    const isDisabled = rest.disabled || false;
+    const { inMenu } = useContext(MenuContext);
+    const { batchSelectionDisabled } = useContext(BatchSelectionContext);
 
-    const renderIcon = (alignment: "left" | "right") => {
-      if (icon && iconAlign === alignment) {
-        return (
-          <IconWrapper
-            iconAlign={iconAlign}
-            hasChildren={hasChildren}
-            data-testid="icon-wrapper"
-            data-role="icon-wrapper"
-          >
-            <Icon
-              type={icon}
-              disabled={isDisabled}
-              ariaLabel={removeAriaLabelOnIcon ? undefined : ariaLabel}
-              tooltipMessage={tooltipMessage}
-              tooltipPosition={tooltipPosition}
-              data-testid="icon"
-            />
-          </IconWrapper>
-        );
+    const isDisabled = disabled || batchSelectionDisabled;
+
+    useEffect(() => {
+      if (isDisabled || !(href || onClick)) {
+        setHasFocus(false);
       }
-      return null;
-    };
-
-    const componentProps = {
-      ref,
-      ...rest,
-      ...(ariaLabel && { "aria-label": ariaLabel }),
-      onFocus: () => setHasFocus(true),
-      onBlur: () => setHasFocus(false),
-      "data-testid": "link-base",
-    };
+    }, [isDisabled, href, onClick]);
 
     return (
-      <LinkBase {...componentProps}>
-        <StyledLinkContent $maxWidth={maxWidth} data-testid="link-content">
-          {iconAlign === "left" && renderIcon("left")}
-          {hasChildren && <span>{children}</span>}
-          {iconAlign === "right" && renderIcon("right")}
-        </StyledLinkContent>
-      </LinkBase>
+      <StyledLink
+        isSkipLink={isSkipLink}
+        disabled={isDisabled}
+        iconAlign={iconAlign}
+        className={className}
+        hasContent={Boolean(children)}
+        variant={variant}
+        isDarkBackground={isDarkBackground}
+        isMenuItem={inMenu}
+        {...tagComponent("link", rest)}
+        {...(isSkipLink && { "data-element": "skip-link" })}
+        hasFocus={hasFocus}
+      >
+        <BaseLink
+          {...rest}
+          ref={ref}
+          href={href}
+          onClick={onClick}
+          disabled={isDisabled}
+          isSkipLink={isSkipLink}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+        >
+          {children}
+        </BaseLink>
+      </StyledLink>
     );
-  }
+  },
 );
 
 Link.displayName = "Link";
