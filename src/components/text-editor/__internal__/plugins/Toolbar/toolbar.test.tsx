e77c7fb8dc0a9b41ccfd66e2b57e1a5f4cb52013
tom.davies <thomas.davies@sage.com>
2025-05-20 15:14:49 +0100

    test(toolbar): add onChange test cases

--- src/components/text-editor/__internal__/plugins/Toolbar/toolbar.test.tsx
+++ src/components/text-editor/__internal__/plugins/Toolbar/toolbar.test.tsx
@@ -11,6 +11,7 @@ import React from "react";
 
 import userEvent from "@testing-library/user-event";
 import { ToolbarPlugin } from "..";
+import TextEditor from "../../../text-editor.component";
 
 function headlessEditor() {
   const editor = createHeadlessEditor({
@@ -180,6 +181,22 @@ describe("Events", () => {
     expect(dispatchSpy).toHaveBeenCalledWith(FORMAT_TEXT_COMMAND, "bold");
   });
 
+  /** Have to assert that the onChange mock count has increased by 1 due to the editor attempting to repeatedly create update listeners when the
+   * tests are run, causing the onChange to be fired during initial render. */
+  it("dispatches an 'onChange' event when the bold button is clicked within the TextEditor", async () => {
+    const user = userEvent.setup();
+    const mockOnChange = jest.fn();
+
+    render(<TextEditor labelText="foo" onChange={mockOnChange} />);
+
+    const callCountBefore = mockOnChange.mock.calls.length;
+
+    const boldButton = screen.getByRole("button", { name: "Bold" });
+    await user.click(boldButton);
+
+    expect(mockOnChange).toHaveBeenCalledTimes(callCountBefore + 1);
+  });
+
   /** Using the mocked toolbar, test that clicking the italic button fires the correct event */
   it("dispatches the 'italic' event when the italic button is clicked", async () => {
     const user = userEvent.setup();
@@ -207,6 +224,22 @@ describe("Events", () => {
     await user.click(italicButton);
     expect(dispatchSpy).toHaveBeenCalledTimes(1);
     expect(dispatchSpy).toHaveBeenCalledWith(FORMAT_TEXT_COMMAND, "italic");
+  });
+
+  /** Have to assert that the onChange mock count has increased by 1 due to the editor attempting to repeatedly create update listeners when the
+   * tests are run, causing the onChange to be fired during initial render. */
+  it("dispatches an 'onChange' event when the italic button is clicked within the TextEditor", async () => {
+    const user = userEvent.setup();
+    const mockOnChange = jest.fn();
+
+    render(<TextEditor labelText="foo" onChange={mockOnChange} />);
+
+    const callCountBefore = mockOnChange.mock.calls.length;
+
+    const italicButton = screen.getByRole("button", { name: "Italic" });
+    await user.click(italicButton);
+
+    expect(mockOnChange).toHaveBeenCalledTimes(callCountBefore + 1);
   });
 
   /** Using the mocked toolbar, test that clicking the ordered list button fires the correct event */
@@ -238,6 +271,24 @@ describe("Events", () => {
     expect(dispatchSpy).toHaveBeenCalledWith(FORMAT_TEXT_COMMAND, "number");
   });
 
+  /** Have to assert that the onChange mock count has increased by 1 due to the editor attempting to repeatedly create update listeners when the
+   * tests are run, causing the onChange to be fired during initial render. */
+  it("dispatches an 'onChange' event when the ordered list button is clicked within the TextEditor", async () => {
+    const user = userEvent.setup();
+    const mockOnChange = jest.fn();
+
+    render(<TextEditor labelText="foo" onChange={mockOnChange} />);
+
+    const callCountBefore = mockOnChange.mock.calls.length;
+
+    const orderedListButton = screen.getByRole("button", {
+      name: "Ordered list",
+    });
+    await user.click(orderedListButton);
+
+    expect(mockOnChange).toHaveBeenCalledTimes(callCountBefore + 1);
+  });
+
   /** Using the mocked toolbar, test that clicking the unordered list button fires the correct event */
   it("dispatches the 'unordered list' event when the unordered list button is clicked", async () => {
     const user = userEvent.setup();
@@ -265,5 +316,23 @@ describe("Events", () => {
     await user.click(ulButton);
     expect(dispatchSpy).toHaveBeenCalledTimes(1);
     expect(dispatchSpy).toHaveBeenCalledWith(FORMAT_TEXT_COMMAND, "bullet");
+  });
+
+  /** Have to assert that the onChange mock count has increased by 1 due to the editor attempting to repeatedly create update listeners when the
+   * tests are run, causing the onChange to be fired during initial render. */
+  it("dispatches an 'onChange' event when the unordered ordered list button is clicked within the TextEditor", async () => {
+    const user = userEvent.setup();
+    const mockOnChange = jest.fn();
+
+    render(<TextEditor labelText="foo" onChange={mockOnChange} />);
+
+    const callCountBefore = mockOnChange.mock.calls.length;
+
+    const unorderedListButton = screen.getByRole("button", {
+      name: "Unordered list",
+    });
+    await user.click(unorderedListButton);
+
+    expect(mockOnChange).toHaveBeenCalledTimes(callCountBefore + 1);
   });
 });
