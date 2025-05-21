3291598afae8746b76b04087f0feaa4d0819861b
Daniel Dipper <daniel.dipper@sage.com>
2025-05-21 11:28:20 +0100

    feat: styled system replacement poc work

---
+++ src/components/dips-box/dips-box.component.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyledDipsBox } from "./dips-box.style";
+import { PaddingProps, MarginProps } from "./utils/spacing-types";
+
+export interface DipsBoxProps extends PaddingProps, MarginProps {
+  children?: React.ReactNode;
+}
+
+export const DipsBox = React.forwardRef<HTMLDivElement, DipsBoxProps>(
+  (props, ref) => {
+    const { children, ...rest } = props;
+
+    return (
+      <StyledDipsBox ref={ref} {...rest}>
+        {children}
+      </StyledDipsBox>
+    );
+  },
+);
+
+export default DipsBox;
+DipsBox.displayName = "DipsBox";
---
+++ src/components/dips-box/utils/spacing-types.ts
@@ -0,0 +1,29 @@
+export interface PaddingProps {
+  padding?: string | number;
+  p?: string | number;
+  pt?: string | number;
+  pr?: string | number;
+  pb?: string | number;
+  pl?: string | number;
+  px?: string | number;
+  py?: string | number;
+  paddingTop?: string | number;
+  paddingRight?: string | number;
+  paddingBottom?: string | number;
+  paddingLeft?: string | number;
+}
+
+export interface MarginProps {
+  margin?: string | number;
+  m?: string | number;
+  mt?: string | number;
+  mr?: string | number;
+  mb?: string | number;
+  ml?: string | number;
+  mx?: string | number;
+  my?: string | number;
+  marginTop?: string | number;
+  marginRight?: string | number;
+  marginBottom?: string | number;
+  marginLeft?: string | number;
+}
