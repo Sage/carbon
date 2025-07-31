9165f5e80c537ec3e1ab7adf3735608f3716ea03
nuria1110 <nuria.torresramon@sage.com>
2025-07-31 16:41:09 +0200

    feat(badge): align with beta DS
    Aligns `Badge` component to the designs for the new beta DS. New `size`, `variant` and `inverse`
    props have been added and `children` are now optional, which allows consumers to render a standalone
    `Badge`. `counter` prop now accepts a number or string value of up to 4 characters, including a `+`
    sign. Props `onClick`, `aria-label` and `color` have been deprecated.

--- src/components/badge/badge.style.ts
+++ src/components/badge/badge.style.ts
@@ -1,107 +1,164 @@
 import styled, { css } from "styled-components";
+import { margin } from "styled-system";
 import StyledIcon from "../icon/icon.style";
 import Button from "../button";
-import Icon from "../icon";
 import applyBaseTheme from "../../style/themes/apply-base-theme";
 import { toColor } from "../../style/utils/color";
 
-const commonStyles = `
-  overflow: hidden;
-  border-radius: var(--borderRadiusCircle);
-  position: absolute;
-  top: -14px;
-  right: -4px;
-  padding: 0;
-  margin-right: 0;
-  background: var(--colorsActionMajorYang100);
-`;
+// size of badge + 2px border
+const getSize = (size?: string) => {
+  switch (size) {
+    case "small":
+      return css`
+        width: 12px;
+        height: 12px;
+      `;
+    case "large":
+      return css`
+        min-width: 28px;
+        height: 28px;
+        padding: 0px 4px 1px;
+      `;
+    // medium
+    default:
+      return css`
+        min-width: 24px;
+        height: 24px;
+        padding: 0px 2px 1px;
+      `;
+  }
+};
+
+// TODO: replace with design tokens
+const getVariantColor = (variant?: string, inverse?: boolean) => {
+  switch (variant) {
+    case "subtle":
+      return `
+        ${inverse ? "#007ED9" : "#0060A7"};
+      `;
+    // typical
+    default:
+      return `
+        ${inverse ? "#E13E53" : "#CD384B"};
+      `;
+  }
+};
+
+const getPosition = (size?: string) => {
+  switch (size) {
+    case "small":
+      return css`
+        top: -3px;
+        right: -2px;
+      `;
+    case "large":
+      return css`
+        top: -14px;
+        right: -8px;
+      `;
+    // medium
+    default:
+      return css`
+        top: -12px;
+        right: -8px;
+      `;
+  }
+};
 
 const StyledBadgeWrapper = styled.div`
+  ${margin}
   position: relative;
   display: inline-block;
 `;
 
 const StyledCounter = styled.div`
   font-weight: 500;
-  font-size: 12px;
-  margin-top: -1px;
+  font-size: 13px;
+  line-height: 150%;
 `;
 
 interface StyledBadgeProps {
-  color: string;
-  isFocused?: boolean;
-  isHovered?: boolean;
+  customColor?: string;
+  size?: "small" | "medium" | "large";
+  variant?: "typical" | "subtle";
+  inverse?: boolean;
+  hasChildren?: boolean;
 }
 
 const StyledBadge = styled.span.attrs(applyBaseTheme).attrs(({ onClick }) => ({
   as: onClick ? Button : undefined,
 }))<StyledBadgeProps>`
-  ${commonStyles}
-  cursor: default;
-  align-items: center;
+  ${margin}
+  box-sizing: border-box;
   display: inline-flex;
+  align-items: center;
   justify-content: center;
-  width: 22px;
-  min-height: 22px;
-  border: solid 2px transparent;
-  z-index: 2;
+  overflow: hidden;
+  border-radius: 999px;
+  border: solid 2px;
 
-  ${({ color, theme }) => css`
-    border-color: ${toColor(theme, color)};
-    color: ${toColor(theme, color)};
-  `};
+  ${({
+    size,
+    variant,
+    inverse,
+    hasChildren,
+    customColor,
+    theme,
+    onClick,
+  }) => css`
+    ${getSize(size)};
+    background-color: ${getVariantColor(variant, inverse)};
 
-  ::-moz-focus-inner {
-    border: none;
-  }
+    border-color: ${inverse
+      ? "var(--colorsUtilityYin100)"
+      : "var(--colorsUtilityYang100)"};
+    color: ${inverse
+      ? "var(--colorsUtilityYin100)"
+      : "var(--colorsUtilityYang100)"};
 
-  ${({ onClick, color, theme, isFocused, isHovered }) => css`
+    ${hasChildren &&
+    css`
+      position: absolute;
+      z-index: 2;
+      ${getPosition(size)};
+    `}
+
     ${onClick &&
-    `
-      ${commonStyles}
-      width: 26px;
-      min-height: 26px;
-      text-align: center;
+    css`
+      min-height: 0;
+      :hover,
+      :focus {
+        padding: 0;
+        border-color: ${getVariantColor(variant, inverse)};
+        background-color: ${getVariantColor(variant, inverse)};
 
-      ::-moz-focus-inner {
-        border: none;
-      }
-
-      border-color: ${toColor(theme, color)};
-      color: ${toColor(theme, color)};
-
-      ${
-        (isFocused || isHovered) &&
-        `
-        && {
-          background: ${toColor(theme, color)};
-          border: none;
-          ${StyledCounter} {
-            display: none;
-          }
-
-          ${StyledIcon} {
-            display: block;
-            width: auto;
-            height: auto;
-            margin-right: 0;
-
-            :before {
-              font-size: 20px;
-              color: var(--colorsActionMajorYang100);
-            }
-          }
+        ${StyledIcon} {
+          margin: 0;
+          color: ${inverse
+            ? "var(--colorsUtilityYin100)"
+            : "var(--colorsUtilityYang100)"};
         }
-      `
       }
-      }
+    `}
+
+    ${customColor &&
+    css`
+      background-color: var(--colorsUtilityYang100);
+      border-color: ${toColor(theme, customColor)};
+      color: ${toColor(theme, customColor)};
+
+      ${onClick &&
+      // tested in playwright, RTL cannot test pseudo-classes
+      /* istanbul ignore next */
+      css`
+        :hover,
+        :focus {
+          background-color: ${toColor(theme, customColor)};
+          border-color: ${toColor(theme, customColor)};
+        }
+      `}
     `}
   `}
 `;
 
-const StyledCrossIcon = styled(Icon)`
-  margin: 0;
-  display: none;
-`;
-
-export { StyledBadge, StyledBadgeWrapper, StyledCrossIcon, StyledCounter };
+export { StyledBadge, StyledBadgeWrapper, StyledCounter };
--- src/components/badge/badge.test.tsx
+++ src/components/badge/badge.test.tsx
@@ -2,159 +2,306 @@ import React from "react";
 import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Badge from "./badge.component";
+import Logger from "../../__internal__/utils/logger";
 
-const renderComponent = (props = {}) =>
+jest.mock("../../__internal__/utils/logger");
+
+test("should log a deprecation warning for onClick prop", () => {
+  const loggerSpy = jest.spyOn(Logger, "deprecate");
+  render(<Badge onClick={() => {}} />);
+
+  expect(loggerSpy).toHaveBeenCalledWith(
+    "The `onClick` prop in `Badge` is deprecated and will soon be removed.",
+  );
+  expect(loggerSpy).toHaveBeenCalledTimes(1);
+
+  loggerSpy.mockRestore();
+  loggerSpy.mockClear();
+});
+
+test("should log a deprecation warning for aria-label prop", () => {
+  const loggerSpy = jest.spyOn(Logger, "deprecate");
+  render(<Badge aria-label="Test label" />);
+
+  expect(loggerSpy).toHaveBeenCalledWith(
+    "The `aria-label` prop in `Badge` is deprecated and will soon be removed.",
+  );
+  expect(loggerSpy).toHaveBeenCalledTimes(1);
+
+  loggerSpy.mockRestore();
+  loggerSpy.mockClear();
+});
+
+test("should log a deprecation warning for color prop", () => {
+  const loggerSpy = jest.spyOn(Logger, "deprecate");
+  render(<Badge color="red" />);
+
+  expect(loggerSpy).toHaveBeenCalledWith(
+    "The `color` prop in `Badge` is deprecated and will soon be removed.",
+  );
+  expect(loggerSpy).toHaveBeenCalledTimes(1);
+
+  loggerSpy.mockRestore();
+  loggerSpy.mockClear();
+});
+
+test("should render badge with children", () => {
   render(
-    <Badge data-role="badge" {...props}>
-      Foo
+    <Badge counter={5}>
+      <span>Test Badge</span>
     </Badge>,
   );
 
-describe("Badge", () => {
-  it("should render number when counter is between 1 and 99", () => {
-    renderComponent({ counter: 50 });
+  expect(screen.getByText("Test Badge")).toBeVisible();
+  expect(screen.getByText("5")).toBeVisible();
+});
 
-    expect(screen.getByText("50")).toBeVisible();
+test("should render counter as a number", () => {
+  render(<Badge counter={50} />);
+
+  expect(screen.getByText("50")).toBeVisible();
+});
+
+test("should render counter as a string", () => {
+  render(<Badge counter="99+" />);
+
+  expect(screen.getByText("99+")).toBeVisible();
+});
+
+test("should render `999+` when counter is a number higher than 999", () => {
+  render(<Badge counter={1000} />);
+
+  expect(screen.getByText("999+")).toBeVisible();
+});
+
+test("should trim the counter when it is a string longer than 4 characters", () => {
+  render(<Badge counter="12345" />);
+
+  expect(screen.getByText("1234")).toBeVisible();
+});
+
+test("should not render badge when counter is not set", () => {
+  render(<Badge data-role="badge" />);
+
+  expect(screen.queryByTestId("badge")).not.toBeInTheDocument();
+});
+
+test("should not render badge when counter is `0` as a number", () => {
+  render(<Badge data-role="badge" counter={0} />);
+
+  expect(screen.queryByTestId("badge")).not.toBeInTheDocument();
+});
+
+test("should not render badge when counter is a decimal number", () => {
+  render(<Badge data-role="badge" counter={3.14} />);
+
+  expect(screen.queryByTestId("badge")).not.toBeInTheDocument();
+});
+
+test("should not render badge when counter is an empty string", () => {
+  render(<Badge data-role="badge" counter="" />);
+
+  expect(screen.queryByTestId("badge")).not.toBeInTheDocument();
+});
+
+test("should not render counter if size is small", () => {
+  render(<Badge data-role="badge" counter={9} size="small" />);
+
+  expect(screen.queryByText("9")).not.toBeInTheDocument();
+  expect(screen.queryByTestId("badge")).toBeVisible();
+});
+
+test("should render with provided id", () => {
+  render(<Badge data-role="badge" counter={9} id="custom-id" />);
+
+  const badge = screen.getByTestId("badge");
+  expect(badge).toHaveAttribute("id", "custom-id");
+});
+
+test("should not render as a button if onClick is not set", () => {
+  render(<Badge counter={9} />);
+
+  expect(screen.getByText("9")).toBeVisible();
+  expect(screen.queryByRole("button")).not.toBeInTheDocument();
+});
+
+test("should render as a button when onClick is set", () => {
+  render(<Badge counter={9} onClick={() => {}} />);
+
+  expect(screen.getByText("9")).toBeVisible();
+  expect(screen.getByRole("button")).toBeVisible();
+});
+
+test("should not render as a button when onClick is set and size is small", () => {
+  render(
+    <Badge data-role="badge" counter={9} onClick={() => {}} size="small" />,
+  );
+
+  expect(screen.getByTestId("badge")).toBeVisible();
+  expect(screen.queryByRole("button")).not.toBeInTheDocument();
+});
+
+test("calls onClick callback when badge is clicked", async () => {
+  const onClick = jest.fn();
+  const user = userEvent.setup();
+
+  render(<Badge counter={9} onClick={onClick} />);
+
+  const badgeButton = screen.getByRole("button");
+  await user.click(badgeButton);
+
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
+
+// coverage
+test("should render cross icon when onClick is set and badge is focused", async () => {
+  render(<Badge counter={9} onClick={() => {}} />);
+
+  const badgeButton = screen.getByRole("button");
+  let badgeCounter = screen.getByText("9");
+
+  expect(badgeCounter).toBeVisible();
+
+  act(() => {
+    badgeButton.focus();
   });
 
-  it("should render number `99` when counter is higher than 99", () => {
-    renderComponent({ counter: 101 });
+  const crossIcon = await screen.findByTestId("badge-cross-icon");
+  expect(crossIcon).toBeVisible();
+  expect(badgeCounter).not.toBeInTheDocument();
 
-    expect(screen.getByText("99")).toBeVisible();
+  act(() => {
+    badgeButton.blur();
   });
 
-  it("should not render badge when counter is `0`", () => {
-    renderComponent({ counter: 0, onClick: () => {} });
+  badgeCounter = await screen.findByText("9");
+  expect(crossIcon).not.toBeInTheDocument();
+  expect(badgeCounter).toBeVisible();
+});
 
-    expect(screen.queryByText("0")).not.toBeInTheDocument();
+// coverage
+test("should render cross icon when onClick is set and badge is hovered", async () => {
+  const user = userEvent.setup();
+  render(<Badge counter={9} onClick={() => {}} inverse />);
+
+  const badgeButton = screen.getByRole("button");
+  let badgeCounter = screen.getByText("9");
+
+  expect(badgeCounter).toBeVisible();
+
+  await user.hover(badgeButton);
+
+  const crossIcon = await screen.findByTestId("badge-cross-icon");
+  expect(crossIcon).toBeVisible();
+  expect(badgeCounter).not.toBeInTheDocument();
+
+  await user.unhover(badgeButton);
+
+  badgeCounter = await screen.findByText("9");
+  expect(crossIcon).not.toBeInTheDocument();
+  expect(badgeCounter).toBeVisible();
+});
+
+test("should have the relevant aria-label when aria-label is specified", () => {
+  render(<Badge counter={9} onClick={() => {}} aria-label="Remove filters" />);
+
+  const badgeButton = screen.getByRole("button");
+  expect(badgeButton).toHaveAccessibleName("Remove filters");
+});
+
+test("should render with provided data- attributes", () => {
+  render(<Badge counter={9} data-element="bar" data-role="baz" />);
+
+  expect(screen.getByTestId("baz")).toHaveAttribute("data-element", "bar");
+});
+
+// coverage
+test("should render with correct style when color prop is specified", () => {
+  render(
+    <Badge data-role="badge" counter={9} color="--colorsSemanticNegative500" />,
+  );
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyleRule(
+    "border-color",
+    "var(--colorsSemanticNegative500)",
+  );
+  expect(badge).toHaveStyleRule("color", "var(--colorsSemanticNegative500)");
+});
+
+// coverage
+test("should render with correct style when variant is typical", () => {
+  render(<Badge data-role="badge" counter={9} variant="typical" />);
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyle({ backgroundColor: "#CD384B" });
+});
+
+// coverage
+test("should render with correct style when variant is subtle", () => {
+  render(<Badge data-role="badge" counter={9} variant="subtle" />);
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyle({ backgroundColor: "#0060A7" });
+});
+
+// coverage
+test("should render with correct style when variant is typical and inverse prop is true", () => {
+  render(<Badge data-role="badge" counter={9} inverse variant="typical" />);
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyle({ backgroundColor: "#E13E53" });
+  expect(badge).toHaveStyleRule("border-color", "var(--colorsUtilityYin100)");
+  expect(badge).toHaveStyleRule("color", "var(--colorsUtilityYin100)");
+});
+
+// coverage
+test("should render with correct style when variant is subtle and inverse prop is true", () => {
+  render(<Badge data-role="badge" counter={9} inverse variant="subtle" />);
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyle({ backgroundColor: "#007ED9" });
+  expect(badge).toHaveStyleRule("border-color", "var(--colorsUtilityYin100)");
+  expect(badge).toHaveStyleRule("color", "var(--colorsUtilityYin100)");
+});
+
+// coverage
+test("should render with correct style size is small and badge has children", () => {
+  render(
+    <Badge data-role="badge" counter={9} size="small">
+      Test
+    </Badge>,
+  );
+
+  const badge = screen.getByTestId("badge");
+
+  expect(badge).toHaveStyle({
+    width: "12px",
+    height: "12px",
+    top: "-3px",
+    right: "-2px",
   });
+});
 
-  it("should not render badge when counter is not set", () => {
-    renderComponent({ onClick: () => {} });
+// coverage
+test("should render with correct style size is large and badge has children", () => {
+  render(
+    <Badge data-role="badge" counter={9} size="large">
+      Test
+    </Badge>,
+  );
 
-    expect(screen.queryByRole("button")).not.toBeInTheDocument();
-  });
+  const badge = screen.getByTestId("badge");
 
-  it("should render as a button element when onClick is set", () => {
-    renderComponent({ counter: 9, onClick: () => {} });
-
-    expect(screen.getByText("9")).toBeVisible();
-    expect(screen.getByRole("button")).toBeVisible();
-  });
-
-  it("should hide the counter text when the badge is focused and displays it when blurred", () => {
-    renderComponent({ counter: 9, onClick: () => {} });
-
-    const badgeButton = screen.getByRole("button");
-    const badgeText = screen.getByText("9");
-
-    act(() => {
-      badgeButton.focus();
-    });
-
-    expect(badgeText).not.toBeVisible();
-
-    act(() => {
-      badgeButton.blur();
-    });
-
-    expect(badgeText).toBeVisible();
-  });
-
-  it("should hide the counter text when the badge is hovered and displays it when unhovered", async () => {
-    const user = userEvent.setup();
-
-    renderComponent({ counter: 9, onClick: () => {} });
-
-    const badgeButton = screen.getByRole("button");
-    const badgeText = screen.getByText("9");
-
-    await user.hover(badgeButton);
-
-    expect(badgeText).not.toBeVisible();
-
-    await user.unhover(badgeButton);
-
-    expect(badgeText).toBeVisible();
-  });
-
-  it("should not render as a button if onClick is not set", () => {
-    renderComponent({ counter: 9 });
-
-    expect(screen.getByText("9")).toBeVisible();
-    expect(screen.queryByRole("button")).not.toBeInTheDocument();
-  });
-
-  it("should have the relevant aria-label when aria-label is specified", () => {
-    renderComponent({
-      counter: 9,
-      onClick: () => {},
-      "aria-label": "Generic aria message",
-    });
-
-    const badgeButton = screen.getByRole("button");
-    expect(badgeButton).toHaveAccessibleName("Generic aria message");
-  });
-
-  it("should render with provided data- attributes", () => {
-    renderComponent({ counter: 9, "data-element": "bar", "data-role": "baz" });
-
-    expect(screen.getByTestId("baz")).toHaveAttribute("data-element", "bar");
-  });
-
-  it("calls onClick callback when badge is clicked", async () => {
-    const onClick = jest.fn();
-    const user = userEvent.setup();
-
-    renderComponent({ counter: 5, onClick });
-
-    const badgeButton = screen.getByRole("button");
-    await user.click(badgeButton);
-
-    expect(onClick).toHaveBeenCalledTimes(1);
-  });
-
-  it("should render with provided id", () => {
-    renderComponent({ counter: 9, id: "custom-id" });
-
-    const badge = screen.getByTestId("badge");
-    expect(badge).toHaveAttribute("id", "custom-id");
-  });
-
-  it("should apply the correct cursor style when onClick is not specified", () => {
-    renderComponent({ counter: 9 });
-
-    const badge = screen.getByTestId("badge");
-    expect(badge).toHaveStyle({ cursor: "default" });
-  });
-
-  it("should apply correct border radius for counter", () => {
-    renderComponent({ counter: 9 });
-
-    const badge = screen.getByTestId("badge");
-    expect(badge).toHaveStyleRule("border-radius", "var(--borderRadiusCircle)");
-  });
-
-  it("should render badge with default style when color prop is not specified", () => {
-    renderComponent({ counter: 9 });
-
-    const badge = screen.getByTestId("badge");
-    expect(badge).toHaveStyleRule(
-      "border-color",
-      "var(--colorsActionMajor500)",
-    );
-    expect(badge).toHaveStyleRule("color", "var(--colorsActionMajor500)");
-  });
-
-  it("should render badge with correct style when color prop is specified", () => {
-    renderComponent({ counter: 9, color: "--colorsSemanticNegative500" });
-
-    const badge = screen.getByTestId("badge");
-
-    expect(badge).toHaveStyleRule(
-      "border-color",
-      "var(--colorsSemanticNegative500)",
-    );
-    expect(badge).toHaveStyleRule("color", "var(--colorsSemanticNegative500)");
+  expect(badge).toHaveStyle({
+    minWidth: "28px",
+    height: "28px",
+    top: "-14px",
+    right: "-8px",
   });
 });
