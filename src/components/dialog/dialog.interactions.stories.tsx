85766bc4d672c053d7b3e43697b44cda098f8e78
Debra Toranska <dobroslawa.toranska@sage.com>
2025-09-08 10:49:06 +0100

    test(dialog): add play functions

---
+++ src/components/dialog/dialog.interactions.stories.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { StoryObj } from "@storybook/react";
+import { userEvent, within, expect, waitFor } from "@storybook/test";
+
+import Dialog from ".";
+import Button from "../button";
+import { allowInteractions } from "../../../.storybook/interaction-toggle/reduced-motion";
+import Textbox from "../textbox";
+import Form from "../form";
+
+type Story = StoryObj<typeof Dialog>;
+
+export default {
+  title: "Dialog/Interactions",
+  component: Dialog,
+  parameters: {
+    layout: "padded",
+    chromatic: { disableSnapshot: true },
+  },
+  decorators: [],
+};
+
+export const FocusManagement: Story = {
+  render: function FocusManagementRender() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <>
+        <Button onClick={() => setIsOpen(true)} data-role="open-dialog-btn">
+          Open Dialog
+        </Button>
+
+        <Dialog
+          open={isOpen}
+          title="Title"
+          subtitle="Subtitle"
+          onCancel={() => setIsOpen(false)}
+          showCloseIcon
+        >
+          <Form
+            stickyFooter
+            leftSideButtons={
+              <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+            }
+            saveButton={
+              <Button buttonType="primary" type="submit">
+                Save
+              </Button>
+            }
+          >
+            <Textbox label="Name" value="" onChange={() => {}} />
+          </Form>
+        </Dialog>
+      </>
+    );
+  },
+
+  play: async ({ canvasElement }) => {
+    if (!allowInteractions()) return;
+
+    const canvas = within(canvasElement);
+    const portal = within(canvasElement.ownerDocument.body);
+
+    const openButton = canvas.getByRole("button", { name: /open dialog/i });
+    await userEvent.click(openButton);
+
+    const dialog = await portal.findByRole("dialog");
+
+    await waitFor(() => expect(dialog).toBeVisible());
+
+    const closeButton = within(dialog).getByRole("button", { name: /close/i });
+    const firstInput = within(dialog).getByLabelText(/name/i);
+
+    await userEvent.tab();
+    await userEvent.tab();
+
+    await expect(firstInput).toHaveFocus();
+
+    await userEvent.tab();
+    await userEvent.tab();
+    await userEvent.tab();
+
+    await expect(closeButton).toHaveFocus();
+  },
+};
+
+FocusManagement.storyName = "Focus Management";
+FocusManagement.parameters = {
+  themeProvider: {
+    chromatic: { theme: "sage" },
+  },
+};
