b9751a55b4596da58f521950397020ed153a7a54
Mihai Albu <mihai.albu@sage.com>
2025-09-23 04:50:06 +0000

    fix(numeral-date): add ability to set ID values against individual inputs
    fixes: #7518

--- src/components/numeral-date/numeral-date.stories.tsx
+++ src/components/numeral-date/numeral-date.stories.tsx
@@ -355,3 +355,27 @@ export const ProgrammaticFocus = () => {
 };
 
 ProgrammaticFocus.storyName = "Programmatic Focus";
+
+export const WithCustomFieldIds: Story = () => {
+  const [dateValue, setDateValue] = useState<NumeralDateProps["value"]>({
+    dd: "01",
+    mm: "02",
+    yyyy: "2020",
+  });
+  const handleChange: NumeralDateProps["onChange"] = (event) => {
+    setDateValue(event.target.value);
+  };
+  return (
+    <NumeralDate
+      value={dateValue}
+      onChange={handleChange}
+      label="Default"
+      inputIds={{
+        day: "date-field-custom-id",
+        month: "month-field-custom-id",
+        year: "year-field-custom-id",
+      }}
+    />
+  );
+};
+WithCustomFieldIds.storyName = "With Custom Field IDs";
