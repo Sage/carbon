3def4de8f09b487a3e19076af90a204a675223bf
Damien Robson <damien.robson@sage.com>
2025-10-03 10:39:32 +0100

    feat(tabs): begin audit alignment work

---
+++ src/components/tabs/new/tabs.types.ts
@@ -0,0 +1,31 @@
+export interface TabPanelProps {
+  children?: React.ReactNode;
+  id: string;
+  index: number;
+  labelledBy: string;
+}
+
+export interface TabListProps {
+  ariaLabel: string;
+  children?: React.ReactNode;
+  orientation?: "horizontal" | "vertical";
+  size?: "medium" | "large";
+}
+
+export interface TabProps {
+  controls: string;
+  id: string;
+  index: number;
+  label: React.ReactNode;
+  leftSlot?: React.ReactNode;
+  rightSlot?: React.ReactNode;
+  size?: "medium" | "large";
+}
+
+export interface TabsProps {
+  children?: React.ReactNode;
+  colorMode?: "light" | "dark";
+  labelledBy?: string;
+  orientation?: "horizontal" | "vertical";
+  size?: "medium" | "large";
+}
