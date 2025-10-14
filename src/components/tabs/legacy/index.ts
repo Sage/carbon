0b4f815589724aabc3a7479ea57bd296b660a88b
Damien Robson <damien.robson@sage.com>
2025-10-14 07:44:15 +0100

    chore(tabs): rename 'legacy' folder to '__legacy__'

--- src/components/tabs/legacy/index.ts
+++
@@ -1,3 +0,0 @@
-export { Tabs, Tab } from "./tabs.component";
-export type { TabsProps } from "./tabs.component";
-export type { TabProps } from "./tab";
--- src/components/tabs/legacy/tab/__internal__/tab.context.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface TabContextProps {
-  setError?: (childId: string, error?: boolean | string) => void;
-  setWarning?: (childId: string, warning?: boolean | string) => void;
-  setInfo?: (childId: string, info?: boolean | string) => void;
-}
-
-export default React.createContext<TabContextProps>({});
