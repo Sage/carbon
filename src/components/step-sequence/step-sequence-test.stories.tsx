844463b1f43ba4f7088dee7673efb061355d3a69
divya jindel <divya.jindel@sage.com>
2026-04-23 09:33:42 +0100

    test(step-sequence): refine testing suite

--- src/components/step-sequence/step-sequence-test.stories.tsx
+++ src/components/step-sequence/step-sequence-test.stories.tsx
@@ -3,6 +3,7 @@ import StepSequence, { StepSequenceProps } from "./step-sequence.component";
 import StepSequenceItem, {
   StepSequenceItemProps,
 } from "./step-sequence-item/step-sequence-item.component";
+import Box from "../box";
 
 export default {
   title: "Step Sequence/Test",
@@ -120,4 +121,114 @@ StepSequenceItemStory.args = {
   hiddenCurrentLabel: "",
   ariaLabel: "Step 1 of 5",
   children: "Step Label",
+};
+
+export const ChromaticSnapshotsStory = () => (
+  <Box display="flex" flexDirection="column" gap={6}>
+    <StepSequence>
+      <StepSequenceItem
+        aria-label="Step 1 of 5"
+        hiddenCompleteLabel="Complete"
+        hiddenCurrentLabel="Current"
+        indicator="1"
+        status="complete"
+      >
+        Name
+      </StepSequenceItem>
+      <StepSequenceItem
+        aria-label="Step 2 of 5"
+        hiddenCompleteLabel="Complete"
+        hiddenCurrentLabel="Current"
+        indicator="2"
+        status="complete"
+      >
+        Delivery Address
+      </StepSequenceItem>
+      <StepSequenceItem
+        aria-label="Step 3 of 5"
+        hiddenCompleteLabel="Complete"
+        hiddenCurrentLabel="Current"
+        indicator="3"
+        status="current"
+      >
+        Delivery Details
+      </StepSequenceItem>
+      <StepSequenceItem
+        aria-label="Step 4 of 5"
+        hiddenCompleteLabel="Complete"
+        hiddenCurrentLabel="Current"
+        indicator="4"
+        status="incomplete"
+      >
+        Payment
+      </StepSequenceItem>
+      <StepSequenceItem
+        aria-label="Step 5 of 5"
+        hiddenCompleteLabel="Complete"
+        hiddenCurrentLabel="Current"
+        indicator="5"
+        status="incomplete"
+      >
+        Confirm
+      </StepSequenceItem>
+    </StepSequence>
+
+    <Box height="420px">
+      <StepSequence orientation="vertical">
+        <StepSequenceItem
+          aria-label="Step 1 of 5"
+          hiddenCompleteLabel="Complete"
+          hiddenCurrentLabel="Current"
+          indicator="1"
+          status="complete"
+        >
+          Name
+        </StepSequenceItem>
+        <StepSequenceItem
+          aria-label="Step 2 of 5"
+          hiddenCompleteLabel="Complete"
+          hiddenCurrentLabel="Current"
+          indicator="2"
+          status="complete"
+        >
+          Delivery Address
+        </StepSequenceItem>
+        <StepSequenceItem
+          aria-label="Step 3 of 5"
+          hiddenCompleteLabel="Complete"
+          hiddenCurrentLabel="Current"
+          indicator="3"
+          status="current"
+        >
+          Delivery Details
+        </StepSequenceItem>
+        <StepSequenceItem
+          aria-label="Step 4 of 5"
+          hiddenCompleteLabel="Complete"
+          hiddenCurrentLabel="Current"
+          indicator="4"
+          hideIndicator
+          status="incomplete"
+        >
+          Payment
+        </StepSequenceItem>
+        <StepSequenceItem
+          aria-label="Step 5 of 5"
+          hiddenCompleteLabel="Complete"
+          hiddenCurrentLabel="Current"
+          indicator="5"
+          hideIndicator
+          status="incomplete"
+        >
+          Confirm
+        </StepSequenceItem>
+      </StepSequence>
+    </Box>
+  </Box>
+);
+
+ChromaticSnapshotsStory.storyName = "Step Sequence Chromatic Snapshots";
+ChromaticSnapshotsStory.parameters = {
+  chromatic: { disableSnapshot: false },
+  themeProvider: { chromatic: { theme: "sage" } },
 };
