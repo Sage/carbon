b3d797d6aae974f332d75a7edda62a098877d52c
Paul Robinson <paul.robinson2@sage.com>
2026-04-24 13:51:33 +0100

    test(progress-tracker): refine testing suite

--- src/components/progress-tracker/progress-tracker-test.stories.tsx
+++ src/components/progress-tracker/progress-tracker-test.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ProgressTracker, { ProgressTrackerProps } from ".";
+import ProgressTracker from ".";
+import Box from "../box";
 import { PROGRESS_TRACKER_SIZES } from "./progress-tracker.config";
 import {
   FlexTileCell,
@@ -11,12 +12,10 @@ import {
 export default {
   component: ProgressTracker,
   title: "Progress Tracker/Test",
-  includeStories: ["Default", "LeftLabelWithLabelWidth", "InsideFlexTile"],
+  includeStories: ["SnapshotCapture"],
   parameters: {
-    info: { disable: true },
-    chromatic: {
-      disableSnapshot: true,
-    },
+    themeProvider: { chromatic: { theme: "sage" } },
+    chromatic: { disableSnapshot: false },
   },
   argTypes: {
     size: {
@@ -51,65 +50,71 @@ export default {
   },
 };
 
-export const Default = (args: ProgressTrackerProps) => {
-  return <ProgressTracker {...args} />;
-};
-
-Default.storyName = "default";
-
-export const LeftLabelWithLabelWidth = (args: ProgressTrackerProps) => {
+export const SnapshotCapture = () => {
   return (
-    <>
+    <Box display="flex" flexDirection="column" gap={3}>
+      Component with props applied to capture in one Chromatic snapshot
+      {/* Default */}
+      <ProgressTracker progress={50} />
+      {/* Sizes */}
+      <ProgressTracker size="small" progress={50} />
+      <ProgressTracker size="large" progress={50} />
+      {/* Custom bar length */}
+      <ProgressTracker progress={50} length="150px" />
+      {/* Color variants */}
+      <ProgressTracker progress={15} currentProgressLabel="15%" />
+      <ProgressTracker progress={50} currentProgressLabel="50%" />
+      <ProgressTracker progress={100} currentProgressLabel="100%" />
+      <ProgressTracker progress={100} error currentProgressLabel="error" />
+      {/* Custom label values */}
       <ProgressTracker
-        mt={2}
-        labelsPosition="left"
-        progress={10}
-        labelWidth="60%"
-        {...args}
+        progress={50}
+        currentProgressLabel="$50"
+        maxProgressLabel="$200"
       />
       <ProgressTracker
-        mt={2}
-        labelsPosition="left"
-        progress={30}
-        labelWidth="100px"
-        {...args}
+        progress={70}
+        currentProgressLabel="Step 3"
+        maxProgressLabel="5"
+        description="Adding VAT"
       />
+      {/* Labels position bottom */}
       <ProgressTracker
-        mt={2}
+        labelsPosition="bottom"
+        progress={50}
+        currentProgressLabel="50%"
+      />
+      {/* Labels position left */}
+      <ProgressTracker
+        labelsPosition="left"
+        progress={50}
+        currentProgressLabel="50%"
+        labelWidth="40px"
+      />
+      {/* Left label with various label widths */}
+      <ProgressTracker labelsPosition="left" progress={10} labelWidth="60%" />
+      <ProgressTracker labelsPosition="left" progress={30} labelWidth="100px" />
+      <ProgressTracker
         labelsPosition="left"
         progress={40}
         labelWidth="fit-content"
-        {...args}
       />
-    </>
+      {/* Inside flex tile */}
+      <Tile m={0} py={0}>
+        <FlexTileContainer>
+          <FlexTileCell py={2}>
+            <FlexTileDivider />
+            <ProgressTracker
+              length="100%"
+              progress={50}
+              currentProgressLabel="Step 3"
+              maxProgressLabel="5"
+            />
+          </FlexTileCell>
+        </FlexTileContainer>
+      </Tile>
+    </Box>
   );
 };
 
-LeftLabelWithLabelWidth.storyName = "left label with label width";
-
-LeftLabelWithLabelWidth.parameters = {
-  themeProvider: { chromatic: { disableSnapshot: false, theme: "sage" } },
-};
-
-export const InsideFlexTile = () => {
-  return (
-    <Tile m={0} py={0}>
-      <FlexTileContainer>
-        <FlexTileCell py={2}>
-          <FlexTileDivider />
-          <ProgressTracker
-            length="100%"
-            progress={50}
-            currentProgressLabel="Step 3"
-            maxProgressLabel="5"
-          />
-        </FlexTileCell>
-      </FlexTileContainer>
-    </Tile>
-  );
-};
-
-InsideFlexTile.storyName = "inside flex tile";
-InsideFlexTile.parameters = {
-  themeProvider: { chromatic: { disableSnapshot: false } },
-};
+SnapshotCapture.storyName = "Snapshot Capture";
