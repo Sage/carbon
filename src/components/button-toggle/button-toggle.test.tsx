375f8e33dc32dba096eff42b9e719d43a69daa6c
nuria1110 <nuria.torresramon@sage.com>
2026-06-16 13:51:08 +0100

    feat(button-toggle): add single toggle support
    Adds support to render a single ButtonToggle.

--- src/components/button-toggle/button-toggle.test.tsx
+++ src/components/button-toggle/button-toggle.test.tsx
@@ -2,32 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { ButtonToggleGroup, ButtonToggle } from ".";
-import Logger from "../../__internal__/utils/logger";
-
-test("logs warning if not used within ButtonToggleGroup", () => {
-  const loggerErrorSpy = jest
-    .spyOn(Logger, "error")
-    .mockImplementation(() => {});
-
-  render(<ButtonToggle>Button</ButtonToggle>);
-
-  expect(loggerErrorSpy).toHaveBeenCalledWith(
-    expect.stringContaining(
-      "Carbon ButtonToggleGroup: Context not found. Have you wrapped your Carbon subcomponents properly? See stack trace for more details.",
-    ),
-  );
-
-  loggerErrorSpy.mockRestore();
-});
 
 test("should call `onClick` when the button is clicked", async () => {
   const onClick = jest.fn();
   const user = userEvent.setup();
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle onClick={onClick}>Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle onClick={onClick}>Button</ButtonToggle>);
 
   await user.click(screen.getByRole("button"));
   expect(onClick).toHaveBeenCalledTimes(1);
@@ -36,11 +15,7 @@ test("should call `onClick` when the button is clicked", async () => {
 test("should call `onFocus` when the button is focused", async () => {
   const onFocus = jest.fn();
   const user = userEvent.setup();
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle onFocus={onFocus}>Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle onFocus={onFocus}>Button</ButtonToggle>);
 
   await user.click(screen.getByRole("button"));
   expect(onFocus).toHaveBeenCalledTimes(1);
@@ -49,11 +24,7 @@ test("should call `onFocus` when the button is focused", async () => {
 test("should call `onBlur` when the button is blurred", async () => {
   const onBlur = jest.fn();
   const user = userEvent.setup();
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle onBlur={onBlur}>Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle onBlur={onBlur}>Button</ButtonToggle>);
 
   await user.click(screen.getByRole("button"));
   await user.tab();
@@ -61,11 +32,7 @@ test("should call `onBlur` when the button is blurred", async () => {
 });
 
 test("should render disabled button with expected styles", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle disabled>Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle disabled>Button</ButtonToggle>);
 
   expect(screen.getByRole("button")).toBeDisabled();
   expect(screen.getByRole("button")).toHaveStyle({ cursor: "not-allowed" });
@@ -74,13 +41,7 @@ test("should render disabled button with expected styles", () => {
 test("should throw an error when neither children or a `buttonIcon` is provided", () => {
   const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
 
-  expect(() =>
-    render(
-      <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-        <ButtonToggle />
-      </ButtonToggleGroup>,
-    ),
-  ).toThrow(
+  expect(() => render(<ButtonToggle />)).toThrow(
     "Either prop `buttonIcon` must be defined, or this node must have children",
   );
 
@@ -88,11 +49,7 @@ test("should throw an error when neither children or a `buttonIcon` is provided"
 });
 
 test("should render with aria-label attribute", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle aria-label="test-label">Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle aria-label="test-label">Button</ButtonToggle>);
 
   expect(screen.getByRole("button")).toHaveAccessibleName("test-label");
 });
@@ -101,16 +58,14 @@ test("should render with aria-labelledby attribute", () => {
   render(
     <>
       <span id="test-id">Test Label</span>
-      <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-        <ButtonToggle aria-labelledby="test-id">Button</ButtonToggle>
-      </ButtonToggleGroup>
+      <ButtonToggle aria-labelledby="test-id">Button</ButtonToggle>
     </>,
   );
 
   expect(screen.getByRole("button")).toHaveAccessibleName("Test Label");
 });
 
-test("should render with aria-pressed true when the button is selected", () => {
+test("should render with aria-pressed true when the button is selected within a group", () => {
   render(
     <ButtonToggleGroup id="test" value="test-value" onChange={() => {}}>
       <ButtonToggle value="test-value">Button</ButtonToggle>
@@ -120,70 +75,73 @@ test("should render with aria-pressed true when the button is selected", () => {
   expect(screen.getByRole("button")).toHaveAttribute("aria-pressed", "true");
 });
 
-test("should render with aria-pressed false when the button is not selected", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle pressed={false}>Button</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
-
-  expect(screen.getByRole("button")).toHaveAttribute("aria-pressed", "false");
-});
-
-test("should render with provided data- tags", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle data-element="test-element" data-role="test-role">
-        Button
-      </ButtonToggle>
-    </ButtonToggleGroup>,
-  );
-
-  expect(screen.getByTestId("test-role")).toHaveAttribute(
-    "data-element",
-    "test-element",
-  );
-});
-
-test("should render with value attribute", () => {
+test("should render with aria-pressed false when the button is not selected within a group", () => {
   render(
     <ButtonToggleGroup id="test" value="" onChange={() => {}}>
       <ButtonToggle value="test-value">Button</ButtonToggle>
     </ButtonToggleGroup>,
   );
 
+  expect(screen.getByRole("button")).toHaveAttribute("aria-pressed", "false");
+});
+
+test("should render with aria-pressed true when `pressed` is true", () => {
+  render(<ButtonToggle pressed>Single Button</ButtonToggle>);
+
+  expect(screen.getByRole("button")).toHaveAttribute("aria-pressed", "true");
+});
+
+test("should render with aria-pressed false when `pressed` is false", () => {
+  render(<ButtonToggle pressed={false}>Single Button</ButtonToggle>);
+
+  expect(screen.getByRole("button")).toHaveAttribute("aria-pressed", "false");
+});
+
+test("should render with provided data- tags", () => {
+  render(
+    <ButtonToggle data-element="test-element" data-role="test-role">
+      Button
+    </ButtonToggle>,
+  );
+
+  expect(screen.getByRole("button")).toHaveAttribute(
+    "data-element",
+    "test-element",
+  );
+  expect(screen.getByRole("button")).toHaveAttribute("data-role", "test-role");
+});
+
+test("should render with value attribute", () => {
+  render(<ButtonToggle value="test-value">Button</ButtonToggle>);
+
   expect(screen.getByRole("button")).toHaveValue("test-value");
 });
 
 test("should render button with text content", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle>Test Button Text</ButtonToggle>
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle>Test Button Text</ButtonToggle>);
 
   expect(screen.getByRole("button")).toHaveTextContent("Test Button Text");
 });
 
+test("should not render with tabindex when not in a group", () => {
+  render(<ButtonToggle pressed={false}>Single Button</ButtonToggle>);
+
+  expect(screen.getByRole("button")).not.toHaveAttribute("tabindex");
+});
+
 test("should render with provided `buttonIcon`", () => {
-  render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle buttonIcon="placeholder" />
-    </ButtonToggleGroup>,
-  );
+  render(<ButtonToggle buttonIcon="placeholder" />);
 
   expect(screen.getByTestId("icon")).toBeVisible();
   expect(screen.getByTestId("icon")).toHaveAttribute("type", "placeholder");
 });
 
 // coverage
-test("should render button with expected styles when selected button is disabled", () => {
+test("should render button with expected styles when pressed button is disabled", () => {
   render(
-    <ButtonToggleGroup id="test" value="test-value" onChange={() => {}}>
-      <ButtonToggle value="test-value" disabled>
-        Button
-      </ButtonToggle>
-    </ButtonToggleGroup>,
+    <ButtonToggle pressed disabled>
+      Button
+    </ButtonToggle>,
   );
 
   expect(screen.getByRole("button")).toHaveStyleRule(
@@ -192,13 +150,51 @@ test("should render button with expected styles when selected button is disabled
   );
 });
 
-// coverage
-test("should render with expected styles when `size` prop is set", () => {
+test("should render extra-small button when rendered in small group", () => {
   render(
-    <ButtonToggleGroup id="test" value="" onChange={() => {}}>
-      <ButtonToggle value="test-value" size="large">
-        Button
-      </ButtonToggle>
+    <ButtonToggleGroup
+      id="test"
+      value="test-value"
+      onChange={() => {}}
+      size="small"
+    >
+      <ButtonToggle value="">Button</ButtonToggle>
+    </ButtonToggleGroup>,
+  );
+
+  expect(screen.getByRole("button")).toHaveStyleRule(
+    "height",
+    "var(--global-size-xs)",
+  );
+});
+
+test("should render small button when rendered in medium group", () => {
+  render(
+    <ButtonToggleGroup
+      id="test"
+      value="test-value"
+      onChange={() => {}}
+      size="medium"
+    >
+      <ButtonToggle value="">Button</ButtonToggle>
+    </ButtonToggleGroup>,
+  );
+
+  expect(screen.getByRole("button")).toHaveStyleRule(
+    "height",
+    "var(--global-size-s)",
+  );
+});
+
+test("should render medium button when rendered in large group", () => {
+  render(
+    <ButtonToggleGroup
+      id="test"
+      value="test-value"
+      onChange={() => {}}
+      size="large"
+    >
+      <ButtonToggle value="">Button</ButtonToggle>
     </ButtonToggleGroup>,
   );
 
