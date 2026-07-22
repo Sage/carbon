92446826fcab44104da18653603a4143ad9f519d
Daniel Dipper <daniel.dipper@sage.com>
2026-07-22 16:33:33 +0100

    build: fix issues with lint and build failures

--- src/components/typography/__internal__/__next__/typography.style.ts
+++ src/components/typography/__internal__/__next__/typography.style.ts
@@ -1,9 +1,11 @@
 import styled, { css } from "styled-components";
 import { space, SpaceProps } from "styled-system";
 import applyBaseTheme from "../../../../style/themes/apply-base-theme";
-import { AllowedCSSTextOverrides } from "./typography.component";
 import visuallyHidden from "../../../../style/utils/visually-hidden";
-import { TypographyProps } from "./typography.component";
+import {
+  AllowedCSSTextOverrides,
+  TypographyProps,
+} from "./typography.component";
 
 type HeadingVariants =
   | "h1"
