d72d8e8230a10a35084d98536c89d9a577560d95
Solange Calabrese <solange.calabrese.ar@gmail.com>
2026-08-14 14:10:25 -0300

    test(sidebar): fix backward background-scroll tab count

--- src/components/sidebar/sidebar.pw.tsx
+++ src/components/sidebar/sidebar.pw.tsx
@@ -217,14 +217,14 @@ test.describe("Browser-specific rendering", () => {
       await expect(boxElement).not.toBeInViewport();
     });
 
-    // TODO: Skipped due to flaky focus behaviour. To review in FE-6428
-    test.skip("tabbing backward through the sidebar and back to the start should not make the background scroll to the bottom", async ({
+    test("tabbing backward through the sidebar and back to the start should not make the background scroll to the bottom", async ({
       mount,
       page,
     }) => {
       await mount(<SidebarBackgroundScrollTestComponent />);
 
-      await continuePressingSHIFTTAB(page, 1);
+      await waitForElementFocus(page, sidebarPreview(page));
+      await continuePressingSHIFTTAB(page, 2);
       const closeIconButtonElement = closeIconButton(page);
 
       await expect(closeIconButtonElement).toBeFocused();
